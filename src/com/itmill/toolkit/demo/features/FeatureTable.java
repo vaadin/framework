58ab5caaefd2aac164006e01bed1cc2877058e6c
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2007-02-09 12:54:57 +0000

    Disable styles from Table properties style-list
    svn changeset:604/svn branch:toolkit

--- src/com/itmill/toolkit/demo/features/FeatureTable.java
+++ src/com/itmill/toolkit/demo/features/FeatureTable.java
@@ -125,11 +125,13 @@ public class FeatureTable extends Feature implements Action.Handler {
 				new Integer(Table.ROW_HEADER_MODE_PROPERTY) }, new Object[] {
 				"Explicit", "Explicit defaults ID", "Hidden", "Icon only",
 				"ID", "Index", "Item", "Property" });
+		/* Disabled theme changer before #356, #357 are fixed
 		Select themes = (Select) propertyPanel.getField("style");
 		themes.addItem("list").getItemProperty(
 				themes.getItemCaptionPropertyId()).setValue("list");
 		themes.addItem("paging").getItemProperty(
 				themes.getItemCaptionPropertyId()).setValue("paging");
+				*/
 		propertyPanel.addProperties("Table Properties", ap);
 
 		t.setRowHeaderMode(Table.ROW_HEADER_MODE_INDEX);
