830cd4174df8c79b2582058de42958f0ad7d583b
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2007-02-19 14:55:57 +0000

    comments
    svn changeset:738/svn branch:toolkit

--- src/com/itmill/toolkit/data/util/QueryContainer.java
+++ src/com/itmill/toolkit/data/util/QueryContainer.java
@@ -1,3 +1,31 @@
+/* *************************************************************************
+ 
+                               IT Mill Toolkit 
+
+               Development of Browser User Interfaces Made Easy
+
+                    Copyright (C) 2000-2006 IT Mill Ltd
+                     
+   *************************************************************************
+
+   This product is distributed under commercial license that can be found
+   from the product package on license.pdf. Use of this product might 
+   require purchasing a commercial license from IT Mill Ltd. For guidelines 
+   on usage, see licensing-guidelines.html
+
+   *************************************************************************
+   
+   For more information, contact:
+   
+   IT Mill Ltd                           phone: +358 2 4802 7180
+   Ruukinkatu 2-4                        fax:   +358 2 4802 7181
+   20540, Turku                          email:  info@itmill.com
+   Finland                               company www: www.itmill.com
+   
+   Primary source for information and releases: www.itmill.com
+
+   ********************************************************************** */
+
 package com.itmill.toolkit.data.util;
 
 import java.sql.Connection;
@@ -15,6 +43,15 @@ import com.itmill.toolkit.data.Item;
 import com.itmill.toolkit.data.Property;
 import com.itmill.toolkit.data.util.ObjectProperty;
 
+/** SQL query container.
+ * Implementation of container interface for SQL tables accessed through
+ * JDBC connection.
+ * 
+ * @author IT Mill Ltd.
+ * @version
+ * @VERSION@
+ * @since 4.0
+ */
 public class QueryContainer implements Container, Container.Ordered, Container.Indexed {
 
     String queryStatement;
