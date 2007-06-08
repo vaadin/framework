2cadb1c068e153297ed2404bf4442fb34c87d94b
Matti Tahvonen <matti.tahvonen@itmill.com>
2007-06-08 11:00:33 +0000

    minor change
    svn changeset:1654/svn branch:trunk

--- src/com/itmill/toolkit/terminal/web/AjaxApplicationManager.java
+++ src/com/itmill/toolkit/terminal/web/AjaxApplicationManager.java
@@ -331,7 +331,7 @@ public class AjaxApplicationManager implements
 
 
 					
-                	outWriter.print(", meta : {");
+                	outWriter.print(", \"meta\" : {");
                 	boolean metaOpen = false;
 
 					
