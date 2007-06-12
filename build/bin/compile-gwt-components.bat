e31b7a286bbb4509188f74a19e3d50e6d687e3e1
Jouni Koivuviita <jouni.koivuviita@itmill.com>
2007-06-12 14:47:29 +0000

    Shell script for Windows.
    svn changeset:1686/svn branch:trunk

---
+++ build/bin/compile-gwt-components.bat
@@ -0,0 +1,1 @@
+@java -cp "%~dp0\../../src;%~dp0\bin;%~dp0\../gwt/gwt-windows-1.4.10/gwt-user.jar;%~dp0\../gwt/gwt-windows-1.4.10/gwt-dev-windows.jar" com.google.gwt.dev.GWTCompiler -out "%~dp0\../../WebContent" %* com.itmill.toolkit.terminal.gwt.Client
