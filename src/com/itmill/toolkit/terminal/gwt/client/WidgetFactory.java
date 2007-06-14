50b5c3287fd5c3a04b44f60e3710e7e460fbe156
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2007-06-14 11:36:57 +0000

    Documented WidgetFactory
    svn changeset:1715/svn branch:trunk

--- src/com/itmill/toolkit/terminal/gwt/client/WidgetFactory.java
+++ src/com/itmill/toolkit/terminal/gwt/client/WidgetFactory.java
@@ -4,8 +4,18 @@ import com.google.gwt.user.client.ui.Widget;
 
 public interface WidgetFactory {
 
+	/** Create an uninitialized component that best matches given UIDL.
+	 * 
+	 * @param uidl UIDL to be painted with returned component.
+	 * @return New uninitialized and unregistered component that can paint given UIDL.
+	 */
 	Widget createWidget(UIDL uidl);
 
+	/** Test if the given component implementation conforms to UIDL.
+	 * 
+	 * @param currentWidget Current implementation of the component
+	 * @param uidl UIDL to test against
+	 * @return true iff createWidget would return a new component of the same class than currentWidget
+	 */
 	boolean isCorrectImplementation(Widget currentWidget, UIDL uidl);
-	
 }
