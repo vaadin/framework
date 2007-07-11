875edebeaf38ca15c859ef61de5f11f4c5821a4f
Jouni Koivuviita <jouni.koivuviita@itmill.com>
2007-07-11 13:46:13 +0000

    Implemented sizeable interface for IPanel and tweaked panel CSS for IE.
    svn changeset:1844/svn branch:trunk

--- src/com/itmill/toolkit/terminal/gwt/client/ui/IPanel.java
+++ src/com/itmill/toolkit/terminal/gwt/client/ui/IPanel.java
@@ -2,6 +2,7 @@ package com.itmill.toolkit.terminal.gwt.client.ui;
 
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.itmill.toolkit.terminal.gwt.client.Client;
 import com.itmill.toolkit.terminal.gwt.client.Paintable;
@@ -17,11 +18,15 @@ public class IPanel extends FlowPanel implements Paintable {
 	
 	private Label caption;
 	
+	private SimplePanel content;
+	
 	public IPanel() {
 		super();
 		setStyleName(CLASSNAME);
 		caption = new Label();
 		caption.setStyleName(CLASSNAME+"-caption");
+		content = new SimplePanel();
+		content.setStyleName(CLASSNAME+"-content");
 	}
 
 	public void updateFromUIDL(UIDL uidl, Client client) {
@@ -32,6 +37,7 @@ public class IPanel extends FlowPanel implements Paintable {
 		this.client = client;
 		this.id = uidl.getId();
 		
+		// TODO optimize: if only the caption has changed, don't re-render whole content
 		clear();
 		
 		if(uidl.hasAttribute("style"))
@@ -48,10 +54,25 @@ public class IPanel extends FlowPanel implements Paintable {
 				addStyleName(CLASSNAME+"-nocaption");
 		}
 		
+		// Size panel
+		// TODO support for different units
+		String widthUnit = "px";
+		String heightUnit = "px";
+		int captionHeight = caption.getOffsetHeight();
+		int height = uidl.hasVariable("height")? uidl.getIntVariable("height") : -1;
+		int w = uidl.hasVariable("width")? uidl.getIntVariable("width") : -1;
+		int h = -1;
+		if(height != -1) 
+			h = height < captionHeight? 0 : height - captionHeight;
+		setWidth(w>=0?w+widthUnit:"auto");
+		content.setHeight(h>=0?h+heightUnit:"auto");
+		
 		UIDL layoutUidl = uidl.getChildUIDL(0);
 		Widget layout = client.getWidget(layoutUidl);
 		((Paintable)layout).updateFromUIDL(layoutUidl, client);
-		add(layout);
+		content.setWidget(layout);
+		
+		add(content);
 		
 	}
 	
