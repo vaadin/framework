f7c2fd5c8c4fa391ad0ccfd8a84848263cac285f
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2007-07-17 11:28:35 +0000

    Resurrecting (a simplified, dummy) WebBrowser
    svn changeset:1868/svn branch:trunk

---
+++ src/com/itmill/toolkit/terminal/gwt/server/WebBrowser.java
@@ -0,0 +1,27 @@
+package com.itmill.toolkit.terminal.gwt.server;
+
+import com.itmill.toolkit.terminal.Terminal;
+
+public class WebBrowser implements Terminal {
+
+	public String getDefaultTheme() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getScreenHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getScreenWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	public String getBrowserApplication() {
+		// TODO Auto-generated method stub
+		return "";
+	}
+
+}
