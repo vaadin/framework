4d4b1a0a6f7cde550851e310bbd7181da844a03b
Matti Tahvonen <matti.tahvonen@itmill.com>
2007-09-21 06:24:53 +0000

    trying to revert changes lost during refactoring
    svn changeset:2357/svn branch:trunk

--- src/com/itmill/toolkit/terminal/gwt/client/ui/Table.java
+++ src/com/itmill/toolkit/terminal/gwt/client/ui/Table.java
@@ -1,6 +1,9 @@
 package com.itmill.toolkit.terminal.gwt.client.ui;
 
-public interface Table {
+import com.google.gwt.user.client.ui.HasWidgets;
+import com.itmill.toolkit.terminal.gwt.client.Paintable;
+
+public interface Table extends Paintable, HasWidgets {
 	final int SELECT_MODE_NONE = 0;
 	final int SELECT_MODE_SINGLE = 1;
 	final int SELECT_MODE_MULTI = 2;
