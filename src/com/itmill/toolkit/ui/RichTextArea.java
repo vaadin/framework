6a0aa49745ed6619a0ca235dd5179d478f220e62
Matti Tahvonen <matti.tahvonen@itmill.com>
2007-10-11 11:58:44 +0000

    RichTextArea server side component
    svn changeset:2495/svn branch:trunk

---
+++ src/com/itmill/toolkit/ui/RichTextArea.java
@@ -0,0 +1,19 @@
+package com.itmill.toolkit.ui;
+
+import com.itmill.toolkit.terminal.PaintException;
+import com.itmill.toolkit.terminal.PaintTarget;
+
+/**
+ * TODO dont' allow getting into bad state
+ * TODO implement Sizeable interface
+ */
+public class RichTextArea extends TextField {
+
+	public void paintContent(PaintTarget target) throws PaintException {
+		target.addAttribute("richtext", true);
+		super.paintContent(target);
+	}
+	
+	
+
+}
