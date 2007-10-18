5665d32546560b9bc2d7dfab78ed31f62988343d
Jani Laakso <jani.laakso@itmill.com>
2007-10-18 13:54:04 +0000

    Refactoring package files.
    svn changeset:2558/svn branch:trunk

---
+++ build/package/start-demo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp lib/jetty/jetty-6.1.5.jar:lib/jetty/jetty-util-6.1.5.jar:lib/jetty/servlet-api-2.5-6.1.5.jar:WebContent/WEB-INF/classes com.itmill.toolkit.launcher.ITMillDesktopLaunch
