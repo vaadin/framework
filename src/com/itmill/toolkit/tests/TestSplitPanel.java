c586e1ecfb68da1733c2197fd93a7792d8eec997
Jani Laakso <jani.laakso@itmill.com>
2007-10-21 14:15:41 +0000

    This fails currently.
    svn changeset:2568/svn branch:trunk

---
+++ src/com/itmill/toolkit/tests/TestSplitPanel.java
@@ -0,0 +1,24 @@
+package com.itmill.toolkit.tests;
+
+import com.itmill.toolkit.ui.*;
+
+public class TestSplitPanel extends com.itmill.toolkit.Application {
+
+	SplitPanel horizontalSplit = new SplitPanel(
+			SplitPanel.ORIENTATION_HORIZONTAL);
+	SplitPanel verticalSplit = new SplitPanel(SplitPanel.ORIENTATION_VERTICAL);
+
+	public void init() {
+		Window mainWindow = new Window("Feature Browser");
+		setMainWindow(mainWindow);
+		
+		verticalSplit.setFirstComponent(new Label("vertical first"));
+		verticalSplit.setSecondComponent(new Label("vertical second"));
+		
+//		horizontalSplit.setFirstComponent(new Label("horizontal first"));
+//		horizontalSplit.setSecondComponent(new Label("horizontal second"));
+
+		mainWindow.setLayout(verticalSplit);
+	}
+
+}
