f0aabb657aeb417d6b430a84416ec3ebc05a42f4
Marc Englund <marc.englund@itmill.com>
2007-11-16 13:42:32 +0000

    Removed some Java 5 code. Not tested.
    svn changeset:2852/svn branch:trunk

--- src/com/itmill/toolkit/tests/TestSelectAndDatefieldInDeepLayouts.java
+++ src/com/itmill/toolkit/tests/TestSelectAndDatefieldInDeepLayouts.java
@@ -1,7 +1,6 @@
 package com.itmill.toolkit.tests;
 
 import java.util.Collection;
-import java.util.UUID;
 import java.util.Vector;
 
 import com.itmill.toolkit.ui.AbstractLayout;
@@ -23,71 +22,73 @@ import com.itmill.toolkit.ui.Select;
  */
 public class TestSelectAndDatefieldInDeepLayouts extends CustomComponent {
 
-	public TestSelectAndDatefieldInDeepLayouts() {
-		OrderedLayout root = (OrderedLayout) getOrderedLayout();
-		setCompositionRoot(root);
+    public TestSelectAndDatefieldInDeepLayouts() {
+        OrderedLayout root = (OrderedLayout) getOrderedLayout();
+        setCompositionRoot(root);
 
-		root.addComponent(getSelect());
-		root.addComponent(getDateField());
-		root.addComponent(getSelect());
-		root.addComponent(getDateField());
+        root.addComponent(getSelect());
+        root.addComponent(getDateField());
+        root.addComponent(getSelect());
+        root.addComponent(getDateField());
 
-		Panel p1 = (Panel) getPanel();
-		root.addComponent(p1);
+        Panel p1 = getPanel();
+        root.addComponent(p1);
 
-		p1.addComponent(getSelect());
-		p1.addComponent(getDateField());
-		p1.addComponent(getSelect());
-		p1.addComponent(getDateField());
+        p1.addComponent(getSelect());
+        p1.addComponent(getDateField());
+        p1.addComponent(getSelect());
+        p1.addComponent(getDateField());
 
-		OrderedLayout l1 = (OrderedLayout) getOrderedLayout();
-		p1.addComponent(l1);
+        OrderedLayout l1 = (OrderedLayout) getOrderedLayout();
+        p1.addComponent(l1);
 
-		l1.addComponent(getSelect());
-		l1.addComponent(getDateField());
-		l1.addComponent(getSelect());
-		l1.addComponent(getDateField());
+        l1.addComponent(getSelect());
+        l1.addComponent(getDateField());
+        l1.addComponent(getSelect());
+        l1.addComponent(getDateField());
 
-		Panel p2 = (Panel) getPanel();
-		l1.addComponent(p2);
+        Panel p2 = getPanel();
+        l1.addComponent(p2);
 
-		p2.addComponent(getSelect());
-		p2.addComponent(getDateField());
-		p2.addComponent(getSelect());
-		p2.addComponent(getDateField());
+        p2.addComponent(getSelect());
+        p2.addComponent(getDateField());
+        p2.addComponent(getSelect());
+        p2.addComponent(getDateField());
 
-	}
+    }
 
-	AbstractLayout getOrderedLayout() {
-		OrderedLayout l = new OrderedLayout();
-		l.setCaption(getCaption("orderedlayout"));
-		return l;
-	}
+    AbstractLayout getOrderedLayout() {
+        OrderedLayout l = new OrderedLayout();
+        l.setCaption(getCaption("orderedlayout"));
+        return l;
+    }
 
-	Panel getPanel() {
-		Panel panel = new Panel();
-		panel.setCaption(getCaption("panel"));
-		return panel;
-	}
+    Panel getPanel() {
+        Panel panel = new Panel();
+        panel.setCaption(getCaption("panel"));
+        return panel;
+    }
 
-	Component getSelect() {
-		return new Select(getCaption("select"), getSelectOptions());
-	}
+    Component getSelect() {
+        return new Select(getCaption("select"), getSelectOptions());
+    }
 
-	Component getDateField() {
-		return new DateField(getCaption("datefield"));
-	}
+    Component getDateField() {
+        return new DateField(getCaption("datefield"));
+    }
 
-	private Collection getSelectOptions() {
-		Collection opts = new Vector(3);
-		opts.add(getCaption("opt 1"));
-		opts.add(getCaption("opt 2"));
-		opts.add(getCaption("opt 3"));
-		return opts;
-	}
+    private Collection getSelectOptions() {
+        Collection opts = new Vector(3);
+        opts.add(getCaption("opt 1"));
+        opts.add(getCaption("opt 2"));
+        opts.add(getCaption("opt 3"));
+        return opts;
+    }
 
-	private String getCaption(String string) {
-		return string + " " + UUID.randomUUID().toString().substring(0, 5);
-	}
+    private String getCaption(String string) {
+        return string + (Math.random() * 99999.9);
+        // This is Java 5 code:
+        // return string + " " + UUID.randomUUID().toString().substring(0, 5);
+    }
 
 }
