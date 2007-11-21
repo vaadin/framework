24febeb1d0904c934a07faf19ae96459b3dea905
Jouni Koivuviita <jouni.koivuviita@itmill.com>
2007-11-21 11:42:15 +0000

    -Link not implements icon, description and errormessage. Fixes #1114. -Small fixes to Panel and Tabsheet stylesheets.
    svn changeset:2875/svn branch:trunk

--- src/com/itmill/toolkit/terminal/gwt/client/ui/ILink.java
+++ src/com/itmill/toolkit/terminal/gwt/client/ui/ILink.java
@@ -1,11 +1,14 @@
 package com.itmill.toolkit.terminal.gwt.client.ui;
 
 import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Widget;
 import com.itmill.toolkit.terminal.gwt.client.ApplicationConnection;
+import com.itmill.toolkit.terminal.gwt.client.ErrorMessage;
 import com.itmill.toolkit.terminal.gwt.client.Paintable;
 import com.itmill.toolkit.terminal.gwt.client.UIDL;
 
@@ -31,14 +34,25 @@ public class ILink extends HTML implements Paintable, ClickListener {
 
     private int height;
 
+    private Element errorIndicatorElement;
+
+    private Element captionElement = DOM.createSpan();
+
+    private ErrorMessage errorMessage;
+
+    private Icon icon;
+
     public ILink() {
         super();
+        DOM.appendChild(getElement(), captionElement);
         addClickListener(this);
         setStyleName(CLASSNAME);
     }
 
     public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
 
+        // Ensure correct implementation,
+        // but don't let container manage caption etc.
         if (client.updateComponent(this, uidl, false)) {
             return;
         }
@@ -50,7 +64,7 @@ public class ILink extends HTML implements Paintable, ClickListener {
             target = uidl.getStringAttribute(target);
         }
         if (uidl.hasAttribute("src")) {
-            // TODO theme soure
+            // TODO theme source
             src = client.translateToolkitUri(uidl.getStringAttribute("src"));
         }
 
@@ -68,8 +82,42 @@ public class ILink extends HTML implements Paintable, ClickListener {
                 : -1;
         width = uidl.hasAttribute("width") ? uidl.getIntAttribute("width") : -1;
 
-        DOM.setInnerHTML(getElement(), uidl.getStringAttribute("caption"));
-        
+        // Set link caption
+        DOM.setInnerText(captionElement, uidl.getStringAttribute("caption"));
+
+        // handle error
+        if (uidl.hasAttribute("error")) {
+            UIDL errorUidl = uidl.getErrors();
+            if (errorIndicatorElement == null) {
+                errorIndicatorElement = DOM.createDiv();
+                DOM.setElementProperty(errorIndicatorElement, "className",
+                        "i-errorindicator");
+                DOM.sinkEvents(errorIndicatorElement, Event.MOUSEEVENTS);
+                sinkEvents(Event.MOUSEEVENTS);
+            }
+            DOM.insertChild(getElement(), errorIndicatorElement, 0);
+            if (errorMessage == null) {
+                errorMessage = new ErrorMessage();
+            }
+            errorMessage.updateFromUIDL(errorUidl);
+
+        } else if (errorIndicatorElement != null) {
+            DOM.setStyleAttribute(errorIndicatorElement, "display", "none");
+        }
+
+        if (uidl.hasAttribute("icon")) {
+            if (icon == null) {
+                icon = new Icon(client);
+                DOM.insertChild(getElement(), icon.getElement(), 0);
+            }
+            icon.setUri(uidl.getStringAttribute("icon"));
+        }
+
+        // handle description
+        if (uidl.hasAttribute("description")) {
+            setTitle(uidl.getStringAttribute("description"));
+        }
+
     }
 
     public void onClick(Widget sender) {
@@ -101,6 +149,38 @@ public class ILink extends HTML implements Paintable, ClickListener {
                 features += ",resizable=yes";
             }
             Window.open(src, target, features);
+        }
+    }
+    
+    public void onBrowserEvent(Event event) {
+        Element target = DOM.eventGetTarget(event);
+        if (errorIndicatorElement != null
+                && DOM.compare(target, errorIndicatorElement)) {
+            switch (DOM.eventGetType(event)) {
+            case Event.ONMOUSEOVER:
+                showErrorMessage();
+                break;
+            case Event.ONMOUSEOUT:
+                hideErrorMessage();
+                break;
+            case Event.ONCLICK:
+                ApplicationConnection.getConsole().log(
+                        DOM.getInnerHTML(errorMessage.getElement()));
+                return;
+            default:
+                break;
+            }
+        }
+        super.onBrowserEvent(event);
+    }
+
+    private void hideErrorMessage() {
+        errorMessage.hide();
+    }
+
+    private void showErrorMessage() {
+        if (errorMessage != null) {
+            errorMessage.showAt(errorIndicatorElement);
         }
     }
 
