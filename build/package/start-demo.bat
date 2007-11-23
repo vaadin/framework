46331601f7e29ce0ec475d4fb6710208543ccb4c
Jani Laakso <jani.laakso@itmill.com>
2007-11-23 08:11:29 +0000

    Added "IT Mill Toolkit Hosted Mode" "IT Mill Toolkit Web Mode" launchers integrated to debug and play buttons on Eclipse. Enhanced build.xml in multiple ways.
    svn changeset:2897/svn branch:trunk

--- build/package/start-demo.bat
+++ build/package/start-demo.bat
@@ -1,1 +1,1 @@
-@java -cp "lib\jetty\jetty-6.1.5.jar;lib\jetty\jetty-util-6.1.5.jar;lib\jetty\servlet-api-2.5-6.1.5.jar;WebContent\WEB-INF\classes" com.itmill.toolkit.launcher.ITMillDesktopLaunch
+@java -cp "lib\jetty\jetty-6.1.5.jar;lib\jetty\jetty-util-6.1.5.jar;lib\jetty\servlet-api-2.5-6.1.5.jar;WebContent\WEB-INF\classes" com.itmill.toolkit.launcher.ITMillToolkitDesktopMode
