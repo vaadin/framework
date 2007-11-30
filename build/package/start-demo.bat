c4832facac82b1fd10cea4a1723fe8b45c93fe88
Jani Laakso <jani.laakso@itmill.com>
2007-11-30 16:13:19 +0000

    Fixed packaging.
    svn changeset:3092/svn branch:trunk

--- build/package/start-demo.bat
+++ build/package/start-demo.bat
@@ -1,1 +1,1 @@
-@java -cp "lib\jetty\jetty-6.1.5.jar;lib\jetty\jetty-util-6.1.5.jar;lib\jetty\servlet-api-2.5-6.1.5.jar;WebContent\WEB-INF\classes" com.itmill.toolkit.launcher.ITMillToolkitDesktopMode
+@java -cp "WebContent\demo\lib\jetty\jetty-6.1.5.jar;WebContent\demo\lib\jetty\jetty-util-6.1.5.jar;WebContent\demo\lib\jetty\servlet-api-2.5-6.1.5.jar;WebContent\WEB-INF\classes" com.itmill.toolkit.launcher.ITMillToolkitDesktopMode
