4ff51ee5690b67bdd28ee90ecc0aa72f02ac71d9
Jani Laakso <jani.laakso@itmill.com>
2007-12-01 13:09:45 +0000

    Fixed packaging according new style.
    svn changeset:3098/svn branch:trunk

--- build/package/start.sh
+++ build/package/start.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+if [ "$1" != "" ] ; then
+  cd $1
+fi
+
 java -cp WebContent/demo/lib/jetty/jetty-6.1.5.jar:WebContent/demo/lib/jetty/jetty-util-6.1.5.jar:WebContent/demo/lib/jetty/servlet-api-2.5-6.1.5.jar:WebContent/WEB-INF/classes com.itmill.toolkit.launcher.ITMillToolkitDesktopMode
