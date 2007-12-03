bc44f9cc77c4425b51c30e6401a6c3ca18bbf4cd
Marc Englund <marc.englund@itmill.com>
2007-12-03 14:47:50 +0000

    some documentation
    svn changeset:3124/svn branch:trunk

--- src/com/itmill/toolkit/demo/featurebrowser/ValueInputExample.java
+++ src/com/itmill/toolkit/demo/featurebrowser/ValueInputExample.java
@@ -12,19 +12,20 @@ import com.itmill.toolkit.ui.Slider;
 import com.itmill.toolkit.ui.TextField;
 
 /**
- * Shows a few variations of Buttons and Links.
+ * Shows some basic fields for value input; TextField, DateField, Slider...
  * 
  * @author IT Mill Ltd.
  */
 public class ValueInputExample extends CustomComponent {
 
+    // used to show the last entered value in the textfields
     Label textfieldValue;
 
     public ValueInputExample() {
-
         OrderedLayout main = new OrderedLayout();
         main.setMargin(true);
         setCompositionRoot(main);
+
         // TextField
         OrderedLayout horiz = new OrderedLayout(
                 OrderedLayout.ORIENTATION_HORIZONTAL);
@@ -35,23 +36,23 @@ public class ValueInputExample extends CustomComponent {
         Panel right = new Panel("Last input");
         right.setStyleName(Panel.STYLE_LIGHT);
         horiz.addComponent(right);
-
+        // "last entered" -label
         textfieldValue = new Label();
         textfieldValue.setContentMode(Label.CONTENT_PREFORMATTED);
         right.addComponent(textfieldValue);
-
+        // listener that will update the last entered value for the TextFields
         Field.ValueChangeListener listener = new Field.ValueChangeListener() {
             public void valueChange(ValueChangeEvent event) {
                 textfieldValue.setValue(event.getProperty().getValue());
             }
         };
-
+        // basic TextField
         TextField tf = new TextField("Basic");
         tf.setColumns(15);
         tf.setImmediate(true);
         tf.addListener(listener);
         left.addComponent(tf);
-
+        // multiline TextField a.k.a TextArea
         tf = new TextField("Area");
         tf.setColumns(15);
         tf.setRows(5);
@@ -59,7 +60,7 @@ public class ValueInputExample extends CustomComponent {
         tf.addListener(listener);
         left.addComponent(tf);
 
-        // DateField
+        // DateFields
         horiz = new OrderedLayout(OrderedLayout.ORIENTATION_HORIZONTAL);
         main.addComponent(horiz);
         left = new Panel("DateField");
@@ -91,12 +92,13 @@ public class ValueInputExample extends CustomComponent {
         left = new Panel("Slider");
         left.setStyleName(Panel.STYLE_LIGHT);
         main.addComponent(left);
-
+        // int slider
         Slider slider = new Slider(0, 100);
         slider.setSize(300);
         slider.setImmediate(true);
         slider.addListener(new Slider.ValueChangeListener() {
             public void valueChange(ValueChangeEvent event) {
+                // update caption when value changes
                 Slider s = (Slider) event.getProperty();
                 s.setCaption("Value: " + s.getValue());
             }
@@ -107,11 +109,12 @@ public class ValueInputExample extends CustomComponent {
             e.printStackTrace(System.err);
         }
         left.addComponent(slider);
-
+        // double slider
         slider = new Slider(0.0, 1.0, 1);
         slider.setImmediate(true);
         slider.addListener(new Slider.ValueChangeListener() {
             public void valueChange(ValueChangeEvent event) {
+                // update caption when value changes
                 Slider s = (Slider) event.getProperty();
                 s.setCaption("Value: " + s.getValue());
             }
