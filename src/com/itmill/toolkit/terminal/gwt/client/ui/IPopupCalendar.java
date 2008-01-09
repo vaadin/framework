9dc2b1ba53422da27d645af0a6ff71ef7b27da6d
Matti Tahvonen <matti.tahvonen@itmill.com>
2008-01-09 14:52:31 +0000

    static id for floating calendars (#1260). Also showing popup with callback function to avoid flickering
    svn changeset:3433/svn branch:trunk

--- src/com/itmill/toolkit/terminal/gwt/client/ui/IPopupCalendar.java
+++ src/com/itmill/toolkit/terminal/gwt/client/ui/IPopupCalendar.java
@@ -4,11 +4,13 @@
 
 package com.itmill.toolkit.terminal.gwt.client.ui;
 
+import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.PopupListener;
 import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.PopupPanel.PositionCallback;
 import com.itmill.toolkit.terminal.gwt.client.ApplicationConnection;
 import com.itmill.toolkit.terminal.gwt.client.Paintable;
 import com.itmill.toolkit.terminal.gwt.client.UIDL;
@@ -36,6 +38,10 @@ public class IPopupCalendar extends ITextualDate implements Paintable,
         popup.setStyleName(IDateField.CLASSNAME + "-popup");
         popup.setWidget(calendar);
         popup.addPopupListener(this);
+
+        DOM.setElementProperty(calendar.getElement(), "id",
+                "PID_TOOLKIT_POPUPCAL");
+
     }
 
     public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
@@ -49,26 +55,36 @@ public class IPopupCalendar extends ITextualDate implements Paintable,
     public void onClick(Widget sender) {
         if (sender == calendarToggle && !open) {
             calendar.updateCalendar();
-            popup.show();
             // clear previous values
             popup.setWidth("");
             popup.setHeight("");
-            final int w = calendar.getOffsetWidth();
-            final int h = calendar.getOffsetHeight();
-            int t = calendarToggle.getAbsoluteTop();
-            int l = calendarToggle.getAbsoluteLeft();
-            if (l + w > Window.getClientWidth() + Window.getScrollLeft()) {
-                l = Window.getClientWidth() + Window.getScrollLeft() - w;
-            }
-            if (t + h > Window.getClientHeight() + Window.getScrollTop()) {
-                t = Window.getClientHeight() + Window.getScrollTop() - h
-                        - calendarToggle.getOffsetHeight() - 30;
-                l += calendarToggle.getOffsetWidth();
-            }
-            popup.setPopupPosition(l, t + calendarToggle.getOffsetHeight() + 2);
-            popup.setWidth(w + "px");
-            popup.setHeight(h + "px");
-            open = true;
+            popup.setPopupPositionAndShow(new PositionCallback() {
+                public void setPosition(int offsetWidth, int offsetHeight) {
+                    final int w = offsetWidth;
+                    final int h = offsetHeight;
+                    int t = calendarToggle.getAbsoluteTop();
+                    int l = calendarToggle.getAbsoluteLeft();
+                    if (l + w > Window.getClientWidth()
+                            + Window.getScrollLeft()) {
+                        l = Window.getClientWidth() + Window.getScrollLeft()
+                                - w;
+                    }
+                    if (t + h > Window.getClientHeight()
+                            + Window.getScrollTop()) {
+                        t = Window.getClientHeight() + Window.getScrollTop()
+                                - h - calendarToggle.getOffsetHeight() - 30;
+                        l += calendarToggle.getOffsetWidth();
+                    }
+                    popup.setPopupPosition(l, t
+                            + calendarToggle.getOffsetHeight() + 2);
+
+                    // fix size
+                    popup.setWidth(w + "px");
+                    popup.setHeight(h + "px");
+                    open = true;
+
+                }
+            });
         } else {
             open = false;
         }
