bfb716d884a6946d4dc88f3c68f6a3b466e74774
Jani Laakso <jani.laakso@itmill.com>
2008-02-12 11:48:05 +0000

    Fixed packaging startup scripts.
    svn changeset:3780/svn branch:trunk

--- build/package/start.bat
+++ build/package/start.bat
@@ -1,1 +1,1 @@
-@java -cp "WebContent\demo\lib\jetty\jetty-6.1.5.jar;WebContent\demo\lib\jetty\jetty-util-6.1.5.jar;WebContent\demo\lib\jetty\servlet-api-2.5-6.1.5.jar;WebContent\WEB-INF\classes" com.itmill.toolkit.launcher.ITMillToolkitDesktopMode
+@java -cp "WebContent\demo\lib\jetty\jetty-6.1.7.jar;WebContent\demo\lib\jetty\jetty-util-6.1.7.jar;WebContent\demo\lib\jetty\servlet-api-2.5-6.1.7.jar;WebContent\WEB-INF\classes" com.itmill.toolkit.launcher.ITMillToolkitDesktopMode
