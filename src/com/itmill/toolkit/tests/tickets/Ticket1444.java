b7668ece9bd3d1347356fb347944687db39a1b6a
Matti Tahvonen <matti.tahvonen@itmill.com>
2008-02-25 09:34:46 +0000

    reduced ticket to OrderedLayout (Absolute renderer)
    svn changeset:3908/svn branch:trunk

--- src/com/itmill/toolkit/tests/tickets/Ticket1444.java
+++ src/com/itmill/toolkit/tests/tickets/Ticket1444.java
@@ -3,7 +3,6 @@ package com.itmill.toolkit.tests.tickets;
 import com.itmill.toolkit.Application;
 import com.itmill.toolkit.ui.Label;
 import com.itmill.toolkit.ui.OrderedLayout;
-import com.itmill.toolkit.ui.Panel;
 import com.itmill.toolkit.ui.Window;
 
 public class Ticket1444 extends Application {
@@ -11,25 +10,19 @@ public class Ticket1444 extends Application {
     public void init() {
 
         final Window mainWin = new Window(
-                "Test app to break label render fuction in IE7");
+                "Test app to break layout fuction in IE7");
         setMainWindow(mainWin);
 
         OrderedLayout ol = new OrderedLayout();
         ol.setHeight("250px");
         ol.setWidth("500px");
 
-        Panel p = new Panel();
-        p.setSizeFull();
-        p.getLayout().setMargin(false);
-        p.getLayout().setSizeFull();
-
         Label red = new Label(
-                "<div style='background:red;width:100%;height:100%;'>I should use whole panel content area.</div>",
+                "<div style='background:red;width:100%;height:100%;'>??</div>",
                 Label.CONTENT_XHTML);
         red.setSizeFull();
 
-        p.addComponent(red);
-        ol.addComponent(p);
+        ol.addComponent(red);
         mainWin.addComponent(ol);
 
     }
