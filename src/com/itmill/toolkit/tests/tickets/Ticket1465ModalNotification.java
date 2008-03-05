d976b0725f031a1418cf583e9b2848b09d32b8d4
Matti Tahvonen <matti.tahvonen@itmill.com>
2008-03-05 15:31:33 +0000

    added test case
    svn changeset:3976/svn branch:trunk

---
+++ src/com/itmill/toolkit/tests/tickets/Ticket1465ModalNotification.java
@@ -0,0 +1,44 @@
+package com.itmill.toolkit.tests.tickets;
+
+import com.itmill.toolkit.Application;
+import com.itmill.toolkit.ui.Button;
+import com.itmill.toolkit.ui.Label;
+import com.itmill.toolkit.ui.Window;
+import com.itmill.toolkit.ui.Button.ClickEvent;
+
+public class Ticket1465ModalNotification extends Application {
+
+    public void init() {
+
+        final Window mainWin = new Window("ButtonPanel containing a table test");
+        setMainWindow(mainWin);
+
+        final Window modal = new Window("Modal window");
+        modal.setModal(true);
+
+        Button b = new Button("click to show notification",
+                new Button.ClickListener() {
+
+                    public void buttonClick(ClickEvent event) {
+                        modal.showNotification(
+                                "Try clicking the button in main window!",
+                                Window.Notification.TYPE_ERROR_MESSAGE);
+
+                    }
+                });
+        modal.addComponent(b);
+
+        mainWin.addWindow(modal);
+
+        b = new Button("click to test modality!", new Button.ClickListener() {
+
+            public void buttonClick(ClickEvent event) {
+                mainWin.addComponent(new Label("clicked"));
+
+            }
+        });
+
+        mainWin.addComponent(b);
+
+    }
+}
