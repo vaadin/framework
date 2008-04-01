ede743dbbef26b6d2d795034e5f0a2cfe86837b6
Jouni Koivuviita <jouni.koivuviita@itmill.com>
2008-04-01 11:03:10 +0000

    Fix: setting a new source for TYPE_BROWSER Embedded component doesn't replace the original IFRAME-element, it only changes the src-attribute value. This way the browser can keep history information of the url changes.
    svn changeset:4106/svn branch:trunk

--- src/com/itmill/toolkit/terminal/gwt/client/ui/IEmbedded.java
+++ src/com/itmill/toolkit/terminal/gwt/client/ui/IEmbedded.java
@@ -4,6 +4,8 @@
 
 package com.itmill.toolkit.terminal.gwt.client.ui;
 
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
 import com.google.gwt.user.client.ui.HTML;
 import com.itmill.toolkit.terminal.gwt.client.ApplicationConnection;
 import com.itmill.toolkit.terminal.gwt.client.Paintable;
@@ -13,19 +15,29 @@ public class IEmbedded extends HTML implements Paintable {
 
     private String heigth;
     private String width;
+    private Element browserElement;
 
     public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
         if (client.updateComponent(this, uidl, true)) {
             return;
         }
 
+        boolean clearBrowserElement = true;
+
         if (uidl.hasAttribute("type")) {
             final String type = uidl.getStringAttribute("type");
             if (type.equals("image")) {
                 setHTML("<img src=\"" + getSrc(uidl, client) + "\"/>");
             } else if (type.equals("browser")) {
-                setHTML("<iframe width=\"100%\" height=\"100%\" frameborder=\"0\" src=\""
-                        + getSrc(uidl, client) + "\"></iframe>");
+                if (browserElement == null) {
+                    setHTML("<iframe width=\"100%\" height=\"100%\" frameborder=\"0\" src=\""
+                            + getSrc(uidl, client) + "\"></iframe>");
+                    browserElement = DOM.getFirstChild(getElement());
+                } else {
+                    DOM.setElementAttribute(browserElement, "src", getSrc(uidl,
+                            client));
+                }
+                clearBrowserElement = false;
             } else {
                 ApplicationConnection.getConsole().log(
                         "Unknown Embedded type '" + type + "'");
@@ -47,10 +59,14 @@ public class IEmbedded extends HTML implements Paintable {
                     "Unknown Embedded; no type or mimetype attribute");
         }
 
+        if (clearBrowserElement) {
+            browserElement = null;
+        }
+
     }
 
     /**
-     * Helper to return translated src attribute from embeddeds uidl
+     * Helper to return translated src-attribute from embedded's UIDL
      * 
      * @param uidl
      * @param client
