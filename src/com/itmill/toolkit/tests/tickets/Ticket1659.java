cd20c16a2a2f38ec97434b8db627bd0d7ff2b38f
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2008-05-16 07:20:01 +0000

    Testcase for #1659
    svn changeset:4519/svn branch:trunk

---
+++ src/com/itmill/toolkit/tests/tickets/Ticket1659.java
@@ -0,0 +1,26 @@
+package com.itmill.toolkit.tests.tickets;
+
+import com.itmill.toolkit.Application;
+import com.itmill.toolkit.terminal.ExternalResource;
+import com.itmill.toolkit.ui.Button;
+import com.itmill.toolkit.ui.Window;
+import com.itmill.toolkit.ui.Button.ClickEvent;
+
+public class Ticket1659 extends Application {
+
+	public void init() {
+		final Window mainWin = new Window();
+		setMainWindow(mainWin);
+		mainWin.addComponent(new Button("Change URI using Application.getURL()", new Button.ClickListener() {
+
+			public void buttonClick(ClickEvent event) {
+				mainWin.open(new ExternalResource(getURL() + "#"+System.currentTimeMillis()));
+			}}));
+		mainWin.addComponent(new Button("Change URI uring Window.getURL()", new Button.ClickListener() {
+
+			public void buttonClick(ClickEvent event) {
+				mainWin.open(new ExternalResource(mainWin.getURL() + "#"+System.currentTimeMillis()));
+			}}));
+	}
+
+}
