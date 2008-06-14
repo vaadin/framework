49906489d014719701ccca4eb42d7b3d2c8cfd1c
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2008-06-14 14:34:30 +0000

    Merged the provided testcase from #1804 ticket to testcase
    svn changeset:4876/svn branch:trunk

--- src/com/itmill/toolkit/tests/tickets/Ticket1804.java
+++ src/com/itmill/toolkit/tests/tickets/Ticket1804.java
@@ -3,6 +3,8 @@ package com.itmill.toolkit.tests.tickets;
 import java.util.Iterator;
 import java.util.LinkedList;
 
+import com.itmill.toolkit.data.Validator;
+import com.itmill.toolkit.data.util.MethodProperty;
 import com.itmill.toolkit.ui.AbstractField;
 import com.itmill.toolkit.ui.Button;
 import com.itmill.toolkit.ui.Label;
@@ -27,6 +29,15 @@ public class Ticket1804 extends com.itmill.toolkit.Application {
 
         s = new Select("Select with null selection NOT allowed; required=true");
         s.setNullSelectionAllowed(false);
+        listOfAllFields.add(s);
+
+        s = new Select("Testcase from the ticket #1804");
+        s.setWidth(190);
+        s.setNullSelectionAllowed(false);
+        TestPojo myPojo = new TestPojo();
+        s.setPropertyDataSource(new MethodProperty(myPojo, "id"));
+        s.addValidator(new EmptyStringValidator(
+                "Selection required for test-field"));
         listOfAllFields.add(s);
 
         for (Iterator i = listOfAllFields.iterator(); i.hasNext();) {
@@ -65,6 +76,40 @@ public class Ticket1804 extends com.itmill.toolkit.Application {
                 main.addWindow(w);
             }
         });
+    }
+
+    class TestPojo {
+        String id = "";
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+    }
+
+    /** Throws an exception when the string is empty or null. */
+    class EmptyStringValidator implements Validator {
+
+        String msg;
+
+        EmptyStringValidator(String msg) {
+            this.msg = msg;
+        }
+
+        public boolean isValid(Object value) {
+            return !(value == null || value.toString().length() == 0);
+        }
+
+        public void validate(Object value) throws InvalidValueException {
+            if (!isValid(value)) {
+                throw new InvalidValueException(msg);
+            }
+        }
+
     }
 
 }
