033a83cd71cab3ad91f22124888024943d61e980
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2008-06-30 15:51:34 +0000

    Added test-cases for all existing layouts (except customlayout)
    svn changeset:4985/svn branch:trunk

--- src/com/itmill/toolkit/tests/tickets/Ticket1710.java
+++ src/com/itmill/toolkit/tests/tickets/Ticket1710.java
@@ -6,8 +6,14 @@ import com.itmill.toolkit.terminal.Sizeable;
 import com.itmill.toolkit.terminal.SystemError;
 import com.itmill.toolkit.terminal.ThemeResource;
 import com.itmill.toolkit.ui.Button;
+import com.itmill.toolkit.ui.Component;
+import com.itmill.toolkit.ui.ComponentContainer;
+import com.itmill.toolkit.ui.ExpandLayout;
 import com.itmill.toolkit.ui.Form;
+import com.itmill.toolkit.ui.GridLayout;
+import com.itmill.toolkit.ui.Label;
 import com.itmill.toolkit.ui.OrderedLayout;
+import com.itmill.toolkit.ui.Panel;
 import com.itmill.toolkit.ui.TextField;
 import com.itmill.toolkit.ui.Window;
 
@@ -19,8 +25,93 @@ public class Ticket1710 extends com.itmill.toolkit.Application {
 
         OrderedLayout lo = new OrderedLayout();
         setMainWindow(new Window("#1710", lo));
+        lo.setMargin(true);
         lo.setSpacing(true);
 
+        // OrderedLayout
+        OrderedLayout orderedVertical = new OrderedLayout();
+        lo.addComponent(new Panel("OrderedLayout Vertical", orderedVertical));
+        orderedVertical.setSpacing(true);
+        addFields(orderedVertical);
+        OrderedLayout orderedHorizontal = new OrderedLayout(
+                OrderedLayout.ORIENTATION_HORIZONTAL);
+        lo
+                .addComponent(new Panel("OrderedLayout Horizontal",
+                        orderedHorizontal));
+        orderedHorizontal.setSpacing(true);
+        addFields(orderedHorizontal);
+
+        // GridLayout
+        GridLayout grid = new GridLayout(1, 1);
+        lo.addComponent(new Panel("Gridlayout with 1 column", grid));
+        grid.setSpacing(true);
+        addFields(grid);
+
+        // GridLayout
+        GridLayout grid2 = new GridLayout(2, 1);
+        lo.addComponent(new Panel("Gridlayout with 2 columns", grid2));
+        grid2.setSpacing(true);
+        addFields(grid2);
+
+        // ExpandLayout
+        ExpandLayout el = new ExpandLayout();
+        Panel elp = new Panel("ExpandLayout width first component expanded", el);
+        el.setHeight(700);
+        addFields(el);
+        Component firstComponent = (Component) el.getComponentIterator().next();
+        firstComponent.setSizeFull();
+        el.expand(firstComponent);
+        lo.addComponent(elp);
+        ExpandLayout elh = new ExpandLayout(ExpandLayout.ORIENTATION_HORIZONTAL);
+        Panel elhp = new Panel(
+                "ExpandLayout width first component expanded; horizontal", elh);
+        elhp.setScrollable(true);
+        elh.setWidth(2000);
+        elh.setHeight(100);
+        addFields(elh);
+        Component firstComponentElh = (Component) elh.getComponentIterator()
+                .next();
+        firstComponentElh.setSizeFull();
+        elh.expand(firstComponentElh);
+        lo.addComponent(elhp);
+
+        // CustomLayout
+        Panel clp = new Panel("CustomLayout");
+        lo.addComponent(clp);
+        clp.addComponent(new Label("<<< Add customlayout testcase here >>>"));
+
+        // Form
+        Panel formPanel = new Panel("Form");
+        formPanel.addComponent(getFormPanelExample());
+        lo.addComponent(formPanel);
+
+    }
+
+    private Form getFormPanelExample() {
+        Form f = new Form();
+        f.setCaption("Test form");
+        Button fb1 = new Button("Test button");
+        fb1.setComponentError(new SystemError("Test error"));
+        f.addField("fb1", fb1);
+        Button fb2 = new Button("Test button");
+        fb2.setComponentError(new SystemError("Test error"));
+        fb2.setSwitchMode(true);
+        f.addField("fb2", fb2);
+        TextField ft1 = new TextField("With caption");
+        ft1.setComponentError(new SystemError("Error"));
+        f.addField("ft1", ft1);
+        TextField ft2 = new TextField();
+        ft2.setComponentError(new SystemError("Error"));
+        ft2.setValue("Without caption");
+        f.addField("ft2", ft2);
+        TextField ft3 = new TextField("With caption and required");
+        ft3.setComponentError(new SystemError("Error"));
+        ft3.setRequired(true);
+        f.addField("ft3", ft3);
+        return f;
+    }
+
+    private void addFields(ComponentContainer lo) {
         Button button = new Button("Test button");
         button.setComponentError(new SystemError("Test error"));
         lo.addComponent(button);
@@ -71,28 +162,5 @@ public class Ticket1710 extends com.itmill.toolkit.Application {
         t7.setRequired(true);
         t7.setIcon(new ThemeResource("icons/16/ok.png"));
         lo.addComponent(t7);
-
-        Form f = new Form();
-        f.setCaption("Test form");
-        Button fb1 = new Button("Test button");
-        fb1.setComponentError(new SystemError("Test error"));
-        f.addField("fb1", fb1);
-        Button fb2 = new Button("Test button");
-        fb2.setComponentError(new SystemError("Test error"));
-        fb2.setSwitchMode(true);
-        f.addField("fb2", fb2);
-        TextField ft1 = new TextField("With caption");
-        ft1.setComponentError(new SystemError("Error"));
-        f.addField("ft1", ft1);
-        TextField ft2 = new TextField();
-        ft2.setComponentError(new SystemError("Error"));
-        ft2.setValue("Without caption");
-        f.addField("ft2", ft2);
-        TextField ft3 = new TextField("With caption and required");
-        ft3.setComponentError(new SystemError("Error"));
-        ft3.setRequired(true);
-        f.addField("ft3", ft3);
-        lo.addComponent(f);
-
     }
 }
