d8494e9e87be26b44bb5e7b12862982abc139723
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2008-06-30 14:58:14 +0000

    Testcase for #1663
    svn changeset:4984/svn branch:trunk

---
+++ src/com/itmill/toolkit/tests/tickets/Ticket1663.java
@@ -0,0 +1,22 @@
+package com.itmill.toolkit.tests.tickets;
+
+import com.itmill.toolkit.terminal.SystemError;
+import com.itmill.toolkit.ui.TextField;
+import com.itmill.toolkit.ui.Window;
+
+public class Ticket1663 extends com.itmill.toolkit.Application {
+
+    public void init() {
+
+        Window main = new Window("#1663");
+        setMainWindow(main);
+
+        TextField tf = new TextField("First name");
+        tf
+                .setDescription("The first name is used for the administration user interfaces only.");
+        tf.setComponentError(new SystemError(
+                "You must enter only one first name."));
+
+        main.addComponent(tf);
+    }
+}
