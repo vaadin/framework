25e59660ecdc8749003bfad308c25466fa3dff3c
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2008-06-30 13:23:18 +0000

    Testcase for #1710
    svn changeset:4981/svn branch:trunk

---
+++ src/com/itmill/toolkit/tests/tickets/Ticket1710.java
@@ -0,0 +1,96 @@
+package com.itmill.toolkit.tests.tickets;
+
+import java.util.LinkedList;
+
+import com.itmill.toolkit.terminal.Sizeable;
+import com.itmill.toolkit.terminal.SystemError;
+import com.itmill.toolkit.terminal.ThemeResource;
+import com.itmill.toolkit.ui.Button;
+import com.itmill.toolkit.ui.Form;
+import com.itmill.toolkit.ui.OrderedLayout;
+import com.itmill.toolkit.ui.TextField;
+import com.itmill.toolkit.ui.Window;
+
+public class Ticket1710 extends com.itmill.toolkit.Application {
+
+    LinkedList listOfAllFields = new LinkedList();
+
+    public void init() {
+
+        OrderedLayout lo = new OrderedLayout();
+        setMainWindow(new Window("#1710", lo));
+        lo.setSpacing(true);
+
+        Button button = new Button("Test button");
+        button.setComponentError(new SystemError("Test error"));
+        lo.addComponent(button);
+
+        Button b2 = new Button("Test button");
+        b2.setComponentError(new SystemError("Test error"));
+        b2.setSwitchMode(true);
+        lo.addComponent(b2);
+
+        TextField t1 = new TextField("With caption");
+        t1.setComponentError(new SystemError("Error"));
+        lo.addComponent(t1);
+
+        TextField t2 = new TextField("With caption and required");
+        t2.setComponentError(new SystemError("Error"));
+        t2.setRequired(true);
+        lo.addComponent(t2);
+
+        TextField t3 = new TextField();
+        t3.setValue("With caption");
+        t3.setComponentError(new SystemError("Error"));
+        lo.addComponent(t3);
+
+        TextField t4 = new TextField();
+        t4.setValue("With caption and required");
+        t4.setComponentError(new SystemError("Error"));
+        t4.setRequired(true);
+        lo.addComponent(t4);
+
+        TextField t5 = new TextField();
+        t5.setValue("With caption - WIDE");
+        t5.setComponentError(new SystemError("Error"));
+        lo.addComponent(t5);
+
+        TextField t6 = new TextField();
+        t6.setValue("With caption and required - WIDE");
+        t6.setComponentError(new SystemError("Error"));
+        t6.setRequired(true);
+        t6.setWidth(100);
+        t6.setWidthUnits(Sizeable.UNITS_PERCENTAGE);
+        lo.addComponent(t6);
+
+        TextField t7 = new TextField();
+        t7.setValue("With caption and required and icon");
+        t7.setComponentError(new SystemError("Error"));
+        t7.setRequired(true);
+        t7.setIcon(new ThemeResource("icons/16/ok.png"));
+        lo.addComponent(t7);
+
+        Form f = new Form();
+        f.setCaption("Test form");
+        Button fb1 = new Button("Test button");
+        fb1.setComponentError(new SystemError("Test error"));
+        f.addField("fb1", fb1);
+        Button fb2 = new Button("Test button");
+        fb2.setComponentError(new SystemError("Test error"));
+        fb2.setSwitchMode(true);
+        f.addField("fb2", fb2);
+        TextField ft1 = new TextField("With caption");
+        ft1.setComponentError(new SystemError("Error"));
+        f.addField("ft1", ft1);
+        TextField ft2 = new TextField();
+        ft2.setComponentError(new SystemError("Error"));
+        ft2.setValue("Without caption");
+        f.addField("ft2", ft2);
+        TextField ft3 = new TextField("With caption and required");
+        ft3.setComponentError(new SystemError("Error"));
+        ft3.setRequired(true);
+        f.addField("ft3", ft3);
+        lo.addComponent(f);
+
+    }
+}
