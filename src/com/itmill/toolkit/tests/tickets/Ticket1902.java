8dcf53e0099914db77830e0a4058a0bc10b2b84f
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2008-07-03 17:26:34 +0000

    More extensive tests for #1902
    svn changeset:5045/svn branch:trunk

--- src/com/itmill/toolkit/tests/tickets/Ticket1902.java
+++ src/com/itmill/toolkit/tests/tickets/Ticket1902.java
@@ -5,24 +5,67 @@ import com.itmill.toolkit.ui.Button;
 import com.itmill.toolkit.ui.OrderedLayout;
 import com.itmill.toolkit.ui.TextField;
 import com.itmill.toolkit.ui.Window;
+import com.itmill.toolkit.ui.Button.ClickEvent;
 
 public class Ticket1902 extends Application {
 
     public void init() {
 
+        // Main layout and main window
+        final OrderedLayout mainLayout = new OrderedLayout();
+        setMainWindow(new Window("Testcase for #1902", mainLayout));
+        setTheme("tests-tickets");
+        mainLayout.setMargin(false);
+        mainLayout.setSpacing(true);
+        mainLayout.addComponent(new Button("mainLayout.setSizeFull()",
+                new Button.ClickListener() {
+                    public void buttonClick(ClickEvent event) {
+                        mainLayout.setSizeFull();
+                        getMainWindow().showNotification(
+                                "Set the main layout size full");
+                    }
+                }));
+        mainLayout.addComponent(new Button("mainLayout.setWidth(\"100%\")",
+                new Button.ClickListener() {
+                    public void buttonClick(ClickEvent event) {
+                        mainLayout.setWidth("100%");
+                        getMainWindow().showNotification(
+                                "Set the main layout width 100%");
+                    }
+                }));
+
+        // 100% wide component
+        Button b2 = new Button("100% wide button");
+        mainLayout.addComponent(b2);
+        b2.setWidth("100%");
+
+        // 400px wide colored layout
         OrderedLayout lo = new OrderedLayout();
-        setMainWindow(new Window("Testcase for #1902", lo));
-
-        lo.setSpacing(false);
-        lo.setMargin(false);
-
-        TextField tf = new TextField("100% wide textfield");
-        lo.addComponent(tf);
-        tf.setWidth("100%");
+        lo.setStyleName("red-background");
+        mainLayout.addComponent(lo);
+        lo.setWidth(400);
 
         Button b = new Button("100% wide button");
         lo.addComponent(b);
         b.setWidth("100%");
 
+        TextField tf = new TextField("100% wide textfield");
+        lo.addComponent(tf);
+        tf.setWidth("100%");
+
+        // 400x100 colored layout
+        OrderedLayout lo2 = new OrderedLayout();
+        lo2.setStyleName("red-background");
+        mainLayout.addComponent(lo2);
+        lo2.setWidth(400);
+        lo2.setHeight(200);
+
+        Button b3 = new Button("100% wide button");
+        lo2.addComponent(b3);
+        b3.setWidth("100%");
+
+        TextField tf2 = new TextField("100% wide textfield");
+        lo2.addComponent(tf2);
+        tf2.setWidth("100%");
     }
 }
