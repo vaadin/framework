116e3761c096440929d94024e55e423a03b32de5
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2008-07-11 07:55:05 +0000

    Test for #1934 : Horizontal ExpandLayout completely broken
    svn changeset:5085/svn branch:trunk

---
+++ src/com/itmill/toolkit/tests/tickets/Ticket1934.java
@@ -0,0 +1,31 @@
+package com.itmill.toolkit.tests.tickets;
+
+import com.itmill.toolkit.Application;
+import com.itmill.toolkit.ui.Button;
+import com.itmill.toolkit.ui.ExpandLayout;
+import com.itmill.toolkit.ui.Label;
+import com.itmill.toolkit.ui.Window;
+
+public class Ticket1934 extends Application {
+
+    public void init() {
+        Window w = new Window(
+                "#1934 : Horizontal ExpandLayout completely broken");
+        setMainWindow(w);
+        w.addComponent(new Label(
+                "Horizontal 500x200 ExpandLayout with two components:"));
+
+        ExpandLayout testedLayout = new ExpandLayout(
+                ExpandLayout.ORIENTATION_HORIZONTAL);
+        testedLayout.setWidth(500);
+        testedLayout.setHeight(200);
+
+        Button b1 = new Button("b1");
+        testedLayout.addComponent(b1);
+        testedLayout.expand(b1);
+        testedLayout.addComponent(new Button("b2"));
+
+        w.addComponent(testedLayout);
+    }
+
+}
