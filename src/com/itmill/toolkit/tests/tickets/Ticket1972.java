bdc3f94efde8402e714d0dff8706a6790ecdc638
Artur Signell <artur.signell@itmill.com>
2008-08-25 08:02:49 +0000

    Ticket test cases
    svn changeset:5251/svn branch:trunk

---
+++ src/com/itmill/toolkit/tests/tickets/Ticket1972.java
@@ -0,0 +1,29 @@
+package com.itmill.toolkit.tests.tickets;
+
+import com.itmill.toolkit.Application;
+import com.itmill.toolkit.ui.GridLayout;
+import com.itmill.toolkit.ui.Label;
+import com.itmill.toolkit.ui.Window;
+
+public class Ticket1972 extends Application {
+
+    public void init() {
+        Window w = new Window(getClass().getSimpleName());
+        setMainWindow(w);
+        setTheme("tests-ticket");
+        GridLayout layout = new GridLayout(3, 3);
+        layout.setStyleName("borders");
+        layout.addComponent(new Label("1-1"));
+        layout.space();
+        layout.space();
+        layout.addComponent(new Label("2-1"));
+        layout.space();
+        layout.space();
+        layout.addComponent(new Label("3-1"));
+        layout.space();
+        layout.addComponent(new Label("3-3"));
+
+        w.setLayout(layout);
+    }
+
+}
---
+++ src/com/itmill/toolkit/tests/tickets/Ticket677.java
@@ -0,0 +1,68 @@
+package com.itmill.toolkit.tests.tickets;
+
+import com.itmill.toolkit.Application;
+import com.itmill.toolkit.ui.Button;
+import com.itmill.toolkit.ui.GridLayout;
+import com.itmill.toolkit.ui.Label;
+import com.itmill.toolkit.ui.OrderedLayout;
+import com.itmill.toolkit.ui.Panel;
+import com.itmill.toolkit.ui.TextField;
+import com.itmill.toolkit.ui.Window;
+import com.itmill.toolkit.ui.Button.ClickEvent;
+import com.itmill.toolkit.ui.Button.ClickListener;
+
+public class Ticket677 extends Application {
+
+    private Panel panel;
+
+    public void init() {
+        Window w = new Window(getClass().getSimpleName());
+        setMainWindow(w);
+        // setTheme("tests-ticket");
+        GridLayout layout = new GridLayout(10, 10);
+        w.setLayout(layout);
+        createUI(layout);
+    }
+
+    private void createUI(GridLayout layout) {
+        panel = new Panel("panel caption");
+        layout.addComponent(panel);
+        layout.addComponent(new Button("Enable", new ClickListener() {
+
+            public void buttonClick(ClickEvent event) {
+                panel.setEnabled(true);
+            }
+
+        }));
+
+        layout.addComponent(new Button("Disable", new ClickListener() {
+
+            public void buttonClick(ClickEvent event) {
+                panel.setEnabled(false);
+            }
+
+        }));
+
+        panel.addComponent(new Label("Label 1"));
+
+        OrderedLayout innerLayout1 = new OrderedLayout(
+                OrderedLayout.ORIENTATION_HORIZONTAL);
+        innerLayout1.setSpacing(true);
+        panel.addComponent(innerLayout1);
+
+        innerLayout1.addComponent(new TextField(
+                "TextField inside orderedLayout"));
+        innerLayout1.addComponent(new Button("Button inside orderedLayout"));
+        innerLayout1.addComponent(new Label("Label inside orderedLayout"));
+
+        panel.addComponent(new Label("Label 2"));
+
+        GridLayout innerLayout2 = new GridLayout(3, 3);
+        innerLayout2.setSpacing(true);
+        innerLayout2.addComponent(new TextField("TextField inside gridLayout"));
+        innerLayout2.addComponent(new Button("Button inside gridLayout"));
+        innerLayout2.addComponent(new Label("Label inside gridLayout"));
+        panel.addComponent(innerLayout2);
+
+    }
+}
