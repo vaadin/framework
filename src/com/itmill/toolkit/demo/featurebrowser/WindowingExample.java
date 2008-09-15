d4c0436c97b6137a9e4763ec38885a10e168e801
Jouni Koivuviita <jouni.koivuviita@itmill.com>
2008-09-15 13:06:14 +0000

    Minor typo fixed + some text formatting for WindowingExample.
    svn changeset:5401/svn branch:trunk

--- src/com/itmill/toolkit/demo/featurebrowser/WindowingExample.java
+++ src/com/itmill/toolkit/demo/featurebrowser/WindowingExample.java
@@ -20,16 +20,16 @@ import com.itmill.toolkit.ui.Button.ClickEvent;
  */
 public class WindowingExample extends CustomComponent {
 
-    public static final String txt = "<p>There are two main types of windows: application-level windows, and"
-            + "\"subwindows\". </p><p> A subwindow is rendered as a \"inline\" popup window"
+    public static final String txt = "<p>There are two main types of windows: application-level windows, and "
+            + "\"sub windows\".</p><p>A sub window is rendered as a \"inline\" popup window"
             + " within the (native) browser window to which it was added. You can create"
-            + " a subwindow by creating a new Window and adding it to a application-level window, for instance"
+            + " a sub window by creating a new Window and adding it to a application-level window, for instance"
             + " your main window. </p><p> In contrast, you create a application-level window by"
             + " creating a new Window and adding it to the Application. Application-level"
             + " windows are not shown by default - you need to open a browser window for"
             + " the url representing the window. You can think of the application-level"
             + " windows as separate views into your application - and a way to create a"
-            + " \"native\" browser window. </p><p> Depending on your needs, it's also"
+            + " \"native\" browser window.</p><p>Depending on your needs, it's also"
             + " possible to create a new window instance (with it's own internal state)"
             + " for each new (native) browser window, or you can share the same instance"
             + " (and state) between several browser windows (the latter is most useful"
