83d6a2c216e021def3e7d331374bf7cca83d8ae2
Artur Signell <artur.signell@itmill.com>
2008-09-22 08:22:39 +0000

    Test case for #2101
    svn changeset:5473/svn branch:trunk

---
+++ src/com/itmill/toolkit/tests/tickets/Ticket2101.java
@@ -0,0 +1,20 @@
+package com.itmill.toolkit.tests.tickets;
+
+import com.itmill.toolkit.Application;
+import com.itmill.toolkit.ui.Button;
+import com.itmill.toolkit.ui.Window;
+
+public class Ticket2101 extends Application {
+
+    public void init() {
+        Window w = new Window(getClass().getSimpleName());
+        setMainWindow(w);
+
+        Button b = new Button(
+                "Button with a long text which will not fit on 50 pixels");
+        b.setWidth("50px");
+
+        w.getLayout().addComponent(b);
+    }
+
+}
