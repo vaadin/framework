a8ac497383dec8b113dcc0e7fb37e2f33bc827e1
Matti Tahvonen <matti.tahvonen@itmill.com>
2008-09-24 11:40:00 +0000

    cleaned test case
    svn changeset:5499/svn branch:trunk

--- src/com/itmill/toolkit/tests/tickets/Ticket2103.java
+++ src/com/itmill/toolkit/tests/tickets/Ticket2103.java
@@ -2,17 +2,10 @@ package com.itmill.toolkit.tests.tickets;
 
 import com.itmill.toolkit.Application;
 import com.itmill.toolkit.data.Item;
-import com.itmill.toolkit.data.Property.ValueChangeEvent;
-import com.itmill.toolkit.data.Property.ValueChangeListener;
 import com.itmill.toolkit.data.util.HierarchicalContainer;
-import com.itmill.toolkit.terminal.Sizeable;
 import com.itmill.toolkit.ui.Accordion;
 import com.itmill.toolkit.ui.Component;
-import com.itmill.toolkit.ui.CustomComponent;
 import com.itmill.toolkit.ui.ExpandLayout;
-import com.itmill.toolkit.ui.OrderedLayout;
-import com.itmill.toolkit.ui.SplitPanel;
-import com.itmill.toolkit.ui.TabSheet;
 import com.itmill.toolkit.ui.Table;
 import com.itmill.toolkit.ui.Window;
 
@@ -21,27 +14,20 @@ public class Ticket2103 extends Application {
 
     public void init() {
         mainWindow = new Window(getClass().getSimpleName());
+        mainWindow.setLayout(new ExpandLayout());
         mainWindow.setSizeFull();
         mainWindow.getLayout().setSizeFull();
-        setMainWindow(mainWindow);
-        mainWindow.setLayout(new SplitMenu());
 
         MyTable table1 = new MyTable(4, "table1");
         table1.loadTable(100);
-
         MyTable table2 = new MyTable(4, "table2");
         table2.loadTable(100);
 
-        /*
-         * MyTable table3 = new MyTable(4, "table3"); table3.loadTable(100);
-         */
-
-        MyAccordion accordion = new MyAccordion(new Component[] { table1,
-                table2 }, "Test");
-
-        Tabs tab = new Tabs(new Component[] { accordion });
-
-        mainWindow.addComponent(tab);
+        MyAccordion a = new MyAccordion(new Component[] { table1, table2 },
+                "FDSF");
+        mainWindow.addComponent(a);
+        setMainWindow(mainWindow);
+        // mainWindow.addComponent(table1);
 
     }
 
@@ -57,15 +43,14 @@ public class Ticket2103 extends Application {
         }
     }
 
-    public class MyTable extends CustomComponent implements ValueChangeListener {
+    public class MyTable extends Table {
 
-        private Table table = new Table();
+        private Table table = this;
         private String[] columns;
         private ExpandLayout layout = new ExpandLayout();
 
         public MyTable(int columnNumber, String id) {
             setDebugId(id);
-            setCompositionRoot(layout);
             setSizeFull();
             columns = initializeColumns(columnNumber);
             table.setWidth("100%");
@@ -76,7 +61,6 @@ public class Ticket2103 extends Application {
             table.setMultiSelect(false);
             table.setNullSelectionAllowed(false);
             // table.setRowHeaderMode(Table.ROW_HEADER_MODE_ID);
-            table.addListener(this);
             table.setContainerDataSource(createContainer());
             layout.addComponent(table);
         }
@@ -110,34 +94,6 @@ public class Ticket2103 extends Application {
             return columns;
         }
 
-        public void valueChange(ValueChangeEvent event) {
-
-        }
-
-    }
-
-    public class SplitMenu extends SplitPanel {
-
-        private OrderedLayout verticalLayout = new OrderedLayout();
-
-        public SplitMenu() {
-            super(SplitPanel.ORIENTATION_HORIZONTAL);
-            setLocked(true);
-            this.setSplitPosition(330, Sizeable.UNITS_PIXELS);
-            addComponent(verticalLayout);
-        }
-    }
-
-    public class Tabs extends TabSheet {
-
-        public Tabs(Component[] components) {
-            this.setWidth("100%");
-            this.setHeight("100%");
-            for (int i = 0; i < components.length; i++) {
-                this.addTab(components[i], components[i].getDebugId(), null);
-            }
-
-        }
     }
 
 }
