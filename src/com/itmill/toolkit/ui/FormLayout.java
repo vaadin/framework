df32c7dda4e7755293778fdfdd999a722e1f3cb9
Matti Tahvonen <matti.tahvonen@itmill.com>
2008-11-10 13:56:56 +0000

    Some documentation and better defaults for formlayout
    svn changeset:5849/svn branch:trunk

--- src/com/itmill/toolkit/ui/FormLayout.java
+++ src/com/itmill/toolkit/ui/FormLayout.java
@@ -4,7 +4,28 @@
 
 package com.itmill.toolkit.ui;
 
+/**
+ * FormLayout is used by {@link Form} to layout fields. It may also be used
+ * separately without {@link Form}.
+ * 
+ * FormLayout is a close relative to vertical {@link OrderedLayout}, but in
+ * FormLayout caption is rendered on left side of component. Required and
+ * validation indicators are between captions and fields.
+ * 
+ * FormLayout does not currently support some advanced methods from
+ * OrderedLayout like setExpandRatio and setComponentAlignment.
+ * 
+ * FormLayout by default has component spacing on. Also margin top and margin
+ * bottom are by default on.
+ * 
+ */
 public class FormLayout extends OrderedLayout {
+
+    public FormLayout() {
+        super();
+        setSpacing(true);
+        setMargin(true, false, true, false);
+    }
 
     public String getTag() {
         return "formlayout";
