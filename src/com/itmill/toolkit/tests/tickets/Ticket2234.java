8cb6cb7a19066236f883c8ef7ebc67aaea8a2ee0
Artur Signell <artur.signell@itmill.com>
2008-11-27 12:25:22 +0000

    #2234 test case
    svn changeset:6013/svn branch:trunk

---
+++ src/com/itmill/toolkit/tests/tickets/Ticket2234.java
@@ -0,0 +1,42 @@
+package com.itmill.toolkit.tests.tickets;
+
+import com.itmill.toolkit.Application;
+import com.itmill.toolkit.data.Item;
+import com.itmill.toolkit.ui.ComboBox;
+import com.itmill.toolkit.ui.OrderedLayout;
+import com.itmill.toolkit.ui.Window;
+
+public class Ticket2234 extends Application {
+
+    public void init() {
+        Window w = new Window(getClass().getSimpleName());
+        setMainWindow(w);
+        // setTheme("tests-tickets");
+        createUI((OrderedLayout) w.getLayout());
+    }
+
+    private void createUI(OrderedLayout layout) {
+        ComboBox combo = new ComboBox("Combobox caption");
+        combo.addContainerProperty("blah", String.class, "");
+        combo.setItemCaptionPropertyId("blah");
+
+        Item item;
+        for (int i = 0; i < 100; i++) {
+            item = combo.addItem(new Object());
+            item.getItemProperty("blah").setValue("Item " + i);
+        }
+
+        layout.addComponent(combo);
+
+        combo = new ComboBox("Combobox caption");
+        combo.addContainerProperty("blah", String.class, "");
+        combo.setItemCaptionPropertyId("blah");
+
+        for (int i = 0; i < 5; i++) {
+            item = combo.addItem(new Object());
+            item.getItemProperty("blah").setValue("Item " + i);
+        }
+
+        layout.addComponent(combo);
+    }
+}
