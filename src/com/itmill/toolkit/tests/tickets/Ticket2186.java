69b6ba8299a55a4b4e5957b624035b572ec49329
Artur Signell <artur.signell@itmill.com>
2008-12-01 13:48:32 +0000

    Test case for #2186
    svn changeset:6052/svn branch:trunk

---
+++ src/com/itmill/toolkit/tests/tickets/Ticket2186.java
@@ -0,0 +1,53 @@
+package com.itmill.toolkit.tests.tickets;
+
+import com.itmill.toolkit.Application;
+import com.itmill.toolkit.ui.Label;
+import com.itmill.toolkit.ui.OrderedLayout;
+import com.itmill.toolkit.ui.Panel;
+import com.itmill.toolkit.ui.Table;
+import com.itmill.toolkit.ui.Window;
+
+public class Ticket2186 extends Application {
+
+    public void init() {
+        Window main = new Window("Quick test");
+        setMainWindow(main);
+
+        OrderedLayout base = new OrderedLayout(
+                OrderedLayout.ORIENTATION_HORIZONTAL);
+        main.setLayout(base);
+
+        OrderedLayout content = new OrderedLayout();
+
+        content.addComponent(new Label("Content."));
+        content.setWidth("500px");
+
+        Table table = new Table();
+
+        table.setPageLength(10);
+
+        table.setWidth("100%");
+
+        table.addContainerProperty("Lähettäjä", String.class, "");
+        table.addContainerProperty("Viestin tyyppi", String.class, "");
+
+        for (int i = 0; i < 15; i++) {
+
+            table.addItem(new Object[] { i + " Joku Ihminen", "Testiviesti" },
+
+            new Object());
+
+        }
+
+        content.addComponent(table);
+
+        Panel right = new Panel("Panel");
+
+        right.addComponent(new Label("Some basic text might show up here."));
+
+        base.addComponent(content);
+
+        base.addComponent(right);
+
+    }
+}
