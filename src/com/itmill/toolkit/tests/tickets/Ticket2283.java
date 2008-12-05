8fe4061d56d1ed74ca67fe0e0c59d3beb2c435c2
Matti Tahvonen <matti.tahvonen@itmill.com>
2008-12-05 16:35:56 +0000

    Fixed bug in test case
    svn changeset:6102/svn branch:trunk

--- src/com/itmill/toolkit/tests/tickets/Ticket2283.java
+++ src/com/itmill/toolkit/tests/tickets/Ticket2283.java
@@ -8,6 +8,7 @@ import com.itmill.toolkit.ui.Layout.AlignmentHandler;
 
 public class Ticket2283 extends Application {
 
+    @Override
     public void init() {
         Window w = new Window(getClass().getSimpleName());
         setMainWindow(w);
@@ -18,8 +19,9 @@ public class Ticket2283 extends Application {
         gl.addComponent(new Label(
                 "Label 1 abc abc abcasdfas dfasd fasdf asdf sadf asdf"));
         gl.addComponent(new Label("Label 2 abc abc abc "));
-        gl.addComponent(new Label("Colspan2, align right"), 0, 1, 1, 1);
-        gl.setComponentAlignment(gl, AlignmentHandler.ALIGNMENT_RIGHT,
+        Label l = new Label("Colspan2, align right");
+        gl.addComponent(l, 0, 1, 1, 1);
+        gl.setComponentAlignment(l, AlignmentHandler.ALIGNMENT_RIGHT,
                 AlignmentHandler.ALIGNMENT_TOP);
         w.setLayout(gl);
 
