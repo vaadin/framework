672504d869ebc23133dc65ad34dadc12cd05441e
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2008-12-15 21:32:16 +0000

    Fixed #2340 : If widgetset is missing, an error should be shown
    svn changeset:6219/svn branch:trunk

--- src/com/itmill/toolkit/terminal/gwt/server/ApplicationServlet.java
+++ src/com/itmill/toolkit/terminal/gwt/server/ApplicationServlet.java
@@ -997,6 +997,15 @@ public class ApplicationServlet extends HttpServlet {
             }
         }
 
+        // Warn if the widgetset has not been loaded after 15 seconds on
+        // inactivity
+        page.write("<script type=\"text/javascript\">\n"
+                + "setTimeout('if (typeof " + widgetset.replace('.', '_')
+                + " == \"undefined\") {alert(\"Failed to load the widgetset: "
+                + staticFilePath + "/" + WIDGETSET_DIRECTORY_PATH + widgetset
+                + "/" + widgetset + ".nocache.js\")};',15000);\n"
+                + "//]]>\n</script>\n");
+
         String style = null;
         reqParam = request.getAttribute(REQUEST_APPSTYLE);
         if (reqParam != null) {
