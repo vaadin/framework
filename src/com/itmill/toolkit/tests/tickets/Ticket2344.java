d238b83069b47bee03101e8089c2434b7dfa8155
Artur Signell <artur.signell@itmill.com>
2008-12-18 11:45:48 +0000

    #2344 & #2364 test cases
    svn changeset:6267/svn branch:trunk

---
+++ src/com/itmill/toolkit/tests/tickets/Ticket2344.java
@@ -0,0 +1,55 @@
+package com.itmill.toolkit.tests.tickets;
+
+import java.util.Random;
+
+import com.itmill.toolkit.Application;
+import com.itmill.toolkit.ui.Button;
+import com.itmill.toolkit.ui.Panel;
+import com.itmill.toolkit.ui.Table;
+import com.itmill.toolkit.ui.VerticalLayout;
+import com.itmill.toolkit.ui.Window;
+
+public class Ticket2344 extends Application {
+
+    private Panel p;
+
+    public void init() {
+        Window main = new Window("Quick test");
+
+        setMainWindow(main);
+
+        // setTheme("quicktest");
+
+        VerticalLayout hl = new VerticalLayout();
+        hl.setWidth("400px");
+        main.setLayout(hl);
+
+        Table t = new Table();
+        t.setWidth("100%");
+
+        t.addContainerProperty("Prop 1", VerticalLayout.class, "");
+        t.addContainerProperty("Prop 2", String.class, "");
+        t.addContainerProperty("Prop 3", String.class, "");
+        t.addContainerProperty("Prop 4", String.class, "");
+        t.addContainerProperty("Prop 5", Button.class, "");
+
+        t.setPageLength(3);
+
+        for (int i = 0; i < 10; i++) {
+
+            VerticalLayout vl = new VerticalLayout();
+            // vl.setWidth(null);
+            Button b = new Button("String 1 2 3");
+            b.setStyleName(Button.STYLE_LINK);
+            vl.addComponent(b);
+            t.addItem(new Object[] { vl, "String 2", "String 3", "String 4",
+
+            new Button("String 5") }, new Integer(new Random().nextInt()));
+
+        }
+
+        hl.addComponent(t);
+
+    }
+
+}
---
+++ src/com/itmill/toolkit/tests/tickets/Ticket2364.java
@@ -0,0 +1,36 @@
+package com.itmill.toolkit.tests.tickets;
+
+import com.itmill.toolkit.Application;
+import com.itmill.toolkit.ui.Form;
+import com.itmill.toolkit.ui.Select;
+import com.itmill.toolkit.ui.VerticalLayout;
+import com.itmill.toolkit.ui.Window;
+
+public class Ticket2364 extends Application {
+
+    public void init() {
+
+        Window main = new Window("The Main Window!!!");
+        setMainWindow(main);
+        Form form = new Form();
+        VerticalLayout formLayout = new VerticalLayout();
+        form.setLayout(formLayout);
+        Select formSelect = new Select("hello");
+        Select select = new Select("hello");
+        form.setEnabled(false);
+        select.setEnabled(false);
+        formLayout.addComponent(formSelect);
+
+        VerticalLayout l2 = new VerticalLayout();
+        l2.addComponent(new Select("hello"));
+        l2.setEnabled(false);
+
+        form.setCaption("Form");
+        main.addComponent(form);
+        l2.setCaption("VerticalLayout");
+        main.addComponent(l2);
+        main.addComponent(select);
+        main.addComponent(new Select("Enabled=true select"));
+    }
+
+}
