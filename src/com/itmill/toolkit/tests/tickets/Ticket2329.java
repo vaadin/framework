283345e3af99ee0ccdc3757baa0a4e858fc6e1d6
Artur Signell <artur.signell@itmill.com>
2008-12-18 13:49:27 +0000

    Test case for #2239
    svn changeset:6278/svn branch:trunk

---
+++ src/com/itmill/toolkit/tests/tickets/Ticket2329.java
@@ -0,0 +1,41 @@
+package com.itmill.toolkit.tests.tickets;
+
+import com.itmill.toolkit.Application;
+import com.itmill.toolkit.ui.Button;
+import com.itmill.toolkit.ui.Component;
+import com.itmill.toolkit.ui.Table;
+import com.itmill.toolkit.ui.VerticalLayout;
+import com.itmill.toolkit.ui.Window;
+import com.itmill.toolkit.ui.Table.ColumnGenerator;
+
+public class Ticket2329 extends Application {
+    private Table table;
+    private VerticalLayout mainLo;
+
+    public void init() {
+        Window mainw = new Window();
+        setMainWindow(mainw);
+        mainLo = (VerticalLayout) mainw.getLayout();
+        table = new Table();
+        for (int i = 0; i < 10000; i++) {
+            table.addItem(i);
+        }
+        TestColumnGenerator cgen = new TestColumnGenerator();
+        table.addGeneratedColumn("col1", cgen);
+        table.addGeneratedColumn("col2", cgen);
+        table.addGeneratedColumn("col3", cgen);
+        table.addGeneratedColumn("col4", cgen);
+        table.addGeneratedColumn("col5", cgen);
+        table.addGeneratedColumn("col6", cgen);
+        table.addGeneratedColumn("col7", cgen);
+        table.setHeight("500px");
+        mainLo.addComponent(table);
+    }
+
+    class TestColumnGenerator implements ColumnGenerator {
+        public Component generateCell(Table source, Object rowId,
+                Object columnId) {
+            return new Button("1");
+        }
+    }
+}
