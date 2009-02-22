47ab8dc2cd534d9361dfd05d1620a4c0420e4009
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2009-02-22 18:29:24 +0000

    ASSIGNED - # 2604: Review Coverflow demo source code http://dev.itmill.com/ticket/2604
    Fixed deprecated method calls
    
    svn changeset:6931/svn branch:trunk

--- src/com/itmill/toolkit/demo/coverflow/gwt/client/CoverflowWidgetSet.java
+++ src/com/itmill/toolkit/demo/coverflow/gwt/client/CoverflowWidgetSet.java
@@ -12,9 +12,8 @@ import com.itmill.toolkit.terminal.gwt.client.UIDL;
 public class CoverflowWidgetSet extends DefaultWidgetSet {
     /** Creates a widget according to its class name. */
     public Paintable createWidget(UIDL uidl) {
-        final String className = resolveWidgetTypeName(uidl);
-        if ("com.itmill.toolkit.demo.coverflow.gwt.client.ui.ICoverflow"
-                .equals(className)) {
+        final Class classType = resolveWidgetType(uidl);
+        if (ICoverflow.class == classType) {
             return new ICoverflow();
         }
 
@@ -22,14 +21,14 @@ public class CoverflowWidgetSet extends DefaultWidgetSet {
         return super.createWidget(uidl);
     }
 
-    /** Resolves UIDL tag name to class name. */
-    protected String resolveWidgetTypeName(UIDL uidl) {
+    /** Resolves UIDL tag name to class . */
+    protected Class resolveWidgetType(UIDL uidl) {
         final String tag = uidl.getTag();
         if ("cover".equals(tag)) {
-            return "com.itmill.toolkit.demo.coverflow.gwt.client.ui.ICoverflow";
+            return ICoverflow.class;
         }
 
         // Let the DefaultWidgetSet handle resolution of default widgets
-        return super.resolveWidgetTypeName(uidl);
+        return super.resolveWidgetType(uidl);
     }
 }
