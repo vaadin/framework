050ee582add077199cbf28b395104c6cfc027f5a
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2009-04-12 09:20:58 +0000

    Added serialization-time measurement to Ticket695
    svn changeset:7388/svn branch:6.0

--- src/com/itmill/toolkit/tests/tickets/Ticket695.java
+++ src/com/itmill/toolkit/tests/tickets/Ticket695.java
@@ -3,6 +3,7 @@ package com.itmill.toolkit.tests.tickets;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
+
 import com.itmill.toolkit.Application;
 import com.itmill.toolkit.ui.Button;
 import com.itmill.toolkit.ui.Window;
@@ -10,31 +11,33 @@ import com.itmill.toolkit.ui.Button.ClickEvent;
 
 public class Ticket695 extends Application {
 
-	private static final long serialVersionUID = 3803150085397590662L;
+    private static final long serialVersionUID = 3803150085397590662L;
 
-	@Override
-	public void init() {
-		final Window w = new Window("Serialization test #695");
-		setMainWindow(w);
-		Button b = new Button("Serialize ApplicationContext");
-		w.addComponent(b);
-		b.addListener(new Button.ClickListener() {
+    @Override
+    public void init() {
+        final Window w = new Window("Serialization test #695");
+        setMainWindow(w);
+        Button b = new Button("Serialize ApplicationContext");
+        w.addComponent(b);
+        b.addListener(new Button.ClickListener() {
 
-			public void buttonClick(ClickEvent event) {
-				ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-				try {
-					ObjectOutputStream oos = new ObjectOutputStream(buffer);
-					oos.writeObject(getContext());
-					w.showNotification("ApplicationContext serialized ("
-							+ buffer.size() + "bytes)");
-				} catch (IOException e) {
-					e.printStackTrace();
-					w
-							.showNotification("ApplicationContext serialization failed - see console for stacktrace");
-				}
+            public void buttonClick(ClickEvent event) {
+                ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+                try {
+                    ObjectOutputStream oos = new ObjectOutputStream(buffer);
+                    long t = System.currentTimeMillis();
+                    oos.writeObject(getContext());
+                    w.showNotification("ApplicationContext serialized ("
+                            + buffer.size() + "bytes) in "
+                            + (System.currentTimeMillis() - t) + "ms");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    w
+                            .showNotification("ApplicationContext serialization failed - see console for stacktrace");
+                }
 
-			}
-		});
-	}
+            }
+        });
+    }
 
 }
