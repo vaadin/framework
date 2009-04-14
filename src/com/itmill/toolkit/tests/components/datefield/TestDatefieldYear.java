88286ad1ea4a02b37ddceea1c291945d40f43ea8
Artur Signell <artur.signell@itmill.com>
2009-04-14 14:40:42 +0000

    Merged test case for #2813
    svn changeset:7411/svn branch:6.0

---
+++ src/com/itmill/toolkit/tests/components/datefield/TestDatefieldYear.java
@@ -0,0 +1,28 @@
+package com.itmill.toolkit.tests.components.datefield;
+
+import java.util.Date;
+
+import com.itmill.toolkit.tests.components.TestBase;
+import com.itmill.toolkit.ui.DateField;
+
+public class TestDatefieldYear extends TestBase {
+
+    @Override
+    protected String getDescription() {
+        return "A popup with resolution year or month should update the textfield when browsing. The value displayed in the textfield should always be the same as the popup shows.";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 2813;
+    }
+
+    @Override
+    protected void setup() {
+        DateField df = new DateField("Year", new Date(2009 - 1900, 4 - 1, 1));
+        df.setResolution(DateField.RESOLUTION_YEAR);
+        df.setResolution(DateField.RESOLUTION_MONTH);
+        addComponent(df);
+
+    }
+}
