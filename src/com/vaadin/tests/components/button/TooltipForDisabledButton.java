cdf098a9eb939b1488f377067f53833073892fd7
Artur Signell <artur.signell@itmill.com>
2009-05-22 16:51:00 +0000

    Test case for #2085
    svn changeset:7975/svn branch:6.0

---
+++ src/com/vaadin/tests/components/button/TooltipForDisabledButton.java
@@ -0,0 +1,48 @@
+package com.vaadin.tests.components.button;
+
+import com.vaadin.tests.components.TestBase;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+
+public class TooltipForDisabledButton extends TestBase {
+
+    @Override
+    protected String getDescription() {
+        return "A disabled button should show a tooltip when hovering it";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 2085;
+    }
+
+    @Override
+    protected void setup() {
+        Button buttonEnabled = new Button("This is an enabled button");
+        Button buttonDisabled = new Button("This is an disabled button");
+        buttonDisabled.setEnabled(false);
+
+        buttonEnabled.setDescription("Tooltip for enabled");
+        buttonDisabled.setDescription("Tooltip for disabled");
+
+        buttonDisabled.addListener(new Button.ClickListener() {
+
+            public void buttonClick(ClickEvent event) {
+                getMainWindow().showNotification("Clicked Disabled");
+            }
+
+        });
+
+        buttonEnabled.addListener(new Button.ClickListener() {
+
+            public void buttonClick(ClickEvent event) {
+                getMainWindow().showNotification("Clicked Enabled");
+            }
+
+        });
+
+        addComponent(buttonEnabled);
+        addComponent(buttonDisabled);
+    }
+
+}
