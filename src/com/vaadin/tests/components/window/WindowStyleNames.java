a86d8c95717dd48bb43b1130b1366651afbf5e10
Artur Signell <artur.signell@itmill.com>
2009-06-12 08:01:18 +0000

    Test case for #3059
    svn changeset:8181/svn branch:6.0

---
+++ src/com/vaadin/tests/components/window/WindowStyleNames.java
@@ -0,0 +1,46 @@
+package com.vaadin.tests.components.window;
+
+import com.vaadin.tests.components.TestBase;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+
+public class WindowStyleNames extends TestBase {
+
+    @Override
+    protected String getDescription() {
+        return "Click 'add style' to add a 'new' style to the window. The 'old' style should disappear and only the 'new' style should be set. Verify using e.g. firebug";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 3059;
+    }
+
+    @Override
+    protected void setup() {
+        setWindowStyle("old");
+        addComponent(new Button("Set style to 'new'", new ClickListener() {
+
+            public void buttonClick(ClickEvent event) {
+                setWindowStyle("new");
+            }
+
+        }));
+
+        addComponent(new Button("Set style to 'custom'", new ClickListener() {
+
+            public void buttonClick(ClickEvent event) {
+                setWindowStyle("custom");
+            }
+
+        }));
+
+    }
+
+    protected void setWindowStyle(String string) {
+        getMainWindow().setStyleName(string);
+
+    }
+
+}
