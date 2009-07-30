989537849400f214998901f27804e3c34874d822
Matti Tahvonen <matti.tahvonen@itmill.com>
2009-07-30 05:15:15 +0000

    fixes #3172
    svn changeset:8420/svn branch:6.0

--- src/com/vaadin/terminal/gwt/client/ui/VTabsheet.java
+++ src/com/vaadin/terminal/gwt/client/ui/VTabsheet.java
@@ -383,6 +383,7 @@ public class VTabsheet extends VTabsheetBase {
 
         super.updateFromUIDL(uidl, client);
         if (cachedUpdate) {
+            rendering = false;
             return;
         }
 
