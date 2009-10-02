c3508feeb4454f783acacb007873a658240f5efa
Artur Signell <artur.signell@itmill.com>
2009-10-02 13:14:00 +0000

    Test case for #3455
    svn changeset:9047/svn branch:6.1

---
+++ src/com/vaadin/tests/components/datefield/DisabledDateFieldWidth.java
@@ -0,0 +1,42 @@
+package com.vaadin.tests.components.datefield;
+
+import com.vaadin.tests.components.TestBase;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.DateField;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Button.ClickEvent;
+
+public class DisabledDateFieldWidth extends TestBase {
+    @Override
+    public void setup() {
+
+        final DateField dateField1 = new DateField("DateField");
+        dateField1.setResolution(DateField.RESOLUTION_YEAR);
+        dateField1.setEnabled(false);
+
+        Button button = new Button("Repaint datefield",
+                new Button.ClickListener() {
+                    public void buttonClick(ClickEvent event) {
+                        dateField1.requestRepaint();
+                    }
+                });
+
+        GridLayout gl = new GridLayout(3, 1);
+        gl.addComponent(dateField1);
+        gl.addComponent(button);
+
+        addComponent(gl);
+    }
+
+    @Override
+    protected String getDescription() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
