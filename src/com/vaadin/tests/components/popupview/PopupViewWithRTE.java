8c7b4b7e8bd9fc8bcdc572ae13c66eddc01ba4d7
Matti Tahvonen <matti.tahvonen@itmill.com>
2009-10-26 16:21:01 +0000

    merged [9380] from 6.1
    svn changeset:9381/svn branch:6.2

---
+++ src/com/vaadin/tests/components/popupview/PopupViewWithRTE.java
@@ -0,0 +1,50 @@
+package com.vaadin.tests.components.popupview;
+
+import com.vaadin.tests.components.TestBase;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.PopupView;
+import com.vaadin.ui.RichTextArea;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.PopupView.Content;
+
+public class PopupViewWithRTE extends TestBase {
+
+    @Override
+    protected String getDescription() {
+        return "Rich text editor should work properly in popupview. Try to edit text below.";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 3043;
+    }
+
+    @Override
+    protected void setup() {
+        PopupView pv = new PopupView(new Content() {
+
+            RichTextArea rte = new RichTextArea();
+
+            VerticalLayout vl = new VerticalLayout();
+
+            public String getMinimizedValueAsHTML() {
+                Object value = rte.getValue();
+                if (value == null || "".equals(value)) {
+                    value = "Initial <b>content</b> for <h3>rte</h3>.";
+                    rte.setValue(value);
+                    rte.setHeight("150px");
+                    rte.setWidth("100%");
+                    vl.addComponent(rte);
+                    vl.setWidth("600px");
+                }
+                return value.toString();
+            }
+
+            public Component getPopupComponent() {
+                return vl;
+            }
+        });
+        getLayout().addComponent(pv);
+    }
+
+}
