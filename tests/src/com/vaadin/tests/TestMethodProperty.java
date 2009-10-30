468df03be6de07f56dd4fe64981048d6057008d9
Matti Tahvonen <matti.tahvonen@itmill.com>
2009-10-30 15:14:55 +0000

    test case for #3624 method property constructor
    svn changeset:9528/svn branch:6.2

---
+++ tests/src/com/vaadin/tests/TestMethodProperty.java
@@ -0,0 +1,48 @@
+package com.vaadin.tests;
+
+import com.vaadin.data.util.MethodProperty;
+
+public class TestMethodProperty {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        MyTest myTest = new MyTest();
+
+        MethodProperty methodProperty2 = new MethodProperty(Integer.TYPE,
+                myTest, "getInt", "setInt", new Object[0],
+                new Object[] { null }, 0);
+
+        methodProperty2.setValue("3");
+
+        System.out.println("Succeeded");
+
+    }
+
+    public static class MyTest {
+
+        int integer;
+
+        public void setInteger(Integer integer) {
+            System.out.println("setInteger");
+            this.integer = integer;
+        }
+
+        public Integer getInteger() {
+            System.out.println("getInteger");
+            return Integer.valueOf(integer);
+        }
+
+        public void setInt(int i) {
+            System.out.println("setInt");
+            integer = i;
+        }
+
+        public int getInt() {
+            System.out.println("getInt");
+            return integer;
+        }
+    }
+}
