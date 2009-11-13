af34849fe63030494f48d539b14ae7974ddea476
Joonas Lehtinen <joonas.lehtinen@itmill.com>
2009-11-13 17:01:20 +0000

    Testcase for #3710
    svn changeset:9797/svn branch:6.2

---
+++ tests/src/com/vaadin/tests/tickets/Ticket3710.java
@@ -0,0 +1,34 @@
+package com.vaadin.tests.tickets;
+
+import com.vaadin.Application;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Form;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.Window;
+
+@SuppressWarnings("serial")
+public class Ticket3710 extends Application {
+
+    @Override
+    public void init() {
+
+        Window w = new Window("Test");
+        setMainWindow(w);
+
+        final Form f = new Form();
+        w.addComponent(f);
+        f.setSizeUndefined();
+        f.setCaption("Test form with a really long caption");
+        f.addField("foo", new TextField("Foo"));
+        f.addField("bar", new TextField("A bit longer field caption"));
+
+        HorizontalLayout hl = new HorizontalLayout();
+        hl.setWidth("100%");
+        Button b = new Button("right aligned");
+        hl.addComponent(b);
+        hl.setComponentAlignment(b, "r");
+        f.setFooter(hl);
+    }
+
+}
