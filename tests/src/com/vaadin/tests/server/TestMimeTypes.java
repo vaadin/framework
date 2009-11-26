19cf454499dc6a80c5aa26ad8be8c642de74636b
Artur Signell <artur.signell@itmill.com>
2009-11-26 13:13:55 +0000

    Simple mime type test
    svn changeset:10075/svn branch:6.2

---
+++ tests/src/com/vaadin/tests/server/TestMimeTypes.java
@@ -0,0 +1,26 @@
+package com.vaadin.tests.server;
+
+import junit.framework.TestCase;
+
+import com.vaadin.Application;
+import com.vaadin.terminal.ClassResource;
+import com.vaadin.ui.Embedded;
+
+public class TestMimeTypes extends TestCase {
+
+    public void testEmbeddedPDF() {
+        Application app = new Application() {
+
+            @Override
+            public void init() {
+                // TODO Auto-generated method stub
+
+            }
+        };
+        Embedded e = new Embedded("A pdf", new ClassResource("file.pddf", app));
+        assertEquals("Invalid mimetype", "application/octet-stream", e
+                .getMimeType());
+        e = new Embedded("A pdf", new ClassResource("file.pdf", app));
+        assertEquals("Invalid mimetype", "application/pdf", e.getMimeType());
+    }
+}
