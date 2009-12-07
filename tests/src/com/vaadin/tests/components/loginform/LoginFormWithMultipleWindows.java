2f75adb34fc34ef7b70ea378d9cc4470d959f766
Jouni Koivuviita <jouni.koivuviita@itmill.com>
2009-12-07 13:04:14 +0000

    Test case for #3807 (LoginFormWithMultipleWindows)
    svn changeset:10179/svn branch:6.2

---
+++ tests/src/com/vaadin/tests/components/loginform/LoginFormWithMultipleWindows.java
@@ -0,0 +1,56 @@
+package com.vaadin.tests.components.loginform;
+
+import com.vaadin.Application;
+import com.vaadin.ui.LoginForm;
+import com.vaadin.ui.Window;
+import com.vaadin.ui.LoginForm.LoginEvent;
+import com.vaadin.ui.LoginForm.LoginListener;
+
+@SuppressWarnings("serial")
+public class LoginFormWithMultipleWindows extends Application {
+
+    /**
+     * =======================================================================
+     * Comment out this to make the LoginForm work as expected
+     * =======================================================================
+     */
+    @Override
+    public Window getWindow(String name) {
+        Window w = super.getWindow(name);
+        if (w == null) {
+            w = new LoginFormWindow();
+            w.setName(name);
+            addWindow(w);
+        }
+        return w;
+
+    }
+
+    @Override
+    public void init() {
+        setMainWindow(new LoginFormWindow());
+    }
+
+    public class LoginFormWindow extends Window {
+        public LoginFormWindow() {
+            super();
+
+            LoginForm loginForm = new LoginForm();
+            loginForm.setSizeUndefined();
+
+            loginForm.addListener(new LoginListener() {
+
+                private static final long serialVersionUID = 1L;
+
+                public void onLogin(LoginEvent event) {
+                    showNotification(event.getLoginParameter("username") + ":"
+                            + event.getLoginParameter("password"));
+
+                }
+            });
+
+            addComponent(loginForm);
+        }
+    }
+
+}
