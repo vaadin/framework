2aac4f3d269bb7f7b2ffa93d193772d2ab1d9cb2
Artur Signell <artur.signell@itmill.com>
2009-12-15 14:58:39 +0000

    Test case for #3864
    svn changeset:10323/svn branch:6.2

---
+++ tests/src/com/vaadin/tests/containers/TableWithFileSystemContainer.java
@@ -0,0 +1,33 @@
+package com.vaadin.tests.containers;
+
+import java.io.File;
+
+import com.vaadin.data.util.FilesystemContainer;
+import com.vaadin.tests.components.TestBase;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.Window;
+
+public class TableWithFileSystemContainer extends TestBase {
+
+    private String testPath = "C:/temp/img";
+
+    @Override
+    public void setup() {
+        setMainWindow(new Window(""));
+        Table table = new Table("Documents", new FilesystemContainer(new File(
+                testPath)));
+        table.setWidth("100%");
+        getMainWindow().addComponent(table);
+    }
+
+    @Override
+    protected String getDescription() {
+        return "The Table uses a FileSystemContainer as datasource. Scrolling to the end should show the last items, not throw an NPE.";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 3864;
+    }
+
+}
