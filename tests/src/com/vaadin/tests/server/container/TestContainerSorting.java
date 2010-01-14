70f7950984cd587f325c2651cd86937b558a93da
Artur Signell <artur.signell@itmill.com>
2010-01-14 11:31:20 +0000

    Test case and fix for #3987 - DefaultItemSorter fails for a filtered container
    svn changeset:10712/svn branch:6.2

--- tests/src/com/vaadin/tests/server/container/TestContainerSorting.java
+++ tests/src/com/vaadin/tests/server/container/TestContainerSorting.java
@@ -32,6 +32,20 @@ public class TestContainerSorting extends TestCase {
         super.setUp();
     }
 
+    public void testFilteredIndexedContainer() {
+        IndexedContainer ic = new IndexedContainer();
+
+        addProperties(ic);
+        populate(ic);
+
+        ic.addContainerFilter(PROPERTY_STRING_ID, "a", true, false);
+        ic.sort(new Object[] { PROPERTY_STRING_ID }, new boolean[] { true });
+        verifyOrder(ic,
+                new String[] { ITEM_ANOTHER_NULL, ITEM_DATA_MINUS1,
+                        ITEM_DATA_MINUS1_NULL, ITEM_DATA_MINUS2,
+                        ITEM_DATA_MINUS2_NULL, });
+    }
+
     public void testIndexedContainer() {
         IndexedContainer ic = new IndexedContainer();
 
