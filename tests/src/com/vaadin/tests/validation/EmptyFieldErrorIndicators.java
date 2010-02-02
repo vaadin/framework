7ce0d53c1935e9ca4967f27bbc21bff3fceff654
Henri Sara <henri.sara@itmill.com>
2010-02-02 08:06:51 +0000

    Fixed EmptyFieldErrorIndicator test not to depend on the current date.
    svn changeset:11095/svn branch:6.3

--- tests/src/com/vaadin/tests/validation/EmptyFieldErrorIndicators.java
+++ tests/src/com/vaadin/tests/validation/EmptyFieldErrorIndicators.java
@@ -1,5 +1,7 @@
 package com.vaadin.tests.validation;
 
+import java.util.Date;
+
 import com.vaadin.data.Validator.InvalidValueException;
 import com.vaadin.data.validator.AbstractValidator;
 import com.vaadin.tests.components.TestBase;
@@ -9,11 +11,9 @@ import com.vaadin.ui.DateField;
 import com.vaadin.ui.Field;
 import com.vaadin.ui.Form;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.InlineDateField;
 import com.vaadin.ui.NativeSelect;
 import com.vaadin.ui.OptionGroup;
 import com.vaadin.ui.Panel;
-import com.vaadin.ui.PopupDateField;
 import com.vaadin.ui.Select;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
@@ -74,13 +74,13 @@ public class EmptyFieldErrorIndicators extends TestBase {
 
     private Form createForm(final boolean required, final boolean failValidator) {
         // hand-crafted form, not using form field factory
-        Form form = new Form() {
+        final Form form = new Form() {
             @Override
             public void addField(Object propertyId, Field field) {
                 super.addField(propertyId, field);
                 field.setRequired(required);
                 field.setRequiredError("Missing required value!");
-                if (failValidator) {
+                if (failValidator && !(field instanceof Button)) {
                     field
                             .addValidator(new AbstractValidator(
                                     "Validation error") {
@@ -94,10 +94,21 @@ public class EmptyFieldErrorIndicators extends TestBase {
         
         form.addField("Field", new TextField("Text"));
         form.addField("Date", new DateField("Date"));
-        form.addField("Inline Date", new InlineDateField("Date"));
-        form.addField("Popup Date", new PopupDateField("Date"));
+        // not good for automated testing with screenshots when null
+        // form.addField("Inline Date", new InlineDateField("Date"));
+        // same as basic DateField
+        // form.addField("Popup Date", new PopupDateField("Date"));
+        Button setDateButton = new Button("Set date");
+        form.addField("Set Date", setDateButton);
+        setDateButton.addListener(new ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                form.getField("Date").setValue(new Date(0));
+            }
+        });
 
-        form.addField("Native Select", new NativeSelect("NativeSelect"));
+        NativeSelect nativeSelect = new NativeSelect("NativeSelect");
+        form.addField("Native Select", nativeSelect);
+        nativeSelect.addItem("Value 1");
 
         // in #4103, the Select component was behaving differently from others
         form.addField("Select", new Select("Select"));
