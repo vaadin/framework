4b388c9c48d00e117d857e13b66a070db228c8af
Artur Signell <artur.signell@itmill.com>
2010-02-18 08:57:15 +0000

    Added finals to method that should never be overridden
    svn changeset:11368/svn branch:6.3_dd

--- src/com/vaadin/event/dd/acceptCriteria/ClientSideCriterion.java
+++ src/com/vaadin/event/dd/acceptCriteria/ClientSideCriterion.java
@@ -6,33 +6,33 @@ import com.vaadin.terminal.PaintException;
 import com.vaadin.terminal.PaintTarget;
 
 public abstract class ClientSideCriterion implements Serializable,
-        AcceptCriterion {
+		AcceptCriterion {
 
-    /**
+	/**
      * 
      */
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    public boolean isClientSideVerifiable() {
-        return true;
-    }
+	public final boolean isClientSideVerifiable() {
+		return true;
+	}
 
-    public void paint(PaintTarget target) throws PaintException {
-        target.startTag("-ac");
-        target.addAttribute("name", getIdentifier());
-        paintContent(target);
-        target.endTag("-ac");
-    }
+	public void paint(PaintTarget target) throws PaintException {
+		target.startTag("-ac");
+		target.addAttribute("name", getIdentifier());
+		paintContent(target);
+		target.endTag("-ac");
+	}
 
-    public void paintContent(PaintTarget target) throws PaintException {
-    }
+	public void paintContent(PaintTarget target) throws PaintException {
+	}
 
-    protected String getIdentifier() {
-        return getClass().getCanonicalName();
-    }
+	protected String getIdentifier() {
+		return getClass().getCanonicalName();
+	}
 
-    public void paintResponse(PaintTarget target) throws PaintException {
-        // NOP, nothing to do as this is client side verified criterion
-    }
+	public final void paintResponse(PaintTarget target) throws PaintException {
+		// NOP, nothing to do as this is client side verified criterion
+	}
 
 }
