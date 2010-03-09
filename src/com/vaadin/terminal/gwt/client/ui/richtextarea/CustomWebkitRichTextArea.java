8c1b559e9bc662722bb442e8662468b95c80bf6a
Matti Tahvonen <matti.tahvonen@itmill.com>
2010-03-09 12:51:44 +0000

    fixes #4279
    svn changeset:11707/svn branch:6.3

---
+++ src/com/vaadin/terminal/gwt/client/ui/richtextarea/CustomWebkitRichTextArea.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.vaadin.terminal.gwt.client.ui.richtextarea;
+
+import com.google.gwt.user.client.Command;
+import com.google.gwt.user.client.DeferredCommand;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.ui.impl.RichTextAreaImplSafari;
+
+/**
+ * TODO remove me when GWT RichTextArea is fixed. See #4279 (vaadin trac)
+ * 
+ */
+class CustomWebkitRichTextArea extends RichTextAreaImplSafari {
+    public CustomWebkitRichTextArea() {
+        DeferredCommand.addCommand(new Command() {
+            public void execute() {
+                hookBlur(getElement());
+            }
+        });
+    }
+
+    private native void hookBlur(Element iframe)
+    /*-{
+
+        iframe.contentDocument.documentElement.onblur = function(evt) {
+          if (iframe.__listener) {
+            iframe.__listener.@com.google.gwt.user.client.ui.Widget::onBrowserEvent(Lcom/google/gwt/user/client/Event;)(evt);
+          }
+        };
+        
+        
+    }-*/;
+}
