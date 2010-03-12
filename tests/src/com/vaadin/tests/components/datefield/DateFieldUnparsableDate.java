092786df23ca4a0dd6fa42c4e201b89cec957d8a
Artur Signell <artur.signell@itmill.com>
2010-03-12 13:43:52 +0000

    Merged: Test case and fix for  * #4263 DateField - returning valid Date in handleUnparsableDateString method works only at the first attempt and  * #4304 Resetting the value in DateField.handleUnparsableDate does not update the client side text
    svn changeset:11821/svn branch:6.2

---
+++ tests/src/com/vaadin/tests/components/datefield/DateFieldUnparsableDate.java
@@ -0,0 +1,92 @@
+package com.vaadin.tests.components.datefield;
+
+import java.util.Date;
+
+import com.vaadin.tests.components.TestBase;
+import com.vaadin.ui.DateField;
+
+public class DateFieldUnparsableDate extends TestBase {
+
+    public class MyDateField extends DateField {
+        public MyDateField(String caption) {
+            super(caption);
+        }
+
+        @Override
+        protected Date handleUnparsableDateString(String dateString)
+                throws ConversionException {
+            return (Date) getValue();
+        }
+    }
+
+    public class MyDateField2 extends DateField {
+        public MyDateField2(String caption) {
+            super(caption);
+        }
+
+        @Override
+        protected Date handleUnparsableDateString(String dateString)
+                throws ConversionException {
+            return null;
+        }
+    }
+
+    public class MyDateField3 extends DateField {
+        public MyDateField3(String caption) {
+            super(caption);
+        }
+
+        @Override
+        protected Date handleUnparsableDateString(String dateString)
+                throws ConversionException {
+            throw new ConversionException("You should not enter invalid dates!");
+        }
+    }
+
+    public class MyDateField4 extends DateField {
+        public MyDateField4(String caption) {
+            super(caption);
+        }
+
+        @Override
+        protected Date handleUnparsableDateString(String dateString)
+                throws ConversionException {
+            if (dateString != null && dateString.equals("today")) {
+                return new Date();
+            }
+            throw new ConversionException("You should not enter invalid dates!");
+        }
+    }
+
+    @Override
+    protected void setup() {
+        MyDateField df = new MyDateField(
+                "Returns the old value for invalid dates");
+        df.setImmediate(true);
+        addComponent(df);
+
+        MyDateField2 df2 = new MyDateField2("Returns empty for invalid dates");
+        df2.setImmediate(true);
+        addComponent(df2);
+
+        MyDateField3 df3 = new MyDateField3(
+                "Throws an exception for invalid dates");
+        df3.setImmediate(true);
+        addComponent(df3);
+
+        MyDateField4 df4 = new MyDateField4("Can convert 'today'");
+        df4.setImmediate(true);
+        addComponent(df4);
+
+    }
+
+    @Override
+    protected String getDescription() {
+        return "DateFields in various configurations (according to caption). All handle unparsable dates differently";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 4236;
+    }
+}
