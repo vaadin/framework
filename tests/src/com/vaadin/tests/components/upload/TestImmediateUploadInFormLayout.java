ab06f5d5cb9e934328d0cb8b8874cb2406a74d9e
Henri Sara <henri.sara@itmill.com>
2010-03-17 07:53:52 +0000

    Test case for #4359: immediate upload in a FormLayout
    svn changeset:11920/svn branch:6.3

---
+++ tests/src/com/vaadin/tests/components/upload/TestImmediateUploadInFormLayout.java
@@ -0,0 +1,49 @@
+package com.vaadin.tests.components.upload;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.util.Collections;
+import java.util.List;
+
+import com.vaadin.tests.components.ComponentTestCase;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.FormLayout;
+import com.vaadin.ui.Upload;
+import com.vaadin.ui.Upload.Receiver;
+
+public class TestImmediateUploadInFormLayout extends ComponentTestCase implements
+        Receiver {
+
+    @Override
+    protected String getDescription() {
+        return "On Firefox 3.6 and Opera 10.10, clicking on an immediate upload in a wide FormLayout has no effect";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 4359;
+    }
+
+    @Override
+    protected void setup() {
+        super.setup();
+
+        FormLayout formLayout = new FormLayout();
+        formLayout.setWidth("100%");
+        Upload u = new Upload("Upload in FormLayout", this);
+        u.setImmediate(true);
+        formLayout.addComponent(u);
+        addTestComponent(formLayout);
+
+    }
+
+    @Override
+    protected List<Component> createActions() {
+        return Collections.EMPTY_LIST;
+    }
+
+    public OutputStream receiveUpload(String filename, String MIMEType) {
+        getMainWindow().showNotification("Receiving upload");
+        return new ByteArrayOutputStream();
+    }
+}
