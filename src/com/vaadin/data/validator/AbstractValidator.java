5b5b4970584df91f136ef5c198ba2d42113deee3
Artur Signell <artur.signell@itmill.com>
2010-05-10 14:53:13 +0000

    Updated javadoc (#4681)
    svn changeset:13121/svn branch:6.4

--- src/com/vaadin/data/validator/AbstractValidator.java
+++ src/com/vaadin/data/validator/AbstractValidator.java
@@ -6,11 +6,15 @@ package com.vaadin.data.validator;
 import com.vaadin.data.Validator;
 
 /**
- * Default Validator base class. See {@link com.vaadin.data.validator.Validator}
- * for more information.
+ * Abstract {@link com.vaadin.data.validator.Validator Validator} implementation
+ * that provides a basic Validator implementation except the
+ * {@link #isValid(Object)} method. Sub-classes need to implement the
+ * {@link #isValid(Object)} method.
  * <p>
- * If the validation fails, the exception thrown contains the error message with
- * its argument 0 replaced with the toString() of the object being validated.
+ * To include the value that failed validation in the exception message you can
+ * use "{0}" in the error message. This will be replaced with the failed value
+ * (converted to string using {@link #toString()}) or "null" if the value is
+ * null.
  * </p>
  * 
  * @author IT Mill Ltd.
@@ -22,17 +26,17 @@ import com.vaadin.data.Validator;
 public abstract class AbstractValidator implements Validator {
 
     /**
-     * Error message.
+     * Error message that is included in an {@link InvalidValueException} if
+     * such is thrown.
      */
     private String errorMessage;
 
     /**
-     * Constructs a validator with an error message.
+     * Constructs a validator with the given error message.
      * 
      * @param errorMessage
-     *            the message included in the exception (with its parameter {0}
-     *            replaced by toString() of the object to be validated) in case
-     *            the validation fails
+     *            the message to be included in an {@link InvalidValueException}
+     *            (with "{0}" replaced by the value that failed validation).
      */
     public AbstractValidator(String errorMessage) {
         this.errorMessage = errorMessage;
@@ -51,19 +55,23 @@ public abstract class AbstractValidator implements Validator {
     }
 
     /**
-     * Gets the message to be displayed in case the value does not validate.
+     * Returns the message to be included in the exception in case the value
+     * does not validate.
      * 
-     * @return the Error Message.
+     * @return the error message provided in the constructor or using
+     *         {@link #setErrorMessage(String)}.
      */
     public String getErrorMessage() {
         return errorMessage;
     }
 
     /**
-     * Sets the message to be displayed in case the value does not validate.
+     * Sets the message to be included in the exception in case the value does
+     * not validate. The exception message is typically shown to the end user.
      * 
      * @param errorMessage
-     *            the Error Message to set.
+     *            the error message. "{0}" is automatically replaced by the
+     *            value that did not validate.
      */
     public void setErrorMessage(String errorMessage) {
         this.errorMessage = errorMessage;
