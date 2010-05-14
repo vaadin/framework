b0699dd777644ae304c70d36547e873a8e8db758
John Alhroos <john.ahlroos@itmill.com>
2010-05-14 06:28:42 +0000

    Yet another fix for #4144
    svn changeset:13184/svn branch:6.3

--- src/com/vaadin/terminal/gwt/client/ui/VFilterSelect.java
+++ src/com/vaadin/terminal/gwt/client/ui/VFilterSelect.java
@@ -595,6 +595,7 @@ public class VFilterSelect extends Composite implements Paintable, Field,
     private boolean filtering = false;
     private boolean selecting = false;
     private boolean tabPressed = false;
+    private boolean initDone = false;
 
     private String lastFilter = "";
     private int lastIndex = -1; // last selected index when using arrows
@@ -856,6 +857,8 @@ public class VFilterSelect extends Composite implements Paintable, Field,
         popupOpenerClicked = false;
 
         updateRootWidth();
+
+        initDone = true;
     }
 
     private void setPromptingOn() {
@@ -1134,7 +1137,10 @@ public class VFilterSelect extends Composite implements Paintable, Field,
         }
         horizPaddingAndBorder = Util.setWidthExcludingPaddingAndBorder(this,
                 width, horizPaddingAndBorder);
-        updateRootWidth();
+
+        if (initDone) {
+            updateRootWidth();
+        }
     }
 
     @Override
@@ -1157,7 +1163,8 @@ public class VFilterSelect extends Composite implements Paintable, Field,
                     - Util.measureMarginLeft(selectedItemIcon.getElement()) : 0;
 
             int w = tbWidth + openerWidth + iconWidth;
-            if (currentPage + 1 <= 0 && suggestionPopupMinWidth > w) {
+            if ((!initDone || currentPage + 1 <= 0)
+                    && suggestionPopupMinWidth > w) {
                 setTextboxWidth(suggestionPopupMinWidth);
                 w = suggestionPopupMinWidth;
             } else {
