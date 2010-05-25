bd94e6ef89a5980ad50b296079f28354e6874d33
Henri Sara <henri.sara@itmill.com>
2010-05-25 12:34:45 +0000

    #4981 and #4935 TabSheet and Accordion javadoc and one import, no functional change
    svn changeset:13354/svn branch:6.3

--- src/com/vaadin/ui/Accordion.java
+++ src/com/vaadin/ui/Accordion.java
@@ -5,6 +5,14 @@ package com.vaadin.ui;
 
 import com.vaadin.terminal.gwt.client.ui.VAccordion;
 
+/**
+ * An accordion is a component similar to a {@link TabSheet}, but with a
+ * vertical orientation and the selected component presented between tabs.
+ * 
+ * Closable tabs are not supported by the accordion.
+ * 
+ * @see TabSheet
+ */
 @SuppressWarnings("serial")
 @ClientWidget(VAccordion.class)
 public class Accordion extends TabSheet {
