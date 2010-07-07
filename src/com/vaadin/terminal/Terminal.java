c1db4bf3b24e780aab85eacde8e3d7f805eb9616
Artur Signell <artur.signell@itmill.com>
2010-07-07 08:49:21 +0000

    Updated javadoc for Terminal (#4919)
    svn changeset:14120/svn branch:6.4

--- src/com/vaadin/terminal/Terminal.java
+++ src/com/vaadin/terminal/Terminal.java
@@ -7,7 +7,9 @@ package com.vaadin.terminal;
 import java.io.Serializable;
 
 /**
- * Interface for different terminal types.
+ * An interface that provides information about the user's terminal.
+ * Implementors typically provide additional information using methods not in
+ * this interface. </p>
  * 
  * @author IT Mill Ltd.
  * @version
@@ -17,45 +19,47 @@ import java.io.Serializable;
 public interface Terminal extends Serializable {
 
     /**
-     * Gets the name of the default theme.
+     * Gets the name of the default theme for this terminal.
      * 
-     * @return the Name of the terminal window.
+     * @return the name of the theme that is used by default by this terminal.
      */
     public String getDefaultTheme();
 
     /**
-     * Gets the width of the terminal window in pixels.
+     * Gets the width of the terminal screen in pixels. This is the width of the
+     * screen and not the width available for the application.
      * 
-     * @return the Width of the terminal window.
+     * @return the width of the terminal screen.
      */
     public int getScreenWidth();
 
     /**
-     * Gets the height of the terminal window in pixels.
+     * Gets the height of the terminal screen in pixels. This is the height of
+     * the screen and not the height available for the application.
      * 
-     * @return the Height of the terminal window.
+     * @return the height of the terminal screen.
      */
     public int getScreenHeight();
 
     /**
-     * Terminal error event.
+     * An error event implementation for Terminal.
      */
     public interface ErrorEvent extends Serializable {
 
         /**
-         * Gets the contained throwable.
+         * Gets the contained throwable, the cause of the error.
          */
         public Throwable getThrowable();
 
     }
 
     /**
-     * Terminal error listener interface.
+     * Interface for listening to Terminal errors.
      */
     public interface ErrorListener extends Serializable {
 
         /**
-         * Invoked when terminal error occurs.
+         * Invoked when a terminal error occurs.
          * 
          * @param event
          *            the fired event.
