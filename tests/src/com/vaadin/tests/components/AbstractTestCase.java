1c96aa8a8df3706a3f89e70ba591468ebcd77d1c
Artur Signell <artur.signell@itmill.com>
2010-09-17 08:37:29 +0000

    Provide browser information to the test cases
    svn changeset:14980/svn branch:6.4

--- tests/src/com/vaadin/tests/components/AbstractTestCase.java
+++ tests/src/com/vaadin/tests/components/AbstractTestCase.java
@@ -1,6 +1,9 @@
 package com.vaadin.tests.components;
 
 import com.vaadin.Application;
+import com.vaadin.service.ApplicationContext;
+import com.vaadin.terminal.gwt.server.AbstractWebApplicationContext;
+import com.vaadin.terminal.gwt.server.WebBrowser;
 
 public abstract class AbstractTestCase extends Application {
 
@@ -8,4 +11,14 @@ public abstract class AbstractTestCase extends Application {
 
     protected abstract Integer getTicketNumber();
 
+    protected WebBrowser getBrowser() {
+        ApplicationContext context = getContext();
+        if (context instanceof AbstractWebApplicationContext) {
+            WebBrowser webBrowser = ((AbstractWebApplicationContext) context)
+                    .getBrowser();
+            return webBrowser;
+        }
+
+        return null;
+    }
 }
