b633f35eaa21edaa8342a828007767b86ed5bf2f
Henri Kerola <henri.kerola@itmill.com>
2010-09-17 11:26:39 +0000

    Style constants for Liferay theme
    svn changeset:14991/svn branch:6.4

---
+++ src/com/vaadin/ui/themes/LiferayTheme.java
@@ -0,0 +1,28 @@
+package com.vaadin.ui.themes;
+
+public class LiferayTheme extends BaseTheme {
+
+    public static final String THEME_NAME = "liferay";
+
+    /***************************************************************************
+     * 
+     * Panel styles
+     * 
+     **************************************************************************/
+
+    /**
+     * Removes borders and background from the panel
+     */
+    public static final String PANEL_LIGHT = "light";
+
+    /***************************************************************************
+     * 
+     * SplitPanel styles
+     * 
+     **************************************************************************/
+
+    /**
+     * Reduces the split handle to a minimal size (1 pixel)
+     */
+    public static final String SPLITPANEL_SMALL = "small";
+}
