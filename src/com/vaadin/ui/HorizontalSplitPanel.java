dcce3dcffd0a43bccfff7b22b9f66bac0238d2b6
Matti Tahvonen <matti.tahvonen@itmill.com>
2010-10-28 10:35:01 +0000

    fixes #5903
    svn changeset:15759/svn branch:6.5

--- src/com/vaadin/ui/HorizontalSplitPanel.java
+++ src/com/vaadin/ui/HorizontalSplitPanel.java
@@ -1,5 +1,8 @@
 package com.vaadin.ui;
 
+import com.vaadin.terminal.gwt.client.ui.VSplitPanelHorizontal;
+
+@ClientWidget(VSplitPanelHorizontal.class)
 public class HorizontalSplitPanel extends SplitPanel {
 
 }
