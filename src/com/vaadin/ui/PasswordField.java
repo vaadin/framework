84358b4546a81a22c9e54e7034a5ab7e5cb204c7
Matti Tahvonen <matti.tahvonen@itmill.com>
2010-11-04 13:13:45 +0000

    javadocs
    svn changeset:15858/svn branch:6.5

--- src/com/vaadin/ui/PasswordField.java
+++ src/com/vaadin/ui/PasswordField.java
@@ -11,22 +11,53 @@ import com.vaadin.terminal.gwt.client.ui.VPasswordField;
 @SuppressWarnings("serial")
 public class PasswordField extends TextField {
 
+    /**
+     * Constructs an empty PasswordField.
+     */
     public PasswordField() {
         super();
     }
 
+    /**
+     * Constructs a PasswordField with given property data source.
+     * 
+     * @param dataSource
+     *            the property dato source for the field
+     */
     public PasswordField(Property dataSource) {
         super(dataSource);
     }
 
+    /**
+     * Constructs a PasswordField with given caption and property data source.
+     * 
+     * @param caption
+     *            the caption for the field
+     * @param dataSource
+     *            the property data source for the field
+     */
     public PasswordField(String caption, Property dataSource) {
         super(caption, dataSource);
     }
 
+    /**
+     * Constructs a PasswordField with given value and caption.
+     * 
+     * @param caption
+     *            the caption for the field
+     * @param value
+     *            the value for the field
+     */
     public PasswordField(String caption, String value) {
         super(caption, value);
     }
 
+    /**
+     * Constructs a PasswordField with given caption.
+     * 
+     * @param caption
+     *            the caption for the field
+     */
     public PasswordField(String caption) {
         super(caption);
     }
