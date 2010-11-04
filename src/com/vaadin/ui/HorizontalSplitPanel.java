96e8b4347410a5e258caa6eb62058ffcb77b840d
Matti Tahvonen <matti.tahvonen@itmill.com>
2010-11-04 13:46:30 +0000

    javadocs
    svn changeset:15860/svn branch:6.5

--- src/com/vaadin/ui/HorizontalSplitPanel.java
+++ src/com/vaadin/ui/HorizontalSplitPanel.java
@@ -2,6 +2,24 @@ package com.vaadin.ui;
 
 import com.vaadin.terminal.gwt.client.ui.VSplitPanelHorizontal;
 
+/**
+ * A horizontal split panel contains two components and lays them horizontally.
+ * The first component is on the left side.
+ * 
+ * <pre>
+ * 
+ *      +---------------------++----------------------+
+ *      |                     ||                      |
+ *      | The first component || The second component |
+ *      |                     ||                      |
+ *      +---------------------++----------------------+
+ *                              
+ *                            ^
+ *                            |
+ *                      the splitter
+ * 
+ * </pre>
+ */
 @ClientWidget(VSplitPanelHorizontal.class)
 public class HorizontalSplitPanel extends SplitPanel {
     public HorizontalSplitPanel() {
--- src/com/vaadin/ui/VerticalSplitPanel.java
+++ src/com/vaadin/ui/VerticalSplitPanel.java
@@ -2,6 +2,23 @@ package com.vaadin.ui;
 
 import com.vaadin.terminal.gwt.client.ui.VSplitPanelVertical;
 
+/**
+ * A vertical split panel contains two components and lays them vertically. The
+ * first component is above the second component.
+ * 
+ * <pre>
+ *      +--------------------------+
+ *      |                          |
+ *      |  The first component     |
+ *      |                          |
+ *      +==========================+  <-- splitter
+ *      |                          |
+ *      |  The second component    |
+ *      |                          |
+ *      +--------------------------+
+ * </pre>
+ * 
+ */
 @ClientWidget(VSplitPanelVertical.class)
 public class VerticalSplitPanel extends SplitPanel {
     public VerticalSplitPanel() {
