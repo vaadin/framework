982802cf745f655e5d5ed318904b41732b9c70fb
Matti Tahvonen <matti.tahvonen@itmill.com>
2010-11-04 12:55:41 +0000

    class doc
    svn changeset:15857/svn branch:6.5

--- src/com/vaadin/ui/PasswordField.java
+++ src/com/vaadin/ui/PasswordField.java
@@ -3,6 +3,10 @@ package com.vaadin.ui;
 import com.vaadin.data.Property;
 import com.vaadin.terminal.gwt.client.ui.VPasswordField;
 
+/**
+ * A field that is used to enter secret text information like passwords. The
+ * clear text is not displayed in the screen.
+ */
 @ClientWidget(VPasswordField.class)
 @SuppressWarnings("serial")
 public class PasswordField extends TextField {
