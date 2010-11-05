f0756724f0feb641b2f3ea9c5195457aebb4b872
Matti Tahvonen <matti.tahvonen@itmill.com>
2010-11-05 12:20:07 +0000

    fixes #5955. A bit more intelligent removal of fields from Form + a method that can be overridden for a custom logic.
    svn changeset:15880/svn branch:6.5

--- src/com/vaadin/ui/Form.java
+++ src/com/vaadin/ui/Form.java
@@ -639,12 +639,31 @@ public class Form extends AbstractField implements Item.Editor, Buffered, Item,
         if (field != null) {
             propertyIds.remove(id);
             fields.remove(id);
-            layout.removeComponent(field);
+            detachField(field);
             field.removeListener(fieldValueChangeListener);
             return true;
         }
 
         return false;
+    }
+
+    /**
+     * Called when a form field is detached from a Form. Typically when a new
+     * Item is assigned to Form via {@link #setItemDataSource(Item)} *
+     * <p>
+     * Override this method to control how the fields are removed from the
+     * layout.
+     * </p>
+     * 
+     * 
+     * @param field
+     *            the field to be detached from the forms layout.
+     */
+    protected void detachField(final Field field) {
+        Component p = field.getParent();
+        if (p instanceof ComponentContainer) {
+            ((ComponentContainer) p).removeComponent(field);
+        }
     }
 
     /**
@@ -781,7 +800,7 @@ public class Form extends AbstractField implements Item.Editor, Buffered, Item,
             final Object[] properties = propertyIds.toArray();
             for (int i = 0; i < properties.length; i++) {
                 Field f = getField(properties[i]);
-                layout.removeComponent(f);
+                detachField(f);
                 if (newLayout instanceof CustomLayout) {
                     ((CustomLayout) newLayout).addComponent(f,
                             properties[i].toString());
