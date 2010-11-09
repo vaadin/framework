066a0db3f99a94aabc7a56d3f36ffb7bb0bf2dc7
Artur Signell <artur.signell@itmill.com>
2010-11-09 13:45:52 +0000

    Test case for #5894
    svn changeset:15932/svn branch:6.5

---
+++ tests/src/com/vaadin/tests/components/menubar/MenuBarRunsOutOfBrowser.java
@@ -0,0 +1,42 @@
+package com.vaadin.tests.components.menubar;
+
+import com.vaadin.terminal.ThemeResource;
+import com.vaadin.tests.components.AbstractTestCase;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.MenuBar;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+public class MenuBarRunsOutOfBrowser extends AbstractTestCase {
+
+    @Override
+    public void init() {
+        setTheme("runo");
+        Window main = new Window("Test");
+        main.setSizeFull();
+        setMainWindow(main);
+        main.getContent().setSizeFull();
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.addItem("Test", new ThemeResource("icons/16/calendar.png"),
+                null);
+        menuBar.addItem("ABC", new ThemeResource("icons/16/document.png"), null);
+        menuBar.addItem("123", new ThemeResource("icons/16/help.png"), null);
+
+        main.getContent().addComponent(menuBar);
+        ((VerticalLayout) main.getContent()).setComponentAlignment(menuBar,
+                Alignment.TOP_RIGHT);
+
+    }
+
+    @Override
+    protected String getDescription() {
+        return "The menubar should be right aligned but not run out of the browser";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 5894;
+    }
+
+}
