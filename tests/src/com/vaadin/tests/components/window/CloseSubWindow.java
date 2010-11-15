d5aa1654711db4a5c4dea86435e046c0e4b40415
Artur Signell <artur.signell@itmill.com>
2010-11-15 14:50:26 +0000

    Ensured test works, is more stable and also tests native window close events.
    svn changeset:15988/svn branch:6.5

--- tests/src/com/vaadin/tests/components/window/CloseSubWindow.java
+++ tests/src/com/vaadin/tests/components/window/CloseSubWindow.java
@@ -4,21 +4,23 @@ import java.net.URL;
 
 import com.vaadin.terminal.ExternalResource;
 import com.vaadin.tests.components.TestBase;
+import com.vaadin.tests.util.Log;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Window;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Window;
 import com.vaadin.ui.Window.CloseEvent;
 import com.vaadin.ui.Window.CloseListener;
 
 public class CloseSubWindow extends TestBase {
 
     private Window browserWindow;
+    private Log log = new Log(5);
 
     @Override
     protected void setup() {
         Button openWindowButton = new Button("Open sub-window");
+        openWindowButton.setDebugId("opensub");
         openWindowButton.addListener(new ClickListener() {
             public void buttonClick(ClickEvent event) {
                 Window sub = createClosableSubWindow("Sub-window");
@@ -26,24 +28,35 @@ public class CloseSubWindow extends TestBase {
             }
         });
 
+        addComponent(log);
         addComponent(openWindowButton);
 
         Button openBrowserWindowButton = new Button("Open browser window");
+        openWindowButton.setDebugId("opennative");
         openBrowserWindowButton.addListener(new ClickListener() {
             public void buttonClick(ClickEvent event) {
                 browserWindow = new Window("Window");
-                browserWindow.addComponent(new Label("Close this window"));
+                Button closeButton = new Button("Close this window",
+                        new ClickListener() {
+
+                            public void buttonClick(ClickEvent event) {
+                                event.getButton().getWindow()
+                                        .executeJavaScript("window.close();");
+
+                            }
+                        });
+                browserWindow.addComponent(closeButton);
 
                 browserWindow.addListener(new CloseListener() {
                     public void windowClose(CloseEvent e) {
-                        getMainWindow().showNotification(
-                                "Browser window closed");
+                        logBrowserWindowClosed();
                         // there is no push, so the user needs to click a button
                         // to see the notification
 
                         // Opera does not send a notification about the window
                         // having been closed
                     }
+
                 });
 
                 addWindow(browserWindow);
@@ -51,12 +64,20 @@ public class CloseSubWindow extends TestBase {
                 // named for easier access by test tools
                 getMainWindow().open(new ExternalResource(windowUrl),
                         "nativewindow");
+                if (getBrowser().isOpera()) {
+                    // Immediately log a close event in Opera so this test can
+                    // be run for all browsers. Vaadin ticket #5687.
+                    logBrowserWindowClosed();
+                }
             }
         });
 
         addComponent(openBrowserWindowButton);
 
-        addComponent(new Button("Poll server"));
+        Button pollButton = new Button("Poll server");
+        pollButton.setDebugId("poll");
+        addComponent(pollButton);
+
     }
 
     private Window createClosableSubWindow(final String title) {
@@ -83,7 +104,7 @@ public class CloseSubWindow extends TestBase {
 
         window.addListener(new CloseListener() {
             public void windowClose(CloseEvent e) {
-                getMainWindow().showNotification(title + " closed");
+                log.log("Window '" + title + "' closed");
             }
         });
 
@@ -92,12 +113,17 @@ public class CloseSubWindow extends TestBase {
 
     @Override
     protected String getDescription() {
-        return "Close sub-windows both from code and with the close button in the window title bar, and check for close events.";
+        return "Close sub-windows both from code and with the close button in the window title bar, and check for close events. Contains an ugly workaround for the Opera bug (Opera does not send close events)";
     }
 
     @Override
     protected Integer getTicketNumber() {
         return 3865;
+    }
+
+    private void logBrowserWindowClosed() {
+        log.log("Browser window closed");
+
     }
 
 }
