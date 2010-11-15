635d3594fcc1eb5169e942ca19dea764fa1cdebb
Artur Signell <artur.signell@itmill.com>
2010-11-15 09:13:13 +0000

    Test case for #5989 - Removing a sub window from another window than its parent closes the window
    svn changeset:15986/svn branch:6.5

---
+++ tests/src/com/vaadin/tests/server/component/window/AddRemoveSubWindow.java
@@ -0,0 +1,86 @@
+package com.vaadin.tests.server.component.window;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.vaadin.Application;
+import com.vaadin.ui.Window;
+
+public class AddRemoveSubWindow {
+
+    public class TestApp extends Application {
+
+        @Override
+        public void init() {
+            Window w = new Window("Main window");
+            setMainWindow(w);
+        }
+    }
+
+    @Test
+    public void addSubWindow() {
+        TestApp app = new TestApp();
+        app.init();
+        Window subWindow = new Window("Sub window");
+        Window mainWindow = app.getMainWindow();
+
+        mainWindow.addWindow(subWindow);
+        // Added to main window so the parent of the sub window should be the
+        // main window
+        assertEquals(subWindow.getParent(), mainWindow);
+
+        try {
+            mainWindow.addWindow(subWindow);
+            assertTrue("Window.addWindow did not throw the expected exception",
+                    false);
+        } catch (IllegalArgumentException e) {
+            // Should throw an exception as it has already been added to the
+            // main window
+        }
+
+        // Try to add the same sub window to another window
+        try {
+            Window w = new Window();
+            w.addWindow(subWindow);
+            assertTrue("Window.addWindow did not throw the expected exception",
+                    false);
+        } catch (IllegalArgumentException e) {
+            // Should throw an exception as it has already been added to the
+            // main window
+        }
+
+    }
+
+    @Test
+    public void removeSubWindow() {
+        TestApp app = new TestApp();
+        app.init();
+        Window subWindow = new Window("Sub window");
+        Window mainWindow = app.getMainWindow();
+        mainWindow.addWindow(subWindow);
+
+        // Added to main window so the parent of the sub window should be the
+        // main window
+        assertEquals(subWindow.getParent(), mainWindow);
+
+        // Remove from the wrong window, should result in an exception
+        try {
+            subWindow.removeWindow(subWindow);
+            // Should not get here
+            assertTrue("Remove window did not throw the expected exception",
+                    false);
+        } catch (IllegalArgumentException e) {
+
+        }
+
+        // Parent should still be set
+        assertEquals(subWindow.getParent(), mainWindow);
+
+        // Remove from the main window and assert it has been removed
+        mainWindow.removeWindow(subWindow);
+        assertNull(subWindow.getParent());
+    }
+}
