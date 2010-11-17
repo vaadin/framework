4f7fdf97334b03c5d21b1fdc9fe9e2b8c498c2b1
Marko Grönroos <magi@iki.fi>
2010-11-17 18:22:27 +0000

    Modified test case for #6002: ticket number, description, formatting.
    svn changeset:16026/svn branch:6.5

--- tests/src/com/vaadin/tests/tickets/Ticket6002.java
+++ tests/src/com/vaadin/tests/tickets/Ticket6002.java
@@ -18,23 +18,25 @@ public class Ticket6002 extends TestBase {
 
         final VerticalLayout mainLayout = new VerticalLayout();
         main.setContent(mainLayout);
-        
-        mainLayout.addComponent(new Label("Replace the floating-point values with an integer"));
 
-        /////////////////////////////////////////////////////
+        mainLayout.addComponent(new Label(
+                "Replace the floating-point values with an integer"));
+
+        // ///////////////////////////////////////////////////
         // Better working case
 
-        final ObjectProperty<Double> property1 =
-            new ObjectProperty<Double>(new Double(42.0));
+        final ObjectProperty<Double> property1 = new ObjectProperty<Double>(
+                new Double(42.0));
 
         // A text field that changes its caption
-        final TextField tf1 = new TextField("Changing this field modifies only the textfield", property1);
+        final TextField tf1 = new TextField(
+                "Changing this field modifies only the textfield", property1);
         tf1.addListener(new Property.ValueChangeListener() {
             public void valueChange(ValueChangeEvent event) {
                 // This value change event is called twice if the new
                 // input value is an integer. The second time is during
                 // paint() of AbstractOrderedLayout.
-                
+
                 System.out.println("Value 2 is: " + property1.getValue());
 
                 tf1.setCaption("With caption " + property1.getValue());
@@ -42,41 +44,45 @@ public class Ticket6002 extends TestBase {
         });
         tf1.setImmediate(true);
         mainLayout.addComponent(tf1);
-        
-        /////////////////////////////////////////////////////
+
+        // ///////////////////////////////////////////////////
         // Totally failing case
-        
-        final ObjectProperty<Double> property2 =
-            new ObjectProperty<Double>(new Double(42.0));
+
+        final ObjectProperty<Double> property2 = new ObjectProperty<Double>(
+                new Double(42.0));
 
         // A text field that adds new components
-        final TextField tf2 = new TextField("Changing this field modifies the layout - do it twice", property2);
+        final TextField tf2 = new TextField(
+                "Changing this field modifies the layout - do it twice",
+                property2);
         tf2.addListener(new Property.ValueChangeListener() {
             public void valueChange(ValueChangeEvent event) {
                 // This value change event is called twice if the new
                 // input value is an integer. The second time is during
                 // paint() of AbstractOrderedLayout.
-                
+
                 System.out.println("Value 1 is: " + property2.getValue());
-                
+
                 // When this listener is called the second time in paint(), the
                 // add operation causes a ConcurrentModificationException
-                mainLayout.addComponent(new Label("Added a component, value is " + property2.getValue()));
+                mainLayout.addComponent(new Label(
+                        "Added a component, value is " + property2.getValue()));
             }
         });
         tf2.setImmediate(true);
         mainLayout.addComponent(tf2);
-        
+
         mainLayout.setSpacing(true);
     }
 
     @Override
     protected String getDescription() {
-        return "";
+        return "Change the numbers to integer value or add 0 in the decimal representation. "
+                + "This causes a secondary call during paint() to reformat the value, which causes ConcurrentModificationException in the second case.";
     }
 
     @Override
     protected Integer getTicketNumber() {
-        return 0;
+        return 6002;
     }
 }
