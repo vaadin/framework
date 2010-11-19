8a500ce66330cb57352ba733d46cc208d53e45be
Artur Signell <artur.signell@itmill.com>
2010-11-19 17:57:23 +0000

    Use parent method for adding icons
    svn changeset:16067/svn branch:6.5

--- tests/src/com/vaadin/tests/components/combobox/ComboBoxes2.java
+++ tests/src/com/vaadin/tests/components/combobox/ComboBoxes2.java
@@ -1,7 +1,5 @@
 package com.vaadin.tests.components.combobox;
 
-import java.util.LinkedHashMap;
-
 import com.vaadin.terminal.Resource;
 import com.vaadin.tests.components.select.AbstractSelectTestCase;
 import com.vaadin.ui.ComboBox;
@@ -21,13 +19,7 @@ public class ComboBoxes2 extends AbstractSelectTestCase<ComboBox> {
 
     private void createItemIconSelect(String category) {
 
-        LinkedHashMap<String, Resource> options = new LinkedHashMap<String, Resource>();
-        options.put("-", null);
-        options.put("16x16", ICON_16_USER_PNG_UNCACHEABLE);
-        options.put("32x32", ICON_32_ATTENTION_PNG_UNCACHEABLE);
-        options.put("64x64", ICON_64_EMAIL_REPLY_PNG_UNCACHEABLE);
-
-        createSelectAction("Icon", category, options, "-",
+        createSelectAction("Icon", category, createIconOptions(false), "-",
                 new Command<ComboBox, Resource>() {
 
                     public void execute(ComboBox c, Resource value, Object data) {
