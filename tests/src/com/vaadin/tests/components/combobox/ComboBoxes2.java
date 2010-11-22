1a5891316dece4453b91d412fb1acef3e5d2bc41
Artur Signell <artur.signell@itmill.com>
2010-11-22 05:56:43 +0000

    Input prompt support
    svn changeset:16078/svn branch:6.5

--- tests/src/com/vaadin/tests/components/combobox/ComboBoxes2.java
+++ tests/src/com/vaadin/tests/components/combobox/ComboBoxes2.java
@@ -1,10 +1,18 @@
 package com.vaadin.tests.components.combobox;
 
+import java.util.LinkedHashMap;
+
 import com.vaadin.terminal.Resource;
 import com.vaadin.tests.components.select.AbstractSelectTestCase;
 import com.vaadin.ui.ComboBox;
 
 public class ComboBoxes2 extends AbstractSelectTestCase<ComboBox> {
+
+    private Command<ComboBox, String> inputPromptCommand = new Command<ComboBox, String>() {
+        public void execute(ComboBox c, String value, Object data) {
+            c.setInputPrompt(value);
+        }
+    };
 
     @Override
     protected Class<ComboBox> getTestClass() {
@@ -15,6 +23,17 @@ public class ComboBoxes2 extends AbstractSelectTestCase<ComboBox> {
     protected void createActions() {
         super.createActions();
         createItemIconSelect(CATEGORY_DATA_SOURCE);
+        createInputPromptAction(CATEGORY_FEATURES);
+    }
+
+    private void createInputPromptAction(String category) {
+        LinkedHashMap<String, String> options = new LinkedHashMap<String, String>();
+        options.put("-", null);
+        options.put("Enter a value", "Enter a value");
+        options.put("- Click here -", "- Click here -");
+        createSelectAction("Input prompt", category, options, "-",
+                inputPromptCommand);
+
     }
 
     private void createItemIconSelect(String category) {
