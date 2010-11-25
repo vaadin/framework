cd198fb9a8faa0d7217c876d230d5b45a93658f7
Artur Signell <artur.signell@itmill.com>
2010-11-25 16:01:07 +0000

    Fix for #6060 - Moving a component from a GridLayout to a layout inside the GridLayout causes the client side widget to be lost
    svn changeset:16167/svn branch:6.5

--- src/com/vaadin/terminal/gwt/client/ui/VGridLayout.java
+++ src/com/vaadin/terminal/gwt/client/ui/VGridLayout.java
@@ -207,7 +207,12 @@ public class VGridLayout extends SimplePanel implements Paintable, Container {
             paintableToCell.remove(w);
             widgetToComponentContainer.remove(w);
             childComponentContainer.removeFromParent();
-            client.unregisterPaintable((Paintable) w);
+            if (childComponentContainer.getWidget() == w) {
+                // Only unregister if the paintable has not been moved to
+                // another layout and detached from this (hence cc.getWidget()
+                // == null, or w.getParent() != this)
+                client.unregisterPaintable((Paintable) w);
+            }
         }
         nonRenderedWidgets = null;
 
