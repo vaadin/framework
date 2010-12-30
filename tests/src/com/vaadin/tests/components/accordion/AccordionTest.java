d2ec280ced4f6280970267282fa04355fc3fa6c0
Artur Signell <artur.signell@itmill.com>
2010-12-30 15:45:40 +0000

    Basic test for Accordion
    svn changeset:16735/svn branch:6.5

---
+++ tests/src/com/vaadin/tests/components/accordion/AccordionTest.java
@@ -0,0 +1,12 @@
+package com.vaadin.tests.components.accordion;
+
+import com.vaadin.tests.components.tabsheet.TabSheetTest;
+import com.vaadin.ui.Accordion;
+
+public class AccordionTest extends TabSheetTest<Accordion> {
+
+    @Override
+    protected Class<Accordion> getTestClass() {
+        return Accordion.class;
+    }
+}
