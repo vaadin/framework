89da5eba0db08012973e6455e026eb43c1cca673
Matti Tahvonen <matti.tahvonen@itmill.com>
2011-01-07 10:22:38 +0000

    #5927 : cleaned and automated test case
    svn changeset:16812/svn branch:6.5

--- tests/src/com/vaadin/tests/components/datefield/CommitInvalid.java
+++ tests/src/com/vaadin/tests/components/datefield/CommitInvalid.java
@@ -1,23 +1,27 @@
 package com.vaadin.tests.components.datefield;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import com.vaadin.data.Property;
+import com.vaadin.data.Validator.InvalidValueException;
 import com.vaadin.data.util.ObjectProperty;
 import com.vaadin.data.validator.IntegerValidator;
 import com.vaadin.tests.components.TestBase;
+import com.vaadin.tests.util.Log;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.DateField;
 import com.vaadin.ui.Form;
-import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 
 public class CommitInvalid extends TestBase {
 
     @Override
     protected String getDescription() {
-        return "DateField with error is committed regardless of the invalidity.";
+        return "DateField with parsing error is committed regardless "
+                + "of the invalidity. Parsing error should be handled"
+                + " as a builtin validator, similar to isRequired()"
+                + " in AbstractField.";
     }
 
     @Override
@@ -25,67 +29,109 @@ public class CommitInvalid extends TestBase {
         return 5927;
     }
 
+    ObjectProperty<Date> dateProperty;
+    private DateField dateField;
+    private ObjectProperty<Integer> integerProperty;
+    private TextField integerField;
+    private Log log;
+
     @Override
     protected void setup() {
+        /*
+         * Create and configure form.
+         */
         final Form form = new Form();
-        addComponent(form);
+        form.setWriteThrough(false); // set write buffering on
+        form.setImmediate(true); // make form (and especially its fields
+                                 // immediate)
 
-        @SuppressWarnings("deprecation")
-        ObjectProperty property = new ObjectProperty(new Date(
-                2009 - 1900, 4 - 1, 1));
+        /*
+         * Create and configure Date Field, the actual component to be tested.
+         */
+        dateProperty = new ObjectProperty<Date>(new Date(2009 - 1900, 4 - 1, 1));
 
-        final DateField df = new DateField("Year", property) {
-            @Override
-            protected Date handleUnparsableDateString(String dateString)
-                    throws ConversionException {
-                throw new Property.ConversionException(
-                        "Date format not recognized");
-            }
-        };
-        df.setResolution(DateField.RESOLUTION_DAY);
-        // df.setReadThrough(false);
-        // df.setWriteThrough(false);
-        // df.setImmediate(true);
-        form.addField("date", df);
-        form.setValidationVisible(true);
+        dateField = new DateField("Year", dateProperty);
+        dateField.setResolution(DateField.RESOLUTION_DAY);
+        dateField.setDebugId("_DF");
+        form.addField("date", dateField);
 
-        final ObjectProperty integer = new ObjectProperty("42");
-        final TextField another = new TextField("Another Field", integer);
-        another.addValidator(new IntegerValidator("Not an integer"));
-        // another.setReadThrough(false);
-        // another.setWriteThrough(false);
-        form.addField("text", another);
+        /*
+         * Create a TextField with integer property (and integer validator,
+         * preventing invalid typed values to be pushded to property) as a
+         * reference component.
+         */
+        integerProperty = new ObjectProperty<Integer>(42);
+        integerField = new TextField("Another Field", integerProperty);
+        integerField.addValidator(new IntegerValidator("Not an integer"));
+        integerField.setDebugId("_IF");
+        form.addField("text", integerField);
 
-        // form.setReadThrough(false);
-        form.setWriteThrough(false);
-
+        /*
+         * Action buttons.
+         */
         Button validate = new Button("Validate");
+        validate.setDebugId("_validate");
+        validate.addListener(new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                try {
+                    form.validate();
+                    log.log("Form is valid");
+                } catch (InvalidValueException e) {
+                    log.log("Validation failed:" + e.getLocalizedMessage());
+                } finally {
+                    printState();
+                }
+            }
+        });
         form.getFooter().addComponent(validate);
 
         Button commit = new Button("Commit");
-        form.getFooter().addComponent(commit);
-
-        validate.addListener(new Button.ClickListener() {
-            public void buttonClick(ClickEvent event) {
-                form.validate();
-            }
-        });
-
+        commit.setDebugId("_commit");
         commit.addListener(new Button.ClickListener() {
             public void buttonClick(ClickEvent event) {
-                form.commit();
-                System.out.println("Field value: "
-                        + ((String) another.getValue()) + ", property value: "
-                        + ((String) integer.getValue()));
+                try {
+                    form.commit();
+                    log.log("Commit succeeded");
+                } catch (InvalidValueException e) {
+                    log.log("Commit failed : " + e.getLocalizedMessage());
+                } finally {
+                    printState();
+                }
             }
         });
 
-        Label value = new Label("Date Value");
-        addComponent(value);
-        value.setPropertyDataSource(property);
+        form.getFooter().addComponent(commit);
+        Button printState = new Button("printState");
+        printState.addListener(new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                printState();
+            }
 
-        Label value2 = new Label("Text Value");
-        addComponent(value2);
-        value2.setPropertyDataSource(integer);
+        });
+        form.getFooter().addComponent(printState);
+
+        log = new Log(7);
+        log.log("Test app started");
+        printState();
+
+        addComponent(form);
+        addComponent(log);
+
+    }
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    private String f(Date date) {
+        if (date == null) {
+            return "null";
+        }
+        return dateFormat.format(date);
+    }
+
+    private void printState() {
+        log.log("Date. Field: " + f((Date) dateField.getValue())
+                + " Property: " + f(dateProperty.getValue()));
+        log.log("Integer: Field: " + integerField.getValue() + " Property: "
+                + integerProperty.getValue());
     }
 }
