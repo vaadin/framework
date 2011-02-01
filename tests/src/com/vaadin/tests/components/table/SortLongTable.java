d139dfe35042dfb2d891c3a62d4c575e703361e8
Artur Signell <artur.signell@itmill.com>
2011-02-01 08:19:26 +0000

    Test for sorting a tall Table
    svn changeset:17101/svn branch:6.5

---
+++ tests/src/com/vaadin/tests/components/table/SortLongTable.java
@@ -0,0 +1,43 @@
+package com.vaadin.tests.components.table;
+
+import com.vaadin.tests.components.AbstractTestCase;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+public class SortLongTable extends AbstractTestCase {
+
+    @Override
+    public void init() {
+        final int NUMBER_OF_ROWS = 100; // Works with 10
+
+        Window mainWindow = new Window("Table Sort Test");
+        mainWindow.setSizeFull();
+        setMainWindow(mainWindow);
+
+        Table ptable = new Table();
+        ptable.addContainerProperty("Sort_me_please", String.class, "--");
+        for (int i = NUMBER_OF_ROWS - 1; i >= 0; i--) {
+            ptable.addItem("" + i).getItemProperty("Sort_me_please")
+                    .setValue("Value " + String.format("%02d", i));
+        }
+
+        ptable.setWidth("100%");
+        ptable.setPageLength(NUMBER_OF_ROWS);
+
+        VerticalLayout vl = new VerticalLayout();
+        vl.addComponent(ptable);
+        mainWindow.addComponent(vl);
+    }
+
+    @Override
+    protected String getDescription() {
+        return "Clicking on the header should sort the column. It should not cause the headers to be scrolled out of view.";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 6367;
+    }
+
+}
