95ec3d17bebff391a6899da17d006c14582a3080
Artur Signell <artur.signell@itmill.com>
2011-03-31 06:16:12 +0000

    Ugly workaround for #6682 that ensures getAbsoluteTop/getAbsoluteLeft always return an integer for Webkit
    svn changeset:18040/svn branch:6.5

---
+++ src/com/google/gwt/dom/client/VaadinDOMImplSafari.java
@@ -0,0 +1,19 @@
+package com.google.gwt.dom.client;
+
+/**
+ * Overridden to workaround GWT issue #6194. Remove this when updating to a
+ * newer GWT that fixes the problem (2.3.0 possibly). Must be in this package as
+ * the whole DOMImpl hierarchy is package private and I really did not want to
+ * copy all the parent classes into this one...
+ */
+class VaadinDOMImplSafari extends DOMImplSafari {
+    @Override
+    public int getAbsoluteLeft(Element elem) {
+        return super.getAbsoluteLeft(elem) | 0;
+    }
+
+    @Override
+    public int getAbsoluteTop(Element elem) {
+        return super.getAbsoluteTop(elem) | 0;
+    }
+}
