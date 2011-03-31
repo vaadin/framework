2d6f626bedd6aaf7e740b368f3d893b2e7853ccc
Artur Signell <artur.signell@itmill.com>
2011-03-31 13:18:19 +0000

    Test for #6731
    svn changeset:18065/svn branch:6.5

---
+++ tests/src/com/vaadin/tests/components/tree/TreeScrollingOnSelection.java
@@ -0,0 +1,71 @@
+package com.vaadin.tests.components.tree;
+
+import com.vaadin.data.util.HierarchicalContainer;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.event.ItemClickEvent.ItemClickListener;
+import com.vaadin.tests.components.TestBase;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.Tree;
+
+public class TreeScrollingOnSelection extends TestBase {
+    private static final long serialVersionUID = 4082075610259697145L;
+
+    private static final String GREEK_ALPHABET = "greek alphabet";
+
+    private static final String[] ALPHABET = { "Alpha", "Beta", "Gamma",
+            "Delta", "Epsilon", "Zeta", "Eta", "Theta", "Iota", "Kappa",
+            "Lambda", "My", "Ny", "Xi", "Omikron", "Pi", "Rho", "Sigma", "Tau",
+            "Ypsilon", "Phi", "Chi", "Psi", "Omega" };
+
+    @Override
+    public void setup() {
+        final Label charLabel = new Label();
+        charLabel.setWidth("200px");
+
+        Tree tree = new Tree("alphabets", getContainer());
+        tree.expandItem(GREEK_ALPHABET);
+        tree.addListener(new ItemClickListener() {
+
+            private static final long serialVersionUID = 5955518276555388126L;
+
+            public void itemClick(ItemClickEvent event) {
+                charLabel.setValue(event.getItemId().toString());
+            }
+        });
+
+        Panel panel = new Panel();
+        panel.setScrollable(true);
+        panel.addComponent(tree);
+        panel.setWidth("200px");
+        panel.setHeight("300px");
+
+        addComponent(panel);
+
+        addComponent(charLabel);
+    }
+
+    private HierarchicalContainer getContainer() {
+        HierarchicalContainer container = new HierarchicalContainer();
+
+        container.addItem(GREEK_ALPHABET);
+
+        for (String character : ALPHABET) {
+            container.addItem(character);
+            container.setChildrenAllowed(character, false);
+            container.setParent(character, GREEK_ALPHABET);
+        }
+
+        return container;
+    }
+
+    @Override
+    protected String getDescription() {
+        return "Selecting an item in the tree inside the Panel should not cause the panel scroll position to change.";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 6731;
+    }
+}
