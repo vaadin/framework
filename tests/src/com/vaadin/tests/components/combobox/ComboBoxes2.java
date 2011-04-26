78cdba59e9bf41cd876734a10b908e5643e97ee2
Artur Signell <artur.signell@itmill.com>
2011-04-26 14:22:12 +0000

    Add support for testing filtering mode
    svn changeset:18472/svn branch:6.6

--- tests/src/com/vaadin/tests/components/combobox/ComboBoxes2.java
+++ tests/src/com/vaadin/tests/components/combobox/ComboBoxes2.java
@@ -3,14 +3,21 @@ package com.vaadin.tests.components.combobox;
 import java.util.LinkedHashMap;
 
 import com.vaadin.terminal.Resource;
-import com.vaadin.tests.components.select.AbstractSelectTestCase;
+import com.vaadin.tests.components.select.SelectTest;
 import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.Select;
 
-public class ComboBoxes2 extends AbstractSelectTestCase<ComboBox> {
+public class ComboBoxes2 extends SelectTest<ComboBox> {
 
     private Command<ComboBox, String> inputPromptCommand = new Command<ComboBox, String>() {
         public void execute(ComboBox c, String value, Object data) {
             c.setInputPrompt(value);
+        }
+    };
+    private Command<ComboBox, Integer> filteringModeCommand = new Command<ComboBox, Integer>() {
+
+        public void execute(ComboBox c, Integer value, Object data) {
+            c.setFilteringMode(value);
         }
     };
 
@@ -24,6 +31,18 @@ public class ComboBoxes2 extends AbstractSelectTestCase<ComboBox> {
         super.createActions();
         createItemIconSelect(CATEGORY_DATA_SOURCE);
         createInputPromptAction(CATEGORY_FEATURES);
+        createFilteringModeAction(CATEGORY_FEATURES);
+    }
+
+    private void createFilteringModeAction(String category) {
+        LinkedHashMap<String, Integer> options = new LinkedHashMap<String, Integer>();
+        options.put("Off", Select.FILTERINGMODE_OFF);
+        options.put("Contains", Select.FILTERINGMODE_CONTAINS);
+        options.put("Starts with", Select.FILTERINGMODE_STARTSWITH);
+
+        createSelectAction("Filtering mode", category, options, "Contains",
+                filteringModeCommand);
+
     }
 
     private void createInputPromptAction(String category) {
--- tests/src/com/vaadin/tests/components/select/SelectTest.java
+++ tests/src/com/vaadin/tests/components/select/SelectTest.java
@@ -2,11 +2,12 @@ package com.vaadin.tests.components.select;
 
 import com.vaadin.ui.Select;
 
-public class SelectTest extends AbstractSelectTestCase<Select> {
+public class SelectTest<T extends Select> extends AbstractSelectTestCase<T> {
 
+    @SuppressWarnings("unchecked")
     @Override
-    protected Class<Select> getTestClass() {
-        return Select.class;
+    protected Class<T> getTestClass() {
+        return (Class<T>) Select.class;
     }
 
 }
