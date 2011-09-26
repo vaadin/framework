9efa4aaaa650e5d7d4690f64a9c17cd86b9079f2
Jonatan Kronqvist <jonatan.kronqvist@itmill.com>
2011-09-26 11:44:54 +0000

    Test case for #7644
    svn changeset:21302/svn branch:6.7

---
+++ tests/src/com/vaadin/tests/components/table/OddEvenRowStyling.java
@@ -0,0 +1,36 @@
+/* 
+@ITMillApache2LicenseForJavaFiles@
+ */
+package com.vaadin.tests.components.table;
+
+import com.vaadin.tests.components.TestBase;
+import com.vaadin.ui.Table;
+
+/**
+ * @author jonatan
+ * 
+ */
+public class OddEvenRowStyling extends TestBase {
+
+    @Override
+    protected void setup() {
+        Table t = new Table();
+        t.setPageLength(10);
+        t.addContainerProperty("foo", String.class, "");
+        for (int i = 0; i < 33; i++) {
+            t.addItem(i).getItemProperty("foo").setValue("bar");
+        }
+        addComponent(t);
+    }
+
+    @Override
+    protected String getDescription() {
+        return "Odd/even row styling should not change when scrolling";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 7644;
+    }
+
+}
