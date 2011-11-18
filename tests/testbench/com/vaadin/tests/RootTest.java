eb7f898f6c8299b868c8ffb9480146b64c15b958
Leif Åstrand <leif@vaadin.com>
2011-11-18 15:04:11 +0200

    Simplify RootTest and move it to the tests package

--- tests/testbench/com/vaadin/tests/RootTest.java
+++ tests/testbench/com/vaadin/tests/RootTest.java
@@ -2,20 +2,15 @@
 @ITMillApache2LicenseForJavaFiles@
  */
 
-package com.vaadin;
+package com.vaadin.tests;
 
 import com.vaadin.terminal.WrappedRequest;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Root;
-import com.vaadin.ui.VerticalLayout;
 
 public class RootTest extends Root {
     @Override
     public void init(WrappedRequest request) {
-        VerticalLayout layout = new VerticalLayout();
-
-        layout.addComponent(new Label("Hello root"));
-
-        setContent(layout);
+        getContent().addComponent(new Label("Hello root"));
     }
 }
