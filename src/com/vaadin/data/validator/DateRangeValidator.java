1e8b3474b73eff9753d65ad6c1234c4e6636d40b
Artur Signell <artur@vaadin.com>
2011-12-15 12:06:29 +0200

    #8110 Added comments that comparison is done directly on the date object

--- src/com/vaadin/data/validator/DateRangeValidator.java
+++ src/com/vaadin/data/validator/DateRangeValidator.java
@@ -10,6 +10,12 @@ import com.vaadin.ui.DateField.Resolution;
 /**
  * Validator for validating that a Date is inside a given range.
  * 
+ * <p>
+ * Note that the comparison is done directly on the Date object so take care
+ * that the hours/minutes/seconds/milliseconds of the min/max values are
+ * properly set.
+ * </p>
+ * 
  * @author Vaadin Ltd.
  * @version
  * @VERSION@
@@ -19,11 +25,16 @@ public class DateRangeValidator extends RangeValidator<Date> {
 
     /**
      * Creates a validator for checking that an Date is within a given range.
-     * 
+     * <p>
      * By default the range is inclusive i.e. both minValue and maxValue are
      * valid values. Use {@link #setMinValueIncluded(boolean)} or
      * {@link #setMaxValueIncluded(boolean)} to change it.
-     * 
+     * </p>
+     * <p>
+     * Note that the comparison is done directly on the Date object so take care
+     * that the hours/minutes/seconds/milliseconds of the min/max values are
+     * properly set.
+     * </p>
      * 
      * @param errorMessage
      *            the message to display in case the value does not validate.
