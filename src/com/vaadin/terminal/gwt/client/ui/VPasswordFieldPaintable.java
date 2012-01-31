bcc43a315b1b64d12db2a5d6e9a782f8b6a51793
Jens Jansson <peppe@vaadin.com>
2012-01-31 09:24:08 +0200

    Splitted VPasswordField into Widget and Paintable.

---
+++ src/com/vaadin/terminal/gwt/client/ui/VPasswordFieldPaintable.java
@@ -0,0 +1,24 @@
+package com.vaadin.terminal.gwt.client.ui;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.ui.Widget;
+import com.vaadin.terminal.gwt.client.ApplicationConnection;
+import com.vaadin.terminal.gwt.client.UIDL;
+
+public class VPasswordFieldPaintable extends VTextFieldPaintable {
+
+    @Override
+    public void updateFromUIDL(UIDL uidl, ApplicationConnection client) {
+        super.updateFromUIDL(uidl, client);
+    }
+
+    @Override
+    protected Widget createWidget() {
+        return GWT.create(VPasswordField.class);
+    }
+
+    @Override
+    public VPasswordField getWidgetForPaintable() {
+        return (VPasswordField) super.getWidgetForPaintable();
+    }
+}
