4bba7b05bb57bc0783a2d72b98a9fcfce681d35a
Leif Åstrand <leif@vaadin.com>
2012-02-13 19:31:17 +0200

    Update Window to work with MeasureManager (#8313)

--- src/com/vaadin/terminal/gwt/client/ui/VWindowPaintable.java
+++ src/com/vaadin/terminal/gwt/client/ui/VWindowPaintable.java
@@ -19,7 +19,8 @@ import com.vaadin.terminal.gwt.client.VPaintableWidget;
 import com.vaadin.terminal.gwt.client.ui.ShortcutActionHandler.BeforeShortcutActionListener;
 
 public class VWindowPaintable extends VAbstractPaintableWidgetContainer
-        implements BeforeShortcutActionListener {
+        implements BeforeShortcutActionListener, ResizeRequired,
+        LayoutPhaseListener {
 
     private static final String CLICK_EVENT_IDENTIFIER = VPanelPaintable.CLICK_EVENT_IDENTIFIER;
 
@@ -301,6 +302,22 @@ public class VWindowPaintable extends VAbstractPaintableWidgetContainer
     @Override
     protected Widget createWidget() {
         return GWT.create(VWindow.class);
+    }
+
+    public void onResize() {
+        getWidgetForPaintable().requestLayout();
+    }
+
+    public void beforeLayout() {
+
+    }
+
+    public void afterLayout() {
+        VWindow window = getWidgetForPaintable();
+        if (window.centered) {
+            window.center();
+            window.updateShadowSizeAndPosition();
+        }
     }
 
 }
