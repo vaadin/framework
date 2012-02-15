5b2e12623424e3603a5983895d122ba333db5b18
Automerge <automerge@vaadin.com>
2012-02-15 02:18:42 +0000

    [merge from 6.7] #8373 Handler DragAndDropService changeVariables error by finding the drop target and using that for error reporting
    svn changeset:23013/svn branch:6.8

--- src/com/vaadin/terminal/gwt/server/AbstractCommunicationManager.java
+++ src/com/vaadin/terminal/gwt/server/AbstractCommunicationManager.java
@@ -1396,12 +1396,16 @@ public abstract class AbstractCommunicationManager implements
                         }
                     }
                 } catch (Exception e) {
+                    Component errorComponent = null;
                     if (owner instanceof Component) {
-                        handleChangeVariablesError(app, (Component) owner, e, m);
-                    } else {
-                        // TODO DragDropService error handling
-                        throw new RuntimeException(e);
+                        errorComponent = (Component) owner;
+                    } else if (owner instanceof DragAndDropService) {
+                        if (m.get("dhowner") instanceof Component) {
+                            errorComponent = (Component) m.get("dhowner");
+                        }
                     }
+
+                    handleChangeVariablesError(app, errorComponent, e, m);
                 }
             } else {
 
