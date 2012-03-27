53bb61089d4225ff3ba1bc75e471720e0fe44f4a
Automerge <automerge@vaadin.com>
2012-03-27 09:15:02 +0000

    [merge from 6.7] #8559/#8560 Additional fixes based on review
    svn changeset:23328/svn branch:6.8

--- src/com/vaadin/terminal/gwt/client/ui/VMenuBar.java
+++ src/com/vaadin/terminal/gwt/client/ui/VMenuBar.java
@@ -1590,12 +1590,10 @@ public class VMenuBar extends SimpleFocusablePanel implements Paintable,
     private void selectFirstItem() {
         for (int i = 0; i < items.size(); i++) {
             CustomMenuItem item = items.get(i);
-            if (!item.isSelectable()) {
-                continue;
+            if (item.isSelectable()) {
+                setSelected(item);
+                break;
             }
-
-            setSelected(item);
-            break;
         }
     }
 
@@ -1611,7 +1609,7 @@ public class VMenuBar extends SimpleFocusablePanel implements Paintable,
 
         menuVisible = true;
         // Select the first item in the newly open submenu
-        subMenu.setSelected(subMenu.getItems().get(0));
+        subMenu.selectFirstItem();
 
     }
 
