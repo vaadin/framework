b771e1989377387bfcb40f1ac1d217baa1236766
Artur Signell <artur.signell@itmill.com>
2012-04-26 13:11:51 +0000

    Removed duplicate code for broken Android detection (#8716)
    svn changeset:23643/svn branch:6.8

--- src/com/vaadin/terminal/gwt/client/ui/FocusableScrollPanel.java
+++ src/com/vaadin/terminal/gwt/client/ui/FocusableScrollPanel.java
@@ -24,7 +24,6 @@ import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.client.ui.impl.FocusImpl;
 import com.vaadin.terminal.gwt.client.BrowserInfo;
-import com.vaadin.terminal.gwt.client.VConsole;
 
 /**
  * A scrollhandlers similar to {@link ScrollPanel}.
@@ -163,7 +162,7 @@ public class FocusableScrollPanel extends SimpleFocusablePanel implements
      *            the new vertical scroll position, in pixels
      */
     public void setScrollPosition(int position) {
-        if (isAndroidWithBrokenScrollTop()) {
+        if (BrowserInfo.get().isAndroidWithBrokenScrollTop()) {
             ArrayList<com.google.gwt.dom.client.Element> elements = TouchScrollDelegate
                     .getElements(getElement());
             for (com.google.gwt.dom.client.Element el : elements) {
@@ -175,11 +174,6 @@ public class FocusableScrollPanel extends SimpleFocusablePanel implements
         } else {
             getElement().setScrollTop(position);
         }
-    }
-
-    private boolean isAndroidWithBrokenScrollTop() {
-        return BrowserInfo.getBrowserString().contains("Android 3")
-                || BrowserInfo.getBrowserString().contains("Android 4");
     }
 
     public void onScroll(ScrollEvent event) {
