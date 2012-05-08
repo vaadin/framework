298b6e005c4da3cd279451c24b3f85ac1e0a6c31
Artur Signell <artur@vaadin.com>
2012-05-08 14:28:44 +0300

    Workaround for incorrect hierarchy (#8797)

--- src/com/vaadin/terminal/gwt/client/ui/customfield/CustomFieldConnector.java
+++ src/com/vaadin/terminal/gwt/client/ui/customfield/CustomFieldConnector.java
@@ -3,11 +3,20 @@
  */
 package com.vaadin.terminal.gwt.client.ui.customfield;
 
+import com.google.gwt.core.client.GWT;
+import com.vaadin.terminal.gwt.client.AbstractFieldState;
+import com.vaadin.terminal.gwt.client.communication.SharedState;
 import com.vaadin.terminal.gwt.client.ui.Connect;
 import com.vaadin.terminal.gwt.client.ui.customcomponent.CustomComponentConnector;
 import com.vaadin.ui.CustomField;
 
 @Connect(value = CustomField.class)
 public class CustomFieldConnector extends CustomComponentConnector {
+    @Override
+    protected SharedState createState() {
+        // Workaround as CustomFieldConnector does not extend
+        // AbstractFieldConnector.
+        return GWT.create(AbstractFieldState.class);
+    }
 
 }
