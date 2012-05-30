42dff513d801eecb011ad245bb6274e1396da66b
Johannes Dahlström <johannes.dahlstrom@vaadin.com>
2012-05-30 14:02:35 +0000

    #1744 Fixed a regression
    svn changeset:23854/svn branch:6.8

--- src/com/vaadin/terminal/gwt/client/ui/VSplitPanel.java
+++ src/com/vaadin/terminal/gwt/client/ui/VSplitPanel.java
@@ -398,7 +398,7 @@ public class VSplitPanel extends ComplexPanel implements Container,
             if (positionReversed) {
                 posAsInt = offsetLength - posAsInt - getSplitterSize();
             }
-            posAsFloat = ((float) posAsInt / (float) getOffsetWidth() * 100);
+            posAsFloat = ((float) posAsInt / (float) offsetLength * 100);
 
         } else {
             posAsFloat = Float.parseFloat(pos.substring(0, pos.length() - 1));
