bd85f96658b15159af3f7e8159047a6057290502
Artur Signell <artur@vaadin.com>
2012-06-21 17:59:55 +0300

    Pass RPC exceptions to Application.terminalError (#9018)

--- src/com/vaadin/terminal/gwt/server/RpcManager.java
+++ src/com/vaadin/terminal/gwt/server/RpcManager.java
@@ -13,5 +13,36 @@ import java.io.Serializable;
  * @since 7.0
  */
 public interface RpcManager extends Serializable {
-    public void applyInvocation(ServerRpcMethodInvocation invocation);
+    public void applyInvocation(ServerRpcMethodInvocation invocation)
+            throws RpcInvocationException;
+
+    /**
+     * Wrapper exception for exceptions which occur during invocation of an RPC
+     * call
+     * 
+     * @author Vaadin Ltd
+     * @version @VERSION@
+     * @since 7.0
+     * 
+     */
+    public static class RpcInvocationException extends Exception {
+
+        public RpcInvocationException() {
+            super();
+        }
+
+        public RpcInvocationException(String message, Throwable cause) {
+            super(message, cause);
+        }
+
+        public RpcInvocationException(String message) {
+            super(message);
+        }
+
+        public RpcInvocationException(Throwable cause) {
+            super(cause);
+        }
+
+    }
+
 }
