48cc0f3973579ee6c5893b27e484afc3c87692ec
Leif Åstrand <leif@vaadin.com>
2012-06-27 17:12:49 +0300

    Javadocs for @StyleSheet and @JavaScript (#9044)

--- src/com/vaadin/annotations/JavaScript.java
+++ src/com/vaadin/annotations/JavaScript.java
@@ -9,8 +9,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.vaadin.terminal.gwt.server.ClientConnector;
+
+/**
+ * If this annotation is present on a {@link ClientConnector} class, the
+ * framework ensures the referenced JavaScript files are loaded before the
+ * corresponding client-side connector is initialized.
+ * 
+ * @author Vaadin Ltd
+ * @version @VERSION@
+ * @since 7.0.0
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface JavaScript {
+    /**
+     * JavaScript files to load before initializing the client-side connector.
+     * 
+     * @return an array of JavaScript file urls
+     */
     public String[] value();
 }
--- src/com/vaadin/annotations/StyleSheet.java
+++ src/com/vaadin/annotations/StyleSheet.java
@@ -9,8 +9,24 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import com.vaadin.terminal.gwt.server.ClientConnector;
+
+/**
+ * If this annotation is present on a {@link ClientConnector} class, the
+ * framework ensures the referenced style sheets are loaded before the
+ * corresponding client-side connector is initialized.
+ * 
+ * @author Vaadin Ltd
+ * @version @VERSION@
+ * @since 7.0.0
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface StyleSheet {
+    /**
+     * Style sheets to load before initializing the client-side connector.
+     * 
+     * @return an array of style sheet urls
+     */
     public String[] value();
 }
