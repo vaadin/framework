060ba3b63cf1c46bb726a6f6f0910924fb185364
Leif Åstrand <leif@vaadin.com>
2012-06-28 18:47:01 +0300

    Describe how paths are resolved in javadoc (#9048)

--- src/com/vaadin/annotations/JavaScript.java
+++ src/com/vaadin/annotations/JavaScript.java
@@ -15,6 +15,16 @@ import com.vaadin.terminal.gwt.server.ClientConnector;
  * If this annotation is present on a {@link ClientConnector} class, the
  * framework ensures the referenced JavaScript files are loaded before the init
  * method for the corresponding client-side connector is invoked.
+ * <p>
+ * Absolute URLs including protocol and host are used as is on the client-side.
+ * Relative urls are mapped to APP/CONNECTOR/[url] which are by default served
+ * from the classpath relative to the class where the annotation is defined.
+ * <p>
+ * Example: {@code @JavaScript( "http://host.com/file1.js", "file2.js"})} on
+ * the class com.example.MyConnector would load the file
+ * http://host.com/file1.js as is and file2.js from /com/example/file2.js on the
+ * server's classpath using the ClassLoader that was used to load
+ * com.example.MyConnector.
  * 
  * @author Vaadin Ltd
  * @version @VERSION@
