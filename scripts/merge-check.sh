76bfc960d36d6cb384d037c4a3a12b132e0d2556
Leif Åstrand <leif@vaadin.com>
2012-07-24 14:56:25 +0300

    Adjust merge check to work more nicely with team city

--- scripts/merge-check.sh
+++ scripts/merge-check.sh
@@ -8,6 +8,9 @@ then
 	exit 3
 fi
 
+testname="merge check for `pwd|sed "s/.*\///"`"
+echo "##teamcity[testStarted name='$testname' captureStandardOutput='<true/false>']"
+
 command="git --no-pager log --no-color $SINCE..$UNTIL"
 # TODO Why do I get whitespace in the beginning of the wc output?
 change_count=`$command --oneline|wc -l|tr -d ' '`
@@ -15,10 +18,14 @@ change_count=`$command --oneline|wc -l|tr -d ' '`
 if [ "$change_count" = "0" ]
 then
 	echo "No unmerged commits"
-	exit 0
+else 
+	command="$command --format=short"
+	message="There are $change_count commits that have not been merged from $UNTIL to $SINCE"
+	echo $message
+	echo ""
+	$command
+	details=`$command|perl -p -e 's/\n/|n/' | sed "s/['\|\[\]]/|\&/g"`
+	echo "##teamcity[testFailed name='$testname' message='$message' details='|n$details']"
 fi
 
-echo "There are $change_count commits that have not been merged from $UNTIL to $SINCE: "
-echo ""
-$command
-exit 1
+echo "##teamcity[testFinished name='$testname']"
