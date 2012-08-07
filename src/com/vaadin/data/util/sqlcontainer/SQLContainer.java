80d8cb69ce6e5dfa4a6a356e91559c062664b360
Henri Sara <henri.sara@itmill.com>
2012-08-07 13:05:15 +0000

    #9155 revert first fix for now - requires proper fix in Select together with this correction
    svn changeset:24095/svn branch:6.8

--- src/com/vaadin/data/util/sqlcontainer/SQLContainer.java
+++ src/com/vaadin/data/util/sqlcontainer/SQLContainer.java
@@ -510,7 +510,9 @@ public class SQLContainer implements Container, Container.Filterable,
      */
     public void removeContainerFilter(Filter filter) {
         filters.remove(filter);
-        refresh();
+        // TODO this cannot be added before ComboBox is fixed
+        // (Select.requestRepaint() must not affect filter string)
+        // refresh();
     }
 
     /**
