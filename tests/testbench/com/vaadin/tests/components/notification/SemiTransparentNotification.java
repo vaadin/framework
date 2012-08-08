42c26abe83d933eb980995044317cc7fb41b8424
Artur Signell <artur.signell@itmill.com>
2012-08-08 14:56:13 +0000

    Test for ensuring that overlays can be transparent (not in IE < 9)
    svn changeset:24124/svn branch:6.8

---
+++ tests/testbench/com/vaadin/tests/components/notification/SemiTransparentNotification.java
@@ -0,0 +1,34 @@
+package com.vaadin.tests.components.notification;
+
+import com.vaadin.tests.components.TestBase;
+import com.vaadin.tests.util.LoremIpsum;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Window.Notification;
+
+public class SemiTransparentNotification extends TestBase {
+
+    @Override
+    protected void setup() {
+        Label l = new Label(LoremIpsum.get(10000));
+        getLayout().setSizeFull();
+        addComponent(l);
+        getMainWindow()
+                .showNotification(
+                        new Notification(
+                                "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This should be a <br/><b>SEMI-TRANSPARENT</b><br/> notification",
+                                Notification.TYPE_WARNING_MESSAGE));
+    }
+
+    @Override
+    protected String getDescription() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
