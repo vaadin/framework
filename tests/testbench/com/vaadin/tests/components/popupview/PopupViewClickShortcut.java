946ce4708d935bfc0b1cd74d121136633799979c
Johannes Dahlström <johannes.dahlstrom@vaadin.com>
2012-08-15 08:59:24 +0000

    TestBench test for #8193 now that #8827 is fixed
    svn changeset:24175/svn branch:6.8

--- tests/testbench/com/vaadin/tests/components/popupview/PopupViewClickShortcut.java
+++ tests/testbench/com/vaadin/tests/components/popupview/PopupViewClickShortcut.java
@@ -46,16 +46,15 @@ public class PopupViewClickShortcut extends TestBase {
         l.setWidth(null);
 
         Button b = new Button("Submit " + caption + " (Ctrl+Alt+"
-                + String.valueOf(Character.toChars(keyCode)) + ")",
-                new Button.ClickListener() {
-                    private int i = 5;
+                + (char) keyCode + ")", new Button.ClickListener() {
+            private int i = 5;
 
-                    public void buttonClick(ClickEvent event) {
-                        log.log("Submitted from "
-                                + event.getButton().getParent().getCaption());
-                        t.addItem(new String[] { "added " + i++ }, i);
-                    }
-                });
+            public void buttonClick(ClickEvent event) {
+                log.log("Submitted from "
+                        + event.getButton().getParent().getCaption());
+                t.addItem(new String[] { "added " + i++ }, i);
+            }
+        });
         b.setClickShortcut(keyCode, ModifierKey.CTRL, ModifierKey.ALT);
 
         l.addComponent(t);
