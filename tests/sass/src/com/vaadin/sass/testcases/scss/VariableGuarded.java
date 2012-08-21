3d757bddfc2a07a77879fb3310e6dbbc49a1a079
Haijian Wang <haijian@vaadin.com>
2012-08-21 12:41:52 +0300

    fixed the hsl bug reported by Jouni, it is caused by the LexicalUnitImpl replace function and the updateVaule function in VariableVisitor

---
+++ tests/sass/src/com/vaadin/sass/testcases/scss/VariableGuarded.java
@@ -0,0 +1,36 @@
+package com.vaadin.sass.testcases.scss;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.w3c.css.sac.CSSException;
+
+import com.vaadin.sass.AbstractTestBase;
+import com.vaadin.sass.ScssStylesheet;
+import com.vaadin.sass.handler.SCSSDocumentHandler;
+import com.vaadin.sass.handler.SCSSDocumentHandlerImpl;
+import com.vaadin.sass.parser.Parser;
+
+public class VariableGuarded extends AbstractTestBase {
+    String scss = "/scss/var-guarded.scss";
+    String css = "/css/var-guarded.css";
+
+    @Test
+    public void testParser() throws CSSException, IOException {
+        Parser parser = new Parser();
+        SCSSDocumentHandler handler = new SCSSDocumentHandlerImpl();
+        parser.setDocumentHandler(handler);
+        parser.parseStyleSheet(getClass().getResource(scss).getPath());
+        ScssStylesheet root = handler.getStyleSheet();
+        Assert.assertEquals(4, root.getChildren().size());
+    }
+
+    @Test
+    public void testCompiler() throws Exception {
+        testCompiler(scss, css);
+        Assert.assertEquals("Original CSS and parsed CSS doesn't match",
+                comparisonCss, parsedScss);
+    }
+}
