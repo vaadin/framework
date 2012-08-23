f4f3a563942a9264fada9275991b5fd9dce73868
Leif Åstrand <leif@vaadin.com>
2012-08-23 12:12:29 +0300

    Update test case for #9274

--- tests/testbench/com/vaadin/tests/vaadincontext/BootstrapModifyRoot.java
+++ tests/testbench/com/vaadin/tests/vaadincontext/BootstrapModifyRoot.java
@@ -19,7 +19,7 @@ package com.vaadin.tests.vaadincontext;
 import com.vaadin.terminal.WrappedRequest;
 import com.vaadin.tests.components.AbstractTestRoot;
 
-public class BoostrapModifyRoot extends AbstractTestRoot {
+public class BootstrapModifyRoot extends AbstractTestRoot {
 
     @Override
     protected void setup(WrappedRequest request) {
@@ -29,7 +29,7 @@ public class BoostrapModifyRoot extends AbstractTestRoot {
 
     @Override
     protected String getTestDescription() {
-        return "There should be a static h1 in the HTML of the bootstrap page for this Root";
+        return "There should be two additional divs in the HTML of the bootstrap page for this Root";
     }
 
     @Override
