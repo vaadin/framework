55c0dfa0e7a737bab8c2486fb213499e6be88aa4
John Ahlroos <john@vaadin.com>
2012-09-06 13:30:32 +0300

    Added a really simple test application for times when you quickly need to test some ui features.

---
+++ uitest/src/com/vaadin/tests/application/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.vaadin.tests.application;
+
+import com.vaadin.server.WrappedRequest;
+import com.vaadin.ui.UI;
+
+/**
+ * A simple application to simplify doing quick testing of features when a full
+ * test case is not needed
+ */
+public class HelloWorld extends UI {
+
+    @Override
+    protected void init(WrappedRequest request) {
+
+        // Type your code here...
+    }
+}
