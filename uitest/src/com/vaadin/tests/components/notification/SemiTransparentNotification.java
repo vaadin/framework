6fc6ae8b4aa1b45f2b5c46f4e6c98574298e6472
Artur Signell <artur@vaadin.com>
2012-09-12 11:35:47 +0300

    Notification should be HTML

--- uitest/src/com/vaadin/tests/components/notification/SemiTransparentNotification.java
+++ uitest/src/com/vaadin/tests/components/notification/SemiTransparentNotification.java
@@ -1,9 +1,11 @@
 package com.vaadin.tests.components.notification;
 
+import com.vaadin.server.Page;
 import com.vaadin.tests.components.TestBase;
 import com.vaadin.tests.util.LoremIpsum;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
+import com.vaadin.ui.Notification.Type;
 
 public class SemiTransparentNotification extends TestBase {
 
@@ -12,9 +14,12 @@ public class SemiTransparentNotification extends TestBase {
         Label l = new Label(LoremIpsum.get(10000));
         getLayout().setSizeFull();
         addComponent(l);
-        Notification
-                .show("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This should be a <br/><b>SEMI-TRANSPARENT</b><br/> notification",
-                        Notification.TYPE_WARNING_MESSAGE);
+
+        Notification n = new Notification(
+                "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;This should be a <br/><b>SEMI-TRANSPARENT</b><br/> notification",
+                Type.WARNING_MESSAGE);
+        n.setHtmlContentAllowed(true);
+        n.show(Page.getCurrent());
     }
 
     @Override
