0961563fd870e9ebdc87aa676e22ebc66ff64aaa
John Alhroos <john.ahlroos@itmill.com>
2012-09-20 06:51:15 +0000

    JavaDoc #8484
    svn changeset:24497/svn branch:6.8

--- src/com/vaadin/terminal/gwt/client/ui/ShortcutActionHandler.java
+++ src/com/vaadin/terminal/gwt/client/ui/ShortcutActionHandler.java
@@ -16,6 +16,7 @@ import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.KeyboardListener;
 import com.google.gwt.user.client.ui.KeyboardListenerCollection;
 import com.google.gwt.user.client.ui.Widget;
+import com.vaadin.terminal.KeyMapper;
 import com.vaadin.terminal.gwt.client.ApplicationConnection;
 import com.vaadin.terminal.gwt.client.BrowserInfo;
 import com.vaadin.terminal.gwt.client.Container;
@@ -303,6 +304,9 @@ class ShortcutKeyCombination {
     }
 }
 
+/**
+ * Internally used class for representing shortcut actions
+ */
 class ShortcutAction {
 
     private final ShortcutKeyCombination sc;
@@ -311,11 +315,40 @@ class ShortcutAction {
     private String targetPID;
     private String targetAction;
 
+    /**
+     * Constructor
+     * 
+     * @param key
+     *            The @link {@link KeyMapper} key of the action.
+     * @param sc
+     *            The key combination that triggers the action
+     * @param caption
+     *            The caption of the action
+     */
     public ShortcutAction(String key, ShortcutKeyCombination sc,
             String caption) {
         this(key, sc, caption, null, null);
     }
 
+    /**
+     * Constructor
+     * 
+     * @param key
+     *            The @link {@link KeyMapper} key of the action.
+     * @param sc
+     *            The key combination that triggers the action
+     * @param caption
+     *            The caption of the action
+     * @param targetPID
+     *            The pid of the component the action is targeting. We use the
+     *            pid, instead of the actual Paintable here, so we can delay the
+     *            fetching of the Paintable in cases where the Paintable does
+     *            not yet exist when the action is painted.
+     * @param targetAction
+     *            The target string of the action. The target string is given to
+     *            the targeted Paintable if the paintable implements the
+     *            {@link ShortcutActionTarget} interface.
+     */
     public ShortcutAction(String key, ShortcutKeyCombination sc,
             String caption, String targetPID, String targetAction) {
         this.sc = sc;
@@ -325,24 +358,40 @@ class ShortcutAction {
         this.targetAction = targetAction;
     }
 
+    /**
+     * Get the key combination that triggers the action
+     */
     public ShortcutKeyCombination getShortcutCombination() {
         return sc;
     }
 
+    /**
+     * Get the caption of the action
+     */
     public String getCaption() {
         return caption;
     }
 
+    /**
+     * Get the {@link KeyMapper} key for the action
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     * Get the pid of the target of the action. Use
+     * {@link ApplicationConnection#getPaintable(String)} to get the actual
+     * Paintable
+     */
     public String getTargetPID() {
         return targetPID;
     }
 
+    /**
+     * Get the target string of the action
+     */
     public String getTargetAction() {
         return targetAction;
     }
-
 }
