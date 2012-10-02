3537127077043c8eeb62aa11f6bdbad635931e54
Henri Sara <hesara@vaadin.com>
2012-10-02 09:22:56 +0300

    Merge from 6.8: Large lazy container for tests
    svn changeset:24517/svn branch:6.8
    
    Change-Id: I215e0471f608bda6bee27309426a96139a147275

---
+++ uitest/src/com/vaadin/tests/util/RangeCollection.java
@@ -0,0 +1,48 @@
+package com.vaadin.tests.util;
+
+import java.util.AbstractCollection;
+import java.util.Iterator;
+
+public class RangeCollection extends AbstractCollection<Integer> {
+
+    public static class RangeIterator implements Iterator<Integer> {
+
+        private int value;
+        private int max;
+
+        public RangeIterator(int max) {
+            this.max = max;
+            value = 0;
+        }
+
+        public boolean hasNext() {
+            return (value < max - 1);
+        }
+
+        public Integer next() {
+            return value++;
+        }
+
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
+    private int size = 0;
+
+    public RangeCollection(int size) {
+        this.size = size;
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new RangeIterator(size - 1);
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+}
