31c3a458389adfbd0980b6e35e8ea1dd4f80465e
Matti Tahvonen <matti.tahvonen@itmill.com>
2012-10-08 11:42:26 +0000

    fixes #9885
    svn changeset:24781/svn branch:6.8

--- src/com/vaadin/terminal/gwt/client/ui/dd/VDragAndDropManager.java
+++ src/com/vaadin/terminal/gwt/client/ui/dd/VDragAndDropManager.java
@@ -264,7 +264,7 @@ public class VDragAndDropManager {
     }
 
     /* Singleton */
-    private VDragAndDropManager() {
+    protected VDragAndDropManager() {
     }
 
     private NativePreviewHandler defaultDragAndDropEventHandler = new DefaultDragAndDropEventHandler();
