04413150ac7ef28e50d9e207080f2a00db5a59be
Leif Åstrand <leif@vaadin.com>
2012-10-11 10:31:11 +0300

    Use class hierachy for resolving @Connect conflicts (#9826)
    Change-Id: Ic268bfb5704a5c9113dee80017196c72b5236b93

--- client-compiler/src/com/vaadin/server/widgetsetutils/metadata/ConnectorInitVisitor.java
+++ client-compiler/src/com/vaadin/server/widgetsetutils/metadata/ConnectorInitVisitor.java
@@ -4,18 +4,13 @@
 
 package com.vaadin.server.widgetsetutils.metadata;
 
-import java.util.Map;
-
 import com.google.gwt.core.ext.TreeLogger;
 import com.google.gwt.core.ext.TreeLogger.Type;
 import com.google.gwt.core.ext.UnableToCompleteException;
 import com.google.gwt.core.ext.typeinfo.JClassType;
-import com.google.gwt.dev.util.collect.HashMap;
 import com.vaadin.shared.ui.Connect;
 
 public class ConnectorInitVisitor extends TypeVisitor {
-
-    private Map<String, JClassType> processedConnections = new HashMap<String, JClassType>();
 
     @Override
     public void visitConnector(TreeLogger logger, JClassType type,
@@ -25,18 +20,6 @@ public class ConnectorInitVisitor extends TypeVisitor {
             logger.log(Type.INFO, type.getName() + " will be in the "
                     + bundle.getName().replaceAll("^_*", "") + " bundle");
             String identifier = connectAnnotation.value().getCanonicalName();
-
-            JClassType previousMapping = processedConnections.put(identifier,
-                    type);
-            if (previousMapping != null) {
-                logger.log(
-                        Type.ERROR,
-                        "Multiple @Connect mappings detected for " + identifier
-                                + ": " + type.getQualifiedSourceName()
-                                + " and "
-                                + previousMapping.getQualifiedSourceName());
-                throw new UnableToCompleteException();
-            }
 
             bundle.setIdentifier(type, identifier);
             bundle.setNeedsGwtConstructor(type);
---
+++ uitest/src/com/vaadin/tests/widgetset/client/IntermediateReplaceConnector.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2012 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.tests.widgetset.client;
+
+import com.vaadin.shared.ui.Connect;
+import com.vaadin.tests.widgetset.server.ReplaceComponent;
+
+@Connect(value = ReplaceComponent.class)
+public class IntermediateReplaceConnector extends ReplacedConnector {
+    @Override
+    protected void init() {
+        super.init();
+        getWidget().setHTML(
+                IntermediateReplaceConnector.class.getName()
+                        + ", should not be used");
+    }
+}
