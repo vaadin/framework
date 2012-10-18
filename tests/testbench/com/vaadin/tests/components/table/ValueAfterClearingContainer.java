0094c9d845fe9e0c2a799d402561e4996135419c
Leif Åstrand <leif@vaadin.com>
2012-10-18 07:53:41 +0000

    Test case for #9986
    svn changeset:24855/svn branch:6.8

---
+++ tests/testbench/com/vaadin/tests/components/table/ValueAfterClearingContainer.java
@@ -0,0 +1,98 @@
+package com.vaadin.tests.components.table;
+
+import com.vaadin.tests.components.TestBase;
+import com.vaadin.tests.util.Log;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Table;
+
+public class ValueAfterClearingContainer extends TestBase {
+
+    private static final String PROPERTY_ID = "property";
+
+    private Log log = new Log(5);
+    private final Table table = new Table();
+
+    @Override
+    protected void setup() {
+        table.setSelectable(true);
+        table.addContainerProperty(PROPERTY_ID, Integer.class, null);
+
+        addComponent(log);
+
+        addComponent(table);
+
+        addComponent(new Button("Add table items", new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                if (!table.getItemIds().isEmpty()) {
+                    getMainWindow().showNotification(
+                            "Only possible when the table is empty");
+                    return;
+                } else {
+                    for (int i = 0; i < 5; i++) {
+                        table.addItem(new Object[] { Integer.valueOf(i) },
+                                Integer.valueOf(i));
+                    }
+                }
+            }
+        }));
+
+        addComponent(new Button("Show value", new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                log.log("Table selection: " + table.getValue());
+            }
+        }));
+
+        addComponent(new Button("Remove items from table",
+                new Button.ClickListener() {
+                    public void buttonClick(ClickEvent event) {
+                        table.removeAllItems();
+                    }
+                }));
+
+        addComponent(new Button("Remove items from container",
+                new Button.ClickListener() {
+                    public void buttonClick(ClickEvent event) {
+                        table.getContainerDataSource().removeAllItems();
+                    }
+                }));
+        addComponent(new Button("Remove selected item from table",
+                new Button.ClickListener() {
+                    public void buttonClick(ClickEvent event) {
+                        Object selection = table.getValue();
+                        if (selection == null) {
+                            getMainWindow().showNotification(
+                                    "There is no selection");
+                            return;
+                        } else {
+                            table.removeItem(selection);
+                        }
+                    }
+                }));
+        addComponent(new Button("Remove selected item from container",
+                new Button.ClickListener() {
+                    public void buttonClick(ClickEvent event) {
+                        Object selection = table.getValue();
+                        if (selection == null) {
+                            getMainWindow().showNotification(
+                                    "There is no selection");
+                            return;
+                        } else {
+                            table.getContainerDataSource()
+                                    .removeItem(selection);
+                        }
+                    }
+                }));
+    }
+
+    @Override
+    protected String getDescription() {
+        return "Table value should be cleared when the selected item is removed from the container.";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return Integer.valueOf(9986);
+    }
+
+}
