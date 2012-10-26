a67d0e777a6647783eecc64950b698c064d0c029
Artur Signell <artur@vaadin.com>
2012-10-26 13:13:36 +0300

    Generic tests for GridLayout and Link
    Change-Id: If14e379a035521fe915f2ad57a58fa58dcc2379d

---
+++ uitest/src/com/vaadin/tests/components/gridlayout/GridLayoutTest.java
@@ -0,0 +1,13 @@
+package com.vaadin.tests.components.gridlayout;
+
+import com.vaadin.tests.components.AbstractLayoutTest;
+import com.vaadin.ui.GridLayout;
+
+public class GridLayoutTest extends AbstractLayoutTest<GridLayout> {
+
+    @Override
+    protected Class<GridLayout> getTestClass() {
+        return GridLayout.class;
+    }
+
+}
