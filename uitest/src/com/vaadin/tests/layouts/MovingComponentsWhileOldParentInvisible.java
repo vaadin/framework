207c76bbeebeaec71f43d1d19fba36e93e6b3351
John Ahlroos <john@vaadin.com>
2012-11-05 14:02:15 +0200

    Do not include LegacyWindow or LabelDataSource.MockUI in test
    Change-Id: I3325d472b98d3311bce222d7d1ffd17898b3194b

--- uitest/src/com/vaadin/tests/layouts/MovingComponentsWhileOldParentInvisible.java
+++ uitest/src/com/vaadin/tests/layouts/MovingComponentsWhileOldParentInvisible.java
@@ -12,6 +12,8 @@ import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.LabelDataSource;
+import com.vaadin.ui.LegacyWindow;
 import com.vaadin.ui.LoginForm;
 import com.vaadin.ui.PopupView;
 import com.vaadin.ui.UI;
@@ -45,7 +47,8 @@ public class MovingComponentsWhileOldParentInvisible extends TestBase {
             if (cls == LoginForm.class || cls == CustomLayout.class
                     || CustomComponent.class.isAssignableFrom(cls)
                     || cls == PopupView.class || cls == Window.class
-                    || cls == UI.class) {
+                    || cls == UI.class || cls == LegacyWindow.class
+                    || cls == LabelDataSource.MockUI.class) {
                 // Does not support addComponent
                 continue;
             }
