66c775a167012f288e49232116e047e19f79cab6
Artur Signell <artur@vaadin.com>
2012-11-22 09:23:52 +0200

    Fixed problem causing DragAndDropService not to receiving events (#10134)
    Change-Id: Ie9cbd37231099b8d4e30dab1dc611e19805c1fc7

--- server/src/com/vaadin/server/AbstractCommunicationManager.java
+++ server/src/com/vaadin/server/AbstractCommunicationManager.java
@@ -1799,7 +1799,8 @@ public abstract class AbstractCommunicationManager implements Serializable {
         String interfaceName = invocationJson.getString(1);
         String methodName = invocationJson.getString(2);
 
-        if (connectorTracker.getConnector(connectorId) == null) {
+        if (connectorTracker.getConnector(connectorId) == null
+                && !connectorId.equals("DD")) {
             getLogger().log(
                     Level.WARNING,
                     "RPC call to " + interfaceName + "." + methodName
