f106cb1aa629d6d9e4b522cc51ec97cb5d5f095b
Pekka Hyvönen <pekka@vaadin.com>
2012-11-23 18:00:00 +0200

    Checkstyle for Vaadin #9065
    Change-Id: I24ca9cd0646a4bc530ef3a183eef1bc4addc555d

---
+++ checkstyle/src/com/vaadin/checks/SerializableClassesCheck.java
@@ -0,0 +1,16 @@
+package com.vaadin.checks;
+
+import java.io.File;
+import java.util.List;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractFileSetCheck;
+
+public class SerializableClassesCheck extends AbstractFileSetCheck {
+
+    @Override
+    protected void processFiltered(File arg0, List<String> arg1) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
