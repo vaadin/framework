vaadin/framework

commit abe78bf793e26b70e78f80da2904ec4671bc7447

Fix for height recalculation issue for TabSheet in TabSheet (#9275)
svn changeset:24482/svn branch:6.8

src/com/vaadin/terminal/gwt/client/ApplicationConnection.java
@@ -2684,4 +2684,15 @@ public class ApplicationConnection {
         return uri;
     }
 
+    /**
+     * Removes the cached size info for the paintable. This is for really edge
+     * cases only and should normally not be called by anyone.
+     * 
+     * @param p
+     *            The paintable whose cached size info should be discarded
+     */
+    public void resetCachedSize(Paintable p) {
+        idToPaintableDetail.get(getPid(p)).setOffsetSize(null);
+        idToPaintableDetail.get(getPid(p)).setRelativeSize(null);
+    }
 }

src/com/vaadin/terminal/gwt/client/ui/VTabsheet.java
@@ -1031,6 +1031,18 @@ public class VTabsheet extends VTabsheetBase implements Focusable,
                     "visibility", "");
             previousVisibleWidget = null;
         }
+
+        /*
+         * We must ensure that the size of the TabSheet is recalculated when
+         * changing tabs. In all standard cases this is taken care of
+         * automatically but this hack is necessary in cases such as #9275 when
+         * a cached tab in an inner tab sheet is selected at the same time as
+         * the selected tab in the outer tab sheet is changed.
+         */
+        if (isDynamicHeight() || isDynamicWidth()) {
+            client.resetCachedSize(this);
+        }
+
     }
 
     @Override

tests/testbench/com/vaadin/tests/components/tabsheet/TabsheetShouldUpdateHeight.html
@@ -1,0 +1,57 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head profile="http://selenium-ide.openqa.org/profiles/test-case">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link rel="selenium.base" href="http://localhost:9999/" />
+<title>New Test</title>
+</head>
+<body>
+<table cellpadding="1" cellspacing="1" border="1">
+<thead>
+<tr><td rowspan="1" colspan="3">New Test</td></tr>
+</thead><tbody>
+<tr>
+	<td>open</td>
+	<td>/run/TabsheetShouldUpdateHeight?restartApplication</td>
+	<td></td>
+</tr>
+<tr>
+	<td>mouseClick</td>
+	<td>//div[@id='runTabsheetShouldUpdateHeight-1507936280']/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div[2]/div/div[1]/div/div[1]/table/tbody/tr/td[2]/div/div/div[1]</td>
+	<td>26,10</td>
+</tr>
+<tr>
+	<td>mouseClick</td>
+	<td>//div[@id='runTabsheetShouldUpdateHeight-1507936280']/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div[1]/table/tbody/tr/td[2]/div/div/div[1]</td>
+	<td>14,2</td>
+</tr>
+<tr>
+	<td>click</td>
+	<td>//div[@id='runTabsheetShouldUpdateHeight-1507936280']/div/div[2]/div/div[2]/div/div/div/div[2]/div/div/span/span</td>
+	<td></td>
+</tr>
+<tr>
+	<td>mouseClick</td>
+	<td>//div[@id='runTabsheetShouldUpdateHeight-1507936280']/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div[2]/div/div[1]/div/div[1]/table/tbody/tr/td[2]/div/div</td>
+	<td>16,26</td>
+</tr>
+<tr>
+	<td>mouseClick</td>
+	<td>//div[@id='runTabsheetShouldUpdateHeight-1507936280']/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div[2]/div/div[1]/div/div[1]/table/tbody/tr/td[1]/div/div/div[1]</td>
+	<td>16,9</td>
+</tr>
+<tr>
+	<td>mouseClick</td>
+	<td>//div[@id='runTabsheetShouldUpdateHeight-1507936280']/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div[2]/div/div[1]/div/div[1]/table/tbody/tr/td[2]/div/div/div[1]</td>
+	<td>20,11</td>
+</tr>
+<tr>
+	<td>screenCapture</td>
+	<td></td>
+	<td>tab3</td>
+</tr>
+
+</tbody></table>
+</body>
+</html>

tests/testbench/com/vaadin/tests/components/tabsheet/TabsheetShouldUpdateHeight.java
@@ -1,0 +1,59 @@
+package com.vaadin.tests.components.tabsheet;
+
+import com.vaadin.tests.components.TestBase;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+
+public class TabsheetShouldUpdateHeight extends TestBase {
+
+    @Override
+    public void setup() {
+        final TabSheet tabsOuter = new TabSheet();
+        final TabSheet tabsInner = new TabSheet();
+
+        final Component tab2;
+
+        tabsInner.addTab(tab2 = getLayoutWithComponents(6, "tab2"), "Tab 2");
+        tabsInner.addTab(getLayoutWithComponents(8, "tab3"), "Tab 3");
+
+        tabsOuter.addTab(tabsInner, "Inner tabs");
+        tabsOuter.addTab(getLayoutWithComponents(10, "tab1"), "Tab 1");
+
+        final Button btnSwitch = new Button("switch to Tab2",
+                new Button.ClickListener() {
+
+                    public void buttonClick(final ClickEvent inEvent) {
+                        tabsOuter.setSelectedTab(tabsInner);
+                        tabsInner.setSelectedTab(tab2);
+                    }
+                });
+
+        addComponent(tabsOuter);
+        addComponent(btnSwitch);
+    }
+
+    private VerticalLayout getLayoutWithComponents(final int inAmount, String id) {
+        final VerticalLayout v = new VerticalLayout();
+        v.setDebugId(id);
+        v.setSpacing(true);
+        v.setMargin(true);
+        for (int i = 0; i < inAmount; i++) {
+            v.addComponent(new TextField("Text field:"));
+        }
+        return v;
+    }
+
+    @Override
+    protected String getDescription() {
+        return "click with mouse first on tab 3 and then on tab 1. now click on the button 'switch to tab2'. then click on tab 3 again and the scrollbars appear";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 9275;
+    }
+}

commit 1d175f8e3251360d2780d874122bc9be18ebcc98

Fixed resizing of TabSheet (#10037)
Regression introduced by #9275

svn changeset:25139/svn branch:6.8

src/com/vaadin/terminal/gwt/client/ApplicationConnection.java
@@ -2693,6 +2693,5 @@ public class ApplicationConnection {
      */
     public void resetCachedSize(Paintable p) {
         idToPaintableDetail.get(getPid(p)).setOffsetSize(null);
-        idToPaintableDetail.get(getPid(p)).setRelativeSize(null);
     }
 }