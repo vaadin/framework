860876d311fdc42174590ed816b0b09f243362e6
John Ahlroos <john@vaadin.com>
2013-02-28 12:07:35 +0200

    Fixed BindException when binding nested properties with BeanFieldGroup.buildAndBind() #11009
    Change-Id: I9bf646b7d8b767c78506a62185ce5dca9dcae7ad

---
+++ server/tests/src/com/vaadin/tests/server/component/fieldgroup/BeanFieldGroupTest.java
@@ -0,0 +1,41 @@
+package com.vaadin.tests.server.component.fieldgroup;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import com.vaadin.data.fieldgroup.BeanFieldGroup;
+
+public class BeanFieldGroupTest {
+
+    public static class MyBean {
+
+        private MyNestedBean nestedBean = new MyNestedBean();
+
+        public MyNestedBean getNestedBean() {
+            return nestedBean;
+        }
+    }
+
+    public static class MyNestedBean {
+
+        private String hello = "Hello world";
+
+        public String getHello() {
+            return hello;
+        }
+    }
+
+    @Test
+    public void buildAndBindNestedProperty() {
+
+        MyBean bean = new MyBean();
+
+        BeanFieldGroup<MyBean> bfg = new BeanFieldGroup<MyBean>(MyBean.class);
+        bfg.setItemDataSource(bean);
+
+        com.vaadin.ui.Field<?> helloField = bfg.buildAndBind("Hello string",
+                "nestedBean.hello");
+        assertEquals(bean.nestedBean.hello, helloField.getValue().toString());
+    }
+
+}
