48b5c927b9e4e614e7cc334fb9c8b0bed42d28c8
Artur Signell <artur@vaadin.com>
2013-03-04 20:06:00 +0200

    Catch NoSuchFieldException instead of NoSuchFieldError (#10944)
    Change-Id: I0ece22fc53388a7c9eb268c00ae46c589859a1f1

--- server/src/com/vaadin/data/fieldgroup/BeanFieldGroup.java
+++ server/src/com/vaadin/data/fieldgroup/BeanFieldGroup.java
@@ -72,7 +72,7 @@ public class BeanFieldGroup<T> extends FieldGroup {
                 java.lang.reflect.Field field1 = cls
                         .getDeclaredField(propertyId);
                 return field1;
-            } catch (NoSuchFieldError e) {
+            } catch (NoSuchFieldException e) {
                 // Try super classes until we reach Object
                 Class<?> superClass = cls.getSuperclass();
                 if (superClass != Object.class) {
