3d311b6c03fd7f883d0a8e3f7358c0633468d01b
Leif Åstrand <leif@vaadin.com>
2013-04-09 11:49:20 +0300

    Fix missing or invalid copyright headers
    Change-Id: I75e8713548a93b29fe97ab6e9aaf8cb65ab611be

--- server/src/com/vaadin/ui/components/calendar/event/CalendarEditableEventProvider.java
+++ server/src/com/vaadin/ui/components/calendar/event/CalendarEditableEventProvider.java
@@ -12,7 +12,9 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- */package com.vaadin.ui.components.calendar.event;
+ */
+
+package com.vaadin.ui.components.calendar.event;
 
 /**
  * An event provider which allows adding and removing events
