f07f8711c9a93714f8298d3db29bd8a280ba6646
James Lefeu <James.Lefeu@liferay.com>
2013-07-24 09:28:28 -0500

    Stylesheet changes

--- theme-compiler/src/com/vaadin/sass/internal/ScssStylesheet.java
+++ theme-compiler/src/com/vaadin/sass/internal/ScssStylesheet.java
@@ -39,6 +39,7 @@ import com.vaadin.sass.internal.resolver.ScssStylesheetResolver;
 import com.vaadin.sass.internal.resolver.VaadinResolver;
 import com.vaadin.sass.internal.tree.BlockNode;
 import com.vaadin.sass.internal.tree.MixinDefNode;
+import com.vaadin.sass.internal.tree.FunctionDefNode;
 import com.vaadin.sass.internal.tree.Node;
 import com.vaadin.sass.internal.tree.VariableNode;
 import com.vaadin.sass.internal.tree.controldirective.IfElseDefNode;
@@ -54,6 +55,8 @@ public class ScssStylesheet extends Node {
     private static final HashMap<String, VariableNode> variables = new HashMap<String, VariableNode>();
 
     private static final Map<String, MixinDefNode> mixinDefs = new HashMap<String, MixinDefNode>();
+
+    private static final Map<String, FunctionDefNode> functionDefs = new HashMap<String, FunctionDefNode>();
 
     private static final HashSet<IfElseDefNode> ifElseDefNodes = new HashSet<IfElseDefNode>();
 
@@ -179,6 +182,7 @@ public class ScssStylesheet extends Node {
     public void compile() throws Exception {
         mainStyleSheet = this;
         mixinDefs.clear();
+        functionDefs.clear();
         variables.clear();
         ifElseDefNodes.clear();
         lastNodeAdded.clear();
@@ -194,7 +198,9 @@ public class ScssStylesheet extends Node {
     }
 
     private void populateDefinitions(Node node) {
-        if (node instanceof MixinDefNode) {
+        if (node instanceof FunctionDefNode) {
+            functionDefs.put(((FunctionDefNode) node).getName(), (FunctionDefNode) node);
+        } else if (node instanceof MixinDefNode) {
             mixinDefs.put(((MixinDefNode) node).getName(), (MixinDefNode) node);
             node.getParentNode().removeChild(node);
         } else if (node instanceof IfElseDefNode) {
@@ -354,6 +360,10 @@ public class ScssStylesheet extends Node {
 
     public static MixinDefNode getMixinDefinition(String name) {
         return mixinDefs.get(name);
+    }
+
+    public static FunctionDefNode getFunctionDefinition(String name) {
+        return functionDefs.get(name);
     }
 
     public void setFileName(String fileName) {
