cdb9a49bd542fe28ed29c52b90ee9875e2556f67
Johannes Dahlström <johannesd@vaadin.com>
2013-09-10 15:36:51 +0300

    Enable native scrolling in all iOS versions newer than 5, not just iOS 6 (#12324)
    Change-Id: I8f9ec7f34aefa5b48fd23f3f000f9455475edefa

--- client/src/com/vaadin/client/BrowserInfo.java
+++ client/src/com/vaadin/client/BrowserInfo.java
@@ -207,8 +207,8 @@ public class BrowserInfo {
             return prefix + OS_ANDROID;
         } else if (browserDetails.isIOS()) {
             String iosClass = prefix + OS_IOS;
-            if (isIOS6()) {
-                iosClass += " " + prefix + OS_IOS + "6";
+            if (getOperatingSystemMajorVersion() == 5) {
+                iosClass += " " + prefix + OS_IOS + "5";
             }
             return iosClass;
         } else if (browserDetails.isWindows()) {
@@ -392,9 +392,9 @@ public class BrowserInfo {
         if (isAndroid() && isWebkit() && getWebkitVersion() >= 534) {
             return false;
         }
-        // iOS 6 Safari supports native scrolling; iOS 5 suffers from #8792
+        // iOS 6+ Safari supports native scrolling; iOS 5 suffers from #8792
         // TODO Should test other iOS browsers
-        if (isIOS6() && isWebkit()) {
+        if (isIOS() && isWebkit() && getOperatingSystemMajorVersion() >= 6) {
             return false;
         }
         return true;
