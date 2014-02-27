fbce1270b1ea9260a6bb1842f5a5747d173b6f0a
Henri Sara <hesara@vaadin.com>
2014-02-27 10:24:38 +0200

    Use Chrome 33 for testing
    Change-Id: I6362097ff836c10b7001c5fd8acd8adf91a94b2f

--- uitest/src/com/vaadin/tests/tb3/MultiBrowserTest.java
+++ uitest/src/com/vaadin/tests/tb3/MultiBrowserTest.java
@@ -41,7 +41,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public abstract class MultiBrowserTest extends PrivateTB3Configuration {
 
     public enum Browser {
-        FIREFOX(BrowserUtil.firefox(24)), CHROME(BrowserUtil.chrome(29)), SAFARI(
+        FIREFOX(BrowserUtil.firefox(24)), CHROME(BrowserUtil.chrome(33)), SAFARI(
                 BrowserUtil.safari(7)), IE8(BrowserUtil.ie(8)), IE9(BrowserUtil
                 .ie(9)), IE10(BrowserUtil.ie(10)), IE11(BrowserUtil.ie(11)), OPERA(
                 BrowserUtil.opera(17));
