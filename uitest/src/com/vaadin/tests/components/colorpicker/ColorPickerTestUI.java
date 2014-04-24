3b420700c1a282cb8a6620299484cc1c002fbb3b
John Ahlroos <john@vaadin.com>
2014-04-24 21:12:15 +0300

    Allow editing colorpicker values in colorpicker in textfield #13469
    This fix adds support for typing in color values according to
    http://www.w3schools.com/cssref/css_colors_legal.asp into the
    colorpicker popup input textfield.
    
    Change-Id: If14ead791725c3052c05aa31e12e237e90c32348

--- uitest/src/com/vaadin/tests/components/colorpicker/ColorPickerTestUI.java
+++ uitest/src/com/vaadin/tests/components/colorpicker/ColorPickerTestUI.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2014 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.vaadin.tests.components.colorpicker;
 
 import java.awt.Graphics;
@@ -13,9 +28,10 @@ import javax.imageio.ImageIO;
 
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.server.StreamResource;
+import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.colorpicker.Color;
 import com.vaadin.shared.ui.label.ContentMode;
-import com.vaadin.tests.components.TestBase;
+import com.vaadin.tests.components.AbstractTestUI;
 import com.vaadin.ui.AbstractColorPicker;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.CheckBox;
@@ -30,10 +46,11 @@ import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.components.colorpicker.ColorChangeEvent;
 import com.vaadin.ui.components.colorpicker.ColorChangeListener;
 
-public class ColorPickerTest extends TestBase implements ColorChangeListener {
+public class ColorPickerTestUI extends AbstractTestUI implements
+        ColorChangeListener {
 
     @Override
-    protected String getDescription() {
+    public String getTestDescription() {
         return "Vaadin 7 compatible ColorPicker";
     }
 
@@ -178,7 +195,7 @@ public class ColorPickerTest extends TestBase implements ColorChangeListener {
     }
 
     @Override
-    protected void setup() {
+    protected void setup(VaadinRequest request) {
         getLayout().setWidth("1000px");
         getLayout().setHeight(null);
         getLayout().addStyleName("colorpicker-mainwindow-content");
