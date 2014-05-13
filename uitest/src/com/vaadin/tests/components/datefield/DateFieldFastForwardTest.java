7dbe71a99b002b2385f1e1bdc69ea4bd1c6a4503
Teemu Suo-Anttila <teemusa@vaadin.com>
2014-05-13 17:05:26 +0300

    Fix DateFieldFastForwardTest to close browser context menu
    Change-Id: Ib8c6e45a8f77630e71b6f5264d6d42dc5efe922d

--- uitest/src/com/vaadin/tests/components/datefield/DateFieldFastForwardTest.java
+++ uitest/src/com/vaadin/tests/components/datefield/DateFieldFastForwardTest.java
@@ -17,11 +17,13 @@ package com.vaadin.tests.components.datefield;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
+import com.vaadin.testbench.elements.VerticalLayoutElement;
 import com.vaadin.tests.tb3.MultiBrowserTest;
 
 public class DateFieldFastForwardTest extends MultiBrowserTest {
@@ -29,20 +31,27 @@ public class DateFieldFastForwardTest extends MultiBrowserTest {
     @Test
     public void testFastForwardOnRightMouseClick() throws Exception {
         openTestURL();
-
+        String firstMonth = getSelectedMonth();
         WebElement nextMonthButton = driver.findElement(By
                 .className("v-button-nextmonth"));
 
         // Click and hold left mouse button to start fast forwarding.
         new Actions(driver).clickAndHold(nextMonthButton).perform();
-        Thread.sleep(1000);
+        sleep(1000);
 
         // Right click and release the left button.
+
         new Actions(driver).contextClick(nextMonthButton)
                 .release(nextMonthButton).perform();
 
         // Now the fast forwarding should be ended, get the expected month.
         String expectedMonth = getSelectedMonth();
+
+        // Make browser context menu disappear, since it will crash IE
+        $(VerticalLayoutElement.class).first().click();
+
+        Assert.assertFalse("Month did not change during fast forward",
+                firstMonth.equals(expectedMonth));
 
         // Wait for a while.
         Thread.sleep(1000);
