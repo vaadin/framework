13ddd2b03c99b563b131c115982ac09453ca749f
Teemu Suo-Anttila <teemusa@vaadin.com>
2014-05-19 11:17:57 +0300

    Move ComboBox tests to correct package
    Change-Id: I2ad5164a637b2b4c96b46f30df036ccbc5cbcd66

--- uitest/src/com/vaadin/tests/components/combobox/ComboboxStyleChangeWidthTest.java
+++ uitest/src/com/vaadin/tests/components/combobox/ComboboxStyleChangeWidthTest.java
@@ -13,7 +13,7 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.tests.components.ui;
+package com.vaadin.tests.components.combobox;
 
 import static org.junit.Assert.assertEquals;
 
