b64e6d302a8e870f47e5fd2b086103cb5894400e
Dmitrii Rogozin <dmitrii@vaadin.com>
2014-05-22 14:04:48 +0300

    Removes double spacing from gridLayout which has empty rows or columns (#8855)
    If row has no elements or only invisible elements, its size will be set to zero.
    
     When row expand ratio was set, its size will be assigned to the value according to an expand ratio.
    
    If component takes several rows of the gridLayout, these rows are considered as non-empty and won't be removed.
    
    Change-Id: I10ddd22a6c9535b9978769bab7b496e11a28b78a

---
+++ uitest/src/com/vaadin/tests/components/gridlayout/GridLayoutHideMiddleCellsTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2000-2013 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.tests.components.gridlayout;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.testbench.elements.ButtonElement;
+import com.vaadin.testbench.elements.GridLayoutElement;
+import com.vaadin.tests.tb3.MultiBrowserTest;
+
+public class GridLayoutHideMiddleCellsTest extends MultiBrowserTest {
+    @Test
+    public void gridLayoutInvisibleElementsTest() {
+        openTestURL();
+        GridLayoutElement gridLayout5x5 = $(GridLayoutElement.class).get(0);
+        GridLayoutElement gridLayout4x4 = $(GridLayoutElement.class).get(1);
+        ButtonElement hidingButton = $(ButtonElement.class).get(0);
+        hidingButton.click();
+        List<WebElement> slots5x5 = gridLayout5x5.findElements(By
+                .className("v-gridlayout-slot"));
+        List<WebElement> slots4x4 = gridLayout4x4.findElements(By
+                .className("v-gridlayout-slot"));
+        assertEquals("Different amount of slots", slots5x5.size(),
+                slots4x4.size());
+
+        for (int i = 0; i < slots5x5.size(); i++) {
+            assertEquals("Different left coordinate for element " + i, slots5x5
+                    .get(i).getCssValue("left"),
+                    slots4x4.get(i).getCssValue("left"));
+        }
+        for (int i = 0; i < slots5x5.size(); i++) {
+            assertEquals("Different top coordinate for element " + i, slots5x5
+                    .get(i).getCssValue("top"),
+                    slots4x4.get(i).getCssValue("top"));
+        }
+    }
+
+}
