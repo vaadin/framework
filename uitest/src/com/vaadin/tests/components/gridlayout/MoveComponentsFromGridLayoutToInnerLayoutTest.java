3e925629ae50281dcbf4af00b0bbe3cfa05164ae
Sauli Tähkäpää <sauli@vaadin.com>
2014-06-13 09:32:47 +0300

    Convert MoveComponentsFromGridLayoutToInnerLayoutTest to TB4. (#8855)
    Change-Id: I90079bad38a82d8a7d3b053daedc99e52633e027

---
+++ uitest/src/com/vaadin/tests/components/gridlayout/MoveComponentsFromGridLayoutToInnerLayoutTest.java
@@ -0,0 +1,22 @@
+package com.vaadin.tests.components.gridlayout;
+
+import com.vaadin.testbench.elements.ButtonElement;
+import com.vaadin.tests.tb3.AbstractTB3Test;
+import com.vaadin.tests.tb3.MultiBrowserTest;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class MoveComponentsFromGridLayoutToInnerLayoutTest extends MultiBrowserTest {
+
+    @Test
+    public void buttonIsMovedInsideInnerLayout() throws IOException {
+        openTestURL();
+
+        $(ButtonElement.class).first().click();
+
+        compareScreen("buttonClicked");
+    }
+}
