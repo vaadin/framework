d0ad91d88c57a159070a1f5d1ff773a07e126871
Felype Santiago Ferreira <felype@vaadin.com>
2014-07-07 17:25:48 +0300

    Adds a scrollbar to table column drop down (#14156).
    Change-Id: I7ce7c61f842a09af1842a14fedec3412120944c7

---
+++ uitest/src/com/vaadin/tests/components/table/TableTooManyColumnsTest.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2000-2014 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.tests.components.table;
+
+import java.io.IOException;
+
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.testbench.By;
+import com.vaadin.testbench.commands.TestBenchElementCommands;
+import com.vaadin.tests.tb3.MultiBrowserTest;
+
+public class TableTooManyColumnsTest extends MultiBrowserTest {
+
+    @Test
+    public void testDropdownTable() throws IOException {
+        openTestURL();
+
+        WebElement element = findElement(By
+                .className("v-table-column-selector"));
+
+        element.click();
+
+        WebElement menu = findElement(By.className("gwt-MenuBar-vertical"));
+
+        TestBenchElementCommands scrollable = testBenchElement(menu);
+        scrollable.scroll(3000);
+
+        compareScreen(getScreenshotBaseName());
+    }
+
+}
