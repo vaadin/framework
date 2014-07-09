8027e4e78b840313925e5672de09fe87422dc904
Johannes Tuikkala <johannes@vaadin.com>
2014-07-09 14:44:03 +0300

    Fix Tabsheet scrollbutton by css position in Chameleon theme (#12154)
    Change-Id: Icd38631f8802fc86949fa29dd517f1aca0499c33

---
+++ uitest/src/com/vaadin/tests/components/tabsheet/TabsheetNotEnoughHorizontalSpaceTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2000-2014 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.tests.components.tabsheet;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.vaadin.testbench.By;
+import com.vaadin.tests.tb3.MultiBrowserTest;
+
+/**
+ * Tests that tabsheet's scroll button are rendered correctly in Chameleon
+ * theme.
+ * 
+ * Ticket #12154
+ * 
+ * @since
+ * @author Vaadin Ltd
+ */
+public class TabsheetNotEnoughHorizontalSpaceTest extends MultiBrowserTest {
+
+    @Test
+    public void testThatTabScrollButtonsAreRenderedCorrectly()
+            throws IOException {
+        openTestURL();
+
+        driver.findElement(By.className("v-tabsheet-scrollerPrev-disabled"));
+        driver.findElement(By.className("v-tabsheet-scrollerNext"));
+
+        compareScreen(getScreenshotBaseName());
+    }
+
+}
