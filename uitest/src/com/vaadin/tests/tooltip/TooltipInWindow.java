a84a2a6d27c35e4b176dc9b3433a824263ea14d0
Juuso Valli <juuso@vaadin.com>
2014-07-17 13:24:50 +0300

    Alter TooltipInWindowTest to inherit from TooltipTest (#14240)
    Change-Id: I27c0a236d4dd654c1cf8d567752af9d1ea3c1de5

--- uitest/src/com/vaadin/tests/tooltip/TooltipInWindow.java
+++ uitest/src/com/vaadin/tests/tooltip/TooltipInWindow.java
@@ -1,12 +1,12 @@
-/* 
+/*
  * Copyright 2000-2014 Vaadin Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -14,7 +14,7 @@
  * the License.
  */
 
-package com.vaadin.tests.components.window;
+package com.vaadin.tests.tooltip;
 
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.tests.components.AbstractTestUI;
@@ -41,9 +41,6 @@ public class TooltipInWindow extends AbstractTestUI {
         TextField tf = new TextField("TextField with a tooltip");
         tf.setDescription("My tooltip");
         tf.setId(id);
-        getTooltipConfiguration().setOpenDelay(0);
-        getTooltipConfiguration().setQuickOpenDelay(0);
-        getTooltipConfiguration().setCloseTimeout(1000);
         return tf;
     }
 
@@ -54,7 +51,7 @@ public class TooltipInWindow extends AbstractTestUI {
 
     @Override
     protected Integer getTicketNumber() {
-        return Integer.valueOf(9172);
+        return 9172;
     }
 
 }
