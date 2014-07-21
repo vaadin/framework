1b218a1f9f4713d23b8563acd6257fb912afcc15
Juuso Valli <juuso@vaadin.com>
2014-07-21 09:14:06 +0300

    Remove obsolete TB2 tooltip test, update TB4 test (#14019)
    Change-Id: I02b68886b888ee0a0d99b41138755f0ea6e2a53d

--- uitest/src/com/vaadin/tests/tooltip/ButtonTooltipsTest.java
+++ uitest/src/com/vaadin/tests/tooltip/ButtonTooltipsTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2000-2014 Vaadin Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -26,7 +26,7 @@ import com.vaadin.tests.tb3.TooltipTest;
 
 /**
  * Tests that tooltip sizes do not change when moving between adjacent elements
- * 
+ *
  * @author Vaadin Ltd
  */
 public class ButtonTooltipsTest extends TooltipTest {
@@ -38,12 +38,12 @@ public class ButtonTooltipsTest extends TooltipTest {
         WebElement buttonOne = $(ButtonElement.class).caption("One").first();
         WebElement buttonTwo = $(ButtonElement.class).caption("Two").first();
 
-        checkTooltip(buttonOne, "long descidescidescpription");
+        checkTooltip(buttonOne, ButtonTooltips.longDescription);
         int originalWidth = getTooltipElement().getSize().getWidth();
         int originalHeight = getTooltipElement().getSize().getHeight();
 
         clearTooltip();
-        checkTooltip(buttonTwo, "Another");
+        checkTooltip(buttonTwo, ButtonTooltips.shortDescription);
         moveMouseTo(buttonOne, 5, 5);
         sleep(100);
         assertThat(getTooltipElement().getSize().getWidth(), is(originalWidth));
