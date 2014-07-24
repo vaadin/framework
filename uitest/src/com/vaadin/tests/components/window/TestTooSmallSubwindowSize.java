358583738aaf0def7b827e9fbe042e008dbdf8cd
Anna Koskinen <anna@vaadin.com>
2014-07-24 18:06:16 +0300

    Upgraded TestTooSmallSubwindowSize from TB2 (#14292)
    Change-Id: I351e94c6827e4585ddeb0060251f9ba7c5c55df4

--- uitest/src/com/vaadin/tests/components/window/TestTooSmallSubwindowSize.java
+++ uitest/src/com/vaadin/tests/components/window/TestTooSmallSubwindowSize.java
@@ -1,15 +1,22 @@
 package com.vaadin.tests.components.window;
 
-import com.vaadin.tests.components.TestBase;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.tests.components.AbstractTestUI;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
-public class TestTooSmallSubwindowSize extends TestBase {
+/**
+ * Tests that the styles work correctly in tiny subwindows that have more
+ * content than can fit.
+ * 
+ * @author Vaadin Ltd
+ */
+public class TestTooSmallSubwindowSize extends AbstractTestUI {
 
     @Override
-    protected String getDescription() {
-        return "The size of the subwindow (outer size) is set to 60x60 pixels. Minimum size for the content area is 150x100, which means the window and shadow should be around 155x155 and the content area 150x100. The decoration at the lower left corner of the window must not be missing either.";
+    protected String getTestDescription() {
+        return "The size of the subwindows (outer size) is set to 60x60 pixels. Make sure the shadows fits the windows instead of the contents. The decorations at the lower right corners of the resizable windows must not be missing either.";
     }
 
     @Override
@@ -18,7 +25,14 @@ public class TestTooSmallSubwindowSize extends TestBase {
     }
 
     @Override
-    protected void setup() {
+    protected void setup(VaadinRequest request) {
+        getUI().addWindow(createNonResizableWindow());
+        getUI().addWindow(createNonResizableWindowWithHorizontalScrollbar());
+        getUI().addWindow(createResizableWindow());
+        getUI().addWindow(createResizableWindowWithHorizontalScrollbar());
+    }
+
+    private Window createNonResizableWindow() {
         VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
         Window w = new Window("Scroll", layout);
@@ -35,13 +49,94 @@ public class TestTooSmallSubwindowSize extends TestBase {
         w.setPositionY(100);
 
         // Set window size
-        w.setWidth(60, Window.UNITS_PIXELS);
-        w.setHeight(60, Window.UNITS_PIXELS);
+        w.setWidth(60, Unit.PIXELS);
+        w.setHeight(60, Unit.PIXELS);
 
         // Disable resizing
-        w.setResizable(true);
+        w.setResizable(false);
 
-        getMainWindow().addWindow(w);
+        return w;
+    }
+
+    private Window createNonResizableWindowWithHorizontalScrollbar() {
+        VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+        Window w = new Window("Scroll", layout);
+        Label desc = new Label(
+                "This is a new child window with a preset"
+                        + " width, height and position. Resizing has been"
+                        + " disabled for this window. Additionally, this text label"
+                        + " is intentionally too large to fit the window. You could"
+                        + " use the scrollbars to view different parts of the window content,"
+                        + " except it's too small for that either.");
+        // disable wrapping
+        desc.setSizeUndefined();
+        layout.addComponent(desc);
+
+        // Set window position
+        w.setPositionX(200);
+        w.setPositionY(100);
+
+        // Set window size
+        w.setWidth(60, Unit.PIXELS);
+        w.setHeight(60, Unit.PIXELS);
+
+        // Disable resizing
+        w.setResizable(false);
+
+        return w;
+    }
+
+    private Window createResizableWindow() {
+        VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+        Window w = new Window("Resize", layout);
+        Label desc = new Label(
+                "This is a new child window with a preset"
+                        + " width, height and position. Resizing has not been"
+                        + " disabled for this window. Additionally, this text label"
+                        + " is intentionally too large to fit the window. You can resize or"
+                        + " use the scrollbars to view different parts of the window content.");
+        layout.addComponent(desc);
+
+        // Set window position
+        w.setPositionX(300);
+        w.setPositionY(100);
+
+        // Set window size
+        w.setWidth(60, Unit.PIXELS);
+        w.setHeight(60, Unit.PIXELS);
+
+        // Don't disable resizing
+
+        return w;
+    }
+
+    private Window createResizableWindowWithHorizontalScrollbar() {
+        VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+        Window w = new Window("Resize", layout);
+        Label desc = new Label(
+                "This is a new child window with a preset"
+                        + " width, height and position. Resizing has not been"
+                        + " disabled for this window. Additionally, this text label"
+                        + " is intentionally too large to fit the window. You can resize"
+                        + " to view different parts of the window content.");
+        // disable wrapping
+        desc.setSizeUndefined();
+        layout.addComponent(desc);
+
+        // Set window position
+        w.setPositionX(400);
+        w.setPositionY(100);
+
+        // Set window size
+        w.setWidth(60, Unit.PIXELS);
+        w.setHeight(60, Unit.PIXELS);
+
+        // Don't disable resizing
+
+        return w;
     }
 
 }
