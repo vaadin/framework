588b52dfbe4823ac6d752bafece7e2aa0870960c
Sauli Tähkäpää <sauli@vaadin.com>
2014-09-12 16:10:48 +0300

    Convert TableScrollsOnSelectionTest to TB4.
    Change-Id: I27b8edbd0761771054dc4e1b0b6b2559957da0c6

--- uitest/src/com/vaadin/tests/components/table/LongMultiselectTest.java
+++ uitest/src/com/vaadin/tests/components/table/LongMultiselectTest.java
@@ -4,15 +4,11 @@ import com.vaadin.testbench.elements.ButtonElement;
 import com.vaadin.testbench.elements.TableElement;
 import com.vaadin.tests.tb3.MultiBrowserTest;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -60,22 +56,7 @@ public class LongMultiselectTest extends MultiBrowserTest {
     }
 
     private void scrollToBottom() {
-        testBenchElement(getTable().findElement(By.className("v-scrollable"))).scroll(ROWCOUNT * 30);
-
-        waitUntilRowIsVisible(LASTSELECTEDROW);
-    }
-
-    private void waitUntilRowIsVisible(final int row) {
-        waitUntil(new ExpectedCondition<Object>() {
-            @Override
-            public Object apply(WebDriver input) {
-                try {
-                   return getTable().getCell(row, 0) != null;
-                } catch (NoSuchElementException e) {
-                    return false;
-                }
-            }
-        });
+        scrollTable(getTable(), ROWCOUNT, LASTSELECTEDROW);
     }
 
 }
