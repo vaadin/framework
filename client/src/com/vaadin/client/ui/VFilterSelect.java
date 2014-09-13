c3bf85412214d56bbbaf19a04ff1b7012e6428f6
Denis Anisimov <denis@vaadin.com>
2014-09-13 21:51:29 +0300

    Open combobox popup on click to icon (#14624).
    Change-Id: I203b92561ab14d427466945845c6fbb62d6eb07b

--- client/src/com/vaadin/client/ui/VFilterSelect.java
+++ client/src/com/vaadin/client/ui/VFilterSelect.java
@@ -1140,6 +1140,7 @@ public class VFilterSelect extends Composite implements Field, KeyDownHandler,
     private class IconWidget extends Widget {
         IconWidget(Icon icon) {
             setElement(icon.getElement());
+            addDomHandler(VFilterSelect.this, ClickEvent.getType());
         }
     }
 
