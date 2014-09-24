0404f7f5ff0c70e4d2bac6abc7d54ad1910090f8
Teemu Pöntelin <teemu@vaadin.com>
2014-09-24 22:48:55 +0300

    Fix ComboBox issue with font-based item icons (#14660)
    Change-Id: I8f3de9556cc62670c28523886998f89383b76305

--- uitest/src/com/vaadin/tests/fonticon/FontIconsTest.java
+++ uitest/src/com/vaadin/tests/fonticon/FontIconsTest.java
@@ -15,9 +15,14 @@
  */
 package com.vaadin.tests.fonticon;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 
 import com.vaadin.tests.tb3.MultiBrowserTest;
 
@@ -27,5 +32,34 @@ public class FontIconsTest extends MultiBrowserTest {
     public void checkScreenshot() throws IOException {
         openTestURL();
         compareScreen("all");
+    }
+
+    @Test
+    public void comboBoxItemIconsOnKeyboardNavigation() throws Exception {
+        openTestURL();
+        WebElement comboBoxInput = getDriver().findElement(
+                By.className("v-filterselect-input"));
+
+        // No initial value.
+        assertEquals("", comboBoxInput.getText());
+
+        // Navigate to the first item with keyboard navigation.
+        sendKeys(comboBoxInput, Keys.ARROW_DOWN, Keys.ARROW_DOWN,
+                Keys.ARROW_DOWN);
+
+        // Value must be "One" without any extra characters.
+        // See ticket #14660
+        assertEquals("One", comboBoxInput.getAttribute("value"));
+
+        // Check also the second item.
+        sendKeys(comboBoxInput, Keys.ARROW_DOWN);
+        assertEquals("Two", comboBoxInput.getAttribute("value"));
+    }
+
+    private void sendKeys(WebElement element, Keys... keys) throws Exception {
+        for (Keys key : keys) {
+            element.sendKeys(key);
+            sleep(10); // For PhantomJS.
+        }
     }
 }
