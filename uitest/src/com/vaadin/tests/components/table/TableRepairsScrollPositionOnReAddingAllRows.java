9bc7760539339b3be38ebc63d137b16a83d7afe5
Anna Miroshnik <anna.miroshnik@arcadia.spb.ru>
2014-10-02 16:02:55 +0400

    Re-adding content in Table causes table to lose scroll position (#14581)
    At this moment behavior of restoring scroll position more like as in
    7.2.6. But restoring is only in case of removeAll() - addAll() (or add()
    one time - then restore index is reset).
    
    In 7.2.6 restoring of scroll position was the result of client defect
    (scrolling in lazyScroller was not changed if variable "firstvisible"
    fromserver was 0). This "defect" was fixed in one of the patches.
    
    Change-Id: I2e2fb8749ec95f3409caeacafff46c4c29159e74

--- uitest/src/com/vaadin/tests/components/table/TableRepairsScrollPositionOnReAddingAllRows.java
+++ uitest/src/com/vaadin/tests/components/table/TableRepairsScrollPositionOnReAddingAllRows.java
@@ -11,6 +11,12 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Table;
 
+/**
+ * Scroll position should be restored when removing and re-adding all rows in
+ * Table.
+ * 
+ * @author Vaadin Ltd
+ */
 public class TableRepairsScrollPositionOnReAddingAllRows extends AbstractTestUI {
 
     private static final long serialVersionUID = 1L;
@@ -19,32 +25,130 @@ public class TableRepairsScrollPositionOnReAddingAllRows extends AbstractTestUI 
     protected void setup(VaadinRequest request) {
         final BeanItemContainer<TableItem> cont = new BeanItemContainer<TableItem>(
                 TableItem.class);
-        final List<TableItem> itemList = new ArrayList<TableItem>();
-
-        Button button1 = new Button("ReAdd rows");
-        button1.setId("button1");
-        button1.addClickListener(new ClickListener() {
-
-            @Override
-            public void buttonClick(com.vaadin.ui.Button.ClickEvent event) {
-                cont.removeAllItems();
-                cont.addAll(itemList);
-            }
-        });
-
-        for (int i = 0; i < 80; i++) {
-            TableItem ti = new TableItem();
-            ti.setName("Name_" + i);
-            itemList.add(ti);
-            cont.addBean(ti);
-        }
+        final List<TableItem> restoringItemList = new ArrayList<TableItem>();
 
         final Table table = new Table();
+        table.setWidth("400px");
         table.setPageLength(-1);
         table.setContainerDataSource(cont);
         table.setSelectable(true);
 
-        getLayout().addComponent(button1);
+        Button buttonRestore = new Button("Restore table rows");
+        buttonRestore.setId("buttonRestore");
+        buttonRestore.addClickListener(new ClickListener() {
+
+            @Override
+            public void buttonClick(com.vaadin.ui.Button.ClickEvent event) {
+                cont.removeAllItems();
+                cont.addAll(restoringItemList);
+            }
+        });
+
+        Button buttonReAddAllViaAddAll = new Button("Re-add rows all at once");
+        buttonReAddAllViaAddAll.setId("buttonReAddAllViaAddAll");
+        buttonReAddAllViaAddAll.addClickListener(new ClickListener() {
+
+            @Override
+            public void buttonClick(com.vaadin.ui.Button.ClickEvent event) {
+                List<TableItem> originalItemIds = new ArrayList<TableItem>(cont
+                        .getItemIds());
+                cont.removeAllItems();
+                cont.addAll(originalItemIds);
+            }
+        });
+
+        Button buttonReplaceByAnotherCollectionViaAddAll = new Button(
+                "Replace by another items (via addAll())");
+        buttonReplaceByAnotherCollectionViaAddAll
+                .setId("buttonReplaceByAnotherCollectionViaAddAll");
+        buttonReplaceByAnotherCollectionViaAddAll
+                .addClickListener(new ClickListener() {
+
+                    @Override
+                    public void buttonClick(
+                            com.vaadin.ui.Button.ClickEvent event) {
+                        cont.removeAllItems();
+                        // create new collection (of different items) with other
+                        // size
+                        List<TableItem> itemList = new ArrayList<TableItem>();
+                        for (int i = 0; i < 79; i++) {
+                            TableItem ti = new TableItem();
+                            ti.setName("AnotherItem1_" + i);
+                            itemList.add(ti);
+                        }
+                        cont.addAll(itemList);
+                    }
+                });
+
+        Button buttonReplaceByAnotherCollectionViaAdd = new Button(
+                "Replace by another items (via add(), add()..)");
+        buttonReplaceByAnotherCollectionViaAdd
+                .setId("buttonReplaceByAnotherCollectionViaAdd");
+        buttonReplaceByAnotherCollectionViaAdd
+                .addClickListener(new ClickListener() {
+
+                    @Override
+                    public void buttonClick(
+                            com.vaadin.ui.Button.ClickEvent event) {
+                        cont.removeAllItems();
+                        for (int i = 0; i < 81; i++) {
+                            TableItem ti = new TableItem();
+                            ti.setName("AnotherItem2_" + i);
+                            // add one by one in container
+                            cont.addBean(ti);
+                        }
+                    }
+                });
+
+        Button buttonReplaceBySubsetOfSmallerSize = new Button(
+                "Replace rows by sub-set of smaller size (size not enought for restoring scroll position)");
+        buttonReplaceBySubsetOfSmallerSize
+                .setId("buttonReplaceBySubsetOfSmallerSize");
+        buttonReplaceBySubsetOfSmallerSize
+                .addClickListener(new ClickListener() {
+
+                    @Override
+                    public void buttonClick(
+                            com.vaadin.ui.Button.ClickEvent event) {
+                        cont.removeAllItems();
+                        cont.addAll(restoringItemList.subList(0, 20));
+                    }
+                });
+
+        Button buttonReplaceByWholeSubsetPlusOnNew = new Button(
+                "Replace rows by whole subset plus one new item");
+        buttonReplaceByWholeSubsetPlusOnNew
+                .setId("buttonReplaceByWholeSubsetPlusOnNew");
+        buttonReplaceByWholeSubsetPlusOnNew
+                .addClickListener(new ClickListener() {
+
+                    @Override
+                    public void buttonClick(
+                            com.vaadin.ui.Button.ClickEvent event) {
+                        cont.removeAllItems();
+
+                        List<TableItem> list = new ArrayList<TableItem>(
+                                restoringItemList);
+                        TableItem ti = new TableItem();
+                        ti.setName("AnotherItem3_" + 80);
+                        list.add(ti);
+                        cont.addAll(list);
+                    }
+                });
+
+        for (int i = 0; i < 80; i++) {
+            TableItem ti = new TableItem();
+            ti.setName("Item_" + i);
+            restoringItemList.add(ti);
+            cont.addBean(ti);
+        }
+
+        getLayout().addComponent(buttonReAddAllViaAddAll);
+        getLayout().addComponent(buttonReplaceByAnotherCollectionViaAddAll);
+        getLayout().addComponent(buttonReplaceByAnotherCollectionViaAdd);
+        getLayout().addComponent(buttonReplaceBySubsetOfSmallerSize);
+        getLayout().addComponent(buttonReplaceByWholeSubsetPlusOnNew);
+        getLayout().addComponent(buttonRestore);
         getLayout().addComponent(table);
     }
 
