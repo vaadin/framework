0abf13890b8c2391a609d63995a862b831e2f078
Anna Miroshnik <anna.miroshnik@arcadia.spb.ru>
2014-10-02 16:02:55 +0400

    Re-adding content in Table causes table to lose scroll position (#14581)
    At this moment behavior of restoring scroll position more like as in 
    7.2.6. But restoring is only in case of removeAll() - addAll() (or add()
    one time - then restore index is reset).
    
    In 7.2.6 restoring of scroll position was the result of client defect 
    (scrolling in lazyScroller was not changed if variable "firstvisible" 
    fromserver was 0). This "defect" was fixed in one of the patches.
    
    Change-Id: I2e2fb8749ec95f3409caeacafff46c4c29159e74

--- uitest/src/com/vaadin/tests/components/table/TableRepairsScrollPositionOnReAddingAllRowsTest.java
+++ uitest/src/com/vaadin/tests/components/table/TableRepairsScrollPositionOnReAddingAllRowsTest.java
@@ -31,6 +31,12 @@ import com.vaadin.testbench.screenshot.ImageComparison;
 import com.vaadin.testbench.screenshot.ReferenceNameGenerator;
 import com.vaadin.tests.tb3.MultiBrowserTest;
 
+/**
+ * Scroll position should be restored when removing and re-adding all rows in
+ * Table.
+ * 
+ * @author Vaadin Ltd
+ */
 public class TableRepairsScrollPositionOnReAddingAllRowsTest extends
         MultiBrowserTest {
 
@@ -39,29 +45,126 @@ public class TableRepairsScrollPositionOnReAddingAllRowsTest extends
             throws InterruptedException {
         openTestURL();
 
-        WebElement buttonReAddRows = findElement(By.id("button1"));
+        WebElement row0 = $(TableElement.class).first().getCell(0, 0);
+        int rowLocation0 = row0.getLocation().getY();
 
+        // case 1
         scrollUp();
 
         waitUntilNot(new ExpectedCondition<Boolean>() {
             @Override
             public Boolean apply(WebDriver input) {
-                return $(TableElement.class).first().getCell(49, 0) == null;
+                return $(TableElement.class).first().getCell(48, 0) == null;
             }
         }, 10);
 
-        WebElement row = $(TableElement.class).first().getCell(49, 0);
+        WebElement row = $(TableElement.class).first().getCell(48, 0);
         int rowLocation = row.getLocation().getY();
 
-        buttonReAddRows.click();
+        // This button is for re-adding all rows (original itemIds) at once
+        // (removeAll() + addAll())
+        hitButton("buttonReAddAllViaAddAll");
 
-        row = $(TableElement.class).first().getCell(49, 0);
+        row = $(TableElement.class).first().getCell(48, 0);
         int newRowLocation = row.getLocation().getY();
 
         // ranged check because IE9 consistently misses the mark by 1 pixel
         assertThat(
-                "Scroll position should be the same as before Re-Adding all rows",
-                (double) newRowLocation, closeTo(rowLocation, 1));
+                "Scroll position should be the same as before Re-Adding rows via addAll()",
+                (double) newRowLocation,
+                closeTo(rowLocation, row.getSize().height + 1));
+
+        // case 2
+        scrollUp();
+
+        waitUntilNot(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver input) {
+                return $(TableElement.class).first().getCell(48, 0) == null;
+            }
+        }, 10);
+
+        row = $(TableElement.class).first().getCell(48, 0);
+        rowLocation = row.getLocation().getY();
+
+        // This button is for replacing all rows at once (removeAll() +
+        // addAll())
+        hitButton("buttonReplaceByAnotherCollectionViaAddAll");
+
+        row = $(TableElement.class).first().getCell(48, 0);
+        newRowLocation = row.getLocation().getY();
+
+        // ranged check because IE9 consistently misses the mark by 1 pixel
+        assertThat(
+                "Scroll position should be the same as before Replacing rows via addAll()",
+                (double) newRowLocation,
+                closeTo(rowLocation, row.getSize().height + 1));
+
+        // case 3
+        // This button is for replacing all rows one by one (removeAll() + add()
+        // + add()..)
+        hitButton("buttonReplaceByAnotherCollectionViaAdd");
+
+        row = $(TableElement.class).first().getCell(0, 0);
+        newRowLocation = row.getLocation().getY();
+
+        // ranged check because IE9 consistently misses the mark by 1 pixel
+        assertThat("Scroll position should be 0", (double) newRowLocation,
+                closeTo(rowLocation0, 1));
+
+        // case 4
+        // This button is for restoring initial list and for scrolling to 0
+        // position
+        hitButton("buttonRestore");
+        scrollUp();
+
+        waitUntilNot(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver input) {
+                return $(TableElement.class).first().getCell(48, 0) == null;
+            }
+        }, 10);
+
+        // This button is for replacing all rows at once but the count of rows
+        // is less then first index to scroll
+        hitButton("buttonReplaceBySubsetOfSmallerSize");
+
+        row = $(TableElement.class).first().getCell(5, 0);
+
+        newRowLocation = row.getLocation().getY();
+
+        // ranged check because IE9 consistently misses the mark by 1 pixel
+        assertThat("Scroll position should be 0", (double) newRowLocation,
+                closeTo(rowLocation0, 1));
+
+        // case 5
+        // This button is for restoring initial list and for scrolling to 0
+        // position
+        hitButton("buttonRestore");
+        scrollUp();
+
+        waitUntilNot(new ExpectedCondition<Boolean>() {
+            @Override
+            public Boolean apply(WebDriver input) {
+                return $(TableElement.class).first().getCell(48, 0) == null;
+            }
+        }, 10);
+
+        row = $(TableElement.class).first().getCell(48, 0);
+        rowLocation = row.getLocation().getY();
+
+        // This button is for replacing by whole original sub-set of items plus
+        // one new
+        hitButton("buttonReplaceByWholeSubsetPlusOnNew");
+
+        row = $(TableElement.class).first().getCell(48, 0);
+        newRowLocation = row.getLocation().getY();
+
+        // ranged check because IE9 consistently misses the mark by 1 pixel
+        assertThat("Scroll position should be the same as before Replacing",
+                (double) newRowLocation,
+                closeTo(rowLocation, row.getSize().height + 1));
+
     }
 
     private void scrollUp() {
@@ -69,6 +172,6 @@ public class TableRepairsScrollPositionOnReAddingAllRowsTest extends
                 By.className("v-table-body-wrapper"));
         JavascriptExecutor js = new TestBenchCommandExecutor(getDriver(),
                 new ImageComparison(), new ReferenceNameGenerator());
-        js.executeScript("arguments[0].scrollTop = " + 1200, actualElement);
+        js.executeScript("arguments[0].scrollTop = " + 1205, actualElement);
     }
 }
