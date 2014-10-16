21d6298a8835e48ba47e941b5de6671bf829935c
Sauli Tähkäpää <sauli@vaadin.com>
2014-10-16 16:01:26 +0300

    Add @since to isEnabled() and setEnabled(). (#14797)
    Change-Id: Ic9b7e8f597afd68c4190b7eaffaee203876e24e0

--- client/src/com/vaadin/client/ui/VPopupView.java
+++ client/src/com/vaadin/client/ui/VPopupView.java
@@ -195,6 +195,8 @@ public class VPopupView extends HTML implements HasEnabled, Iterable<Widget>,
 
     /**
      * Returns true if the popup is enabled, false if not.
+     *
+     * @since 7.3.4
      */
     @Override
     public boolean isEnabled() {
@@ -206,6 +208,7 @@ public class VPopupView extends HTML implements HasEnabled, Iterable<Widget>,
      *
      * @param enabled <code>true</code> to enable the popup, <code>false</code>
      *          to disable it
+     * @since 7.3.4
      */
     @Override
     public void setEnabled(boolean enabled) {
