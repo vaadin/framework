89bd22d61df728a36e3385ce14b0b5bf4d3bb6f8
Maciej Przepióra <matthew@vaadin.com>
2014-10-22 15:28:15 +0300

    Change the way classloader for VaadinService is selected (fixes #11295)
    Change-Id: I2a25b4149f927ee7940edf596e8cebaaa48dcd0d

--- server/src/com/vaadin/server/Constants.java
+++ server/src/com/vaadin/server/Constants.java
@@ -115,6 +115,15 @@ public interface Constants {
             + Constants.SERVLET_PARAMETER_LEGACY_PROPERTY_TOSTRING
             + ". Supported values are 'false','warning','true'";
 
+    static final String CANNOT_ACQUIRE_CLASSLOADER_SEVERE = "\n"
+            + "=================================================================\n"
+            + "Vaadin was unable to acquire class loader from servlet container\n"
+            + "to load your application classes. Setup appropriate security\n"
+            + "policy to allow invoking Thread.getContextClassLoader() from\n"
+            + "VaadinService if you're not using custom class loader.\n"
+            + "NullPointerExceptions will be thrown later."
+            + "=================================================================";
+
     static final String URL_PARAMETER_THEME = "theme";
 
     static final String SERVLET_PARAMETER_PRODUCTION_MODE = "productionMode";
@@ -164,4 +173,5 @@ public interface Constants {
     static final String PORTAL_PARAMETER_VAADIN_THEME = "vaadin.theme";
 
     static final String PORTLET_CONTEXT = "PORTLET_CONTEXT";
+
 }
