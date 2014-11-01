ef8d1ecf8765ce68bbe69021b90f3377ef8d201a
Sauli Tähkäpää <sauli@vaadin.com>
2014-11-01 14:04:39 +0200

    Reverse asc and desc table sorting indicators for Valo. (#15123)
    Change-Id: If649d7ab0b4257cfaa1488dfff88afa8ef122f67

---
+++ uitest/src/com/vaadin/tests/themes/valo/TableSortIndicator.java
@@ -0,0 +1,34 @@
+package com.vaadin.tests.themes.valo;
+
+import com.vaadin.annotations.*;
+import com.vaadin.server.*;
+import com.vaadin.tests.components.*;
+import com.vaadin.ui.*;
+
+@Theme("valo")
+public class TableSortIndicator extends AbstractTestUI {
+    @Override
+    protected void setup(VaadinRequest request) {
+        Table table = new Table();
+        table.addContainerProperty("Index", Integer.class, "");
+
+        for(int i=0;i<10;i++) {
+            table.addItem(new Object[] {i}, i);
+        }
+
+        table.setPageLength(0);
+
+        addComponent(table);
+    }
+
+    @Override
+    protected String getTestDescription() {
+        return "For Valo, sorting indicators should point up when sorted asc " +
+                "and down when sorted desc.";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 15123;
+    }
+}
