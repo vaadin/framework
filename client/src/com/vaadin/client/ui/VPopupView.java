4910442a87c35ca3b1240d2818e4155522b04504
Sauli Tähkäpää <sauli@vaadin.com>
2014-11-02 22:22:14 +0200

    Add @since to VPopupView.
    Change-Id: I1b5544603ab2aa13bc1a51e04ff134fd48113c59

--- client/src/com/vaadin/client/ui/VPopupView.java
+++ client/src/com/vaadin/client/ui/VPopupView.java
@@ -442,6 +442,14 @@ public class VPopupView extends HTML implements HasEnabled, Iterable<Widget>,
         return Collections.singleton((Widget) popup).iterator();
     }
 
+    /**
+     * Checks whether there are operations pending for this widget that must be
+     * executed before reaching a steady state.
+     *
+     * @returns <code>true</code> iff there are operations pending which must be
+     *          executed before reaching a steady state
+     * @since 7.3.4
+     */
     @Override
     public boolean isWorkPending() {
         return popupShowInProgress;
