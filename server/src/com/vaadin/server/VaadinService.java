deebc28ae7069b54e96b03e4067de63df99b78e0
Sauli Tähkäpää <sauli@vaadin.com>
2014-11-11 09:01:07 +0200

    Update @since to VaadinService.
    Change-Id: I1b473b9f5316674c5c5ed9a35d776fb8906eb559

--- server/src/com/vaadin/server/VaadinService.java
+++ server/src/com/vaadin/server/VaadinService.java
@@ -1874,7 +1874,7 @@ public abstract class VaadinService implements Serializable {
      * @throws SecurityException
      *             If current security policy forbids acquiring class loader
      * 
-     * @since
+     * @since 7.3.5
      */
     protected void setDefaultClassLoader() {
         try {
