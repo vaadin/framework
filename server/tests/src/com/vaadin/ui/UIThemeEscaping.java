3c59a1f08d6e2288c4bcd0ac74273ae14c4d4e0b
Leif Åstrand <leif@vaadin.com>
2014-11-14 15:27:49 +0200

    Escape dynamic and configured theme names in the same way. (#15309)
    Change-Id: Ib7fd42e6017d0b78e6d5e6bd7f531f0cd6c8c0ab

---
+++ server/tests/src/com/vaadin/ui/UIThemeEscaping.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2000-2014 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.ui;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.server.VaadinRequest;
+
+public class UIThemeEscaping {
+
+    @Test
+    public void testThemeEscaping() {
+        UI ui = new UI() {
+            @Override
+            protected void init(VaadinRequest request) {
+                // Nothing to do
+            }
+        };
+
+        ui.setTheme("a<å(_\"$");
+
+        String theme = ui.getTheme();
+
+        Assert.assertEquals(
+                "Dangerous characters should be removed from the theme name",
+                "aå_$", theme);
+    }
+
+}
