0739d0b80f46ab48882e4a5dd8c812e781d9e495
Anna Miroshnik <anna.miroshnik@arcadia.spb.ru>
2014-12-11 13:50:51 +0300

    ComboBox: programmatic value reset breaks dropdown (#13217)
    Change-Id: I6f749ab0788324d24e1e7451183ec9f0ff8ed5ba

--- client/src/com/vaadin/client/ui/combobox/ComboBoxConnector.java
+++ client/src/com/vaadin/client/ui/combobox/ComboBoxConnector.java
@@ -305,6 +305,7 @@ public class ComboBoxConnector extends AbstractFieldConnector implements
                     getWidget().tb.setValue("");
                 }
             }
+            getWidget().currentSuggestion = null; // #13217
             getWidget().setSelectedItemIcon(null);
             getWidget().selectedOptionKey = null;
         }
