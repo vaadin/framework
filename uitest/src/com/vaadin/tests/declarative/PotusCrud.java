8a0a1bdb4630f39214a039a2689bfa5a1431a413
Matti Hosio <mhosio@vaadin.com>
2014-12-16 18:09:56 +0200

    Improved potus demo (#7749)
    Change-Id: I330d669238b2c79ca2afda396a5e07833f0af90a

--- uitest/src/com/vaadin/tests/declarative/PotusCrud.java
+++ uitest/src/com/vaadin/tests/declarative/PotusCrud.java
@@ -15,18 +15,107 @@
  */
 package com.vaadin.tests.declarative;
 
-import com.vaadin.annotations.Theme;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.UI;
+import com.vaadin.annotations.DesignRoot;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.data.fieldgroup.FieldGroup;
+import com.vaadin.data.fieldgroup.FieldGroup.CommitException;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.declarative.Design;
 
-@Theme("valo")
-public class PotusCrud extends UI {
+@DesignRoot
+public class PotusCrud extends VerticalLayout {
 
-    @Override
-    protected void init(VaadinRequest request) {
-        setContent(Design
-                .read(getClass().getResourceAsStream("PotusCrud.html")));
+    public Table potusList;
+    public PotusForm potusForm;
+    public Button addNew;
+
+    private FieldGroup fg;
+
+    private BeanItemContainer<Potus> potusContainer = new BeanItemContainer<Potus>(
+            Potus.class);
+
+    public PotusCrud() {
+        Design.read(this);
+        init();
+    }
+
+    private void init() {
+        initTable();
+        initForm();
+        addNew.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                doAdd();
+            }
+        });
+    }
+
+    private void initTable() {
+        potusList.setContainerDataSource(potusContainer);
+        potusList.addValueChangeListener(new ValueChangeListener() {
+            @Override
+            public void valueChange(ValueChangeEvent event) {
+                doEdit();
+            }
+        });
+    }
+
+    private void initForm() {
+        potusForm.save.addClickListener(new Button.ClickListener() {
+            @Override
+            public void buttonClick(ClickEvent event) {
+                doSave();
+            }
+
+        });
+        potusForm.delete.addClickListener(new Button.ClickListener() {
+
+            @Override
+            public void buttonClick(ClickEvent event) {
+                doDelete();
+            }
+        });
+        potusForm.revert.addClickListener(new Button.ClickListener() {
+
+            @Override
+            public void buttonClick(ClickEvent event) {
+                doRevert();
+            }
+        });
+    }
+
+    protected void doRevert() {
+        fg.discard();
+    }
+
+    protected void doDelete() {
+        potusContainer.removeItem(potusList.getValue());
+    }
+
+    protected void doSave() {
+        try {
+            fg.commit();
+        } catch (CommitException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    protected void doAdd() {
+        potusContainer.addBean(new Potus());
+    }
+
+    protected void doEdit() {
+        if (potusList.getValue() != null) {
+            fg = new FieldGroup();
+            fg.setItemDataSource(potusList.getItem(potusList.getValue()));
+            fg.bindMemberFields(potusForm);
+        }
     }
 
 }
