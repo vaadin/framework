6ce22d8ad5c9e81cc2e34a70779413fb2c7040d6
Leif Åstrand <leif@vaadin.com>
2014-12-30 12:18:24 +0200

    Update favicon when changing themes (#15481)
    Change-Id: I3622d211ac5e668f7dce999605118f8f16ffae90

---
+++ uitest/src/com/vaadin/tests/themes/ThemeChangeFaviconTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2000-2014 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.tests.themes;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+import com.vaadin.testbench.elements.ButtonElement;
+import com.vaadin.testbench.parallel.Browser;
+import com.vaadin.tests.tb3.SingleBrowserTest;
+
+public class ThemeChangeFaviconTest extends SingleBrowserTest {
+
+    @Override
+    public List<DesiredCapabilities> getBrowsersToTest() {
+        // Seems like stylesheet onload is not fired on PhantomJS
+        // https://github.com/ariya/phantomjs/issues/12332
+        return Collections.singletonList(Browser.FIREFOX
+                .getDesiredCapabilities());
+    }
+
+    @Test
+    public void changeFavicon() throws InterruptedException {
+        setDebug(true);
+        openTestURL();
+        assertFavicon("reindeer");
+
+        changeTheme("valo");
+        assertFavicon("valo");
+
+        changeTheme("reindeer");
+        assertFavicon("reindeer");
+    }
+
+    private void changeTheme(final String theme) {
+        $(ButtonElement.class).caption(theme).first().click();
+
+        final WebElement rootDiv = findElement(By
+                .xpath("//div[contains(@class,'v-app')]"));
+        waitUntil(new ExpectedCondition<Boolean>() {
+
+            @Override
+            public Boolean apply(WebDriver input) {
+                String rootClass = rootDiv.getAttribute("class").trim();
+
+                return rootClass.contains(theme);
+            }
+        }, 30);
+    }
+
+    private void assertFavicon(String theme) {
+        String faviconUrl = "/VAADIN/themes/" + theme + "/favicon.ico";
+
+        List<WebElement> elements = findElements(By
+                .cssSelector("link[rel~=\"icon\"]"));
+
+        Assert.assertEquals(2, elements.size());
+
+        for (WebElement element : elements) {
+            Assert.assertTrue(element.getAttribute("href")
+                    + " does not end with " + faviconUrl,
+                    element.getAttribute("href").endsWith(faviconUrl));
+        }
+    }
+
+}
