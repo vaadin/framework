47850591adc8f2510ce18b64862c6fefd1f247ad
Teemu Suo-Anttila <teemusa@vaadin.com>
2014-12-31 10:25:46 +0200

    Fix active row range handling when removing rows (#15454)
    Change-Id: I6de22796051503290db216d4c213401d24a7e2a0

---
+++ uitest/src/com/vaadin/tests/components/grid/GridInTabSheetTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2000-2014 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.tests.components.grid;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import com.vaadin.testbench.elements.ButtonElement;
+import com.vaadin.testbench.elements.GridElement;
+import com.vaadin.testbench.elements.NotificationElement;
+import com.vaadin.tests.tb3.MultiBrowserTest;
+
+public class GridInTabSheetTest extends MultiBrowserTest {
+
+    @Test
+    public void testRemoveAllRowsAndAddThreeNewOnes() {
+        setDebug(true);
+        openTestURL();
+
+        for (int i = 0; i < 3; ++i) {
+            removeGridRow();
+        }
+
+        for (int i = 0; i < 3; ++i) {
+            addGridRow();
+            assertEquals("" + (100 + i), getGridElement().getCell(i, 1)
+                    .getText());
+        }
+        assertFalse("There was an unexpected error notification",
+                isElementPresent(NotificationElement.class));
+    }
+
+    private void removeGridRow() {
+        $(ButtonElement.class).caption("Remove row from Grid").first().click();
+    }
+
+    private void addGridRow() {
+        $(ButtonElement.class).caption("Add row to Grid").first().click();
+    }
+
+    private GridElement getGridElement() {
+        return $(GridElement.class).first();
+    }
+}
