c6f185a1c420af5ca67a5dc9200db15dcc8e2365
Denis Anisimov <denis@vaadin.com>
2015-01-04 16:02:32 +0200

    Fix opacity value for disabled label and non-collapsible column (#15489)
    Change-Id: I0d669f1c85feebcaa01c58fb81b5e20802440ef2

---
+++ uitest/src/test/java/com/vaadin/tests/themes/valo/CollapsibleTableColumnTest.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2000-2014 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.tests.themes.valo;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.testbench.elements.TableElement;
+import com.vaadin.tests.tb3.MultiBrowserTest;
+
+/**
+ * Test for non-collapsible column opacity for item in column configuration
+ * menu.
+ * 
+ * @author Vaadin Ltd
+ */
+public class CollapsibleTableColumnTest extends MultiBrowserTest {
+
+    @Test
+    public void nonCollapsibleColumnOpacity() {
+        openTestURL();
+
+        // click on the header to make the column selector visible
+        $(TableElement.class).first().getHeaderCell(0).click();
+        waitForElementVisible(By.className("v-table-column-selector"));
+
+        findElement(By.className("v-table-column-selector")).click();
+
+        List<WebElement> items = findElements(By.className("v-on"));
+        String collapsibleColumnOpacity = items.get(0).getCssValue("opacity");
+        String nonCollapsibleColumnOpacity = items.get(1)
+                .getCssValue("opacity");
+
+        Assert.assertNotEquals("Opacity value is the same for collapsible "
+                + "and non-collapsible column item", collapsibleColumnOpacity,
+                nonCollapsibleColumnOpacity);
+    }
+}
