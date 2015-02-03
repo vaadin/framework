293b62ab6ed402b188685027b100025597a37d00
Sauli Tähkäpää <sauli@vaadin.com>
2015-02-03 11:01:18 +0200

    Add @since for Transport.
    Change-Id: I1d4882c820722094128dac3a6f595d30286ee458

--- shared/src/com/vaadin/shared/ui/ui/Transport.java
+++ shared/src/com/vaadin/shared/ui/ui/Transport.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2000-2014 Vaadin Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -18,7 +18,7 @@ package com.vaadin.shared.ui.ui;
 
 /**
  * Transport modes for Push
- * 
+ *
  * @since 7.1
  * @author Vaadin Ltd
  */
@@ -46,6 +46,12 @@ public enum Transport {
         return identifier;
     }
 
+    /**
+    * Returns a Transport by its identifier. Returns null if no value is found
+    * for the given identifier.
+    *
+    * @since 7.3.10
+    */
     public static Transport getByIdentifier(String identifier) {
         for (Transport t : values()) {
             if (t.getIdentifier().equals(identifier)) {
