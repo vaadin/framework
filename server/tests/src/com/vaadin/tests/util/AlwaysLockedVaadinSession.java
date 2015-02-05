7bab33ebd25421dbb484df3b276af53bb0384eca
Artur Signell <artur@vaadin.com>
2015-02-05 11:22:01 +0200

    Prevent AlwaysLockedVaadinSession from being GCed during test
    Change-Id: Id6e2e6c7e013e8c667f658d5877c5db7d072106b

--- server/tests/src/com/vaadin/tests/util/AlwaysLockedVaadinSession.java
+++ server/tests/src/com/vaadin/tests/util/AlwaysLockedVaadinSession.java
@@ -1,23 +1,13 @@
 package com.vaadin.tests.util;
 
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-
+import com.vaadin.server.MockVaadinSession;
 import com.vaadin.server.VaadinService;
-import com.vaadin.server.VaadinSession;
 
-public class AlwaysLockedVaadinSession extends VaadinSession {
-
-    private ReentrantLock lock;
+public class AlwaysLockedVaadinSession extends MockVaadinSession {
 
     public AlwaysLockedVaadinSession(VaadinService service) {
         super(service);
-        lock = new ReentrantLock();
-        lock.lock();
+        lock();
     }
 
-    @Override
-    public Lock getLockInstance() {
-        return lock;
-    }
 }
