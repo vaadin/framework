043dca01edf501a05a2e239f00da2bf490de6cf2
Johannes Dahlström <johannesd@vaadin.com>
2015-03-18 15:35:47 +0200

    Fix Declarative support for BigDecimal properties (#17205)
    Change-Id: I766172c125b7e771b4bc8e2db1f39d1ae828fea6

--- server/src/com/vaadin/ui/declarative/DesignFormatter.java
+++ server/src/com/vaadin/ui/declarative/DesignFormatter.java
@@ -29,6 +29,7 @@ import java.util.TimeZone;
 import java.util.concurrent.ConcurrentHashMap;
 
 import com.vaadin.data.util.converter.Converter;
+import com.vaadin.data.util.converter.StringToBigDecimalConverter;
 import com.vaadin.data.util.converter.StringToDoubleConverter;
 import com.vaadin.data.util.converter.StringToFloatConverter;
 import com.vaadin.event.ShortcutAction;
@@ -68,8 +69,8 @@ public class DesignFormatter implements Serializable {
      */
     protected void mapDefaultTypes() {
         // numbers use standard toString/valueOf approach
-        for (Class<?> c : new Class<?>[] { Integer.class, Byte.class,
-                Short.class, Long.class, BigDecimal.class }) {
+        for (Class<?> c : new Class<?>[] { Byte.class, Short.class,
+                Integer.class, Long.class }) {
             DesignToStringConverter<?> conv = new DesignToStringConverter(c);
             converterMap.put(c, conv);
             try {
@@ -133,6 +134,16 @@ public class DesignFormatter implements Serializable {
         };
         converterMap.put(Double.class, doubleConverter);
         converterMap.put(double.class, doubleConverter);
+
+        final DecimalFormat bigDecimalFmt = new DecimalFormat("0.###", symbols);
+        bigDecimalFmt.setGroupingUsed(false);
+        bigDecimalFmt.setParseBigDecimal(true);
+        converterMap.put(BigDecimal.class, new StringToBigDecimalConverter() {
+            @Override
+            protected NumberFormat getFormat(Locale locale) {
+                return bigDecimalFmt;
+            };
+        });
 
         // strings do nothing
         converterMap.put(String.class, new Converter<String, String>() {
