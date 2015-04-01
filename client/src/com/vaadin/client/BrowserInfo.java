2d24d34b19d2ea93e8b2058218915672ad9f0f0e
Alexey Fansky <alexey.fansky@effective-soft.com>
2015-04-01 15:08:58 -0700

    Displaying tooltip on touch devices underneath the field (#17150)
    Change-Id: I7381a6212b824f9dafc5fe7359b0e791f15c57b2

--- client/src/com/vaadin/client/BrowserInfo.java
+++ client/src/com/vaadin/client/BrowserInfo.java
@@ -88,7 +88,8 @@ public class BrowserInfo {
         } else if (browserDetails.isIE()) {
             touchDevice = detectIETouchDevice();
         } else {
-            touchDevice = detectTouchDevice();
+            //PhantomJS pretends to be a touch device which breaks some UI tests
+            touchDevice = !browserDetails.isPhantomJS() && detectTouchDevice();
         }
     }
 
