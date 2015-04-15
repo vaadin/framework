438c4d3af4988ddefdd040ff991d46617a54771a
Alexey Fansky <alexey.fansky@effective-soft.com>
2015-04-15 12:12:11 -0700

    Keeping MenuItem custom classes when updating styles (#17427)
    Change-Id: I54b7e700148797f0145c4ec5f1802ea999142ab3

---
+++ uitest/src/com/vaadin/tests/components/menubar/MenuItemStyleRemovedTest.java
@@ -0,0 +1,30 @@
+package com.vaadin.tests.components.menubar;
+
+import com.vaadin.testbench.By;
+import com.vaadin.testbench.elements.ButtonElement;
+import com.vaadin.testbench.elements.MenuBarElement;
+import com.vaadin.tests.tb3.MultiBrowserTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class MenuItemStyleRemovedTest extends MultiBrowserTest {
+
+    @Test
+    public void testCustomStyleShouldStayAfterMenuSelect() {
+        openTestURL();
+
+        $(ButtonElement.class).caption("Add styles").first().click();
+
+        MenuBarElement menu = $(MenuBarElement.class).first();
+        List<WebElement> elements = menu.findElements(By.className("custom-menu-item"));
+        Assert.assertEquals(2, elements.size());
+
+        menu.clickItem("first");
+        menu.clickItem("second");
+        elements = menu.findElements(By.className("custom-menu-item"));
+        Assert.assertEquals(2, elements.size());
+    }
+}
