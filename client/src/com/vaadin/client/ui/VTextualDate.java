e779504e846bf7f9a3b6069d4ee2e9a71c309e8d
Anna Koskinen <anna@vaadin.com>
2015-06-15 14:52:18 +0300

    Input prompt shouldn't get set when input has focus (#18027)
    Backported based on c61187a34, left out the TB4 test due
    incompatibility.
    
    Change-Id: I0298228aa99bb98aecbcff46db9c6533ac4a2947

--- client/src/com/vaadin/client/ui/VTextualDate.java
+++ client/src/com/vaadin/client/ui/VTextualDate.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2000-2014 Vaadin Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -357,7 +357,12 @@ public class VTextualDate extends VDateField implements Field, ChangeHandler,
     }
 
     protected void setText(String text) {
-        if (inputPrompt != null && (text == null || "".equals(text))) {
+        if (inputPrompt != null
+                && (text == null || "".equals(text))
+                && !this.text.getStyleName()
+                        .contains(
+                                VTextField.CLASSNAME + "-"
+                                        + VTextField.CLASSNAME_FOCUS)) {
             text = readonly ? "" : inputPrompt;
             setPrompting(true);
         } else {
