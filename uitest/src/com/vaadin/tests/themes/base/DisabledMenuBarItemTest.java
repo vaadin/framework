bf8b89d00679eb90528028e7a729ee0add39dd90
Teppo Kurki <teppo.kurki@vaadin.com>
2015-06-22 13:52:34 +0300

    Fixed regression by #15381 on IE8
    The opacity for disabled menu items looks very ugly on IE8.
    Fixed by using the previous gray color for text and opacity
    filter only for the item icon.
    
    Change-Id: Ieea18f9e14d3b947c9836956c00721c30685b912

--- uitest/src/com/vaadin/tests/themes/base/DisabledMenuBarItemTest.java
+++ uitest/src/com/vaadin/tests/themes/base/DisabledMenuBarItemTest.java
@@ -21,19 +21,17 @@ public class DisabledMenuBarItemTest extends MultiBrowserTest {
         WebElement element = driver.findElement(By
                 .className("v-menubar-menuitem-disabled"));
 
-        assertThat(element.getCssValue("opacity"), is("0.5"));
-
-        if (browserIsIE8or9()) {
+        if (Browser.IE9.getDesiredCapabilities().equals(
+                getDesiredCapabilities())) {
             assertThat(element.getCssValue("filter"), is("alpha(opacity=50)"));
+        } else if (Browser.IE8.getDesiredCapabilities().equals(
+                getDesiredCapabilities())) {
+            WebElement icon = element.findElement(By.tagName("img"));
+            assertThat(icon.getCssValue("filter"), is("alpha(opacity=50)"));
+        } else {
+            assertThat(element.getCssValue("opacity"), is("0.5"));
         }
 
         compareScreen("transparent");
-    }
-
-    private boolean browserIsIE8or9() {
-        return Browser.IE8.getDesiredCapabilities().equals(
-                getDesiredCapabilities())
-                || Browser.IE9.getDesiredCapabilities().equals(
-                        getDesiredCapabilities());
     }
 }
