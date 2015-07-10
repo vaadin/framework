951101ce39620501ed8aa1751909b7bc697713a3
Artur Signell <artur@vaadin.com>
2015-07-11 02:25:29 +0300

    Take margin/border/padding into account when measuring ComboBox in IE (#17002)
    Change-Id: I8fcf33b78ff239529b794d41088fd9d8aba5c518

--- client/src/com/vaadin/client/ui/VFilterSelect.java
+++ client/src/com/vaadin/client/ui/VFilterSelect.java
@@ -599,7 +599,8 @@ public class VFilterSelect extends Composite implements Field, KeyDownHandler,
                     + "]");
 
             Element menuFirstChild = menu.getElement().getFirstChildElement();
-            final int naturalMenuWidth = menuFirstChild.getOffsetWidth();
+            final int naturalMenuWidth = WidgetUtil
+                    .getRequiredWidth(menuFirstChild);
 
             if (popupOuterPadding == -1) {
                 popupOuterPadding = WidgetUtil
@@ -612,12 +613,18 @@ public class VFilterSelect extends Composite implements Field, KeyDownHandler,
             }
 
             if (BrowserInfo.get().isIE()) {
+                // Must take margin,border,padding manually into account for
+                // menu element as we measure the element child and set width to
+                // the element parent
+                int naturalMenuOuterWidth = naturalMenuWidth
+                        + getMarginBorderPaddingWidth(menu.getElement());
+
                 /*
                  * IE requires us to specify the width for the container
                  * element. Otherwise it will be 100% wide
                  */
-                int rootWidth = Math.max(desiredWidth, naturalMenuWidth)
-                        - popupOuterPadding;
+                int rootWidth = Math.max(desiredWidth - popupOuterPadding,
+                        naturalMenuOuterWidth);
                 getContainerElement().getStyle().setWidth(rootWidth, Unit.PX);
             }
 
@@ -1281,6 +1288,16 @@ public class VFilterSelect extends Composite implements Field, KeyDownHandler,
         setStyleName(CLASSNAME);
 
         sinkEvents(Event.ONPASTE);
+    }
+
+    private static int getMarginBorderPaddingWidth(Element element) {
+        final ComputedStyle s = new ComputedStyle(element);
+        int[] margin = s.getMargin();
+        int[] border = s.getBorder();
+        int[] padding = s.getPadding();
+        return margin[1] + margin[3] + border[1] + border[3] + padding[1]
+                + padding[3];
+
     }
 
     /*
