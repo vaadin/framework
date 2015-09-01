379c6679879cf69ef4253da97f7d557bc33673e4
Artur Signell <artur@vaadin.com>
2015-09-01 14:19:50 +0300

    Nginx + Wildfly 9 test for various proxy configurations (#11733)
    * /nonbuffering a proxy without any buffering
    * /buffering tests a buffering proxy
    * /nonbuffering-timeout tests a proxy without any buffering but with a 10s timeout set
    * /buffering-timeout tests a buffering proxy with a 10s timeout set
    
    Change-Id: I5d99cc29364e3ed888371fa18ee404f912d73223

--- uitest/src/com/vaadin/tests/integration/AbstractServletIntegrationTest.java
+++ uitest/src/com/vaadin/tests/integration/AbstractServletIntegrationTest.java
@@ -16,8 +16,13 @@
 package com.vaadin.tests.integration;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.vaadin.testbench.elements.TableElement;
 
@@ -27,8 +32,11 @@ import com.vaadin.testbench.elements.TableElement;
  * 
  * @author Vaadin Ltd
  */
+@RunWith(ParameterizedTB3Runner.class)
 public abstract class AbstractServletIntegrationTest extends
         AbstractIntegrationTest {
+
+    private String contextPath = "/demo";
 
     @Test
     public void runTest() throws IOException, AssertionError {
@@ -40,7 +48,29 @@ public abstract class AbstractServletIntegrationTest extends
 
     @Override
     protected String getDeploymentPath(Class<?> uiClass) {
-        return "/demo" + super.getDeploymentPath(uiClass);
+        return contextPath + super.getDeploymentPath(uiClass);
+    }
+
+    public void setContextPath(String contextPath) {
+        this.contextPath = contextPath;
+    }
+
+    @Parameters
+    public static Collection<String> getContextPaths() {
+        if (getServerName().equals("wildfly9-nginx")) {
+            ArrayList<String> paths = new ArrayList<String>();
+            paths.add("/buffering/demo");
+            paths.add("/nonbuffering/demo");
+            paths.add("/buffering-timeout/demo");
+            paths.add("/nonbuffering-timeout/demo");
+            return paths;
+        } else {
+            return Collections.emptyList();
+        }
+    }
+
+    protected static String getServerName() {
+        return System.getProperty("server-name");
     }
 
 }
--- uitest/src/com/vaadin/tests/push/BasicPushTest.java
+++ uitest/src/com/vaadin/tests/push/BasicPushTest.java
@@ -16,6 +16,7 @@
 package com.vaadin.tests.push;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -52,8 +53,8 @@ public abstract class BasicPushTest extends MultiBrowserTest {
     }
 
     public static int getClientCounter(AbstractTB3Test t) {
-        WebElement clientCounterElem = t
-                .vaadinElementById(BasicPush.CLIENT_COUNTER_ID);
+        WebElement clientCounterElem = t.findElement(By
+                .id(BasicPush.CLIENT_COUNTER_ID));
         return Integer.parseInt(clientCounterElem.getText());
     }
 
@@ -66,21 +67,21 @@ public abstract class BasicPushTest extends MultiBrowserTest {
     }
 
     public static int getServerCounter(AbstractTB3Test t) {
-        WebElement serverCounterElem = t
-                .vaadinElementById(BasicPush.SERVER_COUNTER_ID);
+        WebElement serverCounterElem = t.findElement(By
+                .id(BasicPush.SERVER_COUNTER_ID));
         return Integer.parseInt(serverCounterElem.getText());
     }
 
     public static WebElement getServerCounterStartButton(AbstractTB3Test t) {
-        return t.vaadinElementById(BasicPush.START_TIMER_ID);
+        return t.findElement(By.id(BasicPush.START_TIMER_ID));
     }
 
     public static WebElement getServerCounterStopButton(AbstractTB3Test t) {
-        return t.vaadinElementById(BasicPush.STOP_TIMER_ID);
+        return t.findElement(By.id(BasicPush.STOP_TIMER_ID));
     }
 
     public static WebElement getIncrementButton(AbstractTB3Test t) {
-        return t.vaadinElementById(BasicPush.INCREMENT_BUTTON_ID);
+        return t.findElement(By.id(BasicPush.INCREMENT_BUTTON_ID));
     }
 
     private void waitUntilClientCounterChanges(final int expectedValue) {
