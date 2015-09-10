ae20007e0793825dcf793ee7e7212d6bcfbf25b8
Artur Signell <artur@vaadin.com>
2015-09-10 10:28:15 +0300

    Chrome 45
    Change-Id: Ifb5c3fb54d7cff188b775b74c36d887efa41c853

--- uitest/src/com/vaadin/tests/VerifyBrowserVersionTest.java
+++ uitest/src/com/vaadin/tests/VerifyBrowserVersionTest.java
@@ -40,7 +40,7 @@ public class VerifyBrowserVersionTest extends MultiBrowserTest {
             // Chrome version does not necessarily match the desired version
             // because of auto updates...
             browserIdentifier = getExpectedUserAgentString(getDesiredCapabilities())
-                    + "44";
+                    + "45";
         } else {
             browserIdentifier = getExpectedUserAgentString(desiredCapabilities)
                     + desiredCapabilities.getVersion();
