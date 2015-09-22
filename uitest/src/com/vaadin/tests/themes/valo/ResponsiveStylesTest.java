d7dfc7477fbfc07893a2ca3271d0d898ad197b53
Marc Englund <marc@vaadin.com>
2015-09-22 09:37:36 +0300

    Removes .v-ui wrapping from valo-menu-responsive for (#18961)
    Removing .v-ui wrapping makes valo-menu-responsive usable anywhere
    on the page (not just top-level).
    
    Change-Id: I5551cb29677ae53be453fc29d0295ebb2dcbf241

---
+++ uitest/src/com/vaadin/tests/themes/valo/ResponsiveStylesTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2000-2014 Vaadin Ltd.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.tests.themes.valo;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.tests.tb3.MultiBrowserTest;
+
+/**
+ * Test for the built-in reponsive ("RWD") styles in Valo.
+ */
+public class ResponsiveStylesTest extends MultiBrowserTest {
+
+    private static final String MENU_STYLENAME = "valo-menu";
+    private static final int NARROW_ELEMENT_INDEX = 0;
+    private static final int NARROW_WIDTH = 112;
+    private static final int WIDE_ELEMENT_INDEX = 1;
+    private static final int WIDE_WIDTH = 146;
+
+    /**
+     * Tests that valo-menu-responsive can be used in any element on the page,
+     * not just as top-level component.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void testValoMenuResponsiveParentSize() throws Exception {
+        openTestURL();
+
+        List<WebElement> menus = findElements(com.vaadin.testbench.By
+                .className(MENU_STYLENAME));
+
+        WebElement narrowMenu = menus.get(NARROW_ELEMENT_INDEX);
+        int narrowWidth = narrowMenu.getSize().width;
+        assertThat(narrowWidth, equalTo(NARROW_WIDTH));
+
+        WebElement wideMenu = menus.get(WIDE_ELEMENT_INDEX);
+        int wideWidth = wideMenu.getSize().width;
+        assertThat(wideWidth, equalTo(WIDE_WIDTH));
+
+        compareScreen("defaultMenuWidths");
+    }
+}
