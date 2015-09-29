e2019e662df71e86fc1aca466b83d3a8351bca37
Henri Sara <hesara@vaadin.com>
2015-09-29 15:02:29 +0300

    Shorten "server connection lost" default timeout (#11733)
    Change-Id: Ida7f107edc63bfda3b91f86410439a84af050d73

--- shared/src/com/vaadin/shared/ui/ui/UIState.java
+++ shared/src/com/vaadin/shared/ui/ui/UIState.java
@@ -132,7 +132,7 @@ public class UIState extends TabIndexState {
         public String dialogTextGaveUp = "Server connection lost.";
         public int reconnectAttempts = 10000;
         public int reconnectInterval = 5000;
-        public int dialogGracePeriod = 1000;
+        public int dialogGracePeriod = 400;
         public boolean dialogModal = false;
     }
 
