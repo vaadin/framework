869b795e6478c34e6593b5f6ace2fe265973ebbd
Artur Signell <artur@vaadin.com>
2015-10-01 22:37:21 +0300

    Change ValoIcons to ThemeIcons and moved to base (#18472)
    Change-Id: I7122252e9b238fdca0383d0698623fe5a9e3f262

--- uitest/src/com/vaadin/tests/components/grid/basicfeatures/GridSortingIndicatorsTest.java
+++ uitest/src/com/vaadin/tests/components/grid/basicfeatures/GridSortingIndicatorsTest.java
@@ -16,8 +16,10 @@
 package com.vaadin.tests.components.grid.basicfeatures;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.vaadin.testbench.elements.ButtonElement;
 import com.vaadin.testbench.parallel.TestCategory;
@@ -25,6 +27,13 @@ import com.vaadin.tests.tb3.MultiBrowserTest;
 
 @TestCategory("grid")
 public class GridSortingIndicatorsTest extends MultiBrowserTest {
+
+    @Override
+    public List<DesiredCapabilities> getBrowsersToTest() {
+        List<DesiredCapabilities> browsers = getBrowsersExcludingPhantomJS();
+        browsers.add(PHANTOMJS2());
+        return browsers;
+    }
 
     @Test
     public void testSortingIndicators() throws IOException {
