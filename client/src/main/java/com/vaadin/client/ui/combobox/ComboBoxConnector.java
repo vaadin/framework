37e09697298224f6b56917247bb39f3ce6c0fb1e
Henri Sara <hesara@vaadin.com>
2015-11-09 12:58:52 +0200

    Request ComboBox pages with RPC (#19929)
    All client to server communication of ComboBox is now done
    with RPC.
    
    There is still an empty changeVariables() methods to
    override the default behavior in AbstractSelect.
    
    Change-Id: I718eeabd5c116ec4e00485ed37a0a6d5389ee1f0

--- client/src/main/java/com/vaadin/client/ui/combobox/ComboBoxConnector.java
+++ client/src/main/java/com/vaadin/client/ui/combobox/ComboBoxConnector.java
@@ -408,8 +408,7 @@ public class ComboBoxConnector extends AbstractFieldConnector implements
      */
     public void requestFirstPage() {
         sendSelection(null);
-        getConnection().updateVariable(getConnectorId(), "filter", "", false);
-        getConnection().updateVariable(getConnectorId(), "page", 0, true);
+        requestPage("", 0);
     }
 
     /**
@@ -426,10 +425,7 @@ public class ComboBoxConnector extends AbstractFieldConnector implements
      *            the page number to get
      */
     public void requestPage(String filter, int page) {
-        getConnection().updateVariable(getConnectorId(), "filter", filter,
-                false);
-        getConnection().updateVariable(getConnectorId(), "page", page,
-                immediate);
+        rpc.requestPage(filter, page);
     }
 
     /**
