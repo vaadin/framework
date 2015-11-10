dca930baca68ec965407b7ab113eb0ef674452b5
Henri Sara <hesara@vaadin.com>
2015-11-10 14:42:41 +0200

    Move combo popup opener clicked flag to connector (#19929)
    The flag is only used by the connector to temporarily block
    certain operations.
    
    Change-Id: I90cabcb26d938a6db342ff549ac2e4b8d8998aeb

--- client/src/com/vaadin/client/ui/VFilterSelect.java
+++ client/src/com/vaadin/client/ui/VFilterSelect.java
@@ -1242,10 +1242,10 @@ public class VFilterSelect extends Composite implements Field, KeyDownHandler,
     public String selectedOptionKey;
 
     /** For internal use only. May be removed or replaced in the future. */
-    public boolean waitingForFilteringResponse = false;
+    private boolean waitingForFilteringResponse = false;
 
     /** For internal use only. May be removed or replaced in the future. */
-    public boolean updateSelectionWhenReponseIsReceived = false;
+    private boolean updateSelectionWhenReponseIsReceived = false;
 
     /** For internal use only. May be removed or replaced in the future. */
     public boolean initDone = false;
@@ -1291,15 +1291,6 @@ public class VFilterSelect extends Composite implements Field, KeyDownHandler,
 
     /** For internal use only. May be removed or replaced in the future. */
     public boolean prompting = false;
-
-    /**
-     * Set true when popupopened has been clicked. Cleared on each UIDL-update.
-     * This handles the special case where are not filtering yet and the
-     * selected value has changed on the server-side. See #2119
-     * <p>
-     * For internal use only. May be removed or replaced in the future.
-     */
-    public boolean popupOpenerClicked;
 
     /** For internal use only. May be removed or replaced in the future. */
     public int suggestionPopupMinWidth = 0;
@@ -1978,7 +1969,7 @@ public class VFilterSelect extends Composite implements Field, KeyDownHandler,
                 boolean immediate = focused
                         || !connector.hasEventListener(EventId.FOCUS);
                 filterOptions(-1, "", immediate);
-                setPopupOpenerClicked(true);
+                connector.popupOpenerClicked();
                 lastFilter = "";
             }
             DOM.eventPreventDefault(DOM.eventGetCurrentEvent());
@@ -2362,25 +2353,6 @@ public class VFilterSelect extends Composite implements Field, KeyDownHandler,
     public void setUpdateSelectionWhenReponseIsReceived(
             boolean updateSelectionWhenReponseIsReceived) {
         this.updateSelectionWhenReponseIsReceived = updateSelectionWhenReponseIsReceived;
-    }
-
-    /**
-     * For internal use only - this method will be removed in the future.
-     * 
-     * @return true if the user has requested opening the popup
-     */
-    public boolean isPopupOpenerClicked() {
-        return popupOpenerClicked;
-    }
-
-    /**
-     * For internal use only - this method will be removed in the future.
-     * 
-     * @param popupOpenerClicked
-     *            true if the user has requested opening the popup
-     */
-    public void setPopupOpenerClicked(boolean popupOpenerClicked) {
-        this.popupOpenerClicked = popupOpenerClicked;
     }
 
 }
