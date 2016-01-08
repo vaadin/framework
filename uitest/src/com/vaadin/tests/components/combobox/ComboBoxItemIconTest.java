cf50732c3c8d94bb6ec542c43b1225fbb46f741e
Henri Sara <hesara@vaadin.com>
2016-01-08 14:03:37 +0200

    Convert ComboBoxItemIcon test to TB4
    Change-Id: I939fe7e5c5b57c007a543f5f7c491c8c9f11e773

---
+++ uitest/src/com/vaadin/tests/components/combobox/ComboBoxItemIconTest.java
@@ -0,0 +1,32 @@
+package com.vaadin.tests.components.combobox;
+
+import org.junit.Test;
+
+import com.vaadin.tests.tb3.MultiBrowserTest;
+import com.vaadin.tests.tb3.newelements.ComboBoxElement;
+
+public class ComboBoxItemIconTest extends MultiBrowserTest {
+    @Test
+    public void testIconsInComboBox() throws Exception {
+        openTestURL();
+
+        ComboBoxElement firstCombo = $(ComboBoxElement.class).first();
+
+        firstCombo.openPopup();
+        compareScreen("first-combobox-open");
+
+        // null item not on the list, so use index 1
+        firstCombo.selectByText(firstCombo.getPopupSuggestions().get(1));
+
+        compareScreen("fi-hu-selected");
+
+        ComboBoxElement secondCombo = $(ComboBoxElement.class).get(1);
+
+        secondCombo.openPopup();
+        compareScreen("second-combobox-open");
+
+        secondCombo.selectByText(secondCombo.getPopupSuggestions().get(2));
+        compareScreen("fi-au-selected");
+    }
+
+}
