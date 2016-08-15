1c78d48668f5f1fc74cdb1802fff668a55fdd309
Artur Signell <artur@vaadin.com>
2016-08-15 12:23:52 +0300

    Relax test to only care about JRE version, not patch set
    Change-Id: I1f959cdd81da9c6e8565260e574593bcd198804f

--- uitest/src/test/java/com/vaadin/tests/VerifyJreVersionTest.java
+++ uitest/src/test/java/com/vaadin/tests/VerifyJreVersionTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2000-2014 Vaadin Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -16,8 +16,8 @@
 package com.vaadin.tests;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
 
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.openqa.selenium.WebElement;
 
@@ -32,7 +32,7 @@ public class VerifyJreVersionTest extends SingleBrowserTest {
         WebElement jreVersionLabel = vaadinElementById("jreVersionLabel");
 
         assertThat(jreVersionLabel.getText(),
-                is("Using Java 1.8.0_60 by Oracle Corporation"));
+                CoreMatchers.startsWith("Using Java 1.8.0_"));
     }
 
 }
