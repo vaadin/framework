b322664b8b21da53e6fc833005fe3968f8352bec
Henri Sara <hesara@vaadin.com>
2016-08-15 12:31:05 +0300

    Update/remove empty @since for 7.7
    Change-Id: Ib46a450c7d6a6d15eb6bea5d53351d182929e52f

--- server/src/test/java/com/vaadin/server/MockServletConfig.java
+++ server/src/test/java/com/vaadin/server/MockServletConfig.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2000-2014 Vaadin Ltd.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,7 +15,7 @@
  */
 
 /**
- * 
+ *
  */
 package com.vaadin.server;
 
@@ -26,8 +26,8 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 
 /**
- * 
- * @since
+ * Mock servlet configuration for tests.
+ *
  * @author Vaadin Ltd
  */
 public class MockServletConfig implements ServletConfig {
@@ -45,7 +45,7 @@ public class MockServletConfig implements ServletConfig {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see javax.servlet.ServletConfig#getServletName()
      */
     @Override
@@ -55,7 +55,7 @@ public class MockServletConfig implements ServletConfig {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see javax.servlet.ServletConfig#getServletContext()
      */
     @Override
@@ -65,7 +65,7 @@ public class MockServletConfig implements ServletConfig {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see javax.servlet.ServletConfig#getInitParameter(java.lang.String)
      */
     @Override
@@ -75,7 +75,7 @@ public class MockServletConfig implements ServletConfig {
 
     /*
      * (non-Javadoc)
-     * 
+     *
      * @see javax.servlet.ServletConfig#getInitParameterNames()
      */
     @Override
