588f3cc7f8f719a6bd38b4c196fce80c8040b44c
Artur Signell <artur@vaadin.com>
2016-08-18 09:40:34 +0300

    Update copyright year to 2016
    Change-Id: I6485dee1ebba12b180a9063ed4b530f7dc63cd77

--- uitest/src/main/java/com/vaadin/tests/layouts/HorizontalLayoutAndCaretPosition.java
+++ uitest/src/main/java/com/vaadin/tests/layouts/HorizontalLayoutAndCaretPosition.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2014 Vaadin Ltd.
+ * Copyright 2000-2016 Vaadin Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -23,7 +23,7 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 
 /*
- * Copyright 2000-2014 Vaadin Ltd.
+ * Copyright 2000-2016 Vaadin Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
