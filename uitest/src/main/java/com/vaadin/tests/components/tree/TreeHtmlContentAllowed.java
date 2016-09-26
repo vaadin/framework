1dc0f49d1adf51fc9d3d71bcde3eb534f6286c57
Sebastian Nyholm <sebastian@vaadin.com>
2016-09-26 14:24:12 +0300

    Fixes VTree so that you can click sub elements when setHtmlContentAllowed is true. (#20097)
    Change-Id: Ic453aaf16ea1d44792db868e04faaba069aab3b7

--- uitest/src/main/java/com/vaadin/tests/components/tree/TreeHtmlContentAllowed.java
+++ uitest/src/main/java/com/vaadin/tests/components/tree/TreeHtmlContentAllowed.java
@@ -30,18 +30,20 @@ public class TreeHtmlContentAllowed extends AbstractTestUI {
         String htmlParent = "Some <b>html</b>";
         String textChild = "Child text";
         String htmlChild = "Child <i>html</i>";
+        String htmlElementChild = "Child <span id='my-html-element'>element html</span>";
 
         final Tree tree = new Tree("A tree");
         tree.addItem(textParent);
         tree.addItem(htmlParent);
         tree.addItem(textChild);
         tree.addItem(htmlChild);
-
+        tree.addItem(htmlElementChild);
         tree.setParent(textChild, textParent);
         tree.setParent(htmlChild, htmlParent);
 
         tree.setChildrenAllowed(textChild, false);
         tree.setChildrenAllowed(htmlChild, false);
+        tree.setChildrenAllowed(htmlElementChild, false);
 
         final CheckBox toggle = new CheckBox("HTML content allowed",
                 tree.isHtmlContentAllowed());
