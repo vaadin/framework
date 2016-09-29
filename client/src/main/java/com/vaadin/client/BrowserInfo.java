eb2f5ac3a714aefce948d447a4004b94e35e44a9
Johannes Tuikkala <johannes@vaadin.com>
2016-09-29 11:03:29 +0300

    Fixed touch scrolling issue in Surface and WP devices (#18737)
    Fixed by using mouse events instead of touchevents when the browser is
    IE or Edge (added isIEOrEdge utility method in the BrowserInfo). Also
    added touch-action: none; css rules into escalator.css to prevent
    default touch behaviour on IE and Edge
    No new unit tests since we do not have automatic touch testing
    possibilities yet.
    Please test manually with Surface: IE and Edge, use for example uitest:
    com.vaadin.tests.components.grid.basics.GridBasics
    
    Change-Id: I5b37f1648e2051ea0ab4d56ab767186b532da07c

--- client/src/main/java/com/vaadin/client/BrowserInfo.java
+++ client/src/main/java/com/vaadin/client/BrowserInfo.java
@@ -245,6 +245,10 @@ public class BrowserInfo {
         return browserDetails.isEdge();
     }
 
+    public boolean isIEOrEdge() {
+        return browserDetails.isIE() || browserDetails.isEdge();
+    }
+
     public boolean isFirefox() {
         return browserDetails.isFirefox();
     }
