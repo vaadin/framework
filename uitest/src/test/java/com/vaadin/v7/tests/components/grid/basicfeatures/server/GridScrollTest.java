a82c5199c2c3024c52441b09cbe1d6138a94751b
Artur Signell <artur@vaadin.com>
2016-10-19 22:50:22 +0300

    Make Grid isWorkPending wait for scroll events to complete (#20417)
    Change-Id: I3cfdac539111ff2e88c7ff81e6d6b9a579160423

--- uitest/src/test/java/com/vaadin/v7/tests/components/grid/basicfeatures/server/GridScrollTest.java
+++ uitest/src/test/java/com/vaadin/v7/tests/components/grid/basicfeatures/server/GridScrollTest.java
@@ -54,4 +54,18 @@ public class GridScrollTest extends GridBasicFeaturesTest {
                 "0. Requested items 247 - 346", getLogRow(0));
         assertEquals("There should be only one log row", " ", getLogRow(1));
     }
+
+    @Test
+    public void workPendingWhileScrolling() {
+        openTestURL("theme=valo");
+        String script = "var c = window.vaadin.clients.runcomvaadintestscomponentsgridbasicfeaturesGridBasicFeatures;\n"
+                // Scroll down and cause lazy loading
+                + "c.getElementByPath(\"//Grid[0]#cell[21]\"); \n"
+                + "return c.isActive();";
+
+        Boolean active = (Boolean) executeScript(script);
+        assertTrue("Grid should be marked to have workPending while scrolling",
+                active);
+    }
+
 }
