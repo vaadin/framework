3edaebc1e28bd4f1a9d9a6d24b293c76299a8f9c
Patrik Lindström <patrik@vaadin.com>
2016-11-02 11:54:49 +0200

    Add lazy/simple resize mode to Grid (#20108)
    Change-Id: I47427efc28c350382dba8c1f50fd332a3f4585e4

--- server/src/main/java/com/vaadin/ui/Grid.java
+++ server/src/main/java/com/vaadin/ui/Grid.java
@@ -80,6 +80,7 @@ import com.vaadin.shared.ui.grid.HeightMode;
 import com.vaadin.shared.ui.grid.SectionState;
 import com.vaadin.shared.util.SharedUtil;
 import com.vaadin.ui.Grid.FooterRow;
+import com.vaadin.ui.Grid.SelectionMode;
 import com.vaadin.ui.components.grid.AbstractSelectionModel;
 import com.vaadin.ui.components.grid.EditorComponentGenerator;
 import com.vaadin.ui.components.grid.EditorImpl;
