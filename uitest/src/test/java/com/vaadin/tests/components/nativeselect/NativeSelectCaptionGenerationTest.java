e4fbf7a5f2ab83d613100ec83aac99a3f88bf8b9
Teemu Suo-Anttila <teemusa@vaadin.com>
2016-11-30 16:14:57 +0200

    Add test for NativeSelect caption generators
    Change-Id: Id074c55b1c29dfb1cc499d918422bcf1c7bce0ba

---
+++ uitest/src/test/java/com/vaadin/tests/components/nativeselect/NativeSelectCaptionGenerationTest.java
@@ -0,0 +1,65 @@
+package com.vaadin.tests.components.nativeselect;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.testbench.TestBenchElement;
+import com.vaadin.testbench.customelements.NativeSelectElement;
+import com.vaadin.testbench.elements.ButtonElement;
+import com.vaadin.tests.tb3.SingleBrowserTest;
+
+public class NativeSelectCaptionGenerationTest extends SingleBrowserTest {
+
+    private static final String[] value = new String[] { "Foo", "Bar", "Baz",
+            "Spam", "Eggs" };
+
+    @Test
+    public void testDefaultDeclarativeCaptions() {
+        openTestURL();
+        List<TestBenchElement> options = $(NativeSelectElement.class).first()
+                .getOptions();
+        for (int i = 0; i < options.size(); ++i) {
+            Assert.assertEquals("Captions don't match.", value[i],
+                    options.get(i).getText());
+        }
+    }
+
+    @Test
+    public void testToStringCaptions() {
+        openTestURL();
+        $(ButtonElement.class).caption("toString").first().click();
+        List<TestBenchElement> options = $(NativeSelectElement.class).first()
+                .getOptions();
+        for (int i = 0; i < options.size(); ++i) {
+            Assert.assertEquals("Captions don't match.", "Option " + (i + 1),
+                    options.get(i).getText());
+        }
+    }
+
+    @Test
+    public void testNumberOnlyCaptions() {
+        openTestURL();
+        $(ButtonElement.class).caption("Only number").first().click();
+        List<TestBenchElement> options = $(NativeSelectElement.class).first()
+                .getOptions();
+        for (int i = 0; i < options.size(); ++i) {
+            Assert.assertEquals("Captions don't match.", "" + (i + 1),
+                    options.get(i).getText());
+        }
+    }
+
+    @Test
+    public void testChangeGeneratorToStringAndBackToDeclarative() {
+        openTestURL();
+        $(ButtonElement.class).caption("toString").first().click();
+        $(ButtonElement.class).caption("Declarative").first().click();
+        List<TestBenchElement> options = $(NativeSelectElement.class).first()
+                .getOptions();
+        for (int i = 0; i < options.size(); ++i) {
+            Assert.assertEquals("Captions don't match.", value[i],
+                    options.get(i).getText());
+        }
+    }
+}
