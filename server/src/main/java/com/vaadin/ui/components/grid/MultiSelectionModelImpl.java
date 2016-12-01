b11a5e894486e6a3b2a5b88b298f10944072459a
Pekka Hyvönen <pekka@vaadin.com>
2016-12-01 09:37:56 +0200

    Add a SelectionMode shorthand for Grid.
    Hides setSelectionModel(...) by making it protected.
    Refactores the usage of constructor / extend method for Abstract-, Single- and MultiSelectionModelImpl.
    
    Fixes vaadin/framework8-issues#519
    
    Change-Id: I48c30886450506639be9ee6e21c45b0c06755c88

--- server/src/main/java/com/vaadin/ui/components/grid/MultiSelectionModelImpl.java
+++ server/src/main/java/com/vaadin/ui/components/grid/MultiSelectionModelImpl.java
@@ -27,12 +27,12 @@ import java.util.stream.Stream;
 
 import com.vaadin.event.selection.MultiSelectionEvent;
 import com.vaadin.event.selection.MultiSelectionListener;
+import com.vaadin.server.data.DataCommunicator;
 import com.vaadin.server.data.DataProvider;
 import com.vaadin.server.data.Query;
 import com.vaadin.shared.Registration;
 import com.vaadin.shared.data.selection.GridMultiSelectServerRpc;
 import com.vaadin.shared.ui.grid.MultiSelectionModelState;
-import com.vaadin.ui.Grid;
 import com.vaadin.ui.Grid.MultiSelectionModel;
 import com.vaadin.ui.MultiSelect;
 import com.vaadin.util.ReflectTools;
@@ -127,22 +127,12 @@ public class MultiSelectionModelImpl<T> extends AbstractSelectionModel<T>
             .findMethod(MultiSelectionListener.class, "accept",
                     MultiSelectionEvent.class);
 
-    private final Grid<T> grid;
-
     private Set<T> selection = new LinkedHashSet<>();
 
     private SelectAllCheckBoxVisible selectAllCheckBoxVisible = SelectAllCheckBoxVisible.DEFAULT;
 
-    /**
-     * Constructs a new multiselection model for the given grid.
-     *
-     * @param grid
-     *            the grid to bind the selection model into
-     */
-    public MultiSelectionModelImpl(Grid<T> grid) {
-        this.grid = grid;
-        extend(grid);
-
+    @Override
+    protected void init() {
         registerRpc(new GridMultiSelectServerRpcImpl());
     }
 
@@ -164,7 +154,7 @@ public class MultiSelectionModelImpl<T> extends AbstractSelectionModel<T>
      * in- memory.
      *
      * @param selectAllCheckBoxVisible
-     *            the mode to use
+     *            the visiblity mode to use
      * @see SelectAllCheckBoxVisible
      */
     public void setSelectAllCheckBoxVisible(
@@ -178,7 +168,7 @@ public class MultiSelectionModelImpl<T> extends AbstractSelectionModel<T>
     /**
      * Gets the current mode for the select all checkbox visibility.
      *
-     * @return the select all checkbox visibility state
+     * @return the select all checkbox visibility mode
      * @see SelectAllCheckBoxVisible
      * @see #isSelectAllCheckBoxVisible()
      */
@@ -245,8 +235,8 @@ public class MultiSelectionModelImpl<T> extends AbstractSelectionModel<T>
             getState(false).selectAllCheckBoxVisible = false;
             break;
         case DEFAULT:
-            getState(false).selectAllCheckBoxVisible = grid.getDataProvider()
-                    .isInMemory();
+            getState(false).selectAllCheckBoxVisible = getGrid()
+                    .getDataProvider().isInMemory();
             break;
         default:
             break;
@@ -384,7 +374,7 @@ public class MultiSelectionModelImpl<T> extends AbstractSelectionModel<T>
             getState().allSelected = true;
         }
 
-        DataProvider<T, ?> dataSource = grid.getDataProvider();
+        DataProvider<T, ?> dataSource = getGrid().getDataProvider();
         // this will fetch everything from backend
         Stream<T> stream = dataSource.fetch(new Query<>());
         LinkedHashSet<T> allItems = new LinkedHashSet<>();
@@ -460,8 +450,10 @@ public class MultiSelectionModelImpl<T> extends AbstractSelectionModel<T>
             set.addAll(addedItems);
 
             // refresh method is NOOP for items that are not present client side
-            removedItems.forEach(grid.getDataCommunicator()::refresh);
-            addedItems.forEach(grid.getDataCommunicator()::refresh);
+            DataCommunicator<T, ?> dataCommunicator = getGrid()
+                    .getDataCommunicator();
+            removedItems.forEach(dataCommunicator::refresh);
+            addedItems.forEach(dataCommunicator::refresh);
         }, userOriginated);
     }
 
@@ -475,7 +467,7 @@ public class MultiSelectionModelImpl<T> extends AbstractSelectionModel<T>
         LinkedHashSet<T> oldSelection = new LinkedHashSet<>(selection);
         handler.accept(selection);
 
-        fireEvent(new MultiSelectionEvent<>(grid, asMultiSelect(), oldSelection,
-                userOriginated));
+        fireEvent(new MultiSelectionEvent<>(getGrid(), asMultiSelect(),
+                oldSelection, userOriginated));
     }
 }
