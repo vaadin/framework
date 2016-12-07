5778c65bdef65337f8cbcc9d0504abbd11f8278f
Denis <denis@vaadin.com>
2016-12-07 13:27:43 +0200

    Don't use V8 FieldEvents classes in v7 compatibility module. (#93)
    * Don't use V8 FieldEvents classes in v7 compatibility module.
    
    Fixes vaadin/framework8-issues#363

--- server/src/main/java/com/vaadin/ui/TabSheet.java
+++ server/src/main/java/com/vaadin/ui/TabSheet.java
@@ -1369,21 +1369,9 @@ public class TabSheet extends AbstractComponentContainer
     }
 
     @Override
-    @Deprecated
-    public void removeBlurListener(BlurListener listener) {
-        removeListener(BlurEvent.EVENT_ID, BlurEvent.class, listener);
-    }
-
-    @Override
     public Registration addFocusListener(FocusListener listener) {
         return addListener(FocusEvent.EVENT_ID, FocusEvent.class, listener,
                 FocusListener.focusMethod);
-    }
-
-    @Override
-    @Deprecated
-    public void removeFocusListener(FocusListener listener) {
-        removeListener(FocusEvent.EVENT_ID, FocusEvent.class, listener);
     }
 
     @Override
