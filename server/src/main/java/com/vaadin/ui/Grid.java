2f83cadb8dbbf0814922f29d3db04c9c861865da
Teemu Suo-Anttila <teemusa@vaadin.com>
2016-12-13 17:54:23 +0200

    Add mention of sorting

--- server/src/main/java/com/vaadin/ui/Grid.java
+++ server/src/main/java/com/vaadin/ui/Grid.java
@@ -79,7 +79,6 @@ import com.vaadin.shared.ui.grid.HeightMode;
 import com.vaadin.shared.ui.grid.SectionState;
 import com.vaadin.shared.util.SharedUtil;
 import com.vaadin.ui.Grid.FooterRow;
-import com.vaadin.ui.Grid.SelectionMode;
 import com.vaadin.ui.components.grid.AbstractSelectionModel;
 import com.vaadin.ui.components.grid.EditorImpl;
 import com.vaadin.ui.components.grid.Footer;
@@ -2489,8 +2488,9 @@ public class Grid<T> extends AbstractListing<T>
     /**
      * Adds a new text column to this {@link Grid} with a value provider. The
      * column will use a {@link TextRenderer}. The value is converted to a
-     * String using {@link Object#toString()}. Identifier for the column is
-     * generated automatically.
+     * String using {@link Object#toString()}. Sorting in memory is executed by
+     * comparing the String values. Identifier for the column is generated
+     * automatically.
      *
      * @param valueProvider
      *            the value provider
