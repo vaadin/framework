189667e0e19abe02e97f975252e781c2db4d3e27
Henri Sara <henri.sara@gmail.com>
2016-12-14 09:59:37 +0200

    Add compatibility layouts with old defaults. (#7967)
    * Add compatibility layouts with old defaults.
    
    Add compatibility versions of HorizontalLayout and VerticalLayout
    without margins or spacing for ease of migration.
    
    Fixes vaadin/framework8-issues#560
    
    * Deprecate compatibility layouts

---
+++ compatibility-server/src/main/java/com/vaadin/v7/ui/HorizontalLayout.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.v7.ui;
+
+import com.vaadin.ui.AbstractOrderedLayout;
+import com.vaadin.ui.Component;
+
+/**
+ * Horizontal layout
+ *
+ * <code>HorizontalLayout</code> is a component container, which shows the
+ * subcomponents in the order of their addition (horizontally).
+ * <p>
+ * This class is a compatibility version that uses the old defaults (no
+ * spacing), whereas the defaults of {@link com.vaadin.ui.HorizontalLayout} have
+ * changed.
+ *
+ * @author Vaadin Ltd.
+ * @since
+ * @deprecated use com.vaadin.ui.HorizontalLayout (with different defaults)
+ */
+@Deprecated
+public class HorizontalLayout extends com.vaadin.ui.HorizontalLayout {
+    /**
+     * Constructs an empty HorizontalLayout.
+     */
+    public HorizontalLayout() {
+        super();
+        setSpacing(false);
+    }
+
+    /**
+     * Constructs a HorizontalLayout with the given components. The components
+     * are added in the given order.
+     *
+     * @see AbstractOrderedLayout#addComponents(Component...)
+     *
+     * @param children
+     *            The components to add.
+     */
+    public HorizontalLayout(Component... children) {
+        this();
+        addComponents(children);
+    }
+
+}
---
+++ compatibility-server/src/main/java/com/vaadin/v7/ui/VerticalLayout.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.v7.ui;
+
+import com.vaadin.ui.AbstractOrderedLayout;
+import com.vaadin.ui.Component;
+
+/**
+ * Vertical layout
+ *
+ * <code>VerticalLayout</code> is a component container, which shows the
+ * subcomponents in the order of their addition (vertically). A vertical layout
+ * is by default 100% wide.
+ * <p>
+ * This class is a compatibility version that uses the old defaults (no spacing
+ * or margins), whereas the defaults of {@link com.vaadin.ui.VerticalLayout}
+ * have changed.
+ *
+ * @author Vaadin Ltd.
+ * @since
+ * @deprecated use com.vaadin.ui.VerticalLayout (with different defaults)
+ */
+@Deprecated
+public class VerticalLayout extends com.vaadin.ui.VerticalLayout {
+    /**
+     * Constructs an empty VerticalLayout.
+     */
+    public VerticalLayout() {
+        super();
+        setSpacing(false);
+        setMargin(false);
+    }
+
+    /**
+     * Constructs a VerticalLayout with the given components. The components are
+     * added in the given order.
+     *
+     * @see AbstractOrderedLayout#addComponents(Component...)
+     *
+     * @param children
+     *            The components to add.
+     */
+    public VerticalLayout(Component... children) {
+        this();
+        addComponents(children);
+    }
+
+}
