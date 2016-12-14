9e3e78a6d41dbf1b8732799537027a4e980ad9f5
Aleksi Hietanen <aleksi@vaadin.com>
2016-12-14 15:57:11 +0200

    Add EditorComponentGenerator interface
    Closes vaadin/framework8-issues#565

--- server/src/main/java/com/vaadin/ui/Grid.java
+++ server/src/main/java/com/vaadin/ui/Grid.java
@@ -35,6 +35,7 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -48,8 +49,8 @@ import com.vaadin.data.BinderValidationStatus;
 import com.vaadin.data.Listing;
 import com.vaadin.data.SelectionModel;
 import com.vaadin.event.ConnectorEvent;
+import com.vaadin.event.ConnectorEventListener;
 import com.vaadin.event.ContextClickEvent;
-import com.vaadin.event.SerializableEventListener;
 import com.vaadin.event.selection.MultiSelectionListener;
 import com.vaadin.event.selection.SelectionListener;
 import com.vaadin.event.selection.SingleSelectionListener;
@@ -77,6 +78,7 @@ import com.vaadin.shared.ui.grid.GridStaticCellType;
 import com.vaadin.shared.ui.grid.HeightMode;
 import com.vaadin.shared.ui.grid.SectionState;
 import com.vaadin.shared.util.SharedUtil;
+import com.vaadin.ui.Grid.FooterRow;
 import com.vaadin.ui.components.grid.AbstractSelectionModel;
 import com.vaadin.ui.components.grid.EditorImpl;
 import com.vaadin.ui.components.grid.Footer;
@@ -991,7 +993,7 @@ public class Grid<T> extends AbstractListing<T>
         private StyleGenerator<T> styleGenerator = item -> null;
         private DescriptionGenerator<T> descriptionGenerator;
 
-        private SerializableFunction<T, Component> componentGenerator;
+        private EditorComponentGenerator<T> componentGenerator;
 
         /**
          * Constructs a new Column configuration with given header caption,
@@ -1702,7 +1704,7 @@ public class Grid<T> extends AbstractListing<T>
          * @return this column
          *
          * @see #setEditorComponent(Component)
-         * @see #setEditorComponentGenerator(SerializableFunction)
+         * @see #setEditorComponentGenerator(EditorComponentGenerator)
          */
         public Column<T, V> setEditable(boolean editable) {
             Objects.requireNonNull(componentGenerator,
@@ -1733,7 +1735,7 @@ public class Grid<T> extends AbstractListing<T>
          *
          * @see Editor#getBinder()
          * @see Editor#setBinder(Binder)
-         * @see #setEditorComponentGenerator(SerializableFunction)
+         * @see #setEditorComponentGenerator(EditorComponentGenerator)
          */
         public Column<T, V> setEditorComponent(Component component) {
             Objects.requireNonNull(component,
@@ -1742,7 +1744,7 @@ public class Grid<T> extends AbstractListing<T>
         }
 
         /**
-         * Sets a component generator to provide editor component for this
+         * Sets a component generator to provide an editor component for this
          * Column. This method can be used to generate any dynamic component to
          * be displayed in the editor row.
          * <p>
@@ -1753,10 +1755,11 @@ public class Grid<T> extends AbstractListing<T>
          *            the editor component generator
          * @return this column
          *
+         * @see EditorComponentGenerator
          * @see #setEditorComponent(Component)
          */
         public Column<T, V> setEditorComponentGenerator(
-                SerializableFunction<T, Component> componentGenerator) {
+                EditorComponentGenerator<T> componentGenerator) {
             Objects.requireNonNull(componentGenerator);
             this.componentGenerator = componentGenerator;
             return setEditable(true);
@@ -1766,8 +1769,10 @@ public class Grid<T> extends AbstractListing<T>
          * Gets the editor component generator for this Column.
          *
          * @return editor component generator
+         *
+         * @see EditorComponentGenerator
          */
-        public SerializableFunction<T, Component> getEditorComponentGenerator() {
+        public EditorComponentGenerator<T> getEditorComponentGenerator() {
             return componentGenerator;
         }
 
