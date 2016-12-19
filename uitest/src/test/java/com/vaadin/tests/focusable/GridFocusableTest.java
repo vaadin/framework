2c737023ef835b33b85b2dd097c0f164ba22b94a
Denis Anisimov <denis@vaadin.com>
2016-12-19 09:50:07 +0200

    Fix Grid focusable test.

--- uitest/src/test/java/com/vaadin/tests/focusable/GridFocusableTest.java
+++ uitest/src/test/java/com/vaadin/tests/focusable/GridFocusableTest.java
@@ -1,5 +1,9 @@
 package com.vaadin.tests.focusable;
 
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.testbench.By;
 import com.vaadin.testbench.customelements.GridElement;
 import com.vaadin.testbench.elements.GridElement.GridCellElement;
 import com.vaadin.tests.components.grid.basics.GridBasics;
@@ -24,5 +28,11 @@ public class GridFocusableTest extends AbstractFocusableComponentTest {
     @Override
     protected GridCellElement getFocusElement() {
         return $(GridElement.class).first().getCell(0, 0);
+    }
+
+    @Override
+    protected WebElement getMenuElement(String menuCaption)
+            throws NoSuchElementException {
+        return super.getMenuElement(menuCaption).findElement(By.xpath(".."));
     }
 }
