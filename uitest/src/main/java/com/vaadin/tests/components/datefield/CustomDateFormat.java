7b6c97bd4b30adab6aec26db5e15b7f1117d418b
Denis <denis@vaadin.com>
2016-12-28 11:58:04 +0200

    Clean up DateField UI tests. Fixes vaadin/framework8-issues#584

--- uitest/src/main/java/com/vaadin/tests/components/datefield/CustomDateFormat.java
+++ uitest/src/main/java/com/vaadin/tests/components/datefield/CustomDateFormat.java
@@ -3,22 +3,23 @@ package com.vaadin.tests.components.datefield;
 import java.time.LocalDate;
 import java.util.Locale;
 
+import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.ui.datefield.Resolution;
-import com.vaadin.tests.components.TestBase;
+import com.vaadin.tests.components.AbstractTestUI;
 import com.vaadin.tests.components.TestDateField;
 import com.vaadin.ui.AbstractDateField;
 
-public class CustomDateFormat extends TestBase {
+public class CustomDateFormat extends AbstractTestUI {
 
     @Override
-    protected void setup() {
+    protected void setup(VaadinRequest request) {
         Locale locale = new Locale("fi", "FI");
         AbstractDateField df = new TestDateField();
         df.setResolution(Resolution.DAY);
         df.setLocale(locale);
         df.setWidth("300px");
 
-        String pattern = "d. MMMM'ta 'yyyy 'klo";
+        String pattern = "d. MMMM'ta 'yyyy 'klo'";
         df.setDateFormat(pattern);
 
         df.setValue(LocalDate.of(2010, 1, 1));
@@ -28,7 +29,7 @@ public class CustomDateFormat extends TestBase {
     }
 
     @Override
-    protected String getDescription() {
+    protected String getTestDescription() {
         return "Month name should be visible in text box if format pattern includes it";
     }
 
