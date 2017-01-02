adbe38eea2174322e637c56399493b62a1fdd444
Denis <denis@vaadin.com>
2017-01-02 13:43:29 +0200

    Grid UI tests migration P2.
    Fixes vaadin/framework8-issues#588

--- uitest/src/main/java/com/vaadin/tests/components/grid/GridCellFocusOnResetSize.java
+++ uitest/src/main/java/com/vaadin/tests/components/grid/GridCellFocusOnResetSize.java
@@ -13,21 +13,22 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.vaadin.v7.tests.components.grid;
+package com.vaadin.tests.components.grid;
 
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.tests.components.AbstractReindeerTestUI;
 import com.vaadin.tests.widgetset.TestingWidgetSet;
-import com.vaadin.tests.widgetset.client.v7.grid.GridDataChangeHandlerWidget;
+import com.vaadin.tests.widgetset.client.grid.GridCellFocusOnResetSizeWidget;
 import com.vaadin.tests.widgetset.server.TestWidgetComponent;
 
 @Widgetset(TestingWidgetSet.NAME)
-public class GridClientDataChangeHandler extends AbstractReindeerTestUI {
+public class GridCellFocusOnResetSize extends AbstractReindeerTestUI {
 
     @Override
     protected void setup(VaadinRequest request) {
         addComponent(
-                new TestWidgetComponent(GridDataChangeHandlerWidget.class));
+                new TestWidgetComponent(GridCellFocusOnResetSizeWidget.class));
     }
+
 }
