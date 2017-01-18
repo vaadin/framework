bfadd837548f564bdc445d9e1ba96354eec2f166
adam <adam@vaadin.com>
2017-01-18 12:05:53 +0200

    Added license headers

--- client/src/main/java/com/vaadin/client/extensions/DragSourceExtensionConnector.java
+++ client/src/main/java/com/vaadin/client/extensions/DragSourceExtensionConnector.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.vaadin.client.extensions;
 
 import java.util.Map;
@@ -36,6 +51,8 @@ public class DragSourceExtensionConnector extends AbstractExtensionConnector {
             for (String format : data.keySet()) {
                 event.setData(format, data.get(format));
             }
+
+            // TODO: 16/01/2017 Add source object to dataTransfer
         }, DragStartEvent.getType());
     }
 
