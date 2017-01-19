8906e0225df334371fc22c7f1e34987067f8f68a
Aleksi Hietanen <aleksi@vaadin.com>
2017-01-19 09:23:23 +0200

    Fix closing of modal window curtains while dragging and resizing

--- uitest/src/test/java/com/vaadin/tests/themes/valo/ModalWindowTest.java
+++ uitest/src/test/java/com/vaadin/tests/themes/valo/ModalWindowTest.java
@@ -1,14 +1,18 @@
 package com.vaadin.tests.themes.valo;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
 import com.vaadin.testbench.By;
 import com.vaadin.testbench.elements.ButtonElement;
 import com.vaadin.tests.ModalWindow;
 import com.vaadin.tests.tb3.SingleBrowserTest;
-import org.junit.Test;
-import org.openqa.selenium.WebElement;
-
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
 
 public class ModalWindowTest extends SingleBrowserTest {
 
@@ -30,7 +34,30 @@ public class ModalWindowTest extends SingleBrowserTest {
                 is("none"));
     }
 
+    @Test
+    public void modal_curtains_close_correctly() {
+        openTestURL();
+
+        openModalWindow();
+        new Actions(getDriver())
+                .moveToElement(findElement(By.className("v-window-header")))
+                .clickAndHold().sendKeys(Keys.ESCAPE).perform();
+        verifyCurtainsNotPresent();
+
+        openModalWindow();
+        new Actions(getDriver())
+                .moveToElement(findElement(By.className("v-window-resizebox")))
+                .clickAndHold().sendKeys(Keys.ESCAPE).perform();
+        verifyCurtainsNotPresent();
+    }
+
     private void openModalWindow() {
         $(ButtonElement.class).get(1).click();
+    }
+
+    private void verifyCurtainsNotPresent() {
+        assertFalse(isElementPresent(By.className("v-window-modalitycurtain")));
+        assertFalse(isElementPresent(By.className("v-window-draggingCurtain")));
+        assertFalse(isElementPresent(By.className("v-window-resizingCurtain")));
     }
 }
