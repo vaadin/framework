9bec628632b5ba8840295bda158dd049ef0e648f
elmot <elmot@vaadin.com>
2017-01-27 13:38:27 +0200

    JSR356 servlet added

--- test/servlet-containers/generic/src/main/java/com/vaadin/tests/IntegrationTestUIProvider.java
+++ test/servlet-containers/generic/src/main/java/com/vaadin/tests/IntegrationTestUIProvider.java
@@ -15,7 +15,7 @@ public class IntegrationTestUIProvider extends UIProvider {
 
     public static final String[] defaultPackages = {
             "com.vaadin.tests.integration",
-            "com.vaadin.tests.integration.push" };
+            "com.vaadin.tests.integration.push"};
 
     @Override
     public Class<? extends UI> getUIClass(UIClassSelectionEvent event) {
@@ -62,9 +62,18 @@ public class IntegrationTestUIProvider extends UIProvider {
         }
     }
 
-    @WebServlet(urlPatterns = "/*", name = "IntegrationTestUIProvider", asyncSupported = true, initParams = {
-            @WebInitParam(name = "UIProvider", value = "com.vaadin.tests.IntegrationTestUIProvider") })
+    @WebServlet(urlPatterns = {"/run/*", "/VAADIN/*"}, name = "IntegrationTestUIProvider", asyncSupported = true, initParams = {
+            @WebInitParam(name = "UIProvider", value = "com.vaadin.tests.IntegrationTestUIProvider")})
     @VaadinServletConfiguration(ui = ServletIntegrationUI.class, productionMode = false)
     public static class MyServlet extends VaadinServlet {
+    }
+
+    @WebServlet(urlPatterns = "/run-jsr356/*", name = "IntegrationUIProvider-Jsr356", asyncSupported = false,
+            initParams = {
+                    @WebInitParam(name = "UIProvider", value = "com.vaadin.tests.IntegrationTestUIProvider"),
+                    @WebInitParam(name = "org.atmosphere.cpr.asyncSupport", value = "org.atmosphere.container.JSR356AsyncSupport")
+            })
+    @VaadinServletConfiguration(ui = ServletIntegrationUI.class, productionMode = false)
+    public static class JSR356Servlet extends VaadinServlet {
     }
 }
