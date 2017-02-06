b485c0b7d27175e0cbf50202129eee5f63cb0b2f
Henri Sara <hesara@vaadin.com>
2017-02-06 11:17:49 +0200

    Add Firefox 45 workaround in ComboBoxElement

--- testbench-api/src/main/java/com/vaadin/testbench/elements/ComboBoxElement.java
+++ testbench-api/src/main/java/com/vaadin/testbench/elements/ComboBoxElement.java
@@ -25,6 +25,7 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 
 import com.vaadin.testbench.By;
+import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elementsbase.ServerClass;
 
 @ServerClass("com.vaadin.ui.ComboBox")
@@ -86,7 +87,7 @@ public class ComboBoxElement extends AbstractSelectElement {
     private boolean selectSuggestion(String text) {
         for (WebElement suggestion : getPopupSuggestionElements()) {
             if (text.equals(suggestion.getText())) {
-                suggestion.click();
+                clickElement(suggestion);
                 return true;
             }
         }
@@ -181,7 +182,7 @@ public class ComboBoxElement extends AbstractSelectElement {
      */
     public boolean openNextPage() {
         try {
-            getSuggestionPopup().findElement(byNextPage).click();
+            clickElement(getSuggestionPopup().findElement(byNextPage));
             return true;
         } catch (WebDriverException e) {
             // PhantomJS driver can throw WDE instead of the more specific
@@ -197,7 +198,7 @@ public class ComboBoxElement extends AbstractSelectElement {
      */
     public boolean openPrevPage() {
         try {
-            getSuggestionPopup().findElement(byPrevPage).click();
+            clickElement(getSuggestionPopup().findElement(byPrevPage));
             return true;
         } catch (WebDriverException e) {
             // PhantomJS driver can throw WDE instead of the more specific
@@ -273,6 +274,15 @@ public class ComboBoxElement extends AbstractSelectElement {
                 Thread.sleep(delay);
             } catch (InterruptedException e) {
             }
+        }
+    }
+
+    private void clickElement(WebElement element) {
+        if (isFirefox()) {
+            // Workaround for Selenium/TB and Firefox 45 issue
+            ((TestBenchElement) (element)).clickHiddenElement();
+        } else {
+            element.click();
         }
     }
 }
