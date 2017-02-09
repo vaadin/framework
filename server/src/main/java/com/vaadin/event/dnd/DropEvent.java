0b8030519d16dd1a0ad19982d7afb4407bd9344d
Adam Wagner <wbadam@users.noreply.github.com>
2017-02-09 07:52:15 +0100

    HTML5 Drag and Drop Support (#8264)
    * Add DragSource Extension (#8169)
    
    * Add DropTarget Extension (#8170)
    
    * Add DragStart Event to DragSource Extension (#8171)
    
    * Make DataTransfer.dropEffect configurable (#8174)
    
    * Make DragSource.dataTransfer data configurable (#8172)
    
    * Add server-side Event for drop (#8177)
    
    * Added license headers
    
    * Extract handler methods, move DropEvent and DropListener to new file, move enums to top
    
    * Replaced LinkedHashMap with Map and added List to preserve order of data
    
    * Add API for adding a JS acceptance criteria for dragover and drop (#8178, #8179)
    
    * Make DragSource Extension extendable (#8175)
    
    * Make DropTarget Extension extendable (#8176)
    
    * Added javadoc to protected methods
    
    * Moved EffectAllowed to shared so that it could be used in shared state directly
    
    * Moved DropEffect to separate file, some review fixes and javadoc
    
    * Added list to DropTargetRpc to preserve order of data
    
    * Remove event listeners on unregister
    
    * Changed method names set/getData() to more descriptive set/getTransferData()
    
    * Add server side dragStart event (#8171)
    
    * Add style to prevent text selection to allow drag
    
    * Remove target indicator style on drop
    
    * Add client side dragend event listener for drag source
    
    * Add server side dragend listener.
    Attach client side listener only when server side listener added.
    
    * Add drag source information to server side dragstart and dragend events.
    
    * Fixed some issues addressed in review
    
    * Trigger server side dragstart only when there is a listener attached
    
    * Criteria script can be set as null to clear
    
    * Use Js Interop instead of JSNI for handling event listeners
    
    * Use elemental package instead of Js Interop for handling event listeners
    
    * Add missing javadoc for public methods
    
    * Add default value "uninitialized" to effectAllowed parameter
    
    * Simple test UI for HTML5 DnD functionality (#8395)
    
    * Add javadoc and other minor changes

---
+++ server/src/main/java/com/vaadin/event/dnd/DropEvent.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.event.dnd;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.vaadin.shared.ui.dnd.DropEffect;
+import com.vaadin.ui.Component;
+
+/**
+ * Server side drop event. Fired when an HTML5 drop happens.
+ *
+ * @see DropTargetExtension#addDropListener(DropListener)
+ */
+public class DropEvent extends Component.Event {
+    private final Map<String, String> data;
+    private final DropEffect dropEffect;
+
+    /**
+     * Creates a new server side drop event.
+     *
+     * @param source
+     *         Drop target component.
+     * @param types
+     *         List of data types from {@code DataTransfer.types}.
+     * @param data
+     *         Map of all data from {@code DataTransfer}.
+     * @param dropEffect
+     *         Parameter from {@code DataTransfer.dropEffect}.
+     */
+    public DropEvent(Component source, List<String> types,
+            Map<String, String> data, DropEffect dropEffect) {
+        super(source);
+
+        // Create a linked map that preserves the order of types
+        this.data = new LinkedHashMap<>();
+        types.forEach(type -> this.data.put(type, data.get(type)));
+
+        this.dropEffect = dropEffect;
+    }
+
+    /**
+     * Get data from the client side {@code DataTransfer} object.
+     *
+     * @param format
+     *         Data format, e.g. {@code text/plain} or {@code text/uri-list}.
+     * @return Data for the given format if exists in the client side {@code
+     * DataTransfer}, otherwise {@code null}.
+     */
+    public String getTransferData(String format) {
+        return data != null ? data.get(format) : null;
+    }
+
+    /**
+     * Get drop effect set for the current drop target.
+     *
+     * @return {@code dropEffect} parameter set for the current drop target.
+     */
+    public DropEffect getDropEffect() {
+        return dropEffect;
+    }
+}
