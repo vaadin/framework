a3d7ed049c5fdfb69ea75349d69a0192da36f227
Olli Tietäväinen <ollit@vaadin.com>
2017-02-14 15:25:09 +0200

    simplify test to use executeScript from parent class #1149

--- uitest/src/main/java/com/vaadin/tests/components/panel/PanelSetScrollTopWithLargeNumber.java
+++ uitest/src/main/java/com/vaadin/tests/components/panel/PanelSetScrollTopWithLargeNumber.java
@@ -16,7 +16,7 @@
 package com.vaadin.tests.components.panel;
 
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.tests.components.AbstractTestUI;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
