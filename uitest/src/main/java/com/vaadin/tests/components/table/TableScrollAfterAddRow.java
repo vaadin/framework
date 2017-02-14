1f879cca1729a616678c851a239a6cff1d74fef3
elmot <elmotelmot.vaadin.com>
2017-02-14 16:21:39 +0200

    Update copyright year to 2017

--- uitest/src/main/java/com/vaadin/tests/components/table/TableScrollAfterAddRow.java
+++ uitest/src/main/java/com/vaadin/tests/components/table/TableScrollAfterAddRow.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2016 Vaadin Ltd.
+ * Copyright 2000-2017 Vaadin Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -26,7 +26,7 @@ import com.vaadin.v7.data.util.IndexedContainer;
 import com.vaadin.v7.ui.Table;
 
 /*
- * Copyright 2000-2016 Vaadin Ltd.
+ * Copyright 2000-2017 Vaadin Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
