6dc682a5f1c321c13fc84756eb4bd2460535b37e
Olli Tietäväinen <ollit@vaadin.com>
2017-02-14 15:25:09 +0200

    simplify test to use executeScript from parent class

--- uitest/src/test/java/com/vaadin/tests/components/panel/PanelSetScrollTopWithLargeNumberTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/panel/PanelSetScrollTopWithLargeNumberTest.java
@@ -1,9 +1,22 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.vaadin.tests.components.panel;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import com.vaadin.testbench.elements.PanelElement;
@@ -29,18 +42,6 @@ public class PanelSetScrollTopWithLargeNumberTest extends MultiBrowserTest {
         assertGreater(
                 "Panel should scroll when scrollTop is set to a number larger than panel height",
                 panelContentScrollTop, 0);
-    }
-
-    private Object executeScript(String script, WebElement element) {
-        final WebDriver driver = getDriver();
-        if (driver instanceof JavascriptExecutor) {
-            final JavascriptExecutor je = (JavascriptExecutor) driver;
-            return je.executeScript(script, element);
-        } else {
-            throw new IllegalStateException("current driver "
-                    + getDriver().getClass().getName() + " is not a "
-                    + JavascriptExecutor.class.getSimpleName());
-        }
     }
 
 }
