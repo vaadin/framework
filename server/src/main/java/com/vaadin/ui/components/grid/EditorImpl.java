1161c3947098e996efc292c5c696383971f1465f
Henri Sara <henri.sara@gmail.com>
2017-02-16 10:38:26 +0200

    Fix @since tag in EditorImpl

--- server/src/main/java/com/vaadin/ui/components/grid/EditorImpl.java
+++ server/src/main/java/com/vaadin/ui/components/grid/EditorImpl.java
@@ -45,7 +45,7 @@ import elemental.json.JsonObject;
  *
  * @param <T>
  *            the grid bean type
- * @since 8.0 s
+ * @since 8.0
  */
 public class EditorImpl<T> extends AbstractGridExtension<T>
         implements Editor<T> {
