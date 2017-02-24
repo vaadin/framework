7e9d1c2826beb365451ce3fc963dd0d668c5a1e8
adam <adam@vaadin.com>
2017-02-24 19:05:56 +0200

    Create initial grid drop target classes

---
+++ client/src/main/java/com/vaadin/client/connectors/grid/GridDropTargetExtensionConnector.java
@@ -0,0 +1,10 @@
+package com.vaadin.client.connectors.grid;
+
+import com.vaadin.client.extensions.DropTargetExtensionConnector;
+import com.vaadin.shared.ui.Connect;
+import com.vaadin.ui.GridDropTargetExtension;
+
+@Connect(GridDropTargetExtension.class)
+public class GridDropTargetExtensionConnector extends
+        DropTargetExtensionConnector {
+}
---
+++ shared/src/main/java/com/vaadin/shared/ui/grid/GridDropTargetExtensionState.java
@@ -0,0 +1,7 @@
+package com.vaadin.shared.ui.grid;
+
+import com.vaadin.shared.ui.dnd.DropTargetState;
+
+public class GridDropTargetExtensionState extends DropTargetState {
+
+}
