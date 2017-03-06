4ab3beb0eff655f21579a67abfed6c745e10ef53
Adam Wagner <wbadam@users.noreply.github.com>
2017-03-06 13:22:37 +0200

    Add possiblity to customize drag data for each row (#8706)
    Fixes #8396

--- server/src/main/java/com/vaadin/ui/GridDragSourceExtension.java
+++ server/src/main/java/com/vaadin/ui/GridDragSourceExtension.java
@@ -15,16 +15,34 @@
  */
 package com.vaadin.ui;
 
+import java.util.Optional;
+
+import com.vaadin.data.provider.DataGenerator;
 import com.vaadin.event.dnd.DragSourceExtension;
+import com.vaadin.server.SerializableFunction;
 import com.vaadin.shared.ui.grid.GridDragSourceExtensionState;
+
+import elemental.json.JsonObject;
 
 /**
  * Makes a Grid's rows draggable for HTML5 drag and drop functionality.
  *
+ * @param <T>
+ *         The Grid bean type.
  * @author Vaadin Ltd.
  * @since
  */
-public class GridDragSourceExtension extends DragSourceExtension<Grid> {
+public class GridDragSourceExtension<T> extends DragSourceExtension<Grid<T>> {
+
+    /**
+     * Drag data generator that appends drag data to each row.
+     */
+    private DataGenerator<T> dragDataGenerator;
+
+    /**
+     * Drag data generator function that is executed for each row.
+     */
+    private SerializableFunction<T, JsonObject> generatorFunction;
 
     /**
      * Extends a Grid and makes it's rows draggable.
@@ -32,8 +50,67 @@ public class GridDragSourceExtension extends DragSourceExtension<Grid> {
      * @param target
      *         Grid to be extended.
      */
-    public GridDragSourceExtension(Grid target) {
+    public GridDragSourceExtension(Grid<T> target) {
         super(target);
+
+        // Create drag data generator
+        dragDataGenerator = this::generateDragData;
+
+        // Add drag data generator to Grid
+        target.addDataGenerator(dragDataGenerator);
+    }
+
+    /**
+     * Drag data generator. Appends drag data to row data json if generator
+     * function is set by the user of this extension.
+     *
+     * @param item
+     *         Row item for data generation.
+     * @param jsonObject
+     *         Row data in json format.
+     */
+    private void generateDragData(T item, JsonObject jsonObject) {
+        Optional.ofNullable(generatorFunction).ifPresent(generator -> jsonObject
+                .put(GridDragSourceExtensionState.JSONKEY_DRAG_DATA,
+                        generator.apply(item)));
+    }
+
+    /**
+     * Sets a generator function for customizing drag data. The function is
+     * executed for each item in the Grid during data generation. Return a
+     * {@link JsonObject} to be appended to the row data.
+     * <p>
+     * Example:
+     * <pre>
+     *     dragSourceExtension.setDragDataGenerator(item -> {
+     *         JsonObject dragData = Json.createObject();
+     *         dragData.put("someKey", item.getValue());
+     *         return dragData;
+     *     });
+     * </pre>
+     *
+     * @param generator
+     *         Function to be executed on row data generation.
+     */
+    public void setDragDataGenerator(
+            SerializableFunction<T, JsonObject> generator) {
+        generatorFunction = generator;
+    }
+
+    /**
+     * Returns the generator function for customizing drag data.
+     *
+     * @return Drag data generator function.
+     */
+    public SerializableFunction<T, JsonObject> getDragDataGenerator() {
+        return generatorFunction;
+    }
+
+    @Override
+    public void remove() {
+        super.remove();
+
+        getParent().removeDataGenerator(dragDataGenerator);
     }
 
     @Override
