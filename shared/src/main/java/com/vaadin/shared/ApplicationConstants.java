3d337da49225558ed4f2a39ceafa8b5b6f5a4ff8
Artur <artur@vaadin.com>
2017-03-09 14:50:46 +0200

    Include charset in text/html responses (#8777)
    Fixes #8775

--- shared/src/main/java/com/vaadin/shared/ApplicationConstants.java
+++ shared/src/main/java/com/vaadin/shared/ApplicationConstants.java
@@ -179,4 +179,9 @@ public class ApplicationConstants implements Serializable {
      */
     public static final String WIDGETSET_VERSION_ID = "wsver";
 
+    /**
+     * Content type to use for text/html responses (should always be UTF-8).
+     */
+    public static final String CONTENT_TYPE_TEXT_HTML_UTF_8 = "text/html; charset=utf-8";
+
 }
