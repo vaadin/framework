2f1d08b131425b94a1105e75ccab461602b9703e
Mirjan Merruko <mirjan@vaadin.com>
2017-03-24 13:45:21 +0200

    Add liferay integration originally developed by Sampsa Sohlman #8834

---
+++ liferay-integration/src/main/java/com/vaadin/osgi/liferay/OSGiUIProvider.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.osgi.liferay;
+
+import org.osgi.framework.ServiceObjects;
+
+import com.vaadin.server.UIClassSelectionEvent;
+import com.vaadin.server.UIProvider;
+import com.vaadin.ui.UI;
+
+/**
+ * 
+ * @author Sampsa Sohlman
+ *
+ */
+@SuppressWarnings("serial")
+public class OSGiUIProvider extends UIProvider {
+    private Class<UI> uiClass;
+
+    @SuppressWarnings("unchecked")
+    public OSGiUIProvider(ServiceObjects<UI> serviceObjects) {
+        super();
+        UI ui = serviceObjects.getService();
+        uiClass = (Class<UI>) ui.getClass();
+        serviceObjects.ungetService(ui);
+    }
+
+    @Override
+    public Class<? extends UI> getUIClass(UIClassSelectionEvent event) {
+        return uiClass;
+    }
+
+    public String getDefaultPortletName() {
+        return uiClass.getName();
+    }
+
+    public String getDefaultDisplayName() {
+        return uiClass.getSimpleName();
+    }
+}
