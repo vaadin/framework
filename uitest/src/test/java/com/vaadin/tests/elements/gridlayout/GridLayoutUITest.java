9f37256eec0f5ee5b05e05326e7abcb4fba5eb8d
Artur Signell <artur@vaadin.com>
2017-04-04 23:39:16 +0300

    Provide GridLayout elements to JS and TB

--- uitest/src/test/java/com/vaadin/tests/elements/gridlayout/GridLayoutUITest.java
+++ uitest/src/test/java/com/vaadin/tests/elements/gridlayout/GridLayoutUITest.java
@@ -17,6 +17,8 @@ package com.vaadin.tests.elements.gridlayout;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
 
 import com.vaadin.testbench.elements.GridLayoutElement;
 import com.vaadin.tests.tb3.SingleBrowserTest;
@@ -39,5 +41,32 @@ public class GridLayoutUITest extends SingleBrowserTest {
                 .id(GridLayoutUI.ONE_ROW_ONE_COL).getColumnCount());
         Assert.assertEquals(10, $(GridLayoutElement.class)
                 .id(GridLayoutUI.TEN_ROWS_TEN_COLS).getColumnCount());
+    }
+
+    @Test
+    public void getCell() {
+        openTestURL();
+        GridLayoutElement grid = $(GridLayoutElement.class)
+                .id(GridLayoutUI.TEN_ROWS_TEN_COLS);
+
+        WebElement cell55 = grid.getCell(5, 5);
+        Assert.assertEquals("v-gridlayout-slot", cell55.getAttribute("class"));
+        Assert.assertEquals("5-5", cell55.getText());
+
+        try {
+            grid.getCell(4, 4);
+            Assert.fail("Should throw for empty cell");
+        } catch (NoSuchElementException e) {
+        }
+
+        WebElement cell77 = grid.getCell(7, 7);
+        Assert.assertEquals("v-gridlayout-slot", cell77.getAttribute("class"));
+        Assert.assertEquals("7-7 8-8", cell77.getText());
+
+        try {
+            grid.getCell(7, 8);
+            Assert.fail("Should throw for merged cell");
+        } catch (NoSuchElementException e) {
+        }
     }
 }
