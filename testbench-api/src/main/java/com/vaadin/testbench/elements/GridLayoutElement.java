9a70e9123384e78d6af8b716128895957692fca0
Artur <artur@vaadin.com>
2017-04-13 10:25:20 +0300

    Provide GridLayout size and cell elements to JS and TB (#9019)

--- testbench-api/src/main/java/com/vaadin/testbench/elements/GridLayoutElement.java
+++ testbench-api/src/main/java/com/vaadin/testbench/elements/GridLayoutElement.java
@@ -15,9 +15,64 @@
  */
 package com.vaadin.testbench.elements;
 
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
 import com.vaadin.testbench.elementsbase.ServerClass;
 
 @ServerClass("com.vaadin.ui.GridLayout")
 public class GridLayoutElement extends AbstractLayoutElement {
+    /**
+     * Gets the total number of rows in the layout.
+     *
+     * @return the number of rows in the layout,
+     */
+    public long getRowCount() {
+        Long res = (Long) getCommandExecutor()
+                .executeScript("return arguments[0].getRowCount()", this);
+        if (res == null) {
+            throw new IllegalStateException("getRowCount returned null");
+        }
 
+        return res.longValue();
+    }
+
+    /**
+     * Gets the total number of columns in the layout.
+     *
+     * @return the number of columns in the layout,
+     */
+    public long getColumnCount() {
+        Long res = (Long) getCommandExecutor()
+                .executeScript("return arguments[0].getColumnCount()", this);
+        if (res == null) {
+            throw new IllegalStateException("getColumnCount returned null");
+        }
+
+        return res.longValue();
+    }
+
+    /**
+     * Gets the cell element at the given position.
+     *
+     * @param row
+     *            the row coordinate
+     * @param column
+     *            the column coordinate
+     * @return the cell element at the given position
+     * @throws NoSuchElementException
+     *             if no cell was found at the given position
+     */
+    public WebElement getCell(int row, int column) {
+        WebElement res = (WebElement) getCommandExecutor().executeScript(
+                "return arguments[0].getCell(" + row + "," + column + ")",
+                this);
+        if (res == null) {
+            throw new NoSuchElementException(
+                    "No cell found at " + row + "," + column);
+        }
+
+        return res;
+
+    }
 }
