5d1420cb614cd761d241f87ee6ac5dfd0922d239
Johannes Tuikkala <johannes@vaadin.com>
2017-04-20 09:58:06 +0300

    Back port the fix for #8840 from 8.0 to 7.7 (#8840)
    Added a new test that verifies the fix for Vaadin 7.7, since Vaadin 8.0
    version used TreeGrid not available for 7.7. Used customised
    IndexedContainer (with ability to remove a range of rows) to enable
    easy reproducibility of the issue.

--- uitest/src/test/java/com/vaadin/tests/components/grid/GridRemoveCachedRowsTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/grid/GridRemoveCachedRowsTest.java
@@ -18,13 +18,13 @@ package com.vaadin.tests.components.grid;
 import org.junit.Test;
 
 import com.vaadin.testbench.elements.ButtonElement;
-import com.vaadin.tests.tb3.MultiBrowserTest;
+import com.vaadin.tests.tb3.SingleBrowserTest;
 
 /**
  * Verifies that there's no client side errors when removing a range of rows
  * starting from the visible ones and ending into the cached ones.
  */
-public class GridRemoveCachedRowsTest extends MultiBrowserTest {
+public class GridRemoveCachedRowsTest extends SingleBrowserTest {
 
     @Test
     public void testNoClientExceptionWhenRemovingARangeOfRows() {
