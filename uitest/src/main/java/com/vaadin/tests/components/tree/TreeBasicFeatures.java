935e852d1bebdb7b8c044e3fb5db397ac1ffff88
Teemu Suo-Anttila <teemusa@vaadin.com>
2017-04-26 14:48:06 +0300

    Add missing feature to Tree
    This patch adds caption and icon generators for Tree. A new custom
    renderer for displaying and tracking icon resources has been added
    along with various bug fixes to renderer handling.

--- uitest/src/main/java/com/vaadin/tests/components/tree/TreeBasicFeatures.java
+++ uitest/src/main/java/com/vaadin/tests/components/tree/TreeBasicFeatures.java
@@ -6,73 +6,16 @@ import java.util.List;
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.data.HierarchyData;
 import com.vaadin.data.provider.InMemoryHierarchicalDataProvider;
+import com.vaadin.icons.VaadinIcons;
+import com.vaadin.server.ClassResource;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.tests.components.AbstractTestUIWithLog;
+import com.vaadin.tests.data.bean.HierarchicalTestBean;
 import com.vaadin.ui.Tree;
 
 @Widgetset("com.vaadin.DefaultWidgetSet")
 public class TreeBasicFeatures extends AbstractTestUIWithLog {
-
-    static class HierarchicalTestBean {
-
-        private final String id;
-        private final int depth;
-        private final int index;
-
-        public HierarchicalTestBean(String parentId, int depth, int index) {
-            id = (parentId == null ? "" : parentId) + "/" + depth + "/" + index;
-            this.depth = depth;
-            this.index = index;
-        }
-
-        public int getDepth() {
-            return depth;
-        }
-
-        public int getIndex() {
-            return index;
-        }
-
-        public String getId() {
-            return id;
-        }
-
-        @Override
-        public String toString() {
-            return depth + " | " + index;
-        }
-
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + ((id == null) ? 0 : id.hashCode());
-            return result;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            HierarchicalTestBean other = (HierarchicalTestBean) obj;
-            if (id == null) {
-                if (other.id != null) {
-                    return false;
-                }
-            } else if (!id.equals(other.id)) {
-                return false;
-            }
-            return true;
-        }
-
-    }
 
     private Tree<HierarchicalTestBean> tree;
     private InMemoryHierarchicalDataProvider<HierarchicalTestBean> inMemoryDataProvider;
@@ -90,6 +33,19 @@ public class TreeBasicFeatures extends AbstractTestUIWithLog {
         tree.addExpandListener(e -> log("ExpandEvent: " + e.getExpandedItem()));
         tree.addCollapseListener(
                 e -> log("ExpandEvent: " + e.getCollapsedItem()));
+
+        tree.setItemIconGenerator(i -> {
+            switch (i.getDepth()) {
+            case 0:
+                return new ThemeResource("../reindeer/common/icons/bullet.png");
+            case 1:
+                return VaadinIcons.FLIGHT_TAKEOFF;
+            case 2:
+                return new ClassResource("/com/vaadin/tests/m.gif");
+            default:
+                return null;
+            }
+        });
     }
 
     private void setupDataProvider() {
