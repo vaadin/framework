aa3ad5db0f3c5e1c95dd0b84c229851460da6449
Artur <artur@vaadin.com>
2017-05-16 15:14:11 +0300

    Clean connector tracker after each access block to stop memory leaks (#9305)
    Immediately clean connectors which the client side does not know about
    
    Fixes #9303

--- server/src/main/java/com/vaadin/server/communication/UidlWriter.java
+++ server/src/main/java/com/vaadin/server/communication/UidlWriter.java
@@ -306,7 +306,6 @@ public class UidlWriter implements Serializable {
             writePerformanceData(ui, writer);
         } finally {
             uiConnectorTracker.setWritingResponse(false);
-            uiConnectorTracker.cleanConnectorMap();
         }
     }
 
