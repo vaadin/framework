d4e66c2c3a33461ecfbc44cc2d0b7254e2567f82
Henri Sara <henri.sara@gmail.com>
2017-05-19 16:30:14 +0300

    Fix menu item selection in tests (#9387)
    When the parent span was not used, the size of the wrong span was used
    in the calculation of mouse locations for activating a menu item. This
    occasionally leads to the wrong submenu being opened on IE as the
    cursor passed over the corner of the next item.

--- uitest/src/test/java/com/vaadin/tests/focusable/GridFocusableTest.java
+++ uitest/src/test/java/com/vaadin/tests/focusable/GridFocusableTest.java
@@ -1,9 +1,5 @@
 package com.vaadin.tests.focusable;
 
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebElement;
-
-import com.vaadin.testbench.By;
 import com.vaadin.testbench.elements.GridElement;
 import com.vaadin.testbench.elements.GridElement.GridCellElement;
 import com.vaadin.tests.components.grid.basics.GridBasics;
@@ -28,11 +24,5 @@ public class GridFocusableTest extends AbstractFocusableComponentTest {
     @Override
     protected GridCellElement getFocusElement() {
         return $(GridElement.class).first().getCell(0, 0);
-    }
-
-    @Override
-    protected WebElement getMenuElement(String menuCaption)
-            throws NoSuchElementException {
-        return super.getMenuElement(menuCaption).findElement(By.xpath(".."));
     }
 }
