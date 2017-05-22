d102edb8554a9e479fb5b9765f7cb0d68feb238b
Aleksi Hietanen <aleksi@vaadin.com>
2017-05-22 14:55:02 +0300

    Fix CheckboxFocusClickTest on Firefox (#9397)

--- uitest/src/test/java/com/vaadin/tests/components/checkbox/CheckboxFocusClickTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/checkbox/CheckboxFocusClickTest.java
@@ -15,12 +15,13 @@
  */
 package com.vaadin.tests.components.checkbox;
 
-import com.vaadin.testbench.By;
-import com.vaadin.testbench.elements.CheckBoxElement;
-import com.vaadin.tests.tb3.MultiBrowserTest;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebElement;
+
+import com.vaadin.testbench.By;
+import com.vaadin.testbench.elements.CheckBoxElement;
+import com.vaadin.tests.tb3.MultiBrowserTest;
 
 public class CheckboxFocusClickTest extends MultiBrowserTest {
 
@@ -31,7 +32,7 @@ public class CheckboxFocusClickTest extends MultiBrowserTest {
         Assert.assertEquals("checked", checkbox.getValue());
         WebElement label = checkbox.findElement(By.xpath("label"));
 
-        clickElement(label);
+        label.click();
         Assert.assertEquals("1. checkbox focused", getLogRow(0));
     }
 }
