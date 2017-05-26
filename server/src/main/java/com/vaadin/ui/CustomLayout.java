74273ae60dde0d1cf4c1d42ae4eeb2f1225eb3ac
Henri Sara <henri.sara@gmail.com>
2017-05-26 15:32:32 +0300

    Make component iterators unmodifiable
    Fixes #9404

--- server/src/main/java/com/vaadin/ui/CustomLayout.java
+++ server/src/main/java/com/vaadin/ui/CustomLayout.java
@@ -20,6 +20,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -205,7 +206,7 @@ public class CustomLayout extends AbstractLayout implements LegacyComponent {
      */
     @Override
     public Iterator<Component> iterator() {
-        return slots.values().iterator();
+        return Collections.unmodifiableCollection(slots.values()).iterator();
     }
 
     /**
