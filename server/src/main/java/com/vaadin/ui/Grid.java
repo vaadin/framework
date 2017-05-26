a8870fe30643120f74432377996b1155e6ba42ae
Pekka Hyvönen <pekka@vaadin.com>
2017-05-26 08:58:05 +0300

    Fix @since tags from #9206 to 8.0.7

--- server/src/main/java/com/vaadin/ui/Grid.java
+++ server/src/main/java/com/vaadin/ui/Grid.java
@@ -2013,7 +2013,7 @@ public class Grid<T> extends AbstractListing<T> implements HasComponents,
 
         /**
          * Gets the DataGenerator for this Column.
-         * 
+         *
          * @return data generator
          */
         private DataGenerator<T> getDataGenerator() {
@@ -2121,17 +2121,19 @@ public class Grid<T> extends AbstractListing<T> implements HasComponents,
     }
 
     /**
-     * Creates a new grid that uses custom data communicator and provided bean type
-     * 
-     * It uses reflection of the provided bean type to automatically set up an initial set of columns. 
-     * All columns will be configured using the same {@link Object#toString()} renderer that is used
-     * by {@link #addColumn(ValueProvider)}.
+     * Creates a new grid that uses custom data communicator and provided bean
+     * type
+     *
+     * It uses reflection of the provided bean type to automatically set up an
+     * initial set of columns. All columns will be configured using the same
+     * {@link Object#toString()} renderer that is used by
+     * {@link #addColumn(ValueProvider)}.
      *
      * @param beanType
      *            the bean type to use, not <code>null</code>
      * @param dataCommunicator
      *            the data communicator to use, not<code>null</code>
-     * @since 8.0.6
+     * @since 8.0.7
      */
     protected Grid(Class<T> beanType, DataCommunicator<T> dataCommunicator) {
         this(BeanPropertySet.get(beanType));
@@ -2146,7 +2148,7 @@ public class Grid<T> extends AbstractListing<T> implements HasComponents,
      *            the custom data communicator to set
      * @see #Grid()
      * @see #Grid(PropertySet, DataCommunicator)
-     * @since 8.0.6
+     * @since 8.0.7
      */
     protected Grid(DataCommunicator<T> dataCommunicator) {
         this(new PropertySet<T>() {
@@ -2194,7 +2196,7 @@ public class Grid<T> extends AbstractListing<T> implements HasComponents,
      *            the property set implementation to use, not <code>null</code>.
      * @param dataCommunicator
      *            the data communicator to use, not<code>null</code>
-     * @since 8.0.6
+     * @since 8.0.7
      */
     protected Grid(PropertySet<T> propertySet,
             DataCommunicator<T> dataCommunicator) {
