39162d36ef1ba08b7f3cc80a928012ea65fcaf6f
Peter Lehto <peter@vaadin.com>
2017-06-09 15:30:29 +0300

    Added getStateParameterMap functionality to Navigator that allows accessing the state parameters through a map

--- server/src/main/java/com/vaadin/navigator/Navigator.java
+++ server/src/main/java/com/vaadin/navigator/Navigator.java
@@ -15,27 +15,15 @@
  */
 package com.vaadin.navigator;
 
-/*
- * Copyright 2000-2016 Vaadin Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
-
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.Page;
@@ -68,6 +56,11 @@ import com.vaadin.ui.UI;
 public class Navigator implements Serializable {
 
     // TODO investigate relationship with TouchKit navigation support
+
+    private static final String DEFAULT_VIEW_SEPARATOR = "/";
+
+    private static final String DEFAULT_STATE_PARAMETER_SEPARATOR = "&";
+    private static final String DEFAULT_STATE_PARAMETER_KEY_VALUE_SEPARATOR = "=";
 
     /**
      * Empty view component.
@@ -366,20 +359,26 @@ public class Navigator implements Serializable {
 
     /**
      * The {@link UI} bound with the Navigator.
+     *
+     * @since 8.0.3
      */
     protected UI ui;
-    
+
     /**
-     * The {@link NavigationStateManager} that is used to get, listen to
-     * and manipulate the navigation state used by the Navigator.
+     * The {@link NavigationStateManager} that is used to get, listen to and
+     * manipulate the navigation state used by the Navigator.
+     *
+     * @since 8.0.3
      */
     protected NavigationStateManager stateManager;
-    
+
     /**
-     *  The {@link ViewDisplay} used by the Navigator.
+     * The {@link ViewDisplay} used by the Navigator.
+     *
+     * @since 8.0.3
      */
     protected ViewDisplay display;
-    
+
     private View currentView = null;
     private List<ViewChangeListener> listeners = new LinkedList<>();
     private List<ViewProvider> providers = new LinkedList<>();
@@ -519,6 +518,10 @@ public class Navigator implements Serializable {
         this.ui.setNavigator(this);
         if (stateManager == null) {
             stateManager = new UriFragmentManager(ui.getPage());
+        }
+        if (stateManager != null && this.stateManager != null
+                && stateManager != this.stateManager) {
+            this.stateManager.setNavigator(null);
         }
         this.stateManager = stateManager;
         this.stateManager.setNavigator(this);
@@ -755,6 +758,62 @@ public class Navigator implements Serializable {
      */
     public String getState() {
         return getStateManager().getState();
+    }
+
+    /**
+     * Returns the current navigation state reported by this Navigator's
+     * {@link NavigationStateManager} as Map<String, String> where each key
+     * represents a parameter in the state. The state parameter separator
+     * character '&' is assumed.
+     * 
+     * @return The navigation state as Map<String, String>.
+     */
+    public Map<String, String> getStateParameterMap() {
+        return getStateParameterMap(DEFAULT_STATE_PARAMETER_SEPARATOR);
+    }
+
+    /**
+     * Returns the current navigation state reported by this Navigator's
+     * {@link NavigationStateManager} as Map<String, String> where each key
+     * represents a parameter in the state. The state parameter separator
+     * character needs to be specified with the separator.
+     * 
+     * @param separator
+     *            the separator String used to split the state to parameters.
+     * @return The navigation state as Map<String, String>.
+     */
+    public Map<String, String> getStateParameterMap(String separator) {
+        return parseStateParameterMap(Objects.requireNonNull(separator));
+    }
+
+    /**
+     * Parses the state parameter map using given separator String.
+     * 
+     * @param separator
+     * @return The navigation state as Map<String, String>.
+     */
+    protected Map<String, String> parseStateParameterMap(String separator) {
+        Map<String, String> parameterMap = new HashMap<>();
+        if (getState() == null || getState().isEmpty()) {
+            return Collections.emptyMap();
+        }
+
+        String state = getState();
+        if (state.contains(DEFAULT_VIEW_SEPARATOR)) {
+            state = state.substring(
+                    Math.max(0, state.indexOf(DEFAULT_VIEW_SEPARATOR) + 1),
+                    state.length());
+        }
+
+        String[] parameters = state.split(separator);
+        for (int i = 0; i < parameters.length; i++) {
+            String[] keyAndValue = parameters[i]
+                    .split(DEFAULT_STATE_PARAMETER_KEY_VALUE_SEPARATOR);
+            parameterMap.put(keyAndValue[0],
+                    keyAndValue.length > 1 ? keyAndValue[1] : "");
+        }
+
+        return parameterMap;
     }
 
     /**
@@ -1009,6 +1068,8 @@ public class Navigator implements Serializable {
      * @param state
      *            state string
      * @return suitable provider
+     *
+     * @since 8.0.3
      */
     protected ViewProvider getViewProvider(String state) {
         String longestViewName = null;
