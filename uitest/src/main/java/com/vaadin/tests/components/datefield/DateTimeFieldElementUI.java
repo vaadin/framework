0dbf73533876b60edfcf05d4f33acad971e3c79e
Artur Signell <artur@vaadin.com>
2017-06-12 14:06:23 +0300

    Also test DateTimeField

--- uitest/src/main/java/com/vaadin/tests/components/datefield/DateTimeFieldElementUI.java
+++ uitest/src/main/java/com/vaadin/tests/components/datefield/DateTimeFieldElementUI.java
@@ -5,12 +5,12 @@ import java.util.Locale;
 
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.tests.components.AbstractReindeerTestUI;
+import com.vaadin.tests.components.AbstractReindeerTestUIWithLog;
 import com.vaadin.ui.DateTimeField;
 import com.vaadin.ui.InlineDateTimeField;
 
 @Widgetset("com.vaadin.DefaultWidgetSet")
-public class DateTimeFieldElementUI extends AbstractReindeerTestUI {
+public class DateTimeFieldElementUI extends AbstractReindeerTestUIWithLog {
     public static final LocalDateTime TEST_DATE_TIME = LocalDateTime.of(2017,
             12, 13, 17, 58);
     public static final LocalDateTime ANOTHER_TEST_DATE_TIME = LocalDateTime
@@ -18,19 +18,34 @@ public class DateTimeFieldElementUI extends AbstractReindeerTestUI {
 
     @Override
     protected void setup(VaadinRequest request) {
-        addComponent(new DateTimeField());
-        addComponent(new InlineDateTimeField());
+        log.setNumberLogRows(false);
+        DateTimeField df = new DateTimeField();
+        df.addValueChangeListener(event -> {
+            log("Default date field value set to " + event.getValue());
+        });
+        addComponent(df);
+        InlineDateTimeField inlineDateTimeField = new InlineDateTimeField();
+        inlineDateTimeField.addValueChangeListener(event -> {
+            log("Default inline date field value set to " + event.getValue());
+        });
+        addComponent(inlineDateTimeField);
 
-        DateTimeField finnishDatefield = new DateTimeField("Finnish");
-        finnishDatefield.setId("fi");
-        finnishDatefield.setLocale(new Locale("fi", "FI"));
-        finnishDatefield.setValue(TEST_DATE_TIME);
-        addComponent(finnishDatefield);
-        DateTimeField usDatefield = new DateTimeField("US");
-        usDatefield.setId("us");
-        usDatefield.setLocale(Locale.US);
-        usDatefield.setValue(TEST_DATE_TIME);
-        addComponent(usDatefield);
+        DateTimeField finnishDateTimeField = new DateTimeField("Finnish");
+        finnishDateTimeField.setId("fi");
+        finnishDateTimeField.setLocale(new Locale("fi", "FI"));
+        finnishDateTimeField.setValue(TEST_DATE_TIME);
+        finnishDateTimeField.addValueChangeListener(event -> {
+            log("Finnish date field value set to " + event.getValue());
+        });
+        addComponent(finnishDateTimeField);
+        DateTimeField usDateTimeField = new DateTimeField("US");
+        usDateTimeField.setId("us");
+        usDateTimeField.setLocale(Locale.US);
+        usDateTimeField.setValue(TEST_DATE_TIME);
+        usDateTimeField.addValueChangeListener(event -> {
+            log("US date field value set to " + event.getValue());
+        });
+        addComponent(usDateTimeField);
     }
 
     @Override
--- uitest/src/test/java/com/vaadin/tests/components/datefield/DateTimeFieldElementTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/datefield/DateTimeFieldElementTest.java
@@ -3,6 +3,8 @@ package com.vaadin.tests.components.datefield;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
+import java.time.LocalDateTime;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,7 +15,7 @@ import com.vaadin.tests.tb3.SingleBrowserTest;
 public class DateTimeFieldElementTest extends SingleBrowserTest {
 
     @Test
-    public void dateFieldElementIsLocated() {
+    public void DateTimeFieldElementIsLocated() {
         openTestURL();
 
         assertThat($(DateTimeFieldElement.class).all().size(), is(3));
@@ -32,6 +34,8 @@ public class DateTimeFieldElementTest extends SingleBrowserTest {
                 .setDateTime(DateTimeFieldElementUI.TEST_DATE_TIME);
         Assert.assertEquals(DateTimeFieldElementUI.TEST_DATE_TIME,
                 defaultInitiallyEmpty.getDateTime());
+        assertServerValue("Default date field",
+                DateTimeFieldElementUI.TEST_DATE_TIME);
 
         DateTimeFieldElement fi = $(DateTimeFieldElement.class).id("fi");
         Assert.assertEquals(DateTimeFieldElementUI.TEST_DATE_TIME,
@@ -39,6 +43,8 @@ public class DateTimeFieldElementTest extends SingleBrowserTest {
         fi.setDateTime(DateTimeFieldElementUI.ANOTHER_TEST_DATE_TIME);
         Assert.assertEquals(DateTimeFieldElementUI.ANOTHER_TEST_DATE_TIME,
                 fi.getDateTime());
+        assertServerValue("Finnish date field",
+                DateTimeFieldElementUI.ANOTHER_TEST_DATE_TIME);
 
         DateTimeFieldElement us = $(DateTimeFieldElement.class).id("us");
         Assert.assertEquals(DateTimeFieldElementUI.TEST_DATE_TIME,
@@ -46,6 +52,14 @@ public class DateTimeFieldElementTest extends SingleBrowserTest {
         us.setDateTime(DateTimeFieldElementUI.ANOTHER_TEST_DATE_TIME);
         Assert.assertEquals(DateTimeFieldElementUI.ANOTHER_TEST_DATE_TIME,
                 us.getDateTime());
+        assertServerValue("US date field",
+                DateTimeFieldElementUI.ANOTHER_TEST_DATE_TIME);
+    }
+
+    private void assertServerValue(String id, LocalDateTime testDateTime) {
+        Assert.assertEquals(id + " value set to " + testDateTime.toString(),
+                getLogRow(0));
+
     }
 
     @Override
