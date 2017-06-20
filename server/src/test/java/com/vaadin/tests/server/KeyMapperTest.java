3d1c90d5a2de6df9cbea4b9558dd6f8df599320c
elmot <me@elmot.xyz>
2017-06-20 06:51:33 +0300

    Do not use asterisk imports

--- server/src/test/java/com/vaadin/tests/server/KeyMapperTest.java
+++ server/src/test/java/com/vaadin/tests/server/KeyMapperTest.java
@@ -1,13 +1,15 @@
 package com.vaadin.tests.server;
 
+import com.vaadin.server.KeyMapper;
+import org.junit.Test;
+
 import java.lang.reflect.Field;
 import java.util.HashMap;
 
-import org.junit.Test;
-
-import com.vaadin.server.KeyMapper;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.fail;
 
 public class KeyMapperTest {
 
