c00bcda9a3636a791866e2f4e208e21137ab005a
elmot <elmotelmot.vaadin.com>
2017-07-28 18:54:45 +0300

    One more test

--- uitest/src/main/java/com/vaadin/tests/components/datefield/DateTextHandling.java
+++ uitest/src/main/java/com/vaadin/tests/components/datefield/DateTextHandling.java
@@ -1,4 +1,4 @@
-package com.vaadin.tests.data;
+package com.vaadin.tests.components.datefield;
 
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.data.Binder;
@@ -8,16 +8,19 @@ import com.vaadin.tests.components.AbstractTestUI;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.DateField;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.VerticalLayout;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
 
-/**
- * Created by elmot on 7/11/2017.
- */
 @Widgetset("com.vaadin.DefaultWidgetSet")
-public class DateValidationUI extends AbstractTestUI {
+public class DateTextHandling extends AbstractTestUI {
+
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM).withLocale(Locale.UK);
+
     @Override
     protected void setup(VaadinRequest request) {
         final VerticalLayout layout = new VerticalLayout();
@@ -26,19 +29,19 @@ public class DateValidationUI extends AbstractTestUI {
             @Override
             protected Result<LocalDate> handleUnparsableDateString(String dateString) {
                 if (dateString.equalsIgnoreCase("Y2K")) {
-                    return Result.ok(LocalDate.of(2000,1,1));
+                    return Result.ok(LocalDate.of(2000, 1, 1));
                 } else {
                     return super.handleUnparsableDateString(dateString);
                 }
-            };
+            }
+
+            ;
         };
         dateField.setParseErrorMessage("Parse error");
         dateField.setDateOutOfRangeMessage("Out of range");
         layout.addComponent(dateField);
-        dateField.addValueChangeListener(event -> {
-            System.out.println(dateField.getValue());
-        });
         Label errorLabel = new Label();
+        errorLabel.setId("errorLabel");
         layout.addComponent(errorLabel);
 
         Binder<Void> binder = new Binder<>();
@@ -46,11 +49,13 @@ public class DateValidationUI extends AbstractTestUI {
 
         Button button = new Button("Validate!");
         button.addClickListener(event1 -> {
-            if (binder.validate().isOk()) {
-                System.out.println("Correct");
+            binder.validate();
+            if (dateField.getValue() == null) {
+                Notification.show("NULL");
             } else {
-                System.out.println(dateField.isEmpty() + "Error!");
+                Notification.show(DATE_TIME_FORMATTER.format(dateField.getValue()));
             }
+
         });
         layout.addComponent(button);
 
