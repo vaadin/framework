9373b365ecd8ba0a8731139df8081ca8bbcf2b9e
Henri Sara <henri.sara@gmail.com>
2017-08-07 10:11:02 +0300

    Make frontend resource loading tests stable on PhantomJS (#9772)

--- uitest/src/test/java/com/vaadin/tests/resources/FrontendInitialResourceUITest.java
+++ uitest/src/test/java/com/vaadin/tests/resources/FrontendInitialResourceUITest.java
@@ -35,7 +35,10 @@ public class FrontendInitialResourceUITest extends MultiBrowserTest {
         } else {
             es = "es6";
         }
-        testBench().disableWaitForVaadin(); // For some reason needed by IE11
+        if (BrowserUtil.isIE(getDesiredCapabilities())) {
+            // For some reason needed by IE11
+            testBench().disableWaitForVaadin();
+        }
 
         Assert.assertEquals("/VAADIN/frontend/" + es + "/logFilename.js",
                 findElement(By.tagName("body")).getText());
