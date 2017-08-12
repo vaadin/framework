c91beada0c12629ce01ab335e29b76d09b0d0c26
Adam <adam@vaadin.com>
2017-08-12 18:31:09 +0300

    Add error level for tooltip

--- client/src/main/java/com/vaadin/client/VTooltip.java
+++ client/src/main/java/com/vaadin/client/VTooltip.java
@@ -140,6 +140,7 @@ public class VTooltip extends VOverlay {
                 && !info.getErrorMessage().isEmpty()) {
             em.setVisible(true);
             em.updateMessage(info.getErrorMessage());
+            em.updateErrorLevel(info.getErrorLevel());
         } else {
             em.setVisible(false);
         }
@@ -461,6 +462,7 @@ public class VTooltip extends VOverlay {
     @Override
     public void hide() {
         em.updateMessage("");
+        em.updateErrorLevel(null);
         description.setHTML("");
 
         updatePosition(null, true);
