19cb89e2af06a702d43e9cce9c449fd82c6e991e
Ansku <Ansku@users.noreply.github.com>
2017-08-22 14:59:49 +0300

    Resize should work within Grid details row (#9808)
    Fixes #7341

--- client/src/main/java/com/vaadin/client/connectors/grid/DetailsManagerConnector.java
+++ client/src/main/java/com/vaadin/client/connectors/grid/DetailsManagerConnector.java
@@ -19,14 +19,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.Widget;
-
 import com.vaadin.client.ComponentConnector;
 import com.vaadin.client.ConnectorMap;
 import com.vaadin.client.LayoutManager;
 import com.vaadin.client.ServerConnector;
+import com.vaadin.client.WidgetUtil;
 import com.vaadin.client.data.DataChangeHandler;
 import com.vaadin.client.extensions.AbstractExtensionConnector;
+import com.vaadin.client.ui.layout.ElementResizeEvent;
+import com.vaadin.client.ui.layout.ElementResizeListener;
 import com.vaadin.client.widget.grid.HeightAwareDetailsGenerator;
 import com.vaadin.client.widgets.Grid;
 import com.vaadin.shared.Registration;
@@ -52,6 +56,21 @@ public class DetailsManagerConnector extends AbstractExtensionConnector {
     private boolean refreshing;
     /* Registration for data change handler. */
     private Registration dataChangeRegistration;
+
+    private final Map<Element, ScheduledCommand> elementToResizeCommand = new HashMap<Element, Scheduler.ScheduledCommand>();
+    private final ElementResizeListener detailsRowResizeListener = new ElementResizeListener() {
+
+        @Override
+        public void onElementResize(ElementResizeEvent e) {
+            if (elementToResizeCommand.containsKey(e.getElement())) {
+                Scheduler.get().scheduleFinally(
+                        elementToResizeCommand.get(e.getElement()));
+            }
+        }
+    };
+
+    /* calculated when the first details row is opened */
+    private Double spacerCellBorderHeights = null;
 
     /**
      * DataChangeHandler for updating the visibility of detail widgets.
@@ -107,7 +126,35 @@ public class DetailsManagerConnector extends AbstractExtensionConnector {
                 return null;
             }
 
-            return getConnector(id).getWidget();
+            Widget widget = getConnector(id).getWidget();
+            getLayoutManager().addElementResizeListener(widget.getElement(),
+                    detailsRowResizeListener);
+            elementToResizeCommand.put(widget.getElement(),
+                    createResizeCommand(rowIndex, widget.getElement()));
+
+            return widget;
+        }
+
+        private ScheduledCommand createResizeCommand(final int rowIndex,
+                final Element element) {
+            return new ScheduledCommand() {
+
+                @Override
+                public void execute() {
+                    // It should not be possible to get here without calculating
+                    // the spacerCellBorderHeights or without having the details
+                    // row open, nor for this command to be triggered while
+                    // layout is running, but it's safer to check anyway.
+                    if (spacerCellBorderHeights != null
+                            && !getLayoutManager().isLayoutRunning()
+                            && getDetailsComponentConnectorId(
+                                    rowIndex) != null) {
+                        double height = getLayoutManager().getOuterHeightDouble(
+                                element) + spacerCellBorderHeights;
+                        getWidget().setDetailsHeight(rowIndex, height);
+                    }
+                }
+            };
         }
 
         @Override
@@ -120,8 +167,16 @@ public class DetailsManagerConnector extends AbstractExtensionConnector {
             getLayoutManager().setNeedsMeasureRecursively(componentConnector);
             getLayoutManager().layoutNow();
 
-            return getLayoutManager().getOuterHeightDouble(
-                    componentConnector.getWidget().getElement());
+            Element element = componentConnector.getWidget().getElement();
+            if (spacerCellBorderHeights == null) {
+                // If theme is changed, new details generator is created from
+                // scratch, so this value doesn't need to be updated elsewhere.
+                spacerCellBorderHeights = WidgetUtil
+                        .getBorderTopAndBottomThickness(
+                                element.getParentElement());
+            }
+
+            return getLayoutManager().getOuterHeightDouble(element);
         }
 
         private ComponentConnector getConnector(String id) {
@@ -143,7 +198,21 @@ public class DetailsManagerConnector extends AbstractExtensionConnector {
                 return;
             }
 
-            // New Details component, hide old one
+            if (id == null) {
+                // Details have been hidden, listeners attached to the old
+                // component need to be removed
+                id = indexToDetailConnectorId.get(rowIndex);
+            }
+
+            // New or removed Details component, hide old one
+            ComponentConnector connector = (ComponentConnector) ConnectorMap
+                    .get(getConnection()).getConnector(id);
+            if (connector != null) {
+                Element element = connector.getWidget().getElement();
+                elementToResizeCommand.remove(element);
+                getLayoutManager().removeElementResizeListener(element,
+                        detailsRowResizeListener);
+            }
             getWidget().setDetailsVisible(rowIndex, false);
             indexToDetailConnectorId.remove(rowIndex);
         }
---
+++ uitest/src/main/java/com/vaadin/v7/tests/components/grid/GridLayoutDetailsRowResize.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2000-2017 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.v7.tests.components.grid;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.tests.components.AbstractTestUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.themes.ValoTheme;
+import com.vaadin.v7.event.ItemClickEvent;
+import com.vaadin.v7.event.ItemClickEvent.ItemClickListener;
+import com.vaadin.v7.ui.Grid;
+import com.vaadin.v7.ui.Grid.DetailsGenerator;
+import com.vaadin.v7.ui.Grid.RowReference;
+import com.vaadin.v7.ui.Label;
+import com.vaadin.v7.ui.VerticalLayout;
+import com.vaadin.v7.ui.themes.Reindeer;
+
+/**
+ * Tests that details row resizes along with the contents properly.
+ *
+ * @author Vaadin Ltd
+ */
+@SuppressWarnings("deprecation")
+public class GridLayoutDetailsRowResize extends AbstractTestUI {
+
+    @Override
+    protected void setup(VaadinRequest request) {
+        final Grid grid = new Grid();
+        grid.setSizeFull();
+        grid.addColumn("name", String.class);
+        grid.addColumn("born", Integer.class);
+
+        grid.addRow("Nicolaus Copernicus", 1543);
+        grid.addRow("Galileo Galilei", 1564);
+        grid.addRow("Johannes Kepler", 1571);
+
+        addComponent(grid);
+
+        grid.setDetailsGenerator(new DetailsGenerator() {
+            @Override
+            public Component getDetails(final RowReference rowReference) {
+                final VerticalLayout detailsLayout = new VerticalLayout();
+                detailsLayout.setId("details");
+                detailsLayout.setSizeFull();
+                detailsLayout.setHeightUndefined();
+
+                final Label lbl1 = new Label("test1");
+                lbl1.setId("lbl1");
+                lbl1.setWidth("200px");
+                detailsLayout.addComponent(lbl1);
+
+                final Label lbl2 = new Label("test2");
+                lbl2.setId("lbl2");
+                detailsLayout.addComponent(lbl2);
+
+                final Label lbl3 = new Label("test3");
+                lbl3.setId("lbl3");
+                detailsLayout.addComponent(lbl3);
+
+                final Label lbl4 = new Label("test4");
+                lbl4.setId("lbl4");
+                lbl4.setVisible(false);
+                detailsLayout.addComponent(lbl4);
+
+                final Button button = new Button("Toggle visibility",
+                        new Button.ClickListener() {
+
+                            @Override
+                            public void buttonClick(ClickEvent event) {
+                                lbl4.setVisible(!lbl4.isVisible());
+                            }
+                        });
+                button.setId("btn");
+                detailsLayout.addComponent(button);
+
+                return detailsLayout;
+            }
+        });
+
+        grid.addItemClickListener(new ItemClickListener() {
+            @Override
+            public void itemClick(final ItemClickEvent event) {
+                final Object itemId = event.getItemId();
+                grid.setDetailsVisible(itemId, !grid.isDetailsVisible(itemId));
+            }
+        });
+
+        addComponent(new Button("Toggle theme", new Button.ClickListener() {
+
+            @Override
+            public void buttonClick(ClickEvent event) {
+                if (ValoTheme.THEME_NAME.equals(getUI().getTheme())) {
+                    getUI().setTheme(Reindeer.THEME_NAME);
+                } else {
+                    getUI().setTheme(ValoTheme.THEME_NAME);
+                }
+            }
+        }));
+
+        addComponent(new Button("Open details", new Button.ClickListener() {
+
+            @Override
+            public void buttonClick(ClickEvent event) {
+                for (Object itemId : grid.getContainerDataSource()
+                        .getItemIds()) {
+                    grid.setDetailsVisible(itemId, true);
+                }
+            }
+        }));
+    }
+
+    @Override
+    protected String getTestDescription() {
+        return "Detail row should be correctly resized when its contents change.";
+    }
+
+    @Override
+    protected Integer getTicketNumber() {
+        return 7341;
+    }
+}
