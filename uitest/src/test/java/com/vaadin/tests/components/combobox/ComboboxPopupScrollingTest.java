c24119222373072e67db3496bb9ff2466c22d734
Henri Sara <henri.sara@gmail.com>
2017-08-25 13:49:00 +0300

    Move ComboBox popup with ComboBox on scroll (#9869)
    Keep the ComboBox popup at the same position relative to the ComboBox
    when the view is scrolled.
    
    Fixes #5043

--- uitest/src/test/java/com/vaadin/tests/components/combobox/ComboboxPopupScrollingTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/combobox/ComboboxPopupScrollingTest.java
@@ -15,11 +15,14 @@
  */
 package com.vaadin.tests.components.combobox;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 
 import com.vaadin.testbench.elements.ComboBoxElement;
+import com.vaadin.testbench.elements.UIElement;
 import com.vaadin.tests.tb3.MultiBrowserTest;
 
 public class ComboboxPopupScrollingTest extends MultiBrowserTest {
@@ -42,6 +45,31 @@ public class ComboboxPopupScrollingTest extends MultiBrowserTest {
     @Test
     public void testNoScrollbarsReindeer() {
         testNoScrollbars("reindeer");
+    }
+
+    @Test
+    public void testComboBoxTracksScrolledPage() {
+        openTestURL("theme=valo");
+
+        ComboBoxElement cb = $(ComboBoxElement.class).last();
+        cb.openPopup();
+        WebElement popup = cb.getSuggestionPopup();
+        Point comboLocation = cb.getLocation();
+        Point popupLocation = popup.getLocation();
+
+        // scroll page
+        $(UIElement.class).first().scroll(100);
+
+        // make sure animation frame is handled
+        sleep(500);
+
+        Point newComboLocation = cb.getLocation();
+        Point newPopupLocation = popup.getLocation();
+        Assert.assertNotEquals("ComboBox didn't move on the page", 0,
+                newComboLocation.y - comboLocation.y);
+        Assert.assertEquals("Popup didn't move with the combo box",
+                newComboLocation.y - comboLocation.y,
+                newPopupLocation.y - popupLocation.y);
     }
 
     private void testNoScrollbars(String theme) {
