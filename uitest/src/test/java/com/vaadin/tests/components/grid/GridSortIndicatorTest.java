ceb1ebfcae17e16e2b473edcc906a4ba2240a630
Knoobie <Knoobie@gmx.de>
2017-09-16 15:43:04 +0200

    added aria-sort test GridSortIndicatorTest

--- uitest/src/test/java/com/vaadin/tests/components/grid/GridSortIndicatorTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/grid/GridSortIndicatorTest.java
@@ -35,18 +35,26 @@ public class GridSortIndicatorTest extends SingleBrowserTest {
         Assert.assertTrue("First column should be sorted ascending",
                 grid.getHeaderCell(0, 0).getAttribute("class")
                         .contains("sort-asc"));
+        Assert.assertEquals("First column should have aria-sort other", "other",
+                grid.getHeaderCell(0, 0).getAttribute("aria-sort"));
         Assert.assertEquals("First column should be first in sort order", "1",
                 grid.getHeaderCell(0, 0).getAttribute("sort-order"));
         Assert.assertTrue("Second column should be sorted ascending",
                 grid.getHeaderCell(0, 1).getAttribute("class")
                         .contains("sort-asc"));
+        Assert.assertEquals("Second column should have aria-sort other", "other",
+                grid.getHeaderCell(0, 1).getAttribute("aria-sort"));
         Assert.assertEquals("Second column should be also sorted", "2",
                 grid.getHeaderCell(0, 1).getAttribute("sort-order"));
 
         $(ButtonElement.class).caption("Sort first").first().click();
+        Assert.assertEquals("First column should have aria-sort ascending", "ascending",
+                grid.getHeaderCell(0, 0).getAttribute("aria-sort"));
         Assert.assertTrue("First column should be sorted ascending",
                 grid.getHeaderCell(0, 0).getAttribute("class")
                         .contains("sort-asc"));
+        Assert.assertEquals("Second column should have aria-sort none", "none",
+                grid.getHeaderCell(0, 1).getAttribute("aria-sort"));
         Assert.assertFalse("Second column should not be sorted",
                 grid.getHeaderCell(0, 1).getAttribute("class")
                         .contains("sort-asc"));
