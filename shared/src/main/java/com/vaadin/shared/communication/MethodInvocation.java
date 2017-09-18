468784026487c7c49df6aea9e7faf438df15ead5
Ahmed Ashour <asashour@yahoo.com>
2017-09-18 16:05:29 +0200

    Implement hashCode() when implementing equals()

--- shared/src/main/java/com/vaadin/shared/communication/MethodInvocation.java
+++ shared/src/main/java/com/vaadin/shared/communication/MethodInvocation.java
@@ -108,8 +108,17 @@ public class MethodInvocation implements Serializable {
         if (!SharedUtil.equals(getParameters(), other.getParameters())) {
             return false;
         }
-
         return true;
+    }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((connectorId == null) ? 0 : connectorId.hashCode());
+        result = prime * result + ((interfaceName == null) ? 0 : interfaceName.hashCode());
+        result = prime * result + ((methodName == null) ? 0 : methodName.hashCode());
+        result = prime * result + Arrays.hashCode(parameters);
+        return result;
     }
 }
