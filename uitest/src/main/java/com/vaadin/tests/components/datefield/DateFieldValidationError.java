c3eb67d82850329fe486daa1f50473168a895f47
Marco Collovati <mcollovati@gmail.com>
2017-09-20 07:32:04 +0200

    Show validation error selecting invalid date from DateField (#10013)
    Component error was cleared after selecting a date from
    calendar popup, even if the date is invalid due to
    validation errors or if user programmatically sets an UserError.
    
    This change inverts the order of setValue() and setComponentError(null) in order
    to preserve errors set by ValueChangeListeners (e.g. Binder validators).
    
    Fixes #10011

---
+++ uitest/src/main/java/com/vaadin/tests/components/datefield/DateFieldValidationError.java
@@ -0,0 +1,31 @@
+package com.vaadin.tests.components.datefield;
+
+import java.time.LocalDate;
+import java.util.Locale;
+
+import com.vaadin.annotations.Widgetset;
+import com.vaadin.data.Binder;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.tests.components.AbstractTestUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.DateField;
+import com.vaadin.ui.Notification;
+
+@Widgetset("com.vaadin.DefaultWidgetSet")
+public class DateFieldValidationError extends AbstractTestUI {
+
+    @Override
+    protected void setup(VaadinRequest request) {
+        DateField df = new DateField();
+        df.setLocale(Locale.US);
+        Binder<Void> binder = new Binder<>();
+        binder.forField(df)
+            .withValidator(localDate -> localDate != null && localDate.isAfter(LocalDate.now()), "Invalid date")
+            .bind(v -> LocalDate.now(), (v, t) -> {
+            /* NO-OP */
+            });
+        addComponent(df);
+        addComponent(new Button("Validate", e -> Notification.show(binder.validate().isOk() ? "OK" : "Fail")));
+    }
+
+}
