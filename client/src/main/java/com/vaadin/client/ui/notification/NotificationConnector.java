9f9efe0058397992fda43e104c90b79039d41c0f
Ahmed Ashour <asashour@yahoo.com>
2017-09-27 12:44:01 +0200

    Support addCloseListener for Notification (#10027)
    Converts Notification to an Extension and adds support for listening to the closing of notifications.
    
    Fixes #888

---
+++ client/src/main/java/com/vaadin/client/ui/notification/NotificationConnector.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2000-2016 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.vaadin.client.ui.notification;
+
+import com.google.gwt.event.logical.shared.CloseEvent;
+import com.google.gwt.event.logical.shared.CloseHandler;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.vaadin.client.ServerConnector;
+import com.vaadin.client.extensions.AbstractExtensionConnector;
+import com.vaadin.client.ui.VNotification;
+import com.vaadin.shared.ui.Connect;
+import com.vaadin.shared.ui.notification.NotificationServerRpc;
+import com.vaadin.shared.ui.notification.NotificationState;
+import com.vaadin.ui.Notification;
+
+/**
+ * The client-side connector for the {@code Notification}.
+ *
+ * @author Vaadin Ltd.
+ *
+ * @since
+ */
+@Connect(value = Notification.class)
+public class NotificationConnector extends AbstractExtensionConnector {
+
+    @Override
+    public NotificationState getState() {
+        return (NotificationState) super.getState();
+    }
+
+    @Override
+    protected void extend(ServerConnector target) {
+        NotificationState state = getState();
+        VNotification n = VNotification.showNotification(
+                target.getConnection(),
+                state.caption, state.description,
+                state.htmlContentAllowed, getResourceUrl("icon"),
+                state.styleName, state.position, state.delay);
+
+        n.addCloseHandler(new CloseHandler<PopupPanel>() {
+
+            @Override
+            public void onClose(CloseEvent<PopupPanel> event) {
+              NotificationServerRpc rpc =
+                      getRpcProxy(NotificationServerRpc.class);
+              rpc.closed();
+            }
+        });
+    }
+
+}
