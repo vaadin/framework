816ec0a75cd0e869d30708bea18abf65cde39aae
Ahmed Ashour <asashour@yahoo.com>
2017-10-02 20:13:36 +0200

    SharedState.registeredEventListeners to be a Map

--- client/src/main/java/com/vaadin/client/ui/AbstractConnector.java
+++ client/src/main/java/com/vaadin/client/ui/AbstractConnector.java
@@ -20,7 +20,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import com.google.gwt.core.client.JsArrayString;
 import com.google.gwt.dom.client.Element;
@@ -498,8 +498,8 @@ public abstract class AbstractConnector
      */
     @Override
     public boolean hasEventListener(String eventIdentifier) {
-        Set<String> reg = getState().registeredEventListeners;
-        return (reg != null && reg.contains(eventIdentifier));
+        Map<String, Integer> reg = getState().registeredEventListeners;
+        return (reg != null && reg.keySet().contains(eventIdentifier));
     }
 
     /**
