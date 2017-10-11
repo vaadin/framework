73c7f2fa3b3a6a9fa0b66d9b273ed9f730e7e141
Teemu Suo-Anttila <teemusa@vaadin.com>
2017-10-11 15:19:45 +0300

    Add test and reference screenshots for ErrorLevels

---
+++ uitest/src/test/java/com/vaadin/tests/binder/BinderValidatorErrorLevelTest.java
@@ -0,0 +1,41 @@
+package com.vaadin.tests.binder;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.shared.ui.ErrorLevel;
+import com.vaadin.testbench.By;
+import com.vaadin.testbench.elements.TextFieldElement;
+import com.vaadin.tests.tb3.SingleBrowserTest;
+
+public class BinderValidatorErrorLevelTest extends SingleBrowserTest {
+
+    @Test
+    public void testErrorLevelStyleNames() throws IOException {
+        openTestURL();
+
+        for (ErrorLevel l : ErrorLevel.values()) {
+            TextFieldElement textField = $(TextFieldElement.class)
+                    .caption(l.name()).first();
+
+            // Screenshot the whole slot
+            compareScreen(textField.findElement(By.xpath("..")),
+                    l.name().toLowerCase());
+
+            Assert.assertTrue("Error style for " + l.name() + " not present",
+                    textField.getAttribute("class").contains(
+                            "v-textfield-error-" + l.name().toLowerCase()));
+            textField.setValue("long enough text");
+            Assert.assertFalse("Error style for " + l.name() + " still present",
+                    textField.getAttribute("class").contains(
+                            "v-textfield-error-" + l.name().toLowerCase()));
+            textField.setValue("foo");
+            Assert.assertTrue(
+                    "Error style for " + l.name() + " should be present again.",
+                    textField.getAttribute("class").contains(
+                            "v-textfield-error-" + l.name().toLowerCase()));
+        }
+    }
+}
