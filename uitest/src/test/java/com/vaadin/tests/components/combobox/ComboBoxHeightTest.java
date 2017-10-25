9b1725aca8e40e30bec0d68ccc65e432549a764c
Adam Wagner <adam@vaadin.com>
2017-10-25 18:22:53 +0300

    Create combo box height unit test

---
+++ uitest/src/test/java/com/vaadin/tests/components/combobox/ComboBoxHeightTest.java
@@ -0,0 +1,29 @@
+package com.vaadin.tests.components.combobox;
+
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.testbench.elements.ComboBoxElement;
+import com.vaadin.tests.tb3.SingleBrowserTest;
+
+public class ComboBoxHeightTest extends SingleBrowserTest {
+
+    @Test
+    public void testPopupHeight() {
+        openTestURL();
+
+        ComboBoxElement comboBox = $(ComboBoxElement.class).first();
+
+        comboBox.openPopup();
+        WebElement suggestionPopup = comboBox.getSuggestionPopup();
+
+        int suggestionPopupBottom =
+                suggestionPopup.getLocation().getY() + suggestionPopup.getSize()
+                        .getHeight();
+
+        assertGreaterOrEqual(
+                "Combo box suggestion popup should not exceed the browser's viewport",
+                driver.manage().window().getSize().getHeight(),
+                suggestionPopupBottom);
+    }
+}
