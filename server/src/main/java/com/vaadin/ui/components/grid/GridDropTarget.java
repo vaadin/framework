19f19f63c754070d43cfc3edb7dfd8490c5c7c2d
Pekka Hyvönen <pekka@vaadin.com>
2017-10-25 14:09:11 +0300

    Refactored the GridDragger to match FW style
    Unit tests, Test UI and documentation to come next.

--- server/src/main/java/com/vaadin/ui/components/grid/GridDropTarget.java
+++ server/src/main/java/com/vaadin/ui/components/grid/GridDropTarget.java
@@ -59,6 +59,16 @@ public class GridDropTarget<T> extends DropTargetExtension<Grid<T>> {
     }
 
     /**
+     * Gets the grid this extension has been attached to.
+     *
+     * @return the grid for this extension
+     * @since
+     */
+    public Grid<T> getGrid() {
+        return getParent();
+    }
+
+    /**
      * Sets the drop mode of this drop target.
      * <p>
      * When using {@link DropMode#ON_TOP}, and the grid is either empty or has
