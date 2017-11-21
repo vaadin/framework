b64dc36da04295f1e307843136f72938df95ed3e
Ahmed Ashour <asashour@yahoo.com>
2017-11-21 12:36:26 +0100

    FileResource to use bufferSize, and StreamResource to use getCacheTime() (#10247)

--- server/src/test/java/com/vaadin/tests/server/FileResourceTest.java
+++ server/src/test/java/com/vaadin/tests/server/FileResourceTest.java
@@ -15,10 +15,14 @@
  */
 package com.vaadin.tests.server;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
+import java.net.URISyntaxException;
 
 import org.junit.Test;
 
+import com.vaadin.server.DownloadStream;
 import com.vaadin.server.FileResource;
 
 public class FileResourceTest {
@@ -33,4 +37,24 @@ public class FileResourceTest {
         new FileResource(new File("nonexisting")).getStream();
     }
 
+    @Test
+    public void bufferSize() throws URISyntaxException {
+        File file = new File(getClass().getResource("../styles.scss").toURI());
+        FileResource resource = new FileResource(file) {
+            @Override
+            public long getCacheTime() {
+                return 5;
+            }
+        };
+        resource.setBufferSize(100);
+        resource.setCacheTime(200);
+
+        DownloadStream downloadStream = resource.getStream();
+        assertEquals(
+                "DownloadStream buffer size must be same as resource buffer size",
+                resource.getBufferSize(), downloadStream.getBufferSize());
+        assertEquals(
+                "DownloadStream cache time must be same as resource cache time",
+                resource.getCacheTime(), downloadStream.getCacheTime());
+    }
 }
