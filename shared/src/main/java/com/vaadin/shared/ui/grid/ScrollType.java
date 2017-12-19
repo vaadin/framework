e40be80c0340f99ddf0361702b8a06c573956809
Jeffrey Gordon <jeffreyrgordon@gmail.com>
2017-12-19 00:39:23 -0700

    Add files via upload
    Added scroll type for tree select(T item) method.

---
+++ shared/src/main/java/com/vaadin/shared/ui/grid/ScrollType.java
@@ -0,0 +1,15 @@
+package com.vaadin.shared.ui.grid;
+
+public enum ScrollType {
+	
+    /**
+	*Automatically scrolls to the currently selected object in the grid.
+     */
+	SCROLL,
+	
+	/**
+	 *Disables automatic scrolling to the secleced object in the grid.
+	*/
+	NOSCROLL
+
+}
