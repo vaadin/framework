vaadin/framework

commit 10bd7544fd22dccd7e8e32cb236eb50f102892d3

Improve naming of fields and variables (#10242)
* Variable names to conform to naming convention.

* Use static constants where it makes sense

client/src/main/java/com/vaadin/client/Profiler.java
@@ -47,7 +47,7 @@ public class Profiler {
 
     private static RelativeTimeSupplier RELATIVE_TIME_SUPPLIER;
 
-    private static final String evtGroup = "VaadinProfiler";
+    private static final String EVT_GROUP = "VaadinProfiler";
 
     private static ProfilerResultConsumer consumer;
 
@@ -290,7 +290,7 @@ public class Profiler {
 
         private native String getEvtGroup()
         /*-{
-            return this.evtGroup;
+            return this.EVT_GROUP;
         }-*/;
 
         private native double getMillis()
@@ -325,7 +325,7 @@ public class Profiler {
 
         public final String getEventName() {
             String group = getEvtGroup();
-            if (evtGroup.equals(group)) {
+            if (EVT_GROUP.equals(group)) {
                 return getSubSystem();
             } else {
                 return group + "." + getSubSystem();
@@ -387,7 +387,7 @@ public class Profiler {
     private static final native void logGwtEvent(String name, String type)
     /*-{
         $wnd.__gwtStatsEvent({
-            evtGroup: @com.vaadin.client.Profiler::evtGroup,
+            evtGroup: @com.vaadin.client.Profiler::EVT_GROUP,
             moduleName: @com.google.gwt.core.client.GWT::getModuleName()(),
             millis: (new Date).getTime(),
             sessionId: undefined,
@@ -463,7 +463,7 @@ public class Profiler {
         Set<Node> extendedTimeNodes = new HashSet<>();
         for (int i = 0; i < gwtStatsEvents.length(); i++) {
             GwtStatsEvent gwtStatsEvent = gwtStatsEvents.get(i);
-            if (!evtGroup.equals(gwtStatsEvent.getEvtGroup())) {
+            if (!EVT_GROUP.equals(gwtStatsEvent.getEvtGroup())) {
                 // Only log our own events to avoid problems with events which
                 // are not of type start+end
                 continue;

commit 0a5b95164d3cdfed4a1d67c909979b72ba555162

Make profiler work again
Fixes problem introduced in 10bd7544fd22dccd7e8e32cb236eb50f102892d3

client/src/main/java/com/vaadin/client/Profiler.java
@@ -289,7 +289,7 @@ public class Profiler {
 
         private native String getEvtGroup()
         /*-{
-            return this.EVT_GROUP;
+            return this.evtGroup;
         }-*/;
 
         private native double getMillis()