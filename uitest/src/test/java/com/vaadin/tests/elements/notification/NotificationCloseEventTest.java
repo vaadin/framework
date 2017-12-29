aa48dce85e055895f515870b3b7ba040e46ad213
Artur Signell <artur@vaadin.com>
2017-12-29 15:31:03 +0200

    Add Notification.close() to hide a notification from the server
    Also fixes the problem that notifications were never removed on the server side
    
    Fixes #2114, #10481

---
+++ uitest/src/test/java/com/vaadin/tests/elements/notification/NotificationCloseEventTest.java
@@ -0,0 +1,52 @@
+package com.vaadin.tests.elements.notification;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.vaadin.testbench.elements.ButtonElement;
+import com.vaadin.testbench.elements.NotificationElement;
+import com.vaadin.tests.tb3.SingleBrowserTest;
+
+public class NotificationCloseEventTest extends SingleBrowserTest {
+
+    @Override
+    protected Class<?> getUIClass() {
+        return NotificationGetTypeAndDescription.class;
+    }
+
+    @Test
+    public void testCloseByUser() {
+        openTestURL();
+        ButtonElement error = $(ButtonElement.class).caption("error").first();
+        error.click();
+        $(NotificationElement.class).get(0).close();
+        Assert.assertEquals("1. Notification (error) closed by user",
+                getLogRow(0));
+    }
+
+    @Test
+    public void testCloseByServer() {
+        openTestURL();
+        ButtonElement warning = $(ButtonElement.class).caption("warning")
+                .first();
+        warning.click();
+        ButtonElement close = $(ButtonElement.class)
+                .caption("Hide all notifications").first();
+        close.click();
+
+        Assert.assertEquals("1. Notification (warning) closed from server",
+                getLogRow(0));
+    }
+
+    @Test
+    public void notificationsStayAwayAfterRefresh() {
+        openTestURL();
+        ButtonElement warning = $(ButtonElement.class).caption("warning")
+                .first();
+        warning.click();
+        $(NotificationElement.class).first().close();
+        openTestURL();
+
+        Assert.assertEquals(0, $(NotificationElement.class).all().size());
+    }
+}
