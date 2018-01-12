8270659148a750b11d7b7cea567b9d710949957a
Teemu Suo-Anttila <tsuoanttila@users.noreply.github.com>
2018-01-12 15:34:25 +0200

    Update Spring and CDI versions in BOM (#10530)

--- test/cdi/src/main/java/com/vaadin/test/cdi/views/NameBasedView.java
+++ test/cdi/src/main/java/com/vaadin/test/cdi/views/NameBasedView.java
@@ -1,8 +1,9 @@
 package com.vaadin.test.cdi.views;
 
 import com.vaadin.cdi.CDIView;
-import com.vaadin.cdi.ViewContextStrategy.ViewName;
+import com.vaadin.cdi.viewcontextstrategy.ViewContextByName;
 
-@CDIView(value = "name", contextStrategy = ViewName.class)
+@CDIView(value = "name")
+@ViewContextByName
 public class NameBasedView extends GreetingView {
 }
