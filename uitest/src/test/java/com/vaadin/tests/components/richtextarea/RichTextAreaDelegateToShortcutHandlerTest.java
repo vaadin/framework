7f57608a24e4c83baea180ab6e41001e2df8e654
Marco Collovati <mcollovati@gmail.com>
2018-01-17 01:02:26 +0100

    Exclude phantomjs from browsers to test

--- uitest/src/test/java/com/vaadin/tests/components/richtextarea/RichTextAreaDelegateToShortcutHandlerTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/richtextarea/RichTextAreaDelegateToShortcutHandlerTest.java
@@ -1,15 +1,23 @@
 package com.vaadin.tests.components.richtextarea;
 
+import java.util.List;
+
 import com.vaadin.testbench.elements.RichTextAreaElement;
 import com.vaadin.tests.tb3.MultiBrowserTest;
 import org.junit.Test;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
 
 public class RichTextAreaDelegateToShortcutHandlerTest extends MultiBrowserTest {
+
+    @Override
+    public List<DesiredCapabilities> getBrowsersToTest() {
+        return getBrowsersExcludingPhantomJS();
+    }
 
     @Test
     public void shouldDelegateToShortcutActionHandler() {
