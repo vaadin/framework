481e0bd0dbed00d7ef686d3fadffdb44edd49956
elmot <elmotelmot.vaadin.com>
2018-02-14 17:25:43 +0200

    Multiple triggers per menu, fixes

---
+++ uitest/src/main/java/com/vaadin/tests/extensions/EventTriggerExtensionTest.java
@@ -0,0 +1,23 @@
+package com.vaadin.tests.extensions;
+
+import com.vaadin.server.AbstractExtension;
+import com.vaadin.server.EventTrigger;
+import com.vaadin.shared.extension.PartInformationState;
+
+public class EventTriggerExtensionTest extends AbstractExtension {
+
+    @Override
+    protected PartInformationState getState() {
+        return (PartInformationState) super.getState();
+    }
+
+    @Override
+    protected PartInformationState getState(boolean markAsDirty) {
+        return (PartInformationState) super.getState(markAsDirty);
+    }
+
+    public void extend(EventTrigger eventTrigger) {
+        super.extend(eventTrigger.getConnector());
+        getState().partInformation = eventTrigger.getPartInformation();
+    }
+}
