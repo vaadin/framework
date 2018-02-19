cd9a16726a474c0e48030115cf3d8f53d4dbdf6a
Artur Signell <artur@vaadin.com>
2018-02-19 13:40:24 +0200

    Correctly handle data providers with overriden getId
    Fixes #10498

--- server/src/main/java/com/vaadin/ui/AbstractMultiSelect.java
+++ server/src/main/java/com/vaadin/ui/AbstractMultiSelect.java
@@ -323,16 +323,26 @@ public abstract class AbstractMultiSelect<T> extends AbstractListing<T>
 
         // if there are duplicates, some item is both added & removed, just
         // discard that and leave things as was before
-        addedItems.removeIf(item -> removedItems.remove(item));
+        DataProvider<T, ?> dataProvider = internalGetDataProvider();
 
-        if (selection.containsAll(addedItems)
-                && Collections.disjoint(selection, removedItems)) {
+        addedItems.removeIf(item -> {
+            Object addedId = dataProvider.getId(item);
+            return removedItems.stream().map(dataProvider::getId)
+                    .anyMatch(addedId::equals);
+        });
+
+        if (isAllSelected(addedItems) && isNoneSelected(removedItems)) {
             return;
         }
 
         updateSelection(set -> {
             // order of add / remove does not matter since no duplicates
-            set.removeAll(removedItems);
+            set.removeIf(item -> {
+                Object itemId = dataProvider.getId(item);
+
+                return removedItems.stream().map(dataProvider::getId)
+                        .anyMatch(itemId::equals);
+            });
             set.addAll(addedItems);
         }, userOriginated);
     }
@@ -357,6 +367,26 @@ public abstract class AbstractMultiSelect<T> extends AbstractListing<T>
         Object id = dataProvider.getId(item);
         return selection.stream().map(dataProvider::getId).anyMatch(id::equals);
 
+    }
+
+    private boolean isAllSelected(Collection<T> items) {
+        for (T item : items) {
+            if (!isSelected(item)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isNoneSelected(Collection<T> items) {
+        for (T item : items) {
+            if (isSelected(item)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     /**
