a063a1eb866af690c7c26d4701e9c04c470385a9
Tatu Lund <tatu@vaadin.com>
2018-03-27 13:21:24 +0300

    Fixing formatting

--- uitest/src/test/java/com/vaadin/tests/components/grid/basics/GridEventListenersTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/grid/basics/GridEventListenersTest.java
@@ -7,22 +7,24 @@ import com.vaadin.testbench.elements.GridElement;
 
 public class GridEventListenersTest extends GridBasicsTest {
 
-	@Test
-	public void testItemClickListener() {
-		selectMenuPath("Component", "State", "Item click listener");
-		selectMenuPath("Component", "State", "Selection model", "none");
-		checkItemClickOnRow(0);
-		checkItemClickOnRow(2);
-		GridElement grid = getGridElement();
-		grid.getHeaderCell(0, 7);
-		checkItemClickOnRow(0);
-		checkItemClickOnRow(2);
-	}
+    @Test
+    public void testItemClickListener() {
+        selectMenuPath("Component", "State", "Item click listener");
+        selectMenuPath("Component", "State", "Selection model", "none");
+        checkItemClickOnRow(0);
+        checkItemClickOnRow(2);
+        GridElement grid = getGridElement();
+        grid.getHeaderCell(0, 7);
+        checkItemClickOnRow(0);
+        checkItemClickOnRow(2);
+    }
 
-	private void checkItemClickOnRow(int row) {
-		GridElement grid = getGridElement();
-		grid.getCell(row, 2).click();
-		String logRow = getLogRow(0);
-		Assert.assertTrue("Log row '" + logRow + "' did not contain index " + row, logRow.endsWith("Index " + row));
-	}
+    private void checkItemClickOnRow(int row) {
+        GridElement grid = getGridElement();
+        grid.getCell(row, 2).click();
+        String logRow = getLogRow(0);
+        Assert.assertTrue(
+                "Log row '" + logRow + "' did not contain index " + row,
+                logRow.endsWith("Index " + row));
+    }
 }
