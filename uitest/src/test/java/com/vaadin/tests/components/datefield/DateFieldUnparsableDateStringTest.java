afdea6a569894745693b171347dfedfe75dcc257
Anastasia Smirnova <anasmi@utu.fi>
2018-04-10 19:51:15 +0300

    Changing test file further
    Test is working locally, but fails in TC. New attempt to fix

--- uitest/src/test/java/com/vaadin/tests/components/datefield/DateFieldUnparsableDateStringTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/datefield/DateFieldUnparsableDateStringTest.java
@@ -3,7 +3,6 @@ package com.vaadin.tests.components.datefield;
 import com.vaadin.testbench.By;
 import com.vaadin.tests.tb3.MultiBrowserTest;
 import org.junit.Test;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 import static org.junit.Assert.assertEquals;
@@ -11,15 +10,17 @@ import static org.junit.Assert.assertEquals;
 public class DateFieldUnparsableDateStringTest extends MultiBrowserTest {
 
     @Test
-    public void testInvalidText() {
+    public void testInvalidText() throws InterruptedException {
         openTestURL();
-        waitForElementVisible(By.className("v-datefield"));
+        Thread.sleep(1000);
 
-        getInput().sendKeys("0304", Keys.TAB);
+        getInput().sendKeys("0304");
+        findElement(By.tagName("body")).click();
         assertEquals("03.04.2018", getInput().getAttribute("value"));
         getInput().clear();
 
-        getInput().sendKeys("0304", Keys.TAB);
+        getInput().sendKeys("0304");
+        findElement(By.tagName("body")).click();
         assertEquals("03.04.2018", getInput().getAttribute("value"));
     }
 
