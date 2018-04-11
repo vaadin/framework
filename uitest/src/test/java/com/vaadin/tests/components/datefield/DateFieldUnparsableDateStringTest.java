800a6fb88767e004677814afbda3e1c9998d987b
Anastasia Smirnova <anasmi@utu.fi>
2018-04-11 07:25:39 +0300

    Using another approach in testing functionality

--- uitest/src/test/java/com/vaadin/tests/components/datefield/DateFieldUnparsableDateStringTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/datefield/DateFieldUnparsableDateStringTest.java
@@ -1,8 +1,10 @@
 package com.vaadin.tests.components.datefield;
 
 import com.vaadin.testbench.By;
+import com.vaadin.testbench.elements.AbstractDateFieldElement;
 import com.vaadin.tests.tb3.MultiBrowserTest;
 import org.junit.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 import static org.junit.Assert.assertEquals;
@@ -10,21 +12,18 @@ import static org.junit.Assert.assertEquals;
 public class DateFieldUnparsableDateStringTest extends MultiBrowserTest {
 
     @Test
-    public void testInvalidText() throws InterruptedException {
+    public void testInvalidText() {
         openTestURL();
-        Thread.sleep(1000);
-
-        getInput().sendKeys("0304");
+        waitForElementVisible(By.className("v-datefield"));
+        WebElement dateTextbox = $(AbstractDateFieldElement.class).first()
+                .findElement(By.className("v-textfield"));
+        dateTextbox.sendKeys("0304", Keys.TAB);
         findElement(By.tagName("body")).click();
-        assertEquals("03.04.2018", getInput().getAttribute("value"));
-        getInput().clear();
+        assertEquals("03.04.2018", dateTextbox.getAttribute("value"));
 
-        getInput().sendKeys("0304");
+        dateTextbox.clear();
+        dateTextbox.sendKeys("0304", Keys.TAB);
         findElement(By.tagName("body")).click();
-        assertEquals("03.04.2018", getInput().getAttribute("value"));
-    }
-
-    private WebElement getInput() {
-        return findElement(By.xpath("//input"));
+        assertEquals("03.04.2018", dateTextbox.getAttribute("value"));
     }
 }
