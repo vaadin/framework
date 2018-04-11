54074e02336c6da3ed3025744a0dff62ac69df72
Anastasia Smirnova <anasmi@utu.fi>
2018-04-11 14:05:37 +0300

    Skipping Phantom JS
    Skip Phantom JS to see if others work correctly.
    Locally Phantom test runs correctly, fails on TC

--- uitest/src/test/java/com/vaadin/tests/components/datefield/DateFieldUnparsableDateStringTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/datefield/DateFieldUnparsableDateStringTest.java
@@ -2,10 +2,14 @@ package com.vaadin.tests.components.datefield;
 
 import com.vaadin.testbench.By;
 import com.vaadin.testbench.elements.AbstractDateFieldElement;
+import com.vaadin.testbench.parallel.Browser;
 import com.vaadin.tests.tb3.MultiBrowserTest;
 import org.junit.Test;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -27,5 +31,12 @@ public class DateFieldUnparsableDateStringTest extends MultiBrowserTest {
         Thread.sleep(1000);
         findElement(By.tagName("body")).click();
         assertEquals("03.04.2018", dateTextbox.getAttribute("value"));
+    }
+
+    @Override
+    public List<DesiredCapabilities> getBrowsersToTest() {
+        // Ignoring Phantom JS
+        return getBrowserCapabilities(Browser.IE11, Browser.FIREFOX,
+                Browser.CHROME);
     }
 }
