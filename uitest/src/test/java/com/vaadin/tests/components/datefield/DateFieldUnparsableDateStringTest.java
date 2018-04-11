7980856b750f808b21a07bf9538f3eab0a790bb1
Anastasia Smirnova <anasmi@utu.fi>
2018-04-11 11:05:43 +0300

    Adding waiting methods
    Adding sleep methods, also changing to Key.Enter

--- uitest/src/test/java/com/vaadin/tests/components/datefield/DateFieldUnparsableDateStringTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/datefield/DateFieldUnparsableDateStringTest.java
@@ -12,17 +12,19 @@ import static org.junit.Assert.assertEquals;
 public class DateFieldUnparsableDateStringTest extends MultiBrowserTest {
 
     @Test
-    public void testInvalidText() {
+    public void testInvalidText() throws InterruptedException {
         openTestURL();
         waitForElementVisible(By.className("v-datefield"));
         WebElement dateTextbox = $(AbstractDateFieldElement.class).first()
                 .findElement(By.className("v-textfield"));
-        dateTextbox.sendKeys("0304", Keys.TAB);
+        dateTextbox.sendKeys("0304", Keys.ENTER);
         findElement(By.tagName("body")).click();
+        Thread.sleep(1000);
         assertEquals("03.04.2018", dateTextbox.getAttribute("value"));
 
         dateTextbox.clear();
-        dateTextbox.sendKeys("0304", Keys.TAB);
+        dateTextbox.sendKeys("0304", Keys.ENTER);
+        Thread.sleep(1000);
         findElement(By.tagName("body")).click();
         assertEquals("03.04.2018", dateTextbox.getAttribute("value"));
     }
