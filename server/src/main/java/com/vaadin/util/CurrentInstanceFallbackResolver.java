a8ba1bae2b274cbf54b56aec32a37a6b43c6563c
Gilberto Torrezan <gilberto-torrezan@users.noreply.github.com>
2018-06-13 10:08:04 +0300

    Add fallback resolvers for CurrentInstance (#10974)
    This allow applications to inject custom default instances when the
    current instances cannot be found by regular means.
    
    For example, when VaadinServlet.getCurrent() would return null, a
    fallback resolver could be invoked to properly create the servlet and
    return it.

---
+++ server/src/main/java/com/vaadin/util/CurrentInstanceFallbackResolver.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2000-2018 Vaadin Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.vaadin.util;
+
+import java.io.Serializable;
+
+/**
+ * Fallback that is used to revolve current instances when they are not
+ * available by regular means.
+ * <p>
+ * This interface is used internally by the framework and it's not meant for
+ * public usage.
+ *
+ * @author Vaadin Ltd.
+ *
+ * @param <T>
+ *            the type of the instances returned by this resolver
+ *
+ * @see CurrentInstance#get(Class)
+ * @see CurrentInstance#defineFallbackResolver(Class,
+ *      CurrentInstanceFallbackResolver)
+ *
+ * @since
+ *
+ */
+public interface CurrentInstanceFallbackResolver<T> extends Serializable {
+
+    /**
+     * Resolves a current instance for the type {@code T}.
+     *
+     * @return the current instance, or <code>null</code> if none can be found
+     */
+    T resolve();
+
+}
