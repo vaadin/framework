e0d10059de7719af37be2a02b7797047b957ef2b
Teemu Suo-Anttila <teemusa@vaadin.com>
2018-06-18 14:19:00 +0300

    Fix TabSheet attaching and detaching components
    This patch reverts the fix #10557 and replaces it with a
    proper solution from Grid perspective.
    
    Fixes #10987
    Fixes #10985

--- uitest/src/main/java/com/vaadin/tests/components/grid/GridInTabSheet.java
+++ uitest/src/main/java/com/vaadin/tests/components/grid/GridInTabSheet.java
@@ -7,6 +7,7 @@ import java.util.stream.IntStream;
 
 import com.vaadin.annotations.Widgetset;
 import com.vaadin.data.ValueProvider;
+import com.vaadin.data.converter.StringToIntegerConverter;
 import com.vaadin.data.provider.DataProvider;
 import com.vaadin.data.provider.ListDataProvider;
 import com.vaadin.server.VaadinRequest;
@@ -15,6 +16,7 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.Grid.SelectionMode;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.TabSheet;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.renderers.NumberRenderer;
 
 @Widgetset("com.vaadin.DefaultWidgetSet")
@@ -48,7 +50,12 @@ public class GridInTabSheet extends AbstractTestUIWithLog {
         final Grid<Integer> grid = new Grid<>();
         grid.setSelectionMode(SelectionMode.MULTI);
         grid.addColumn(ValueProvider.identity(), new NumberRenderer())
-                .setId("count");
+                .setId("count").setEditorBinding(
+                        grid.getEditor().getBinder().forField(new TextField())
+                                .withConverter(new StringToIntegerConverter(""))
+                                .bind(i -> i, (i, v) -> {
+                                }));
+        grid.getEditor().setEnabled(true);
 
         LinkedList<Integer> items = IntStream.range(0, 3).boxed()
                 .collect(Collectors.toCollection(LinkedList::new));
