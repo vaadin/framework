9dc83e285ced088124783f280e316da7b5a6033f
Elmot <elmot@vaadin.com>
2018-08-31 16:45:09 +0300

    Restore 2nd part of InterruptUploadTest

--- uitest/src/test/java/com/vaadin/tests/components/upload/InterruptUploadTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/upload/InterruptUploadTest.java
@@ -1,6 +1,7 @@
 package com.vaadin.tests.components.upload;
 
 import com.vaadin.testbench.elements.LabelElement;
+import com.vaadin.testbench.elements.WindowElement;
 import com.vaadin.tests.tb3.MultiBrowserTest;
 import com.vaadin.tests.util.LoremIpsum;
 import org.junit.Test;
@@ -20,26 +21,43 @@ import static org.junit.Assert.assertTrue;
 
 public class InterruptUploadTest extends MultiBrowserTest {
 
+    private static final String EXPECTED_COUNTER_TEXT = " (counting interrupted at ";
+
     @Test
     public void testInterruptUpload() throws Exception {
         openTestURL();
 
         File tempFile = createTempFile();
-        // Schedule upload cancel in a second
-        ((JavascriptExecutor) getDriver()).executeScript(
-                "setTimeout( function () {window.document.querySelector(\".v-window .v-button\").click()},2000)");
+        scheduleUploadCancel();
 
         fillPathToUploadInput(tempFile.getPath());
 
         // Wait for 3 seconds until everything is done.
         Thread.sleep(3000);
 
-        String expected = " (counting interrupted at ";
         String actual = $(LabelElement.class).caption("Line breaks counted")
                 .first().getText();
         assertTrue("Line break count note does not match expected (was: "
-                + actual + ")", actual.contains(expected));
+                + actual + ")", actual.contains(EXPECTED_COUNTER_TEXT));
 
+        $(WindowElement.class).first().close();
+        waitForElementNotPresent(By.className("v-window"));
+
+        // Check if second upload happens
+        tempFile = createTempFile();
+        scheduleUploadCancel();
+        fillPathToUploadInput(tempFile.getPath());
+
+        actual = $(LabelElement.class).caption("Line breaks counted").first()
+                .getText();
+        assertTrue("Line break count note does not match expected (was: "
+                + actual + ")", actual.contains(EXPECTED_COUNTER_TEXT));
+    }
+
+    private void scheduleUploadCancel() {
+        // Schedule upload cancel in 2 seconds
+        ((JavascriptExecutor) getDriver()).executeScript(
+                "setTimeout( function () {window.document.querySelector(\".v-window .v-button\").click()},2000)");
     }
 
     /**
