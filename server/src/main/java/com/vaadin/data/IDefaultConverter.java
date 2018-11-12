07c967f326d5bb84cada93bfa091f76ce4ba40ef
Mikkel Nylend <mrminy@users.noreply.github.com>
2018-11-12 11:01:06 +0100

    Add files via upload
    Added an optional converter interface that can be injected into the Vaadin 8 Binder. This makes it possible to setup default converters for your application.

---
+++ server/src/main/java/com/vaadin/data/IDefaultConverter.java
@@ -0,0 +1,66 @@
+package com.vaadin.data.converter;
+
+import com.vaadin.data.Binder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
+
+public interface IDefaultConverter {
+
+	/**
+	 * Converts a {@link Binder.BindingBuilder} to a given class for an entity property
+	 * @param builder the field builder
+	 * @param propertyType the entity property class type
+	 * @return the converted binder builder
+	 */
+	default Binder.BindingBuilder build(Binder.BindingBuilder builder, Class<?> fieldType, Class<?> propertyType) {
+		if (isSupported(fieldType, propertyType)){
+			if (fieldType.equals(String.class)){
+				if (propertyType == Integer.class){
+					// Convert java.lang.String to java.lang.Integer
+					builder = builder.withNullRepresentation("").withConverter(new StringToIntegerConverter("Must be an integer!"));
+				}else if (propertyType == Double.class){
+					// Convert java.lang.String to java.lang.Double
+					builder = builder.withNullRepresentation("").withConverter(new StringToDoubleConverter("Must be a double!"));
+				}else if (propertyType == Long.class){
+					// Convert java.lang.String to java.lang.Long
+					builder = builder.withNullRepresentation("").withConverter(new StringToLongConverter("Must be a long!"));
+				}else if (propertyType == Float.class){
+					// Convert java.lang.String to java.lang.Float
+					builder = builder.withNullRepresentation("").withConverter(new StringToFloatConverter("Must be a float!"));
+				}
+			}else if (fieldType.equals(LocalDateTime.class)){
+				if (propertyType == Date.class){
+					// Convert java.time.LocalDateTime to java.util.
+					builder = builder.withNullRepresentation("").withConverter(new LocalDateTimeToDateConverter(ZoneId.systemDefault()));
+				}
+			}else if (fieldType.equals(LocalDate.class)){
+				if (propertyType == Date.class){
+					// Convert java.time.LocalDate to java.util.Date
+					builder = builder.withNullRepresentation("").withConverter(new LocalDateToDateConverter(ZoneId.systemDefault()));
+				}
+			}
+		}
+		return builder;
+	}
+
+	default Map<Class<?>, Set<Class<?>>> getSupportedConverts(){
+		HashMap<Class<?>, Set<Class<?>>> map = new HashMap<>();
+		map.put(String.class, new HashSet<>(Arrays.asList(String.class, Integer.class, Double.class, Long.class)));
+		map.put(LocalDateTime.class, new HashSet<>(Collections.singletonList(Date.class)));
+		return map;
+	}
+
+	/**
+	 * Is it possible to convert the field class to a given property type class?
+	 * @param fieldClass class of the field (String for {@link com.vaadin.ui.TextField})
+	 * @param propertyType class of the property type in the entity
+	 * @return true if it possible, false if not
+	 */
+	default boolean isSupported(Class<?> fieldClass, Class<?> propertyType){
+		Set<Class<?>> supported = getSupportedConverts().get(fieldClass);
+		return supported != null && supported.size() > 0 && supported.contains(propertyType);
+	}
+}
