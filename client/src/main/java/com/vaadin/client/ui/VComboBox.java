8a5820cb26b37aaec560a7d411d32a7b42239635
Tatu Lund <tatu@vaadin.com>
2019-02-27 14:38:02 +0200

    Fixing autocompletion issue with ComboBox on newer Chrome versions
    Newer Chrome versions do not work with random number hack to prevent auto completion, but it finally supports autocomplete=off.

--- client/src/main/java/com/vaadin/client/ui/VComboBox.java
+++ client/src/main/java/com/vaadin/client/ui/VComboBox.java
@@ -1372,7 +1372,12 @@ public class VComboBox extends Composite implements Field, KeyDownHandler,
              * way, and they might be useful in a combo box where new items are
              * allowed.
              */
-            getElement().setAttribute("autocomplete", Math.random() + "");
+            if (BrowserInfo.get().isChrome()) {
+                // Chrome supports "off" and random number does not work with Chrome
+                getElement().setAttribute("autocomplete", "off");        		
+            } else {
+                getElement().setAttribute("autocomplete", Math.random() + "");
+            }
         }
 
         /**
