ddca3a18caf66c8a986e10bc8d9bb9633034ebb7
ZheSun88 <zhe@vaadin.com>
2019-03-06 16:24:06 +0200

    Review fixes

--- client/src/main/java/com/vaadin/client/ui/VComboBox.java
+++ client/src/main/java/com/vaadin/client/ui/VComboBox.java
@@ -1638,7 +1638,7 @@ public class VComboBox extends Composite implements Field, KeyDownHandler,
                     !isWaitingForFilteringResponse() || popupOpenerClicked);
 
             // currentSuggestion should be set to match the value of the ComboBox
-            resetCurrentSuggestion(selectedKey, selectedCaption,
+            resetCurrentSuggestionIfNecessary(selectedKey, selectedCaption,
                     selectedIconUri);
 
             cancelPendingPostFiltering();
