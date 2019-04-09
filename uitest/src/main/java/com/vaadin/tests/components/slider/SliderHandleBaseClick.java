8972e09df154109b0ffb6f0b4c06216c2f023998
Anastasia Smirnova <anasmi@utu.fi>
2019-04-09 14:59:26 +0300

    Introducing control over clicking behaviour
    - User will have to enable process of the click event on handle calling `slider.setEnableClickHandler(true);`
    - Clean-up the handler logic in VSlider.java
    - Format tests

--- uitest/src/main/java/com/vaadin/tests/components/slider/SliderHandleBaseClick.java
+++ uitest/src/main/java/com/vaadin/tests/components/slider/SliderHandleBaseClick.java
@@ -4,28 +4,33 @@ import com.vaadin.annotations.Widgetset;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.tests.components.AbstractTestUI;
 import com.vaadin.ui.Slider;
+import com.vaadin.ui.Button;
 
 @Widgetset("com.vaadin.DefaultWidgetSet")
-public class SliderHandleBaseClick  extends AbstractTestUI {
+public class SliderHandleBaseClick extends AbstractTestUI {
 
-        @Override
-        protected void setup(VaadinRequest request) {
-                Slider slider = new Slider();
-                slider.setWidth(500, Unit.PIXELS);
-                slider.setMin(0);
-                slider.setMax(15);
-                slider.setValue(3.0);
-                addComponent(slider);
-        }
+    @Override
+    protected void setup(VaadinRequest request) {
+        Slider slider = new Slider();
+        slider.setWidth(500, Unit.PIXELS);
+        slider.setMin(0);
+        slider.setMax(15);
+        slider.setValue(3.0);
+        slider.setEnableClickHandler(true);
+        addComponent(slider);
+        addComponent(new Button("Apply/remove click action", e -> {
+            slider.setEnableClickHandler(!slider.isEnableClickHandler());
+        }));
+    }
 
-        @Override
-        protected String getTestDescription() {
-                return "Slider should update its value, when clicked on the base";
-        }
+    @Override
+    protected String getTestDescription() {
+        return "Slider should update its value, when clicked on the base";
+    }
 
-        @Override
-        protected Integer getTicketNumber() {
-                return 1496;
-        }
+    @Override
+    protected Integer getTicketNumber() {
+        return 1496;
+    }
 
 }
--- uitest/src/test/java/com/vaadin/tests/components/slider/SliderHandleBaseClickTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/slider/SliderHandleBaseClickTest.java
@@ -7,24 +7,24 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 public class SliderHandleBaseClickTest extends MultiBrowserTest {
-        @Test
-        public void testHandlerHasMoved() {
-                openTestURL();
+    @Test
+    public void testHandlerHasMoved() {
+        openTestURL();
 
-                WebElement base = findElement(By.className("v-slider-base"));
+        WebElement base = findElement(By.className("v-slider-base"));
 
-                int offsetStep=base.getSize().getWidth()/10;
-                new Actions(driver).dragAndDropBy(base, offsetStep, 0).perform();
-                testBench().waitForVaadin();
+        int offsetStep = base.getSize().getWidth() / 10;
+        new Actions(driver).dragAndDropBy(base, offsetStep, 0).perform();
+        testBench().waitForVaadin();
 
-                double valueBefore = Double.valueOf(
-                        findElement(By.className("v-slider-feedback")).getText());
+        double valueBefore = Double.valueOf(
+                findElement(By.className("v-slider-feedback")).getText());
 
-                new Actions(driver).dragAndDropBy(base, offsetStep*5, 0).perform();
-                testBench().waitForVaadin();
-                double valueAfter= Double.valueOf(
-                        findElement(By.className("v-slider-feedback")).getText());
-                assertGreater("Unexpected feedback value {1} > {0}", valueAfter,
-                        valueBefore);
-        }
+        new Actions(driver).dragAndDropBy(base, offsetStep * 5, 0).perform();
+        testBench().waitForVaadin();
+        double valueAfter = Double.valueOf(
+                findElement(By.className("v-slider-feedback")).getText());
+        assertGreater("Unexpected feedback value {1} > {0}", valueAfter,
+                valueBefore);
+    }
 }
