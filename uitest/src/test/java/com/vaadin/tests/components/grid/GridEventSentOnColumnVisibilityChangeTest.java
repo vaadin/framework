fc5b1c5ec85982db0c459e9db8a200d66e99d2ab
ZheSun88 <zhe@vaadin.com>
2019-05-15 13:40:19 +0300

    Add Test

---
+++ uitest/src/test/java/com/vaadin/tests/components/grid/GridEventSentOnColumnVisibilityChangeTest.java
@@ -0,0 +1,41 @@
+package com.vaadin.tests.components.grid;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.testbench.By;
+import com.vaadin.testbench.elements.GridElement;
+import com.vaadin.tests.tb3.SingleBrowserTest;
+
+public class GridEventSentOnColumnVisibilityChangeTest extends SingleBrowserTest {
+
+    @Test
+    public void changeVisibilityAssertLog() {
+        openTestURL();
+
+        GridElement grid = $(GridElement.class).first();
+        getSidebarOpenButton(grid).click();
+        //hide the first column
+        getSidebarPopup().findElements(By.tagName("td")).get(0).click();
+
+        Assert.assertEquals("There should have only log/event",
+                1, getLogs().size());
+        Assert.assertTrue("Log content should match",
+                "1. UserOriginated: false".equals(getLogRow(0)));
+    }
+
+    protected WebElement getSidebarOpenButton(GridElement grid) {
+        List<WebElement> elements = grid
+                .findElements(By.className("v-grid-sidebar-button"));
+        return elements.isEmpty() ? null : elements.get(0);
+    }
+
+    protected WebElement getSidebarPopup() {
+        List<WebElement> elements = findElements(
+                By.className("v-grid-sidebar-popup"));
+        return elements.isEmpty() ? null : elements.get(0);
+    }
+}
