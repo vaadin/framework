d1d44798483d46b081be64790bbce0766a8896c8
ZheSun88 <zhe@vaadin.com>
2019-06-11 16:10:15 +0300

    Remove unnecessary test

--- uitest/src/test/java/com/vaadin/tests/components/grid/GridExtensionCommunicationTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/grid/GridExtensionCommunicationTest.java
@@ -19,14 +19,8 @@ public class GridExtensionCommunicationTest extends SingleBrowserTest {
         GridCellElement cell = $(GridElement.class).first().getCell(0, 1);
         cell.click(getXOffset(cell, 5), getYOffset(cell, 5));
 
-        int expectedX = cell.getLocation().getX() + 5;
-        int expectedY = cell.getLocation().getY() + 4;
-
         assertEquals(
                 "1. Click on Person first name 1 last name 1 on column second",
                 getLogRow(1));
-        assertEquals("2. MouseEventDetails: left (" + expectedX + ", "
-                + expectedY + ")", getLogRow(0));
-
     }
 }
