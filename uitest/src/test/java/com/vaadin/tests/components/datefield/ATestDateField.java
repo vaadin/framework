6393d7805e0f7e628a3f248b5428f960224881e2
Anna Koskinen <anna@vaadin.com>
2019-08-07 21:44:19 +0300

    Read-only styles shouldn't override borderless background styles.
    - Fixed for ComboBox and DateField styles, other tested components
    already do it right.
    
    Fixes #11671

---
+++ uitest/src/test/java/com/vaadin/tests/components/datefield/ATestDateField.java
@@ -0,0 +1,21 @@
+package com.vaadin.tests.components.datefield;
+
+public class ATestDateField {
+
+    // LocalDate special = LocalDate.of(2017, Month.NOVEMBER, 11);
+    //
+    // DateStyleGenerator sg = new DateStyleGenerator() {
+    //
+    // @Override
+    // public String getStyle(DateField source, LocalDate date) {
+    // if (special.isEqual(date)) {
+    // return "special-style";
+    // }
+    // return null;
+    // }
+    //
+    // };
+    //
+    // dateField.setStyleName(special, "special-style");
+
+}
