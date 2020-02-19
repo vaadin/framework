953d19e0fbdea9ea4118ecb7d505f474d640e121
Tatu Lund <tatu@vaadin.com>
2020-02-19 09:31:11 +0200

    Added missing import

--- uitest/src/test/java/com/vaadin/tests/components/datefield/DateFieldFaultyInputNotValidTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/datefield/DateFieldFaultyInputNotValidTest.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Actions;
 
 import com.vaadin.testbench.elements.ButtonElement;
