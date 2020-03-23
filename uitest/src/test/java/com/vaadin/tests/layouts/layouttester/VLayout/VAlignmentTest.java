8ef17840d3e96ec94bc843b6966e82c91961c3ab
Anna Koskinen <Ansku@users.noreply.github.com>
2020-03-23 17:48:20 +0200

    Tweaked layouttester tests to be more informative. (#11800) (#11813)

--- uitest/src/test/java/com/vaadin/tests/layouts/layouttester/VLayout/VAlignmentTest.java
+++ uitest/src/test/java/com/vaadin/tests/layouts/layouttester/VLayout/VAlignmentTest.java
@@ -1,6 +1,30 @@
 package com.vaadin.tests.layouts.layouttester.VLayout;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.interactions.Actions;
+
+import com.vaadin.testbench.elements.TextFieldElement;
 import com.vaadin.tests.layouts.layouttester.BaseAlignmentTest;
 
 public class VAlignmentTest extends BaseAlignmentTest {
+
+    @Override
+    public void layoutAlignment() throws IOException {
+        super.layoutAlignment();
+
+        // The layout is too high to fit into one screenshot, we need to scroll
+        // and take another.
+
+        List<TextFieldElement> textFields = $(TextFieldElement.class).all();
+        assertEquals(9, textFields.size());
+        TextFieldElement lastTextField = textFields.get(8);
+
+        new Actions(driver).moveToElement(lastTextField).build().perform();
+
+        compareScreen("alignment-scrolled");
+    }
 }
