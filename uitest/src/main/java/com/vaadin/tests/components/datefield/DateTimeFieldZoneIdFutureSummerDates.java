898b7bd0c3be7c923d12f977ba14113a79412823
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2020-04-20 15:27:28 +0300

    Fix formatting of test classes

--- uitest/src/main/java/com/vaadin/tests/components/datefield/DateTimeFieldZoneIdFutureSummerDates.java
+++ uitest/src/main/java/com/vaadin/tests/components/datefield/DateTimeFieldZoneIdFutureSummerDates.java
@@ -27,142 +27,159 @@ public class DateTimeFieldZoneIdFutureSummerDates extends AbstractTestUI {
 
     static final String INITIAL_ZONE_ID = "CET";
     static final Locale INITIAL_LOCALE = Locale.US;
-    static final LocalDateTime INITIAL_DATE_TIME = LocalDateTime.of(LocalDate.now().getYear() + 21, Month.JULY, 1, 0,
-	    0);
-    static final LocalDate INITIAL_START_DATE = LocalDate.of(INITIAL_DATE_TIME.getYear() - 5, Month.JULY, 1);
-    static final LocalDate INITIAL_END_DATE = LocalDate.of(INITIAL_DATE_TIME.getYear() + 5, Month.JULY, 1);
+    static final LocalDateTime INITIAL_DATE_TIME = LocalDateTime
+            .of(LocalDate.now().getYear() + 21, Month.JULY, 1, 0, 0);
+    static final LocalDate INITIAL_START_DATE = LocalDate
+            .of(INITIAL_DATE_TIME.getYear() - 5, Month.JULY, 1);
+    static final LocalDate INITIAL_END_DATE = LocalDate
+            .of(INITIAL_DATE_TIME.getYear() + 5, Month.JULY, 1);
     private static final String TARGET_FORMAT_PATTERN = "dd MMM yyyy - z";
     private static final String RANGE_FORMAT_PATTERN = "yyyy";
 
     @Override
     protected String getTestDescription() {
-	return "DateTimeField should correctly show the daylight saving (summer time) zone name "
-		+ "of a date that occurs within a user-defined range";
+        return "DateTimeField should correctly show the daylight saving (summer time) zone name "
+                + "of a date that occurs within a user-defined range";
     }
 
     @Override
     protected Integer getTicketNumber() {
-	return 11919;
+        return 11919;
     }
 
     @Override
     protected void setup(VaadinRequest request) {
-	final ComboBox<String> zoneIdComboBox = getZoneIdComboBox();
-	addComponent(zoneIdComboBox);
+        final ComboBox<String> zoneIdComboBox = getZoneIdComboBox();
+        addComponent(zoneIdComboBox);
 
-	final ComboBox<Locale> localeIdComboBox = getLocaleIdComboBox();
-	addComponent(localeIdComboBox);
+        final ComboBox<Locale> localeIdComboBox = getLocaleIdComboBox();
+        addComponent(localeIdComboBox);
 
-	final DateField transitionStartyear = getDateField(START_YEAR_DATEFIELD_ID, INITIAL_START_DATE,
-		RANGE_FORMAT_PATTERN, "DST Transitions start year (inclusive):");
-	addComponent(transitionStartyear);
+        final DateField transitionStartyear = getDateField(
+                START_YEAR_DATEFIELD_ID, INITIAL_START_DATE,
+                RANGE_FORMAT_PATTERN,
+                "DST Transitions start year (inclusive):");
+        addComponent(transitionStartyear);
 
-	final DateField transitionEndyear = getDateField(END_YEAR_DATEFIELD_ID, INITIAL_END_DATE, RANGE_FORMAT_PATTERN,
-		"DST Transitions End year (inclusive):");
-	addComponent(transitionEndyear);
+        final DateField transitionEndyear = getDateField(END_YEAR_DATEFIELD_ID,
+                INITIAL_END_DATE, RANGE_FORMAT_PATTERN,
+                "DST Transitions End year (inclusive):");
+        addComponent(transitionEndyear);
 
-	String captionVarField = "A DateTimeField with custom start"
-		+ " and end years between which DST zone names are displayed:";
-	final DateTimeField dateTimeFieldWithCustomRange = getDateTimeField(VARIABLE_RANGE_DATEFIELD_ID,
-		INITIAL_DATE_TIME, TARGET_FORMAT_PATTERN, INITIAL_LOCALE, INITIAL_ZONE_ID, captionVarField);
-	dateTimeFieldWithCustomRange.setDaylightSavingTimeRange(INITIAL_START_DATE.getYear(),
-		INITIAL_END_DATE.getYear());
+        String captionVarField = "A DateTimeField with custom start"
+                + " and end years between which DST zone names are displayed:";
+        final DateTimeField dateTimeFieldWithCustomRange = getDateTimeField(
+                VARIABLE_RANGE_DATEFIELD_ID, INITIAL_DATE_TIME,
+                TARGET_FORMAT_PATTERN, INITIAL_LOCALE, INITIAL_ZONE_ID,
+                captionVarField);
+        dateTimeFieldWithCustomRange.setDaylightSavingTimeRange(
+                INITIAL_START_DATE.getYear(), INITIAL_END_DATE.getYear());
 
-	addComponent(dateTimeFieldWithCustomRange);
+        addComponent(dateTimeFieldWithCustomRange);
 
-	transitionStartyear.addValueChangeListener(event -> {
-	    int startYear = event.getValue().getYear();
-	    int endYear = transitionEndyear.getValue().getYear();
-	    if (startYear > endYear) {
-		showDateRangeError(transitionStartyear);
-	    } else {
-		clearErrors(transitionStartyear, transitionEndyear);
-		dateTimeFieldWithCustomRange.setDaylightSavingTimeRange(startYear, endYear);
-	    }
-	});
+        transitionStartyear.addValueChangeListener(event -> {
+            int startYear = event.getValue().getYear();
+            int endYear = transitionEndyear.getValue().getYear();
+            if (startYear > endYear) {
+                showDateRangeError(transitionStartyear);
+            } else {
+                clearErrors(transitionStartyear, transitionEndyear);
+                dateTimeFieldWithCustomRange
+                        .setDaylightSavingTimeRange(startYear, endYear);
+            }
+        });
 
-	transitionEndyear.addValueChangeListener(event -> {
-	    int startYear = transitionStartyear.getValue().getYear();
-	    int endYear = event.getValue().getYear();
-	    if (startYear > endYear) {
-		showDateRangeError(transitionEndyear);
-	    } else {
-		clearErrors(transitionStartyear, transitionEndyear);
-		dateTimeFieldWithCustomRange.setDaylightSavingTimeRange(startYear, endYear);
-	    }
-	});
+        transitionEndyear.addValueChangeListener(event -> {
+            int startYear = transitionStartyear.getValue().getYear();
+            int endYear = event.getValue().getYear();
+            if (startYear > endYear) {
+                showDateRangeError(transitionEndyear);
+            } else {
+                clearErrors(transitionStartyear, transitionEndyear);
+                dateTimeFieldWithCustomRange
+                        .setDaylightSavingTimeRange(startYear, endYear);
+            }
+        });
 
-	String captionFixedField = "A default DateTimeField (By default, "
-		+ "DST zones are displayed between 1980 and 20 years into the future):";
-	final DateTimeField dateTimeFieldWithDefaultRange = getDateTimeField(FIXED_RANGE_DATEFIELD_ID,
-		INITIAL_DATE_TIME, TARGET_FORMAT_PATTERN, INITIAL_LOCALE, INITIAL_ZONE_ID, captionFixedField);
-	addComponent(dateTimeFieldWithDefaultRange);
+        String captionFixedField = "A default DateTimeField (By default, "
+                + "DST zones are displayed between 1980 and 20 years into the future):";
+        final DateTimeField dateTimeFieldWithDefaultRange = getDateTimeField(
+                FIXED_RANGE_DATEFIELD_ID, INITIAL_DATE_TIME,
+                TARGET_FORMAT_PATTERN, INITIAL_LOCALE, INITIAL_ZONE_ID,
+                captionFixedField);
+        addComponent(dateTimeFieldWithDefaultRange);
 
-	zoneIdComboBox.addValueChangeListener(event -> {
-	    final String value = event.getValue();
-	    if (value == null) {
-		dateTimeFieldWithCustomRange.setZoneId(null);
-		dateTimeFieldWithDefaultRange.setZoneId(null);
-	    } else {
-		dateTimeFieldWithCustomRange.setZoneId(ZoneId.of(value));
-		dateTimeFieldWithDefaultRange.setZoneId(ZoneId.of(value));
-	    }
-	});
+        zoneIdComboBox.addValueChangeListener(event -> {
+            final String value = event.getValue();
+            if (value == null) {
+                dateTimeFieldWithCustomRange.setZoneId(null);
+                dateTimeFieldWithDefaultRange.setZoneId(null);
+            } else {
+                dateTimeFieldWithCustomRange.setZoneId(ZoneId.of(value));
+                dateTimeFieldWithDefaultRange.setZoneId(ZoneId.of(value));
+            }
+        });
 
-	localeIdComboBox.addValueChangeListener(event -> {
-	    dateTimeFieldWithCustomRange.setLocale(event.getValue());
-	    dateTimeFieldWithDefaultRange.setLocale(event.getValue());
-	});
+        localeIdComboBox.addValueChangeListener(event -> {
+            dateTimeFieldWithCustomRange.setLocale(event.getValue());
+            dateTimeFieldWithDefaultRange.setLocale(event.getValue());
+        });
     }
 
-    private DateTimeField getDateTimeField(String id, LocalDateTime initialDateTime, String dateFormat, Locale locale,
-	    String zoneId, String caption) {
-	final DateTimeField dateTimeField = new DateTimeField();
-	dateTimeField.setId(id);
-	dateTimeField.setValue(initialDateTime);
-	dateTimeField.setDateFormat(dateFormat);
-	dateTimeField.setLocale(locale);
-	dateTimeField.setZoneId(ZoneId.of(zoneId));
-	dateTimeField.setCaption(caption);
-	return dateTimeField;
+    private DateTimeField getDateTimeField(String id,
+            LocalDateTime initialDateTime, String dateFormat, Locale locale,
+            String zoneId, String caption) {
+        final DateTimeField dateTimeField = new DateTimeField();
+        dateTimeField.setId(id);
+        dateTimeField.setValue(initialDateTime);
+        dateTimeField.setDateFormat(dateFormat);
+        dateTimeField.setLocale(locale);
+        dateTimeField.setZoneId(ZoneId.of(zoneId));
+        dateTimeField.setCaption(caption);
+        return dateTimeField;
     }
 
-    private DateField getDateField(String id, LocalDate initialDate, String dateFormat, String caption) {
-	final DateField dateField = new DateField();
-	dateField.setId(id);
-	dateField.setDateFormat(dateFormat);
-	dateField.setCaption(caption);
-	dateField.setValue(initialDate);
-	return dateField;
+    private DateField getDateField(String id, LocalDate initialDate,
+            String dateFormat, String caption) {
+        final DateField dateField = new DateField();
+        dateField.setId(id);
+        dateField.setDateFormat(dateFormat);
+        dateField.setCaption(caption);
+        dateField.setValue(initialDate);
+        return dateField;
     }
 
-    private void clearErrors(DateField transitionStartyear, DateField transitionEndyear) {
-	transitionStartyear.setComponentError(null);
-	transitionEndyear.setComponentError(null);
+    private void clearErrors(DateField transitionStartyear,
+            DateField transitionEndyear) {
+        transitionStartyear.setComponentError(null);
+        transitionEndyear.setComponentError(null);
     }
 
     private void showDateRangeError(DateField dateField) {
-	dateField.setComponentError(new UserError("Start year must be less than or equal to end year!"));
+        dateField.setComponentError(new UserError(
+                "Start year must be less than or equal to end year!"));
     }
 
     private ComboBox<Locale> getLocaleIdComboBox() {
-	final ComboBox<Locale> localeIdComboBox = new ComboBox<>();
-	localeIdComboBox.setId(LOCALE_ID);
-	final Stream<Locale> localeStream = Stream.of(Locale.getAvailableLocales())
-		.sorted((l1, l2) -> l1.toString().compareTo(l2.toString()));
-	localeIdComboBox.setItems(localeStream);
-	localeIdComboBox.setValue(INITIAL_LOCALE);
-	localeIdComboBox.setCaption("Locale:");
-	return localeIdComboBox;
+        final ComboBox<Locale> localeIdComboBox = new ComboBox<>();
+        localeIdComboBox.setId(LOCALE_ID);
+        final Stream<Locale> localeStream = Stream
+                .of(Locale.getAvailableLocales())
+                .sorted((l1, l2) -> l1.toString().compareTo(l2.toString()));
+        localeIdComboBox.setItems(localeStream);
+        localeIdComboBox.setValue(INITIAL_LOCALE);
+        localeIdComboBox.setCaption("Locale:");
+        return localeIdComboBox;
     }
 
     private ComboBox<String> getZoneIdComboBox() {
-	final ComboBox<String> zoneIdComboBox = new ComboBox<>();
-	zoneIdComboBox.setId(ZONE_ID);
-	final Set<String> zoneIdSet = new TreeSet<>(ZoneId.getAvailableZoneIds());
-	zoneIdComboBox.setItems(zoneIdSet);
-	zoneIdComboBox.setValue(INITIAL_ZONE_ID);
-	zoneIdComboBox.setCaption("Zone:");
-	return zoneIdComboBox;
+        final ComboBox<String> zoneIdComboBox = new ComboBox<>();
+        zoneIdComboBox.setId(ZONE_ID);
+        final Set<String> zoneIdSet = new TreeSet<>(
+                ZoneId.getAvailableZoneIds());
+        zoneIdComboBox.setItems(zoneIdSet);
+        zoneIdComboBox.setValue(INITIAL_ZONE_ID);
+        zoneIdComboBox.setCaption("Zone:");
+        return zoneIdComboBox;
     }
 }
--- uitest/src/test/java/com/vaadin/tests/components/datefield/DateTimeFieldZoneIdFutureSummerDatesTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/datefield/DateTimeFieldZoneIdFutureSummerDatesTest.java
@@ -21,7 +21,8 @@ import com.vaadin.testbench.elements.DateFieldElement;
 import com.vaadin.testbench.elements.DateTimeFieldElement;
 import com.vaadin.tests.tb3.SingleBrowserTest;
 
-public class DateTimeFieldZoneIdFutureSummerDatesTest extends SingleBrowserTest {
+public class DateTimeFieldZoneIdFutureSummerDatesTest
+        extends SingleBrowserTest {
 
     private static final String TESTING_ZONE_ID = "CET";
     private static final String TESTING_LOCALE = Locale.US.toString();
@@ -34,107 +35,121 @@ public class DateTimeFieldZoneIdFutureSummerDatesTest extends SingleBrowserTest 
 
     ComboBoxElement zoneIdComboBox;
     ComboBoxElement localeIdComboBox;
-    
+
     @Before
     public void init() {
-	openTestURL();
+        openTestURL();
 
-	dateTimeFieldWithVariableRange = $(DateTimeFieldElement.class).id(VARIABLE_RANGE_DATEFIELD_ID);
-	dateTimeFieldWithDefaultRange = $(DateTimeFieldElement.class).id(FIXED_RANGE_DATEFIELD_ID);
+        dateTimeFieldWithVariableRange = $(DateTimeFieldElement.class)
+                .id(VARIABLE_RANGE_DATEFIELD_ID);
+        dateTimeFieldWithDefaultRange = $(DateTimeFieldElement.class)
+                .id(FIXED_RANGE_DATEFIELD_ID);
 
-	transitionStartyear = $(DateFieldElement.class).id(START_YEAR_DATEFIELD_ID);
-	transitionEndyear = $(DateFieldElement.class).id(END_YEAR_DATEFIELD_ID);
+        transitionStartyear = $(DateFieldElement.class)
+                .id(START_YEAR_DATEFIELD_ID);
+        transitionEndyear = $(DateFieldElement.class).id(END_YEAR_DATEFIELD_ID);
 
-	zoneIdComboBox = $(ComboBoxElement.class).id(ZONE_ID);
-	zoneIdComboBox.selectByText(TESTING_ZONE_ID);
-	localeIdComboBox = $(ComboBoxElement.class).id(LOCALE_ID);
-	localeIdComboBox.selectByText(TESTING_LOCALE);
+        zoneIdComboBox = $(ComboBoxElement.class).id(ZONE_ID);
+        zoneIdComboBox.selectByText(TESTING_ZONE_ID);
+        localeIdComboBox = $(ComboBoxElement.class).id(LOCALE_ID);
+        localeIdComboBox.selectByText(TESTING_LOCALE);
     }
 
     @Test
     public void dateTimeFieldWithCustomRangeShouldShowDSTWithinRange() {
-	final int testingRangeCentralYear = LocalDate.now().getYear() + 50;
-	final int testingRangeUpperYear = testingRangeCentralYear + 3;
-	final int testingRangeLowerYear = testingRangeCentralYear - 3;
+        final int testingRangeCentralYear = LocalDate.now().getYear() + 50;
+        final int testingRangeUpperYear = testingRangeCentralYear + 3;
+        final int testingRangeLowerYear = testingRangeCentralYear - 3;
 
-	transitionEndyear.setDate(LocalDate.of(testingRangeUpperYear, 1, 1));
-	transitionStartyear.setDate(LocalDate.of(testingRangeLowerYear, 1, 1));
+        transitionEndyear.setDate(LocalDate.of(testingRangeUpperYear, 1, 1));
+        transitionStartyear.setDate(LocalDate.of(testingRangeLowerYear, 1, 1));
 
-	LocalDateTime testingDateTime = LocalDateTime.of(testingRangeCentralYear, Month.JULY, 1, 0, 0);
-	dateTimeFieldWithVariableRange.setDateTime(testingDateTime);
-	assertEndsWith(dateTimeFieldWithVariableRange, "CEST");
+        LocalDateTime testingDateTime = LocalDateTime
+                .of(testingRangeCentralYear, Month.JULY, 1, 0, 0);
+        dateTimeFieldWithVariableRange.setDateTime(testingDateTime);
+        assertEndsWith(dateTimeFieldWithVariableRange, "CEST");
 
-	testingDateTime = LocalDateTime.of(testingRangeUpperYear, Month.JULY, 1, 0, 0);
-	dateTimeFieldWithVariableRange.setDateTime(testingDateTime);
-	assertEndsWith(dateTimeFieldWithVariableRange, "CEST");
+        testingDateTime = LocalDateTime.of(testingRangeUpperYear, Month.JULY, 1,
+                0, 0);
+        dateTimeFieldWithVariableRange.setDateTime(testingDateTime);
+        assertEndsWith(dateTimeFieldWithVariableRange, "CEST");
 
-	testingDateTime = LocalDateTime.of(testingRangeLowerYear, Month.JULY, 1, 0, 0);
-	dateTimeFieldWithVariableRange.setDateTime(testingDateTime);
-	assertEndsWith(dateTimeFieldWithVariableRange, "CEST");
+        testingDateTime = LocalDateTime.of(testingRangeLowerYear, Month.JULY, 1,
+                0, 0);
+        dateTimeFieldWithVariableRange.setDateTime(testingDateTime);
+        assertEndsWith(dateTimeFieldWithVariableRange, "CEST");
     }
 
     @Test
     public void dateTimeFieldWithCustomRangeShouldNotShowDSTOutsideRange() {
-	final int testingRangeCentralYear = LocalDate.now().getYear() + 50;
-	final int testingRangeUpperYear = testingRangeCentralYear + 3;
-	final int testingRangeLowerYear = testingRangeCentralYear - 3;
+        final int testingRangeCentralYear = LocalDate.now().getYear() + 50;
+        final int testingRangeUpperYear = testingRangeCentralYear + 3;
+        final int testingRangeLowerYear = testingRangeCentralYear - 3;
 
-	transitionEndyear.setDate(LocalDate.of(testingRangeUpperYear, 1, 1));
-	transitionStartyear.setDate(LocalDate.of(testingRangeLowerYear, 1, 1));
+        transitionEndyear.setDate(LocalDate.of(testingRangeUpperYear, 1, 1));
+        transitionStartyear.setDate(LocalDate.of(testingRangeLowerYear, 1, 1));
 
-	// This year is out of specified range
-	LocalDateTime testingDateTime = LocalDateTime.of(LocalDate.now().getYear(), Month.JULY, 1, 0, 0);
-	dateTimeFieldWithVariableRange.setDateTime(testingDateTime);
-	assertEndsWith(dateTimeFieldWithVariableRange, "CET");
+        // This year is out of specified range
+        LocalDateTime testingDateTime = LocalDateTime
+                .of(LocalDate.now().getYear(), Month.JULY, 1, 0, 0);
+        dateTimeFieldWithVariableRange.setDateTime(testingDateTime);
+        assertEndsWith(dateTimeFieldWithVariableRange, "CET");
 
-	// One year after the specified range
-	testingDateTime = LocalDateTime.of(testingRangeUpperYear + 1, Month.JULY, 1, 0, 0);
-	dateTimeFieldWithVariableRange.setDateTime(testingDateTime);
-	assertEndsWith(dateTimeFieldWithVariableRange, "CET");
+        // One year after the specified range
+        testingDateTime = LocalDateTime.of(testingRangeUpperYear + 1,
+                Month.JULY, 1, 0, 0);
+        dateTimeFieldWithVariableRange.setDateTime(testingDateTime);
+        assertEndsWith(dateTimeFieldWithVariableRange, "CET");
 
-	// One year before the specified range
-	testingDateTime = LocalDateTime.of(testingRangeLowerYear - 1, Month.JULY, 1, 0, 0);
-	dateTimeFieldWithVariableRange.setDateTime(testingDateTime);
-	assertEndsWith(dateTimeFieldWithVariableRange, "CET");
+        // One year before the specified range
+        testingDateTime = LocalDateTime.of(testingRangeLowerYear - 1,
+                Month.JULY, 1, 0, 0);
+        dateTimeFieldWithVariableRange.setDateTime(testingDateTime);
+        assertEndsWith(dateTimeFieldWithVariableRange, "CET");
     }
 
     @Test
     public void dateTimeFieldWithDefaultRangeShouldShowDSTFrom1980Until20FutureYears() {
-	// The 1980 to 20 future years range is the hard-coded default range
-	// for which DST is shown if user doesn't provide a custom range
+        // The 1980 to 20 future years range is the hard-coded default range
+        // for which DST is shown if user doesn't provide a custom range
 
-	final int testingRangeLowerYear = 1980;
-	LocalDateTime testingDateTime = LocalDateTime.of(testingRangeLowerYear, Month.JULY, 1, 0, 0);
-	dateTimeFieldWithDefaultRange.setDateTime(testingDateTime);
-	assertEndsWith(dateTimeFieldWithDefaultRange, "CEST");
+        final int testingRangeLowerYear = 1980;
+        LocalDateTime testingDateTime = LocalDateTime.of(testingRangeLowerYear,
+                Month.JULY, 1, 0, 0);
+        dateTimeFieldWithDefaultRange.setDateTime(testingDateTime);
+        assertEndsWith(dateTimeFieldWithDefaultRange, "CEST");
 
-	final int testingRangeUpperYear = LocalDate.now().getYear() + 20;
-	testingDateTime = LocalDateTime.of(testingRangeUpperYear, Month.JULY, 1, 0, 0);
-	dateTimeFieldWithDefaultRange.setDateTime(testingDateTime);
-	assertEndsWith(dateTimeFieldWithDefaultRange, "CEST");
+        final int testingRangeUpperYear = LocalDate.now().getYear() + 20;
+        testingDateTime = LocalDateTime.of(testingRangeUpperYear, Month.JULY, 1,
+                0, 0);
+        dateTimeFieldWithDefaultRange.setDateTime(testingDateTime);
+        assertEndsWith(dateTimeFieldWithDefaultRange, "CEST");
 
-	final int testingCurrYear = LocalDate.now().getYear();
-	testingDateTime = LocalDateTime.of(testingCurrYear, Month.JULY, 1, 0, 0);
-	dateTimeFieldWithDefaultRange.setDateTime(testingDateTime);
-	assertEndsWith(dateTimeFieldWithDefaultRange, "CEST");
+        final int testingCurrYear = LocalDate.now().getYear();
+        testingDateTime = LocalDateTime.of(testingCurrYear, Month.JULY, 1, 0,
+                0);
+        dateTimeFieldWithDefaultRange.setDateTime(testingDateTime);
+        assertEndsWith(dateTimeFieldWithDefaultRange, "CEST");
     }
 
     @Test
     public void dateTimeFieldWithDefaultRangeShouldNotShowDSTBefore1980() {
-	final LocalDateTime testingDateTime = LocalDateTime.of(1979, Month.JULY, 1, 0, 0);
-	dateTimeFieldWithDefaultRange.setDateTime(testingDateTime);
-	assertEndsWith(dateTimeFieldWithDefaultRange, "CET");
+        final LocalDateTime testingDateTime = LocalDateTime.of(1979, Month.JULY,
+                1, 0, 0);
+        dateTimeFieldWithDefaultRange.setDateTime(testingDateTime);
+        assertEndsWith(dateTimeFieldWithDefaultRange, "CET");
     }
 
     @Test
     public void dateTimeFieldWithDefaultRangeShouldNotShowDSTAfter20FutureYears() {
-	final LocalDateTime testingDateTime = LocalDateTime.of(LocalDate.now().getYear() + 21, Month.JULY, 1, 0, 0);
-	dateTimeFieldWithDefaultRange.setDateTime(testingDateTime);
-	assertEndsWith(dateTimeFieldWithDefaultRange, "CET");
+        final LocalDateTime testingDateTime = LocalDateTime
+                .of(LocalDate.now().getYear() + 21, Month.JULY, 1, 0, 0);
+        dateTimeFieldWithDefaultRange.setDateTime(testingDateTime);
+        assertEndsWith(dateTimeFieldWithDefaultRange, "CET");
     }
-    
+
     private void assertEndsWith(DateTimeFieldElement element, String suffix) {
-	final String text = element.getValue();
-	assertTrue(text + " should end with " + suffix, text.endsWith(suffix));
+        final String text = element.getValue();
+        assertTrue(text + " should end with " + suffix, text.endsWith(suffix));
     }
 }
