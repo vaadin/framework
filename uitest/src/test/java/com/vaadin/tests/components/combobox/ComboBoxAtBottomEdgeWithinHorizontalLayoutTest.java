70a46a8f413f975954ace1a5d16ee3a22452fcf8
Anna Koskinen <Ansku@users.noreply.github.com>
2020-06-26 15:50:51 +0300

    Update ComboBox popup position comparison to use correct top value. (#12041)
    Fixes #12029

--- uitest/src/test/java/com/vaadin/tests/components/combobox/ComboBoxAtBottomEdgeWithinHorizontalLayoutTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/combobox/ComboBoxAtBottomEdgeWithinHorizontalLayoutTest.java
@@ -3,6 +3,7 @@ package com.vaadin.tests.components.combobox;
 import org.junit.Test;
 import org.openqa.selenium.WebElement;
 
+import com.vaadin.testbench.By;
 import com.vaadin.testbench.elements.ComboBoxElement;
 import com.vaadin.tests.tb3.MultiBrowserTest;
 
@@ -24,6 +25,40 @@ public class ComboBoxAtBottomEdgeWithinHorizontalLayoutTest
                 "Popup should not open below the ComboBox at the "
                         + "bottom edge of the viewport. ComboBox: %s, Popup: %s",
                 cbBottom, popupBottom), cbBottom, popupBottom);
+    }
+
+    @Test
+    public void ensurePopupPositionUpdatesWhenFiltered() {
+        openTestURL();
+
+        ComboBoxElement cb = $(ComboBoxElement.class).first();
+        cb.openPopup();
+        WebElement popup = cb.getSuggestionPopup();
+
+        int initialTop = popup.getLocation().getY();
+
+        // filter a bit
+        cb.findElement(By.vaadin("#textbox")).sendKeys("2");
+        int updatedTop = popup.getLocation().getY();
+        assertLessThan(String.format(
+                "Popup should be repositioned when "
+                        + "filtered. Initial: %s, Updated: %s",
+                initialTop, updatedTop), initialTop, updatedTop);
+        int cbBottom = cb.getLocation().getY() + cb.getSize().getHeight();
+        assertGreaterOrEqual(String.format(
+                "Popup should still open above the ComboBox when "
+                        + "filtered a bit. ComboBox: %s, Popup: %s",
+                cbBottom, updatedTop), cbBottom, updatedTop);
+
+        // filter more
+        cb.clear();
+        cb.findElement(By.vaadin("#textbox")).sendKeys("1");
+        popup = cb.getSuggestionPopup();
+        updatedTop = popup.getLocation().getY();
+        assertLessThanOrEqual(String.format(
+                "Popup should open below the ComboBox when "
+                        + "filtered down to one result. ComboBox: %s, Popup: %s",
+                cbBottom, updatedTop), cbBottom, updatedTop);
     }
 
 }
