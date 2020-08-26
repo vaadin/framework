f90d6435aa7e88a85b76b1ee826937ce3cce212a
Clemens von Schwerin <clemens.vonschwerin@gmail.com>
2020-08-26 15:40:11 +0200

    Update ServletPortletHelper.java
    This is needed to get /UIDL working behind a reverse proxy. Without the change pathInfo would be "/UIDL" but prefix would be "/UIDL/" in line 105.

--- server/src/main/java/com/vaadin/server/ServletPortletHelper.java
+++ server/src/main/java/com/vaadin/server/ServletPortletHelper.java
@@ -97,6 +97,10 @@ public class ServletPortletHelper implements Serializable {
         if (!prefix.startsWith("/")) {
             prefix = '/' + prefix;
         }
+        
+        if (!pathInfo.endsWith("/") && prefix.endsWith("/")) {
+			pathInfo += '/';
+		}
 
         if (pathInfo.startsWith(prefix)) {
             return true;
