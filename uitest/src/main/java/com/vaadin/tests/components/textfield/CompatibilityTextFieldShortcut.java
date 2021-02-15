098d7f9fb07c799ae26fa1777208aca316d49c50
Tatu Lund <tatu@vaadin.com>
2021-02-15 19:50:29 +0200

    Focus via button click

--- uitest/src/main/java/com/vaadin/tests/components/textfield/CompatibilityTextFieldShortcut.java
+++ uitest/src/main/java/com/vaadin/tests/components/textfield/CompatibilityTextFieldShortcut.java
@@ -5,6 +5,7 @@ import com.vaadin.event.ShortcutListener;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.Registration;
 import com.vaadin.tests.components.AbstractTestUI;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Notification;
 import com.vaadin.v7.ui.TextField;
@@ -17,11 +18,12 @@ public class CompatibilityTextFieldShortcut extends AbstractTestUI {
     @Override
     protected void setup(VaadinRequest request) {
         TextField textField = new TextField("F8 shortcut when focused");
-        ShortcutListener c = new ShortcutListener("ShortcutForMAMedRemarks", ShortcutAction.KeyCode.F8, null) {
+        ShortcutListener c = new ShortcutListener("ShortcutForMAMedRemarks",
+                ShortcutAction.KeyCode.F8, null) {
 
             @Override
             public void handleAction(Object sender, Object target) {
-                Notification.show("Received F8: "+textField.getValue());
+                Notification.show("Received F8: " + textField.getValue());
             }
         };
 
@@ -36,7 +38,12 @@ public class CompatibilityTextFieldShortcut extends AbstractTestUI {
             listenerRegistration.remove();
         });
 
-        Label label = new Label("F8 will have an effect only if the following component is focused.");
+        Label label = new Label(
+                "F8 will have an effect only if the following component is focused.");
+        Button button = new Button("focus");
+        button.addClickListener(event -> {
+            textField.focus();
+        });
         addComponents(label, textField);
     }
 }
