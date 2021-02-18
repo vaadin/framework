03bf3999cd4529dde611a7dc4e932a91c6bcd656
Tatu Lund <tatu@vaadin.com>
2021-02-18 13:59:00 +0200

    Forgot to add components

--- uitest/src/main/java/com/vaadin/tests/components/datefield/DateFieldPreventInvalidInput.java
+++ uitest/src/main/java/com/vaadin/tests/components/datefield/DateFieldPreventInvalidInput.java
@@ -24,6 +24,7 @@ public class DateFieldPreventInvalidInput extends AbstractTestUI {
         dateField.addValueChangeListener(event -> {
             value.setValue(dateField.getValue().toString());
         });
+        addComponents(dateField,label,button);
     }
 
 }
