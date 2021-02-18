95df94b8163faf31025753fddbd26839232ef2bc
Tatu Lund <tatu@vaadin.com>
2021-02-18 13:52:58 +0200

    Add test case

---
+++ uitest/src/test/java/com/vaadin/tests/components/datefield/DateFieldPreventInvalidInputTest.java
@@ -0,0 +1,39 @@
+package com.vaadin.tests.components.datefield;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import com.vaadin.testbench.By;
+import com.vaadin.testbench.elements.ButtonElement;
+import com.vaadin.testbench.elements.DateFieldElement;
+import com.vaadin.testbench.elements.LabelElement;
+import com.vaadin.tests.tb3.SingleBrowserTest;
+
+public class DateFieldPreventInvalidInputTest extends SingleBrowserTest {
+
+    @Test
+    public void modifyValueAndPressEnter() {
+        openTestURL();
+        DateFieldElement dateField = $(DateFieldElement.class).first();
+
+        WebElement dateTextbox = dateField
+                .findElement(By.className("v-textfield"));
+        ButtonElement button = $(ButtonElement.class).first();
+        LabelElement label = $(LabelElement.class).first();
+
+        // DateField is set not accept invalid input, this date is not in range
+        dateTextbox.sendKeys("01/01/21", Keys.TAB);
+        assertEquals("Expected value un-changed", "no-value", label.getText());
+
+        // Set DateField accept invalid input
+        button.click();
+
+        dateTextbox.sendKeys("01/01/21", Keys.TAB);
+        Assert.assertNotEquals("Expected value changed", "no-value",
+                label.getText());
+    }
+}
