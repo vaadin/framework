f2e5857222fb1785e01ae6465ed2504fac62967f
Olli Tietäväinen <ollit@vaadin.com>
2021-03-03 14:18:38 +0200

    update to Atmosphere runtime 2.4.30.vaadin3

--- server/src/main/java/com/vaadin/server/Constants.java
+++ server/src/main/java/com/vaadin/server/Constants.java
@@ -66,7 +66,7 @@ public interface Constants {
             + "=================================================================";
 
     // Keep the version number in sync with pom.xml
-    static final String REQUIRED_ATMOSPHERE_RUNTIME_VERSION = "2.4.30.vaadin2";
+    static final String REQUIRED_ATMOSPHERE_RUNTIME_VERSION = "2.4.30.vaadin3";
 
     static final String INVALID_ATMOSPHERE_VERSION_WARNING = "\n"
             + "=================================================================\n"
