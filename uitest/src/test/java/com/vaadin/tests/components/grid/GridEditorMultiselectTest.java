a34752a6c474d2e565bde559dcaa819e29050aa7
Anna Koskinen <anna@vaadin.com>
2021-03-16 16:07:50 +0200

    Test fixes

--- uitest/src/test/java/com/vaadin/tests/components/grid/GridEditorMultiselectTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/grid/GridEditorMultiselectTest.java
@@ -1,5 +1,8 @@
 package com.vaadin.tests.components.grid;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.util.List;
 
 import org.junit.Test;
@@ -9,9 +12,6 @@ import com.vaadin.testbench.By;
 import com.vaadin.testbench.elements.GridElement;
 import com.vaadin.testbench.parallel.TestCategory;
 import com.vaadin.tests.tb3.MultiBrowserTest;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 @TestCategory("grid")
 public class GridEditorMultiselectTest extends MultiBrowserTest {
@@ -27,7 +27,9 @@ public class GridEditorMultiselectTest extends MultiBrowserTest {
     public void testSelectCheckboxesEnabledBackOnSave() {
         openTestURL();
         GridElement grid = openEditor();
-        grid.getEditor().save();
+        waitForElementPresent(By.className("v-grid-editor-save"));
+        sleep(100); // wait for repositioning
+        findElement(By.className("v-grid-editor-save")).click();
         waitForElementNotPresent(By.className("v-grid-editor-cells"));
         assertCheckboxesEnabled(grid, true);
     }
@@ -36,14 +38,17 @@ public class GridEditorMultiselectTest extends MultiBrowserTest {
     public void testSelectCheckboxesEnabledBackOnCancel() {
         openTestURL();
         GridElement grid = openEditor();
-        grid.getEditor().cancel();
-        sleep(100);
+        waitForElementPresent(By.className("v-grid-editor-cancel"));
+        sleep(100); // wait for repositioning
+        findElement(By.className("v-grid-editor-cancel")).click();
+        waitForElementNotPresent(By.className("v-grid-editor-cells"));
         assertCheckboxesEnabled(grid, true);
     }
 
     private GridElement openEditor() {
         GridElement grid = $(GridElement.class).first();
         grid.getRow(0).doubleClick();
+        waitForElementPresent(By.className("v-grid-editor-cells"));
         assertTrue("Grid editor should be displayed.",
                 grid.getEditor().isDisplayed());
         return grid;
