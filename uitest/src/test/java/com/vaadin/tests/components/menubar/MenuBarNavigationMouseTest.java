50d5896e98d056a3d4cc0e1693c3438b4a8a8ef3
Anna Koskinen <anna@vaadin.com>
2021-04-30 17:29:14 +0300

    Attempts to reduce test flakiness

--- uitest/src/test/java/com/vaadin/tests/components/menubar/MenuBarNavigationMouseTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/menubar/MenuBarNavigationMouseTest.java
@@ -3,11 +3,15 @@ package com.vaadin.tests.components.menubar;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.openqa.selenium.interactions.Actions;
 
+import com.vaadin.testbench.elements.LabelElement;
 import com.vaadin.testbench.elements.MenuBarElement;
 import com.vaadin.tests.tb3.MultiBrowserTest;
 
 public class MenuBarNavigationMouseTest extends MultiBrowserTest {
+
+    private LabelElement label;
 
     @Override
     protected Class<?> getUIClass() {
@@ -18,6 +22,10 @@ public class MenuBarNavigationMouseTest extends MultiBrowserTest {
     public void testMenuBarMouseNavigation() throws Exception {
         openTestURL();
         MenuBarElement menuBar = $(MenuBarElement.class).first();
+        label = $(LabelElement.class).first();
+
+        // move to Label to ensure all mouse moves are treated the same
+        resetMousePosition();
 
         // clicks separated to different calls for more informative errors
         menuBar.clickItem("File");
@@ -26,15 +34,25 @@ public class MenuBarNavigationMouseTest extends MultiBrowserTest {
         assertEquals("1. MenuItem File/Export../As PDF... selected",
                 getLogRow(0));
 
+        resetMousePosition();
+
         menuBar.clickItem("Edit");
         menuBar.clickItem("Copy");
         assertEquals("2. MenuItem Edit/Copy selected", getLogRow(0));
 
+        resetMousePosition();
+
         menuBar.clickItem("Help");
         assertEquals("3. MenuItem Help selected", getLogRow(0));
+
+        resetMousePosition();
 
         menuBar.clickItem("File");
         menuBar.clickItem("Exit");
         assertEquals("4. MenuItem File/Exit selected", getLogRow(0));
+    }
+
+    private void resetMousePosition() {
+        new Actions(driver).moveToElement(label).perform();
     }
 }
