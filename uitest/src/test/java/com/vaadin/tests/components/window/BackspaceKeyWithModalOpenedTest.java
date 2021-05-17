fd2af141785dc5128880a25969dc8f36eac2b6e9
Anna Koskinen <Ansku@users.noreply.github.com>
2021-05-17 14:23:54 +0300

    Fix TabSheetElement clicks for IE, test tweaks. (#12291) (#12305)
    - Backspace navigation hasn't worked in years thanks to new browser
    standards, removed related tests. Left a test for regular backspace use
    within a modal window.
    - Enabled a modal window test for Chrome since it seems to be working
    now, removed extending of another test class to avoid running the tests
    twice without any configuration changes.
    - Adjusted browser width limitation.
    - Added screenshots.

--- uitest/src/test/java/com/vaadin/tests/components/window/BackspaceKeyWithModalOpenedTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/window/BackspaceKeyWithModalOpenedTest.java
@@ -4,13 +4,10 @@ import static com.vaadin.tests.components.window.BackspaceKeyWithModalOpened.BTN
 import static com.vaadin.tests.components.window.BackspaceKeyWithModalOpened.BTN_OPEN_MODAL_ID;
 import static org.junit.Assert.assertEquals;
 import static org.openqa.selenium.Keys.BACK_SPACE;
-import static org.openqa.selenium.Keys.TAB;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 
 import com.vaadin.testbench.By;
 import com.vaadin.testbench.elements.ButtonElement;
@@ -31,38 +28,6 @@ public class BackspaceKeyWithModalOpenedTest extends MultiBrowserTest {
         textField.sendKeys("textt");
         textField.sendKeys(BACK_SPACE);
         assertEquals("text", textField.getValue());
-        checkButtonsCount();
-    }
-
-    /**
-     * Tests that backspace action outside textfield is prevented
-     */
-    @Test
-    public void testWithFocusOnModal() throws Exception {
-        // Try to send back actions to the browser.
-        new Actions(getDriver()).sendKeys(BACK_SPACE).perform();
-
-        checkButtonsCount();
-    }
-
-    /**
-     * Tests that backspace action in the bottom component is prevented.
-     *
-     * Ignored because the fix to #8855 stops the top and bottom components from
-     * functioning as focus traps. Meanwhile, navigation with Backspace is not
-     * anymore supported by reasonable browsers.
-     */
-    @Test
-    @Ignore
-    public void testWithFocusOnBottom() throws Exception {
-        TextFieldElement textField = getTextField();
-
-        // tab in last field set focus on bottom component
-        textField.sendKeys(TAB);
-
-        // Try to send back actions to the browser.
-        new Actions(getDriver()).sendKeys(BACK_SPACE).perform();
-
         checkButtonsCount();
     }
 
