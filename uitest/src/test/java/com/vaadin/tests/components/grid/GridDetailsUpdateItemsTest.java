9ffdbc8c29ae2a4fb01a1996d506e3b6042ebb7f
Anna Koskinen <Ansku@users.noreply.github.com>
2021-05-20 12:25:09 +0300

    Trigger repositioning after full refresh of current details. (#12312) (#12313)
    Fixes #12310

--- uitest/src/test/java/com/vaadin/tests/components/grid/GridDetailsUpdateItemsTest.java
+++ uitest/src/test/java/com/vaadin/tests/components/grid/GridDetailsUpdateItemsTest.java
@@ -2,9 +2,11 @@ package com.vaadin.tests.components.grid;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 import com.vaadin.testbench.TestBenchElement;
 import com.vaadin.testbench.elements.ButtonElement;
@@ -18,7 +20,8 @@ public class GridDetailsUpdateItemsTest extends MultiBrowserTest {
     public void testDetailsUpdateWithItems() {
         openTestURL();
         GridElement grid = $(GridElement.class).first();
-        ButtonElement button = $(ButtonElement.class).first();
+        ButtonElement button = $(ButtonElement.class)
+                .caption("Change with details").first();
 
         String details0 = grid.getDetails(0).getText();
 
@@ -39,6 +42,28 @@ public class GridDetailsUpdateItemsTest extends MultiBrowserTest {
         // ensure positioning is correct
         assertDirectlyAbove(detailCell0, cell1_0);
         assertDirectlyAbove(cell1_0, detailCell1);
+    }
+
+    @Test
+    public void testRemovingDetailsWithItemUpdateRepositionsRowsCorrectly() {
+        openTestURL();
+        GridElement grid = $(GridElement.class).first();
+        ButtonElement button = $(ButtonElement.class)
+                .caption("Change without details").first();
+
+        assertFalse("Details not found when there should be some.",
+                grid.findElements(By.className("v-grid-spacer")).isEmpty());
+
+        // change the contents
+        button.click();
+
+        waitForElementNotPresent(By.className("v-grid-spacer"));
+
+        GridCellElement cell0_0 = grid.getCell(0, 0);
+        GridCellElement cell1_0 = grid.getCell(1, 0);
+
+        // ensure positioning is correct
+        assertDirectlyAbove(cell0_0, cell1_0);
     }
 
     private void assertDirectlyAbove(TestBenchElement above,
