b7e552068ea542a78d39919278368ea85887f9db
Anna Koskinen <Ansku@users.noreply.github.com>
2021-06-14 08:56:15 +0300

    Chrome 91 test fixes (#12323)

--- uitest/src/test/java/com/vaadin/tests/VerifyBrowserVersionTest.java
+++ uitest/src/test/java/com/vaadin/tests/VerifyBrowserVersionTest.java
@@ -25,7 +25,7 @@ public class VerifyBrowserVersionTest extends MultiBrowserTest {
             // Chrome version does not necessarily match the desired version
             // because of auto updates...
             browserIdentifier = getExpectedUserAgentString(
-                    getDesiredCapabilities()) + "90";
+                    getDesiredCapabilities()) + "91";
         } else if (BrowserUtil.isFirefox(getDesiredCapabilities())) {
             browserIdentifier = getExpectedUserAgentString(
                     getDesiredCapabilities()) + "88";
