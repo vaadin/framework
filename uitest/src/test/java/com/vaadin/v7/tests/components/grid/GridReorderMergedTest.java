c3b82adbae3b119f36fb2e2d766cdee4966a401d
Anna Koskinen <Ansku@users.noreply.github.com>
2021-10-13 17:13:17 +0300

    Fix compatibility Grid column reorder of partially hidden joined cells.
    (#12427)
    
    Adds the fix from #12386 to the drag source as well.
    
    
    
    Fixes #12377

--- uitest/src/test/java/com/vaadin/v7/tests/components/grid/GridReorderMergedTest.java
+++ uitest/src/test/java/com/vaadin/v7/tests/components/grid/GridReorderMergedTest.java
@@ -24,4 +24,21 @@ public class GridReorderMergedTest extends MultiBrowserTest {
         assertEquals("Unexpected column order,", "6",
                 grid.getHeaderCell(1, 1).getText());
     }
+
+    @Test
+    public void dragMergedReverse() {
+        openTestURL();
+        GridElement grid = $(GridElement.class).first();
+        GridCellElement headerCell0_0 = grid.getHeaderCell(0, 0);
+        GridCellElement headerCell0_4 = grid.getHeaderCell(0, 4);
+        new Actions(driver).clickAndHold(headerCell0_4)
+                .moveByOffset(-headerCell0_0.getSize().getWidth(),
+                        headerCell0_0.getSize().getHeight() / 2)
+                .release().perform();
+
+        // ensure the second merged block got dragged over the first merged
+        // block entirely
+        assertEquals("Unexpected column order,", "6",
+                grid.getHeaderCell(1, 1).getText());
+    }
 }
