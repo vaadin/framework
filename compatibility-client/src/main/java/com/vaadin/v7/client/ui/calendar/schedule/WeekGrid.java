cb232f2bffcbd31c64beaa23f1d8dedd9f2ffbac
Anna Koskinen <Ansku@users.noreply.github.com>
2022-01-11 12:52:29 +0200

    Update copyright year range (#12507)

--- compatibility-client/src/main/java/com/vaadin/v7/client/ui/calendar/schedule/WeekGrid.java
+++ compatibility-client/src/main/java/com/vaadin/v7/client/ui/calendar/schedule/WeekGrid.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2021 Vaadin Ltd.
+ * Copyright 2000-2022 Vaadin Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
