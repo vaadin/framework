f06186a58a463d83f4095e5327aa4d16c5826678
Anna Koskinen <Ansku@users.noreply.github.com>
2022-01-12 14:30:59 +0200

    Remove duplicate copyright and javadoc sections. (#12511)

--- uitest/src/main/java/com/vaadin/tests/components/table/TableScrollAfterAddRow.java
+++ uitest/src/main/java/com/vaadin/tests/components/table/TableScrollAfterAddRow.java
@@ -15,10 +15,6 @@
  */
 package com.vaadin.tests.components.table;
 
-/**
- *
- * @author Vaadin Ltd
- */
 import com.vaadin.data.Item;
 import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.server.VaadinRequest;
@@ -28,22 +24,6 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.NativeButton;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.VerticalLayout;
-
-/*
- * Copyright 2000-2022 Vaadin Ltd.
- *
- * Licensed under the Commercial Vaadin Developer License version 4.0 (CVDLv4);
- * you may not use this file except in compliance with the License. You may obtain
- * a copy of the License at
- *
- * https://vaadin.com/license/cvdl-4.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 
 /**
  *
